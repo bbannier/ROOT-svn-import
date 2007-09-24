c6a2c4e0b241550ef4a88600d799a49f580d50ea
lfranco <lfranco@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-09-24 15:54:29 +0000

    Tiiiime to plaaaayyysvn infosvn info Start merging code for the stub removal
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/dictOpt@20073 27541ba8-7e3a-0410-8455-c3a389f83636

--- fitpanel/Module.mk
+++ fitpanel/Module.mk
@@ -17,14 +17,28 @@ FITPANELDS   := $(MODDIRS)/G__FitPanel.cxx
 FITPANELDO   := $(FITPANELDS:.cxx=.o)
 FITPANELDH   := $(FITPANELDS:.cxx=.h)
 
+#LF
+FITPANELTMPDS    := $(MODDIRS)/G__FitPanelTmp.cxx
+FITPANELTMPDO    := $(FITPANELTMPDS:.cxx=.o)
+FITPANELTMPDH    := $(FITPANELTMPDS:.cxx=.h)
+FITPANELTMP2DS   := $(MODDIRS)/G__FitPanelTmp2.cxx
+FITPANELTMP2DO   := $(FITPANELTMP2DS:.cxx=.o)
+FITPANELTMP2DH   := $(FITPANELTMP2DS:.cxx=.h)
+
 FITPANELH    := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 FITPANELS    := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 FITPANELO    := $(FITPANELS:.cxx=.o)
 
 FITPANELDEP  := $(FITPANELO:.o=.d) $(FITPANELDO:.o=.d)
 
+#LF
+FITPANELTMPDEP  := $(FITPANELTMPDO:.o=.d)
+
 FITPANELLIB  := $(LPATH)/libFitPanel.$(SOEXT)
 FITPANELMAP  := $(FITPANELLIB:.$(SOEXT)=.rootmap)
+
+#LF
+FITPANELNM       := $(FITPANELLIB:.$(SOEXT)=.nm)
 
 # used in the main Makefile
 ALLHDRS      += $(patsubst $(MODDIRI)/%.h,include/%.h,$(FITPANELH))
@@ -38,14 +52,37 @@ INCLUDEFILES += $(FITPANELDEP)
 include/%.h:    $(FITPANELDIRI)/%.h
 		cp $< $@
 
-$(FITPANELLIB): $(FITPANELO) $(FITPANELDO) $(ORDER_) $(MAINLIBS) $(FITPANELLIBDEP)
+#LF
+$(FITPANELLIB):   $(FITPANELO) $(FITPANELTMPDO) $(FITPANELTMP2DO) $(FITPANELDO) $(ORDER_) $(MAINLIBS) $(FITPANELLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libFitPanel.$(SOEXT) $@ "$(FITPANELO) $(FITPANELDO)" \
+		   "$(SOFLAGS)" libFitPanel.$(SOEXT) $@ "$(FITPANELO) $(FITPANELTMPDO) $(FITPANELTMP2DO) $(FITPANELDO)" \
 		   "$(FITPANELLIBEXTRA)"
 
-$(FITPANELDS):  $(FITPANELH) $(FITPANELL) $(ROOTCINTTMPEXE)
-		@echo "Generating dictionary $@..."
-		$(ROOTCINTTMP) -f $@ -c $(FITPANELH) $(FITPANELL)
+#LF
+$(FITPANELTMPDS):   $(FITPANELH) $(FITPANELL) $(ROOTCINTTMPEXE)
+		@echo "Generating first dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 1 -c $(FITPANELH) $(FITPANELL)
+
+#LF
+$(FITPANELTMP2DS):  $(FITPANELH) $(FITPANELL) $(ROOTCINTTMPEXE)
+		@echo "Generating second dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 2 -c $(FITPANELH) $(FITPANELL)
+
+#LF
+$(FITPANELDS):    $(FITPANELH) $(FITPANELL) $(ROOTCINTTMPEXE) $(FITPANELNM)
+		@echo "Generating third dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -L $(ROOTSYS)/$(FITPANELNM) -. 3 -c $(FITPANELH) $(FITPANELL)
+
+#LF
+$(FITPANELDICTMAP): $(RLIBMAP) $(MAKEFILEDEP) $(FITPANELL)
+		$(RLIBMAP) -o $(FITPANELDICTMAP) -l $(FITPANELDICTLIB) \
+		-d $(FITPANELLIB) $(FITPANELLIBDEPM) -c $(FITPANELL)
+#LF
+$(FITPANELNM):      $(FITPANELO) $(FITPANELTMPDO) $(FITPANELTMP2DO) 
+		@echo "Generating symbols file $@..."
+		nm -g -p --defined-only $(FITPANELTMPDO) | awk '{printf("%s\n", $$3)'} > $(FITPANELNM)
+		nm -g -p --defined-only $(FITPANELTMP2DO) | awk '{printf("%s\n", $$3)'} >> $(FITPANELNM)
+		nm -g -p --defined-only $(FITPANELO) | awk '{printf("%s\n", $$3)'} >> $(FITPANELNM)
 
 $(FITPANELMAP): $(RLIBMAP) $(MAKEFILEDEP) $(FITPANELL)
 		$(RLIBMAP) -o $(FITPANELMAP) -l $(FITPANELLIB) \
@@ -56,7 +93,12 @@ all-fitpanel:   $(FITPANELLIB) $(FITPANELMAP)
 clean-fitpanel:
 		@rm -f $(FITPANELO) $(FITPANELDO)
 
-clean::         clean-fitpanel
+clean::         clean-fitpanel clean-pds-fitpanel
+
+#LF
+clean-pds-fitpanel:	
+		rm -f $(FITPANELTMPDS) $(FITPANELTMPDO) $(FITPANELTMPDH) \
+		$(FITPANELTMPDEP) $(FITPANELTMP2DS) $(FITPANELTMP2DO) $(FITPANELTMP2DH) $(FITPANELNM)
 
 distclean-fitpanel: clean-fitpanel
 		@rm -f $(FITPANELDEP) $(FITPANELDS) $(FITPANELDH) $(FITPANELLIB) $(FITPANELMAP)
