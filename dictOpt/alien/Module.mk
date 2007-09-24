f4ef2a2b44a416730c0b334b51569f2a9ae80bdf
lfranco <lfranco@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-09-24 15:53:49 +0000

    Tiiiime to plaaaayyysvn infosvn info Start merging code for the stub removal
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20070 27541ba8-7e3a-0410-8455-c3a389f83636

--- dictOpt/alien/Module.mk
+++ dictOpt/alien/Module.mk
@@ -17,11 +17,22 @@ ALIENDS      := $(MODDIRS)/G__Alien.cxx
 ALIENDO      := $(ALIENDS:.cxx=.o)
 ALIENDH      := $(ALIENDS:.cxx=.h)
 
+#LF
+ALIENTMPDS    := $(MODDIRS)/G__AlienTmp.cxx
+ALIENTMPDO    := $(ALIENTMPDS:.cxx=.o)
+ALIENTMPDH    := $(ALIENTMPDS:.cxx=.h)
+ALIENTMP2DS   := $(MODDIRS)/G__AlienTmp2.cxx
+ALIENTMP2DO   := $(ALIENTMP2DS:.cxx=.o)
+ALIENTMP2DH   := $(ALIENTMP2DS:.cxx=.h)
+
 ALIENH       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 ALIENS       := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 ALIENO       := $(ALIENS:.cxx=.o)
 
 ALIENDEP     := $(ALIENO:.o=.d) $(ALIENDO:.o=.d)
+
+#LF
+ALIENTMPDEP  := $(ALIENTMPDO:.o=.d)
 
 ALIENLIB     := $(LPATH)/libRAliEn.$(SOEXT)
 ALIENMAP     := $(ALIENLIB:.$(SOEXT)=.rootmap)
@@ -31,6 +42,9 @@ ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(ALIENH))
 ALLLIBS     += $(ALIENLIB)
 ALLMAPS     += $(ALIENMAP)
 
+#LF
+ALIENNM       := $(ALIENLIB:.$(SOEXT)=.nm)
+
 # include all dependency files
 INCLUDEFILES += $(ALIENDEP)
 
@@ -38,16 +52,40 @@ INCLUDEFILES += $(ALIENDEP)
 include/%.h:    $(ALIENDIRI)/%.h
 		cp $< $@
 
-$(ALIENLIB):    $(ALIENO) $(ALIENDO) $(ORDER_) $(MAINLIBS) $(ALIENLIBDEP)
+#LF
+$(ALIENLIB):   $(ALIENO) $(ALIENTMPDO) $(ALIENTMP2DO) $(ALIENDO) $(ORDER_) $(MAINLIBS) $(ALIENLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libRAliEn.$(SOEXT) $@ "$(ALIENO) $(ALIENDO)" \
+		   "$(SOFLAGS)" libRAliEn.$(SOEXT) $@ "$(ALIENO) $(ALIENTMPDO) $(ALIENTMP2DO) $(ALIENDO)" \
 		   "$(ALIENLIBEXTRA) $(ALIENLIBDIR) $(ALIENCLILIB)"
 
-$(ALIENDS):     $(ALIENH) $(ALIENL) $(ROOTCINTTMPEXE)
-		@echo "Generating dictionary $@..."
-		$(ROOTCINTTMP) -f $@ -c $(ALIENH) $(ALIENL)
+#LF
+$(ALIENTMPDS):   $(ALIENH) $(ALIENL) $(ROOTCINTTMPEXE)
+		@echo "Generating first dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 1 -c $(ALIENH) $(ALIENL)
 
-$(ALIENMAP):    $(RLIBMAP) $(MAKEFILEDEP) $(ALIENL)
+#LF
+$(ALIENTMP2DS):  $(ALIENH) $(ALIENL) $(ROOTCINTTMPEXE)
+		@echo "Generating second dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 2 -c $(ALIENH) $(ALIENL)
+
+#LF
+$(ALIENDS):    $(ALIENH) $(ALIENL) $(ROOTCINTTMPEXE) $(ALIENNM)
+		@echo "Generating third dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -L $(ROOTSYS)/$(ALIENNM) -. 3 -c $(ALIENH) $(ALIENL)
+
+#LF
+$(ALIENDICTMAP): $(RLIBMAP) $(MAKEFILEDEP) $(ALIENL)
+		$(RLIBMAP) -o $(ALIENDICTMAP) -l $(ALIENDICTLIB) \
+		-d $(ALIENLIB) $(ALIENLIBDEPM) -c $(ALIENL)
+#LF
+$(ALIENNM):      $(ALIENO) $(ALIENTMPDO) $(ALIENTMP2DO) 
+		@echo "Generating symbols file $@..."
+		nm -g -p --defined-only $(ALIENTMPDO) | awk '{printf("%s\n", $$3)'} > $(ALIENNM)
+		nm -g -p --defined-only $(ALIENTMP2DO) | awk '{printf("%s\n", $$3)'} >> $(ALIENNM)
+		nm -g -p --defined-only $(ALIENO) | awk '{printf("%s\n", $$3)'} >> $(ALIENNM)
+
+#LF
+$(ALIENMAP):   $(RLIBMAP) $(MAKEFILEDEP) $(ALIENL)
 		$(RLIBMAP) -o $(ALIENMAP) -l $(ALIENLIB) \
 		   -d $(ALIENLIBDEPM) -c $(ALIENL)
 
@@ -56,7 +94,12 @@ all-alien:      $(ALIENLIB) $(ALIENMAP)
 clean-alien:
 		@rm -f $(ALIENO) $(ALIENDO)
 
-clean::         clean-alien
+clean::         clean-alien clean-pds-alien
+
+#LF
+clean-pds-alien:	
+		rm -f $(ALIENTMPDS) $(ALIENTMPDO) $(ALIENTMPDH) \
+		$(ALIENTMPDEP) $(ALIENTMP2DS) $(ALIENTMP2DO) $(ALIENTMP2DH) $(ALIENNM)
 
 distclean-alien: clean-alien
 		@rm -f $(ALIENDEP) $(ALIENDS) $(ALIENDH) $(ALIENLIB) $(ALIENMAP)
