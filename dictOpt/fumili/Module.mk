7fb2b5229d76e690b8a159fac355a97a0ebf81eb
lfranco <lfranco@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-09-24 15:55:03 +0000

    Tiiiime to plaaaayyysvn infosvn info Start merging code for the stub removal
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20074 27541ba8-7e3a-0410-8455-c3a389f83636

--- dictOpt/fumili/Module.mk
+++ dictOpt/fumili/Module.mk
@@ -17,14 +17,28 @@ FUMILIDS     := $(MODDIRS)/G__Fumili.cxx
 FUMILIDO     := $(FUMILIDS:.cxx=.o)
 FUMILIDH     := $(FUMILIDS:.cxx=.h)
 
+#LF
+FUMILITMPDS    := $(MODDIRS)/G__FumiliTmp.cxx
+FUMILITMPDO    := $(FUMILITMPDS:.cxx=.o)
+FUMILITMPDH    := $(FUMILITMPDS:.cxx=.h)
+FUMILITMP2DS   := $(MODDIRS)/G__FumiliTmp2.cxx
+FUMILITMP2DO   := $(FUMILITMP2DS:.cxx=.o)
+FUMILITMP2DH   := $(FUMILITMP2DS:.cxx=.h)
+
 FUMILIH      := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 FUMILIS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 FUMILIO      := $(FUMILIS:.cxx=.o)
 
 FUMILIDEP    := $(FUMILIO:.o=.d) $(FUMILIDO:.o=.d)
 
+#LF
+FUMILITMPDEP  := $(FUMILITMPDO:.o=.d)
+
 FUMILILIB    := $(LPATH)/libFumili.$(SOEXT)
 FUMILIMAP    := $(FUMILILIB:.$(SOEXT)=.rootmap)
+
+#LF
+FUMILINM       := $(FUMILILIB:.$(SOEXT)=.nm)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(FUMILIH))
@@ -38,14 +52,37 @@ INCLUDEFILES += $(FUMILIDEP)
 include/%.h:    $(FUMILIDIRI)/%.h
 		cp $< $@
 
-$(FUMILILIB):   $(FUMILIO) $(FUMILIDO) $(ORDER_) $(MAINLIBS) $(FUMILILIBDEP)
+#LF
+$(FUMILILIB):   $(FUMILIO) $(FUMILITMPDO) $(FUMILITMP2DO) $(FUMILIDO) $(ORDER_) $(MAINLIBS) $(FUMILILIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libFumili.$(SOEXT) $@ "$(FUMILIO) $(FUMILIDO)" \
+		   "$(SOFLAGS)" libFumili.$(SOEXT) $@ "$(FUMILIO) $(FUMILITMPDO) $(FUMILITMP2DO) $(FUMILIDO)" \
 		   "$(FUMILILIBEXTRA)"
 
-$(FUMILIDS):    $(FUMILIH) $(FUMILIL) $(ROOTCINTTMPEXE)
-		@echo "Generating dictionary $@..."
-		$(ROOTCINTTMP) -f $@ -c $(FUMILIH) $(FUMILIL)
+#LF
+$(FUMILITMPDS):   $(FUMILIH) $(FUMILIL) $(ROOTCINTTMPEXE)
+		@echo "Generating first dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 1 -c $(FUMILIH) $(FUMILIL)
+
+#LF
+$(FUMILITMP2DS):  $(FUMILIH) $(FUMILIL) $(ROOTCINTTMPEXE)
+		@echo "Generating second dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 2 -c $(FUMILIH) $(FUMILIL)
+
+#LF
+$(FUMILIDS):    $(FUMILIH) $(FUMILIL) $(ROOTCINTTMPEXE) $(FUMILINM)
+		@echo "Generating third dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -L $(ROOTSYS)/$(FUMILINM) -. 3 -c $(FUMILIH) $(FUMILIL)
+
+#LF
+$(FUMILIDICTMAP): $(RLIBMAP) $(MAKEFILEDEP) $(FUMILIL)
+		$(RLIBMAP) -o $(FUMILIDICTMAP) -l $(FUMILIDICTLIB) \
+		-d $(FUMILILIB) $(FUMILILIBDEPM) -c $(FUMILIL)
+#LF
+$(FUMILINM):      $(FUMILIO) $(FUMILITMPDO) $(FUMILITMP2DO) 
+		@echo "Generating symbols file $@..."
+		nm -g -p --defined-only $(FUMILITMPDO) | awk '{printf("%s\n", $$3)'} > $(FUMILINM)
+		nm -g -p --defined-only $(FUMILITMP2DO) | awk '{printf("%s\n", $$3)'} >> $(FUMILINM)
+		nm -g -p --defined-only $(FUMILIO) | awk '{printf("%s\n", $$3)'} >> $(FUMILINM)
 
 $(FUMILIMAP):   $(RLIBMAP) $(MAKEFILEDEP) $(FUMILIL)
 		$(RLIBMAP) -o $(FUMILIMAP) -l $(FUMILILIB) \
@@ -56,7 +93,12 @@ all-fumili:     $(FUMILILIB) $(FUMILIMAP)
 clean-fumili:
 		@rm -f $(FUMILIO) $(FUMILIDO)
 
-clean::         clean-fumili
+clean::         clean-fumili clean-pds-fumili
+
+#LF
+clean-pds-fumili:	
+		rm -f $(FUMILITMPDS) $(FUMILITMPDO) $(FUMILITMPDH) \
+		$(FUMILITMPDEP) $(FUMILITMP2DS) $(FUMILITMP2DO) $(FUMILITMP2DH) $(FUMILINM)
 
 distclean-fumili: clean-fumili
 		@rm -f $(FUMILIDEP) $(FUMILIDS) $(FUMILIDH) $(FUMILILIB) $(FUMILIMAP)
