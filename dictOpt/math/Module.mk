3fff6d6ab71ca4bdff48e93c172e6e9e97ae38df
lfranco <lfranco@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-09-24 16:06:44 +0000

    Time to play... Start merging code for the stub removal
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20083 27541ba8-7e3a-0410-8455-c3a389f83636

--- dictOpt/math/Module.mk
+++ dictOpt/math/Module.mk
@@ -17,6 +17,14 @@ MATHDS       := $(MODDIRS)/G__Math.cxx
 MATHDO       := $(MATHDS:.cxx=.o)
 MATHDH       := $(MATHDS:.cxx=.h)
 
+#LF
+MATHTMPDS    := $(MODDIRS)/G__MathTmp.cxx
+MATHTMPDO    := $(MATHTMPDS:.cxx=.o)
+MATHTMPDH    := $(MATHTMPDS:.cxx=.h)
+MATHTMP2DS    := $(MODDIRS)/G__MathTmp2.cxx
+MATHTMP2DO    := $(MATHTMP2DS:.cxx=.o)
+MATHTMP2DH    := $(MATHTMP2DS:.cxx=.h)
+
 MATHH1       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 MATHH2       := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/Math/*.h))
 MATHH        := $(MATHH1) $(MATHH2)
@@ -25,8 +33,18 @@ MATHO        := $(MATHS:.cxx=.o)
 
 MATHDEP      := $(MATHO:.o=.d) $(MATHDO:.o=.d)
 
-MATHLIB      := $(LPATH)/libRMath.$(SOEXT)
+#LF
+MATHTMPDEP   := $(MATHTMPDO:.o=.d)
+
+# LF
 MATHMAP      := $(MATHLIB:.$(SOEXT)=.rootmap)
+MATHLIB      := $(MODDIRS)/libRMath.$(SOEXT)
+MATHNM       := $(MATHLIB:.$(SOEXT)=.nm)
+#MATHDICTLIB  := $(LPATH)/libRMathDict.$(SOEXT)
+#MATHDICTMAP  := $(MATHDICTLIB:.$(SOEXT)=.rootmap)
+
+#LF
+MATHNM       := $(MATHLIB:.$(SOEXT)=.nm)
 
 # used in the main Makefile
 ALLHDRS      += $(patsubst $(MODDIRI)/%.h,include/%.h,$(MATHH))
@@ -47,26 +65,56 @@ include/Math/%.h: $(MATHDIRI)/Math/%.h
 include/%.h:    $(MATHDIRI)/%.h
 		cp $< $@
 
-$(MATHLIB):     $(MATHO) $(MATHDO) $(ORDER_) $(MAINLIBS)
-		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libRMath.$(SOEXT) $@ "$(MATHO) $(MATHDO)" \
-		   "$(MATHLIBEXTRA)"
+#LF
+#$(MATHLIB):     $(MATHO) $(MATHTMPDO) $(ORDER_) $(MAINLIBS)
+#		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+#		"$(SOFLAGS)" libRMath.$(SOEXT) $@ "$(MATHO)" $(MATHTMPDO)\
+#		"$(MATHLIBEXTRA)"
 
-$(MATHDS):      $(MATHH) $(MATHL) $(ROOTCINTTMPEXE)
-		@echo "Generating dictionary $@..."
-		$(ROOTCINTTMP) -f $@ -c $(MATHH) $(MATHL)
+#LF
+#$(MATHDICTLIB): $(MATHDO) $(ORDER_) $(MAINLIBS)
+#		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+#		"$(SOFLAGS)" libRMathDict.$(SOEXT) $@ "$(MATHDO)" "$(MATHTMP2DO)"\
+#		"$(MATHDICTLIBEXTRA)"
 
-$(MATHMAP):     $(RLIBMAP) $(MAKEFILEDEP) $(MATHL)
-		$(RLIBMAP) -o $(MATHMAP) -l $(MATHLIB) \
-		   -d $(MATHLIBDEPM) -c $(MATHL)
+#LF
+$(MATHTMPDS):   $(MATHH) $(MATHL) $(ROOTCINTTMPEXE)
+		@echo "Generating first dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 1 -c $(MATHH) $(MATHL)
+
+#LF
+$(MATHTMP2DS):  $(MATHH) $(MATHL) $(ROOTCINTTMPEXE)
+		@echo "Generating second dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 2 -c $(MATHH) $(MATHL)
+
+#LF
+$(MATHDS):      $(MATHH) $(MATHL) $(ROOTCINTTMPEXE) $(MATHNM)
+		@echo "Generating third dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -L $(ROOTSYS)/$(MATHNM) -. 3 -c $(MATHH) $(MATHL)
+
+#LF
+#$(MATHDICTMAP): $(RLIBMAP) $(MAKEFILEDEP) $(MATHL)
+#		$(RLIBMAP) -o $(MATHDICTMAP) -l $(MATHDICTLIB) \
+3		-d $(MATHLIB) $(MATHLIBDEPM) -c $(MATHL)
+#LF
+$(MATHNM):      $(MATHO) $(MATHTMPDO) $(MATHTMP2DO) 
+		@echo "Generating symbols file $@..."
+		nm -g -p --defined-only $(MATHTMPDO) | awk '{printf("%s\n", $$3)'} > $(MATHNM)
+		nm -g -p --defined-only $(MATHTMP2DO) | awk '{printf("%s\n", $$3)'} >> $(MATHNM)
+		nm -g -p --defined-only $(MATHO) | awk '{printf("%s\n", $$3)'} >> $(MATHNM)
 
 #all-math:       $(MATHLIB) $(MATHMAP)
-all-math:       $(MATHO) $(MATHDO)
+all-math:       $(MATHO) $(MATHTMPDO) $(MATHTMP2DO) $(MATHDO)
 
 clean-math:
-		@rm -f $(MATHO) $(MATHDO)
+		@rm -f $(MATHO) $(MATHTMPDO) $(MATHTMP2DO) $(MATHDO)
 
-clean::         clean-math
+clean::         clean-math clean-pds-math
+
+#LF
+clean-pds-math:	
+		rm -f $(MATHTMPDS) $(MATHTMPDO) $(MATHTMPDH) \
+		$(MATHTMPDEP) $(MATHTMP2DS) $(MATHTMP2DO) $(MATHTMP2DH) $(MATHNM)
 
 distclean-math: clean-math
 		@rm -f $(MATHDEP) $(MATHDS) $(MATHDH) $(MATHLIB) $(MATHMAP)
--- dictOpt/minuit/Module.mk
+++ dictOpt/minuit/Module.mk
@@ -17,14 +17,33 @@ MINUITDS     := $(MODDIRS)/G__Minuit.cxx
 MINUITDO     := $(MINUITDS:.cxx=.o)
 MINUITDH     := $(MINUITDS:.cxx=.h)
 
+#LF
+MINUITTMPDS    := $(MODDIRS)/G__MinuitTmp.cxx
+MINUITTMPDO    := $(MINUITTMPDS:.cxx=.o)
+MINUITTMPDH    := $(MINUITTMPDS:.cxx=.h)
+MINUITTMP2DS    := $(MODDIRS)/G__MinuitTmp2.cxx
+MINUITTMP2DO    := $(MINUITTMP2DS:.cxx=.o)
+MINUITTMP2DH    := $(MINUITTMP2DS:.cxx=.h)
+
 MINUITH      := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
 MINUITS      := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
 MINUITO      := $(MINUITS:.cxx=.o)
 
 MINUITDEP    := $(MINUITO:.o=.d) $(MINUITDO:.o=.d)
 
+#LF
+MINUITTMPDEP   := $(MINUITTMPDO:.o=.d)
+
 MINUITLIB    := $(LPATH)/libMinuit.$(SOEXT)
+
+#LF
 MINUITMAP    := $(MINUITLIB:.$(SOEXT)=.rootmap)
+
+MINUITDICTLIB  := $(LPATH)/libMinuitDict.$(SOEXT)
+MINUITDICTMAP  := $(MINUITDICTLIB:.$(SOEXT)=.rootmap)
+
+#LF
+MINUITNM       := $(MINUITLIB:.$(SOEXT)=.nm)
 
 # used in the main Makefile
 ALLHDRS     += $(patsubst $(MODDIRI)/%.h,include/%.h,$(MINUITH))
@@ -38,25 +57,54 @@ INCLUDEFILES += $(MINUITDEP)
 include/%.h:    $(MINUITDIRI)/%.h
 		cp $< $@
 
-$(MINUITLIB):   $(MINUITO) $(MINUITDO) $(ORDER_) $(MAINLIBS) $(MINUITLIBDEP)
+#LF
+$(MINUITLIB):     $(MINUITO) $(MINUITTMPDO) $(MINUITTMP2DO) $(MINUITDO) $(ORDER_) $(MAINLIBS) $(MINUITLIBDEP)
 		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
-		   "$(SOFLAGS)" libMinuit.$(SOEXT) $@ "$(MINUITO) $(MINUITDO)" \
-		   "$(MINUITLIBEXTRA)"
+		"$(SOFLAGS)" libMinuit.$(SOEXT) $@ "$(MINUITO) $(MINUITTMPDO) $(MINUITTMP2DO) $(MINUITDO)" \
+		"$(MINUITLIBEXTRA)"
 
-$(MINUITDS):    $(MINUITH) $(MINUITL) $(ROOTCINTTMPEXE)
-		@echo "Generating dictionary $@..."
-		$(ROOTCINTTMP) -f $@ -c $(MINUITH) $(MINUITL)
+#LF
+#$(MINUITDICTLIB): $(MINUITDO) $(ORDER_) $(MAINLIBS) $(MINUITDICTLIBDEP)
+#		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+#		"$(SOFLAGS)" libMinuitDict.$(SOEXT) $@ "$(MINUITDO) $(MINUITTMP2DO)"\
+#		"$(MINUITDICTLIBEXTRA)"
 
-$(MINUITMAP):   $(RLIBMAP) $(MAKEFILEDEP) $(MINUITL)
+#LF
+$(MINUITTMPDS):   $(MINUITH) $(MINUITL) $(ROOTCINTTMPEXE)
+		@echo "Generating first dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 1 -c $(MINUITH) $(MINUITL)
+
+#LF
+$(MINUITTMP2DS):  $(MINUITH) $(MINUITL) $(ROOTCINTTMPEXE)
+		@echo "Generating second dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -. 2 -c $(MINUITH) $(MINUITL)
+
+#LF
+$(MINUITDS):      $(MINUITH) $(MINUITL) $(ROOTCINTTMPEXE) $(MINUITNM)
+		@echo "Generating third dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -L $(ROOTSYS)/$(MINUITNM) -. 3 -c $(MINUITH) $(MINUITL)
+#LF
+$(MINUITMAP): $(RLIBMAP) $(MAKEFILEDEP) $(MINUITL)
 		$(RLIBMAP) -o $(MINUITMAP) -l $(MINUITLIB) \
-		   -d $(MINUITLIBDEPM) -c $(MINUITL)
+		-d $(MINUITLIBDEPM) -c $(MINUITL)
+#LF
+$(MINUITNM):      $(MINUITO) $(MINUITTMPDO) $(MINUITTMP2DO)
+		@echo "Generating symbols file $@..."
+		nm -g -p --defined-only $(MINUITTMPDO) | awk '{printf("%s\n", $$3)'} > $(MINUITNM)
+		nm -g -p --defined-only $(MINUITTMP2DO) | awk '{printf("%s\n", $$3)'} >> $(MINUITNM)
+		nm -g -p --defined-only $(MINUITO) | awk '{printf("%s\n", $$3)'} >> $(MINUITNM)
 
 all-minuit:     $(MINUITLIB) $(MINUITMAP)
 
 clean-minuit:
 		@rm -f $(MINUITO) $(MINUITDO)
 
-clean::         clean-minuit
+clean::         clean-minuit clean-pds-minuit
+
+#LF
+clean-pds-minuit:	
+		rm -f $(MINUITTMPDS) $(MINUITTMPDO) $(MINUITTMPDH) \
+		$(MINUITTMPDEP) $(MINUITTMP2DS) $(MINUITTMP2DO) $(MINUITTMP2DH) $(MINUITNM)
 
 distclean-minuit: clean-minuit
 		@rm -f $(MINUITDEP) $(MINUITDS) $(MINUITDH) $(MINUITLIB) $(MINUITMAP)
