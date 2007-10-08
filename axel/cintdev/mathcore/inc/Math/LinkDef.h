de5a7f5c36f0a66159feadb172cdeb5921a2b7fd
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-10-08 09:22:27 +0000

    Create a dev branch for testing CINT's trunk with ROOT
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20259 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ axel/cintdev/mathcore/inc/Math/LinkDef.h
@@ -0,0 +1,23 @@
+// @(#)root/mathcore:$Id$
+
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclass;
+#pragma link C++ nestedtypedef;
+
+#pragma link C++ namespace ROOT;
+#pragma link C++ namespace ROOT::Math;
+
+#pragma link C++ class ROOT::Math::Minimizer+;
+#pragma link C++ class ROOT::Math::IntegratorMultiDim+;
+
+
+#include "LinkDef_Func.h" 
+#include "LinkDef_GenVector.h" 
+
+#endif
