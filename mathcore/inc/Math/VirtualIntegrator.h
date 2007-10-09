c6189b767adac461985a2d4860a2d46a3d882c60
moneta <moneta@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-10-09 20:04:23 +0000

    provide dummy implemetation  for  base class functions
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/mathDev@20282 27541ba8-7e3a-0410-8455-c3a389f83636

--- mathcore/inc/Math/VirtualIntegrator.h
+++ mathcore/inc/Math/VirtualIntegrator.h
@@ -1,5 +1,15 @@
-// Interface for numerical integration
-// in one and many dimensions
+// @(#)root/mathcore:$Id$
+// Author: Magdalena Slawinska 10/2007
+
+
+/**********************************************************************
+ *                                                                    *
+ * Copyright (c) 2007  LCG ROOT Math Team, CERN/PH-SFT                *
+ *                                                                    *
+ *                                                                    *
+ **********************************************************************/
+
+// Header file for class Minimizer
 
 #ifndef ROOT_Math_VirtualIntegrator
 #define ROOT_Math_VirtualIntegrator
@@ -8,32 +18,55 @@
 #include "Math/IFunctionfwd.h"
 #endif
 
+#ifndef ROOT_Math_Error
+#include "Math/Error.h"
+#endif
+
 
 
 namespace ROOT {
 namespace Math {
 
 
+/**
+   Interface for numerical integration
+   in one and many dimensions
+
+   @ingroup  Integration
+
+*/
 class VirtualIntegrator{
-   public:
 
-  //1-dim integration
-  virtual double Integrate(double, double);
+public:
+
+   // destructor: no operation
+   virtual ~VirtualIntegrator() {}
+
+   //1-dim integration
+   virtual double Integrate(double, double) {
+      MATH_ERROR_MSG("cannot call VirtualIntegrator::Integrate");
+      return 0;
+   }
 
 
-  //multi-dim integration
-  virtual double Integrate(unsigned int dim, const double*, const double*);
+   //multi-dim integration
+   virtual double Integrate(unsigned int , const double*, const double*) { 
+      MATH_ERROR_MSG("cannot call VirtualIntegrator::Integrate");
+      return 0;
+   }
 
-  //setting a 1-dim function
-  virtual void SetFunction(const IGenFunction &f);
 
- //setting a multi-dim function
-  virtual void SetFunction(const IMultiGenFunction &f);
-
+   //setting a 1-dim function
+   virtual void SetFunction(const IGenFunction &) {}
+   
+   //setting a multi-dim function
+   virtual void SetFunction(const IMultiGenFunction &) {}
+   
 };
 
 
 }//namespace Math
 }//namespace ROOT
+
 
 #endif /* ROOT_Math_VirtualIntegrator */
