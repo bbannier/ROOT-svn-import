6b3f3e19b6f058005eab40cbc87c228e16868a15
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-10-16 22:07:39 +0000

    Synchronize to trunk r20381
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/proof@20383 27541ba8-7e3a-0410-8455-c3a389f83636

--- base/inc/RVersion.h
+++ base/inc/RVersion.h
@@ -14,10 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.17/03"
-#define ROOT_RELEASE_DATE "Aug 30 2007"
-#define ROOT_RELEASE_TIME "15:07:04"
-#define ROOT_VERSION_CODE 332035
+#define ROOT_RELEASE "5.17/05"
+#define ROOT_RELEASE_DATE "Oct 16 2007"
+#define ROOT_RELEASE_TIME "15:44:42"
+#define ROOT_SVN_REVISION 20369
+#define ROOT_SVN_BRANCH "trunk"
+#define ROOT_VERSION_CODE 332037
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
