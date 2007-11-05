51f50b40c9e608903ad4d42a7a7ec57fe5b6b81d
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-11-05 06:15:28 +0000

    Update RVersion.h
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20643 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof-tags/v5.17.05-PROOF.00/base/inc/RVersion.h
+++ proof-tags/v5.17.05-PROOF.00/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.17/05"
-#define ROOT_RELEASE_DATE "Oct 16 2007"
-#define ROOT_RELEASE_TIME "15:44:42"
-#define ROOT_SVN_REVISION 20369
-#define ROOT_SVN_BRANCH "trunk"
-#define ROOT_VERSION_CODE 332037
+#define ROOT_RELEASE "5.17/100"
+#define ROOT_RELEASE_DATE "Nov 4 2007"
+#define ROOT_RELEASE_TIME "08:00:00"
+#define ROOT_SVN_REVISION 20643
+#define ROOT_SVN_BRANCH "branches/dev/proof-tags/v5-17-05-PROOF-00"
+#define ROOT_VERSION_CODE 332132
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
