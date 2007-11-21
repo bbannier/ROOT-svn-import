e1b9bb4a1837739c638ad92ad73ceffb303f709f
wouter <wouter@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-11-21 16:58:28 +0000

    o stressRooFit
        - Add regression test support for Double_t values
          and RooTables
    
        - In post-test analysis mode, analyze count of
          (silent) ERROR messages and fail test if they
          occcur
    
        - Eliminate memory leaks in testing framework
    
        - Add arguments '-n <N>' to run single test and
          argument '-mc' to run suite in memory checking 
          mode in which case no references files are opened
          to avoid ROOT I/O related noise in valgrind leak reports.
    
        - Link in 21 new tests
    
    
      o TestBasic2 ... TestBasic22
    
        - New stress testing modules. Modules 4,6,7,11,21 and 22
          are empty for now. All others are fully functional.
    
    
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@20965 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/roofitcore/test/stressRooFit.cxx
+++ roofit/roofitcore/test/stressRooFit.cxx
@@ -17,6 +17,9 @@
 #include "TWebFile.h"
 #include "RooRandom.h"
 #include "RooFitResult.h"
+#include "RooDouble.h"
+#include "Roo1DTable.h"
+
 using namespace std ;
    
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*//
@@ -27,12 +30,12 @@ using namespace std ;
 //*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*_*//
 
 
-Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose) ;
+Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose, Int_t oneTest, Bool_t dryRun) ;
 
 static TDirectory* gMemDir = 0 ;
 
 //------------------------------------------------------------------------
-void StatusPrint(Int_t id,const TString &title,Bool_t status)
+void StatusPrint(Int_t id,const TString &title,Int_t status)
 {
   // Print test program number and its title
   const Int_t kMAX = 65;
@@ -41,7 +44,7 @@ void StatusPrint(Int_t id,const TString &title,Bool_t status)
   TString header = TString("Test ")+number+" : "+title;
   const Int_t nch = header.Length();
   for (Int_t i = nch; i < kMAX; i++) header += '.';
-  cout << header << (status ? "OK" : "FAILED") << endl;
+  cout << header << (status>0 ? "OK" : (status<0 ? "SKIPPED" : "FAILED")) << endl;
 }
 
 
@@ -55,6 +58,8 @@ public:
   void clearSilentMode() ;
   void regPlot(RooPlot* frame, const char* refName) ;  
   void regResult(RooFitResult* r, const char* refName) ;
+  void regValue(Double_t value, const char* refName) ;
+  void regTable(RooTable* t, const char* refName) ;
   Bool_t runTest() ;
   Bool_t runCompTests() ;
 
@@ -65,6 +70,8 @@ protected:
   Int_t _verb ;
   list<pair<RooPlot*, string> > _regPlots ;
   list<pair<RooFitResult*, string> > _regResults ;
+  list<pair<Double_t, string> > _regValues ;
+  list<pair<RooTable*,string> > _regTables ;
 } ;
 
 
@@ -79,13 +86,38 @@ RooFitTestUnit::~RooFitTestUnit()
 
 void RooFitTestUnit::regPlot(RooPlot* frame, const char* refName) 
 {
-  _regPlots.push_back(make_pair(frame,refName)) ;
+  if (_refFile) {
+    _regPlots.push_back(make_pair(frame,refName)) ;
+  } else {
+    delete frame ;
+  }
 }
 
 void RooFitTestUnit::regResult(RooFitResult* r, const char* refName) 
 {
-  _regResults.push_back(make_pair(r,refName)) ;
+  if (_refFile) {
+    _regResults.push_back(make_pair(r,refName)) ;
+  } else {
+    delete r ;
+  }
 }
+
+void RooFitTestUnit::regValue(Double_t d, const char* refName) 
+{
+  if (_refFile) {
+    _regValues.push_back(make_pair(d,refName)) ;
+  }
+}
+
+void RooFitTestUnit::regTable(RooTable* t, const char* refName) 
+{
+  if (_refFile) {
+    _regTables.push_back(make_pair(t,refName)) ;
+  } else {
+    delete t ;
+  }
+}
+
 
 Bool_t RooFitTestUnit::runCompTests() 
 {
@@ -97,33 +129,34 @@ Bool_t RooFitTestUnit::runCompTests()
     if (!_write) {
 
       // Comparison mode
- 
-     // Retrieve benchmark
+      
+      // Retrieve benchmark
       RooPlot* bmark = dynamic_cast<RooPlot*>(_refFile->Get(iter->second.c_str())) ;
       if (!bmark) {
 	cout << "stressRooFit ERROR: cannot retrieve RooPlot " << iter->second << " from reference file, skipping " << endl ;
 	ret = kFALSE ;
+	++iter ;
 	continue ;
       }
       
       if (_verb) {
 	cout << "comparing RooPlot " << iter->first << " to benchmark " << iter->second << " = " << bmark << endl ;      
       }
-
+      
       Stat_t nItems = iter->first->numItems() ;
       for (Stat_t i=0 ; i<nItems ; i++) {
 	TObject* obj = iter->first->getObject((Int_t)i) ;
-
+	
 	// Retrieve corresponding object from reference frame
 	TObject* objRef = bmark->findObject(obj->GetName()) ;
-
+	
 	if (!objRef) {
 	  cout << "stressRooFit ERROR: cannot retrieve object " << obj->GetName() << " from reference  RooPlot " << iter->second << ", skipping" << endl ;
 	  ret = kFALSE ;
 	  ++iter ;
 	  continue ;
 	}
-
+	
 	// Histogram comparisons
 	if (obj->IsA()==RooHist::Class()) {
 	  RooHist* testHist = static_cast<RooHist*>(obj) ;
@@ -144,6 +177,9 @@ Bool_t RooFitTestUnit::runCompTests()
 	}	
 	
       }
+      
+      // Delete RooPlot when comparison is finished to avoid noise in leak checking
+      delete iter->first ;
 
     } else {
 
@@ -184,6 +220,9 @@ Bool_t RooFitTestUnit::runCompTests()
 	     <<   " fails comparison with counterpart in reference RooFitResult " << bmark->GetName() << endl ;
 	ret = kFALSE ;
       }
+
+      // Delete RooFitResult when comparison is finished to avoid noise in leak checking
+      delete iter2->first ;
       
         
     } else {
@@ -199,6 +238,91 @@ Bool_t RooFitTestUnit::runCompTests()
     ++iter2 ;
   }
 
+  list<pair<Double_t, string> >::iterator iter3 = _regValues.begin() ;
+  while (iter3!=_regValues.end()) {
+
+    if (!_write) {
+
+      // Comparison mode
+ 
+     // Retrieve benchmark
+      RooDouble* ref = dynamic_cast<RooDouble*>(_refFile->Get(iter3->second.c_str())) ;
+      if (!ref) {
+	cout << "stressRooFit ERROR: cannot retrieve RooDouble " << iter3->second << " from reference file, skipping " << endl ;
+	++iter3 ;
+	ret = kFALSE ;
+	continue ;
+      }
+      
+      if (_verb) {
+	cout << "comparing value " << iter3->first << " to benchmark " << iter3->second << " = " << (Double_t)(*ref) << endl ;      
+      }
+
+      if (fabs(iter3->first - (Double_t)(*ref))>1e-6 ) {
+	cout << "stressRooFit ERROR: comparison of value " << iter3->first <<   " fails comparison with reference " << ref->GetName() << endl ;
+	ret = kFALSE ;
+      }
+     
+        
+    } else {
+
+      // Writing mode
+      
+      cout <<"stressRooFit: Writing reference Double_t " << iter3->first << " as benchmark " << iter3->second << endl ;
+      _refFile->cd() ;
+      RooDouble* rd = new RooDouble(iter3->first) ;
+      rd->Write(iter3->second.c_str()) ;
+      gMemDir->cd() ;
+    }
+      
+    ++iter3 ;
+  }
+
+
+  list<pair<RooTable*, string> >::iterator iter4 = _regTables.begin() ;
+  while (iter4!=_regTables.end()) {
+
+    if (!_write) {
+
+      // Comparison mode
+ 
+     // Retrieve benchmark
+      RooTable* bmark = dynamic_cast<RooTable*>(_refFile->Get(iter4->second.c_str())) ;
+      if (!bmark) {
+	cout << "stressRooFit ERROR: cannot retrieve RooTable " << iter4->second << " from reference file, skipping " << endl ;
+	++iter4 ;
+	ret = kFALSE ;
+	continue ;
+      }
+
+      if (_verb) {
+	cout << "comparing RooTable " << iter4->first << " to benchmark " << iter4->second << " = " << bmark << endl ;      
+      }
+
+      if (!iter4->first->isIdentical(*bmark)) {
+	cout << "stressRooFit ERROR: comparison of object " << iter4->first->IsA()->GetName() << "::" << iter4->first->GetName() 
+	     <<   " fails comparison with counterpart in reference RooTable " << bmark->GetName() << endl ;
+	ret = kFALSE ;
+      }
+
+      // Delete RooTable when comparison is finished to avoid noise in leak checking
+      delete iter4->first ;
+      
+        
+    } else {
+
+      // Writing mode
+      
+      cout <<"stressRooFit: Writing reference RooTable " << iter4->first << " as benchmark " << iter4->second << endl ;
+      _refFile->cd() ;
+      iter4->first->Write(iter4->second.c_str()) ;
+      gMemDir->cd() ;
+    }
+      
+    ++iter4 ;
+  }
+
+
 
   return ret ;
 }
@@ -207,7 +331,9 @@ void RooFitTestUnit::setSilentMode()
 {
   RooMsgService::instance().setSilentMode(kTRUE) ;
   for (Int_t i=0 ; i<RooMsgService::instance().numStreams() ; i++) {
-    RooMsgService::instance().setStreamStatus(i,kFALSE) ;
+    if (RooMsgService::instance().getStream(i).minLevel<RooMsgService::ERROR) {
+      RooMsgService::instance().setStreamStatus(i,kFALSE) ;
+    }
   }
 }
 
@@ -230,6 +356,11 @@ Bool_t RooFitTestUnit::runTest()
     cout << "*** Begin of output of Unit Test at normal verbosity *************" << endl ;
   }  
 
+  RooMsgService::instance().clearErrorCount() ;
+
+  // Reset random generator seed to make results independent of test ordering
+  RooRandom::randomGenerator()->SetSeed(12345) ;
+
   if (!testCode()) return kFALSE ;
 
   if (_verb<2) { 
@@ -238,36 +369,65 @@ Bool_t RooFitTestUnit::runTest()
     cout << "*** End of output of Unit Test at normal verbosity ***************" << endl ;
   }
 
+  if (RooMsgService::instance().errorCount()>0) {
+    cout << "RooFitUnitTest: ERROR messages were logged, failing test" << endl ;
+    return kFALSE ;
+  }
+
   return runCompTests() ;
 }
 
 
 
 #include "TestBasic1.cxx"
-
+#include "TestBasic2.cxx"
+#include "TestBasic3.cxx"
+#include "TestBasic4.cxx"
+#include "TestBasic5.cxx"
+#include "TestBasic6.cxx"
+#include "TestBasic7.cxx"
+#include "TestBasic8.cxx"
+#include "TestBasic9.cxx"
+#include "TestBasic10.cxx"
+#include "TestBasic11.cxx"
+#include "TestBasic12.cxx"
+#include "TestBasic13.cxx"
+#include "TestBasic14.cxx"
+#include "TestBasic15.cxx"
+#include "TestBasic16.cxx"
+#include "TestBasic17.cxx"
+#include "TestBasic18.cxx"
+#include "TestBasic19.cxx"
+#include "TestBasic20.cxx"
+#include "TestBasic21.cxx"
+#include "TestBasic22.cxx"
 
 
 //______________________________________________________________________________
-Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose)
+Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose, Int_t oneTest, Bool_t dryRun)
 {
   // Save memory directory location
   gMemDir = gDirectory ;
 
   TFile* fref = 0 ;
-  if (TString(refFile).Contains("http:")) {
-    if (writeRef) {
-      cout << "stressRooFit ERROR: reference file must be local file in writing mode" << endl ;
+  if (!dryRun) {
+    if (TString(refFile).Contains("http:")) {
+      if (writeRef) {
+	cout << "stressRooFit ERROR: reference file must be local file in writing mode" << endl ;
+	return kFALSE ;
+      }
+      fref = new TWebFile(refFile) ;
+    } else {
+      fref = new TFile(refFile,writeRef?"RECREATE":"") ;
+    }
+    if (fref->IsZombie()) {
+      cout << "stressRooFit ERROR: cannot open reference file " << refFile << endl ;
       return kFALSE ;
     }
-    fref = new TWebFile(refFile) ;
-  } else {
-    fref = new TFile(refFile,writeRef?"RECREATE":"") ;
-  }
-  if (fref->IsZombie()) {
-    cout << "stressRooFit ERROR: cannot open reference file " << refFile << endl ;
-    return kFALSE ;
   }
 
+  // Add dedicated logging stream for errors that will remain active in silent mode
+  RooMsgService::instance().addStream(RooMsgService::ERROR) ;
 
   cout << "******************************************************************" <<endl;
   cout << "*  RooFit - S T R E S S suite                                    *" <<endl;
@@ -281,10 +441,117 @@ Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose)
   cout << "******************************************************************" <<endl;
   
   gBenchmark->Start("StressRooFit");
-  
-  TestBasic1 test1(fref,writeRef,doVerbose) ;
-  StatusPrint(1,"Basic1",test1.runTest());
-  
+
+  if (oneTest<0 || oneTest==1) {
+    TestBasic1 test1(fref,writeRef,doVerbose) ;
+    StatusPrint( 1,"Generate,Fit and Plot on basic p.d.f",test1.runTest());
+  }
+
+  if (oneTest<0 || oneTest==2) {
+    TestBasic2 test2(fref,writeRef,doVerbose) ;
+    StatusPrint( 2,"Addition operator p.d.f",test2.runTest());
+  }
+
+  if (oneTest<0 || oneTest==3) {
+    TestBasic3 test3(fref,writeRef,doVerbose) ;
+    StatusPrint( 3,"Product operator p.d.f",test3.runTest());
+  }
+
+  if (oneTest<0 || oneTest==4) {
+    TestBasic4 test4(fref,writeRef,doVerbose) ;
+    StatusPrint( 4,"Conditional product operator p.d.f",01); // to do
+  }
+
+  if (oneTest<0 || oneTest==5) {
+    TestBasic5 test5(fref,writeRef,doVerbose) ;
+    StatusPrint( 5,"Analytically convolved p.d.f",test5.runTest());
+  }
+
+  if (oneTest<0 || oneTest==6) {
+    TestBasic6 test6(fref,writeRef,doVerbose) ;
+    StatusPrint( 6,"Simultaneous operator p.d.f",-1); // to do
+  }
+
+  if (oneTest<0 || oneTest==7) {
+    TestBasic7 test7(fref,writeRef,doVerbose) ;
+    StatusPrint( 7,"Addition oper. p.d.f with range transformed fractions",-1); // to do
+  }
+
+  if (oneTest<0 || oneTest==8) {
+    TestBasic8 test8(fref,writeRef,doVerbose) ;
+    StatusPrint( 8,"Multiple observable configurations of p.d.f.s",test8.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==9) {
+    TestBasic9 test9(fref,writeRef,doVerbose) ;
+    StatusPrint( 9,"Formula expressions",test9.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==10) {
+    TestBasic10 test10(fref,writeRef,doVerbose) ;
+    StatusPrint(10,"Functions of discrete variables",test10.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==11) {
+    TestBasic11 test11(fref,writeRef,doVerbose) ;
+    StatusPrint(11,"Workspace persistence",-1) ; // to do
+  }
+
+  if (oneTest<0 || oneTest==12) {
+    TestBasic12 test12(fref,writeRef,doVerbose) ;
+    StatusPrint(12,"Extended likelihood constructions",test12.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==13) {
+    TestBasic13 test13(fref,writeRef,doVerbose) ;
+    StatusPrint(13,"Data import and persistence",test13.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==14) {
+    TestBasic14 test14(fref,writeRef,doVerbose) ;
+    StatusPrint(14,"Plotting with variable bin sizes",test14.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==15) {
+    TestBasic15 test15(fref,writeRef,doVerbose) ;
+    StatusPrint(15,"Likelihood ratio projections",test15.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==16) {
+    TestBasic16 test16(fref,writeRef,doVerbose) ;
+    StatusPrint(16,"Complex slice projections",test16.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==17) {
+    TestBasic17 test17(fref,writeRef,doVerbose) ;
+    StatusPrint(17,"Interactive fitting",test17.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==18) {
+    TestBasic18 test18(fref,writeRef,doVerbose) ;
+    StatusPrint(18,"Lagrange multipliers",test18.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==19) {
+    TestBasic19 test19(fref,writeRef,doVerbose) ;
+    StatusPrint(19,"Chi^2 fits",test19.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==20) {
+    TestBasic20 test20(fref,writeRef,doVerbose) ;
+    StatusPrint(20,"Use of conditional observables",test20.runTest()) ;
+  }
+
+  if (oneTest<0 || oneTest==21) {
+    TestBasic21 test21(fref,writeRef,doVerbose) ;
+    StatusPrint(21,"Constant term optimization",-1) ; // to do
+  }
+
+  if (oneTest<0 || oneTest==22) {
+    TestBasic22 test22(fref,writeRef,doVerbose) ;
+    StatusPrint(22,"Numeric Integration",-1) ; // to do
+  }
+
   gBenchmark->Stop("StressRooFit");
   
   
@@ -319,7 +586,13 @@ Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose)
   
   printf("Time at the end of job = %f seconds\n",timer.CpuTime());
 
-  fref->Close() ;
+  if (fref) {
+    fref->Close() ;
+    delete fref ;
+  }
+
+  delete gBenchmark ;
+  gBenchmark = 0 ;
 
   return 0;
 }
@@ -329,10 +602,10 @@ Int_t stressRooFit(const char* refFile, Bool_t writeRef, Int_t doVerbose)
 
 int main(int argc,const char *argv[]) 
 {
-  RooRandom::randomGenerator()->SetSeed(12345) ;
-
   Bool_t doWrite = kFALSE ;
   Int_t doVerbose = 0 ;
+  Int_t oneTest   = -1 ;
+  Int_t dryRun    = kFALSE ;
 
   // Parse command line arguments 
   for (Int_t i=1 ;  i<argc ; i++) {
@@ -340,6 +613,11 @@ int main(int argc,const char *argv[])
     if (arg=="-w") {
       cout << "stressRooFit: running in writing mode to updating reference file" << endl ;
       doWrite = kTRUE ;
+    }
+
+    if (arg=="-mc") {
+      cout << "stressRooFit: running in memcheck mode, no regression tests are performed" << endl ;
+      dryRun=kTRUE ;
     }
 
     if (arg=="-v") {
@@ -351,10 +629,16 @@ int main(int argc,const char *argv[])
       cout << "stressRooFit: running in very verbose mode" << endl ;
       doVerbose = 2 ;
     }
+
+    if (arg=="-n") {
+      cout << "stressRooFit: running single test " << argv[i+1] << endl ;
+      oneTest = atoi(argv[++i]) ;      
+    }
+
   }
 
   gBenchmark = new TBenchmark();
-  stressRooFit("stressRooFit_ref.root",doWrite,doVerbose);  
+  stressRooFit("stressRooFit_ref.root",doWrite,doVerbose,oneTest,dryRun);  
   return 0;
 }
 
