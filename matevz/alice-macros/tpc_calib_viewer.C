1b67b5f3123462b2c89abce333cf769adcff1f89
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-12-03 22:17:32 +0000

    Remove transient version of Reve.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@21165 27541ba8-7e3a-0410-8455-c3a389f83636

--- matevz/alice-macros/tpc_calib_viewer.C
+++
@@ -1,8 +0,0 @@
-void tpc_calib_viewer(const char* file="CalibTree.root")
-{
-  Reve::RGBrowser* b = gEve->GetBrowser();
-  b->StartEmbedding(1);
-  AliTPCCalibViewerGUI::ShowGUI(file);
-  b->StopEmbedding();
-  b->SetTabTitle("TPC Calib", 1);
-}
