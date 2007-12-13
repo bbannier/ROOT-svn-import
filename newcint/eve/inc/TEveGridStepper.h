8cce2709bd6a9b09d54c125dc807707b317aa50b
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-12-13 11:32:03 +0000

    Create dev branch for new CINT to test both cint7 and a new build system which allows parallel use of cint(5) and cint7
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@21359 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ newcint/eve/inc/TEveGridStepper.h
@@ -0,0 +1,65 @@
+// @(#)root/eve:$Id$
+// Authors: Matevz Tadel & Alja Mrak-Tadel: 2006, 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TEveGridStepper
+#define ROOT_TEveGridStepper
+
+#include "TEveUtil.h"
+
+#include "TObject.h"
+
+class TEveTrans;
+
+class TEveGridStepper : public TObject
+{
+   friend class TEveGridStepperSubEditor;
+
+private:
+   Int_t *fLimitArr[3], *fValueArr[3]; //! Internal traversal variables.
+
+   TEveGridStepper(const TEveGridStepper&);            // Not implemented
+   TEveGridStepper& operator=(const TEveGridStepper&); // Not implemented
+
+public:
+   enum EStepMode_e { kSM_XYZ, kSM_YXZ, kSM_XZY };
+
+protected:
+   EStepMode_e fMode;       // Stepping mode, order of filling.
+
+   Int_t   fCx, fCy, fCz;   // Current positions during filling / traversal.
+   Int_t   fNx, fNy, fNz;   // Number of slots in each direction.
+   Float_t fDx, fDy, fDz;   // Step size in each direction.
+   Float_t fOx, fOy, fOz;   // Initial offset for each direction.
+
+public:
+   TEveGridStepper(Int_t sm=kSM_XYZ);
+   virtual ~TEveGridStepper() {}
+
+   void Reset();
+   void Subtract(TEveGridStepper& s);
+   void SetNs(Int_t nx, Int_t ny, Int_t nz=1)
+   { fNx = nx; fNy = ny; fNz = nz; }
+   void SetDs(Float_t dx, Float_t dy, Float_t dz=0)
+   { fDx = dx; fDy = dy; fDz = dz; }
+   void SetOs(Float_t ox, Float_t oy, Float_t oz=0)
+   { fOx = ox; fOy = oy; fOz = oz; }
+
+   Bool_t Step();
+
+   void GetPosition(Float_t* p);
+
+   void SetTrans(TEveTrans* mx);
+   void SetTransAdvance(TEveTrans* mx);
+
+   ClassDef(TEveGridStepper, 1); // Provide discrete position coordinates for placement of objects on regular grids.
+}; // end class TEveGridStepper
+
+#endif
---
+++ newcint/mathmore/src/QuantFuncMathMore.cxx
@@ -0,0 +1,47 @@
+// @(#)root/mathmore:$Id$
+// Authors: L. Moneta, A. Zsenei   08/2005 
+
+#include "gsl/gsl_cdf.h"
+
+
+namespace ROOT {
+namespace Math {
+
+
+
+
+  double chisquared_quantile(double z, double r) {
+
+    return gsl_cdf_chisq_Pinv(z, r);
+
+  }
+
+
+  double gamma_quantile(double z, double alpha, double theta) {
+
+    return gsl_cdf_gamma_Pinv(z, alpha, theta);
+
+  }
+
+
+  double tdistribution_quantile_c(double z, double r) {
+
+    return gsl_cdf_tdist_Qinv(z, r);
+
+  }
+
+
+
+  double tdistribution_quantile(double z, double r) {
+
+    return gsl_cdf_tdist_Pinv(z, r);
+
+  }
+
+
+
+
+
+
+} // namespace Math
+} // namespace ROOT
