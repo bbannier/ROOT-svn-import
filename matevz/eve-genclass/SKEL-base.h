4446bce1714ac5d6c8d8b42c572cdea8c02d6300
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-12-14 13:49:36 +0000

    Add skeleton files for creation of Eve objects, editors and gl renderers.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@21378 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ matevz/eve-genclass/SKEL-base.h
@@ -0,0 +1,34 @@
+// @(#)root/eve:$Id$
+// Author: Matevz Tadel 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_CLASS
+#define ROOT_CLASS
+
+#include "TObject.h"
+
+class CLASS : public TObject
+{
+private:
+   CLASS(const CLASS&);            // Not implemented
+   CLASS& operator=(const CLASS&); // Not implemented
+
+protected:
+
+public:
+   CLASS();
+   virtual ~CLASS() {}
+
+   ClassDef(CLASS, 1); // Short description.
+};
+
+}
+
+#endif
---
+++ matevz/eve-genclass/SKEL-gl.cxx
@@ -0,0 +1,64 @@
+// @(#)root/eve:$Id$
+// Author: Matevz Tadel 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#include "CLASS.h"
+#include "STEM.h"
+
+#include "TGLRnrCtx.h"
+#include "TGLIncludes.h"
+
+//______________________________________________________________________________
+// OpenGL renderer class for STEM.
+//
+
+ClassImp(CLASS);
+
+//______________________________________________________________________________
+CLASS::CLASS() :
+   TGLObject(), fM(0)
+{
+   // Constructor.
+
+   // fDLCache = kFALSE; // Disable display list.
+}
+
+/******************************************************************************/
+
+//______________________________________________________________________________
+Bool_t CLASS::SetModel(TObject* obj, const Option_t* /*opt*/)
+{
+   // Set model object.
+
+   if (SetModelCheckClass(obj, STEM::Class())) {
+      fM = dynamic_cast<STEM*>(obj);
+      return kTRUE;
+   }
+   return kFALSE;
+}
+
+//______________________________________________________________________________
+void CLASS::SetBBox()
+{
+   // Set bounding box.
+
+   // !! This ok if master sub-classed from TAttBBox
+   SetAxisAlignedBBox(((STEM*)fExternalObj)->AssertBBox());
+}
+
+/******************************************************************************/
+
+//______________________________________________________________________________
+void CLASS::DirectDraw(TGLRnrCtx & rnrCtx) const
+{
+   // Render with OpenGL.
+
+   // printf("CLASS::DirectDraw Style %d, LOD %d\n", flags.Style(), flags.LOD());
+}
