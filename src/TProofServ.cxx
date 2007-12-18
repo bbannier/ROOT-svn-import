e323c47d86e0b99f1abb7a59ed05baa5b29faf93
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2007-12-18 16:40:33 +0000

    TXSocket       - disconnect asynchronous request handler at closing    TProofPlayer       - use option "nodelete" when clearing the input list to avoid double deletion    TProofServ       - Add notification message in the exception handler
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/proof/proof@21459 27541ba8-7e3a-0410-8455-c3a389f83636

--- src/TProofServ.cxx
+++ src/TProofServ.cxx
@@ -5351,6 +5351,8 @@ void TProofServ::HandleException(Int_t sig)
 {
    // Exception handler: we do not try to recover here, just exit.
 
+   Error("HandleException","exception triggered by signal: %d", sig);
+
    gSystem->Exit(sig);
 }
 
