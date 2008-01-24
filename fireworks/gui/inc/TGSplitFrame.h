00e1b01e61a1f4c5a37a690de1d5a2b77179ef6b
bellenot <bellenot@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-01-24 13:54:13 +0000

    - Added new GUI classes :    TGSplitFrame       Horizontally or vertically splittable composite frame.       Once split, each part will contain a new TGSplitFrame,       separated by a TGSplitter.    TGShapedFrame       TGCompositeFrame on which a custom shape is applied,       starting from a picture file, by taking its transparency       mask, and then applying the picture as background    TGShapedToolTip       Specific TGShapedFrame with an embedded canvas and       text to display advanced informations with graphics
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@21842 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ fireworks/gui/inc/TGSplitFrame.h
@@ -0,0 +1,50 @@
+// @(#)root/gui:$Id$
+// Author: Bertrand Bellenot 23/01/2008
+
+/*************************************************************************
+ * Copyright (C) 1995-2008, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TGSplitFrame
+#define ROOT_TGSplitFrame
+
+#ifndef ROOT_TGFrame
+#include "TGFrame.h"
+#endif
+
+class TGSplitter;
+
+class TGSplitFrame : public TGCompositeFrame {
+
+private:
+   TGSplitFrame(const TGSplitFrame&); // Not implemented
+   TGSplitFrame& operator=(const TGSplitFrame&); // Not implemented
+
+protected:
+   TGFrame        *fFrame;       // Pointer to the embedded frame (if any)
+   TGSplitter     *fSplitter;    // Pointer to the (H/V) Splitter (if any)
+   TGSplitFrame   *fFirst;       // Pointer to the first child (if any)
+   TGSplitFrame   *fSecond;      // Pointer to the second child (if any)
+
+public:
+   TGSplitFrame(const TGWindow *p = 0, UInt_t w = 1, UInt_t h = 1,
+                UInt_t options = 0);
+   virtual ~TGSplitFrame();
+
+   virtual void   HSplit();
+   virtual void   VSplit();
+   virtual void   Cleanup();
+   TGSplitFrame  *GetFirst() const { return fFirst; }
+   TGSplitFrame  *GetSecond() const { return fSecond; }
+   TGSplitter    *GetSplitter() const { return fSplitter; }
+
+   virtual void   SavePrimitive(ostream &out, Option_t *option = "");
+
+   ClassDef(TGSplitFrame, 0) // Splittable composite frame
+};
+
+#endif
