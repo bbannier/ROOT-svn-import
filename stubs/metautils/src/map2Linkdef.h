99c77f775699a5516cf5cd2e66a0a9ad5f35e4c0
lfranco <lfranco@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-02-18 19:53:32 +0000

    fix properties
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@22213 27541ba8-7e3a-0410-8455-c3a389f83636

--- stubs/metautils/src/map2Linkdef.h
+++ stubs/metautils/src/map2Linkdef.h
@@ -12,7 +12,7 @@ using namespace std;
 #pragma create TClass map<long,void*>;
 #pragma create TClass map<long,char*>;
 
-pragma create TClass map<double,int>;
+#pragma create TClass map<double,int>;
 #pragma create TClass map<double,long>;
 #pragma create TClass map<double,float>;
 #pragma create TClass map<double,double>;
