e39fe129a39b24de90b955d717c0d4702830d0a8
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-02-26 14:05:12 +0000

    Add subdir "build", "ROOT" for sub-Makefiles
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/stubs@22340 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/Cint.mk
+++ cint/Cint.mk
@@ -27,7 +27,7 @@ ifeq ($(subst msvc,,$(G__CFG_ARCH)),$(G__CFG_ARCH))
   ALLDLLS += longlong.cintdll ipc.cintdll posix.cintdll
 endif
 
-BUILDDLL = chmod a+x setup; PATH=../..:$$PATH LD_LIBRARY_PATH=../..:$$LD_LIBRARY_PATH ./setup
+BUILDDLL = chmod a+x setup; PATH=../../..:$$PATH LD_LIBRARY_PATH=../../..:$$LD_LIBRARY_PATH ./setup
 MKTYPES  = $(G__CFG_COREVERSION)/lib/posix/mktypes$(G__CFG_EXEEXT)
 MAKEINCL = $(G__CFG_COREVERSION)/include/mkincld$(G__CFG_EXEEXT)
 IOSENUMH = $(G__CFG_COREVERSION)/include/iosenum.h
@@ -42,7 +42,7 @@ dlls: $(ALLDLLS)
 ipc.cintdll posix.cintdll: $(G__CFG_COREVERSION)/include/sys/types.h
 
 %.cintdll: $(MAKECINT) $(CINTLIBSTATIC) $(CINT)
-	(cd lib/$*; $(BUILDDLL))
+	(cd $(G__CFG_COREVERSION)/lib/$*; $(BUILDDLL))
 
 $(G__CFG_COREVERSION)/include/systypes.h: $(MKTYPES)
 	cd $(dir $(MKTYPES)) \
