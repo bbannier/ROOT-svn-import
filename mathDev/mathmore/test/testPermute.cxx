de3ab29e14aabf4046c5c47dd18b226bfa467253
dmgonzal <dmgonzal@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-03-04 17:23:04 +0000

    testPermute now compares the total time with the GSL algorithm. Also, the raw vectors are changed to use std::vector for windows compilation
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@22446 27541ba8-7e3a-0410-8455-c3a389f83636

--- mathDev/mathmore/test/testPermute.cxx
+++ mathDev/mathmore/test/testPermute.cxx
@@ -3,6 +3,8 @@
 #include <algorithm>
 #include <vector>
 
+#include <gsl/gsl_permutation.h>
+
 #include <TRandom2.h>
 #include <TMath.h>
 #include <TStopwatch.h>
@@ -22,51 +24,51 @@ const int arraysize = (maxsize-minsize) + 1;
 
 using namespace std;
 
-// ostream& operator <<(ostream& os, const Int_t v[])
-// {
-//    os << '[';
-//    for ( Int_t i = 0; i < n - 1; i++) {
-//       os << v[i] << ' ';
-//    }
-//    os << v[n-1] << ']';
+ ostream& operator <<(ostream& os, const vector<Int_t>& v)
+{
+   os << "[ ";
+   for ( vector<Int_t>::const_iterator i = v.begin(); i != v.end() ; ++i) {
+      os << *i << ' ';
+   }
+   os << ']';
 
-//    return os;
-// }
+   return os;
+}
 
-void initArray(Int_t n, Int_t array[])
+void initArray(Int_t n, vector<Int_t>& array)
 {
    TRandom2 r( time( 0 ) );
    for ( Int_t i = 0; i < n; i++) {
       array[i] = r.Integer( maxint ); 
    }
-   sort(array, array + n);
+   sort(array.begin(), array.end());
 }
 
 bool checkPermute()
 {
-   Int_t n = minsize;
+   const Int_t n = minsize;
 
-   Int_t original[n];
-   Int_t vM[n];
-   Int_t vS[n];
+   vector<Int_t> original(n);
+   vector<Int_t> vM(n);
+   vector<Int_t> vS(n);
 
    bool equals = true;
 
    initArray(n, original);
 
    // TMATH
-   copy(original, original + n, vM);
-   copy(original, original + n, vS);
+   copy(original.begin(), original.end(), vM.begin());
+   copy(original.begin(), original.end(), vS.begin());
    //cout << original << vM << vS << endl;
 
-   while ( TMath::Permute(n, vM) ) {
-      std::next_permutation(vS, vS + n);
+   while ( TMath::Permute(n, &vM[0]) ) {
+      std::next_permutation(&vS[0], &vS[n]);
       //cout << vM << vS << endl;
-      equals &= equal(vM, vM + n, vS);
+      equals &= equal(vM.begin(), vM.end(), vS.begin());
    }
 
-   TMath::Permute(n, vM);
-   std::next_permutation(vS, vS+n);
+   TMath::Permute(n, &vM[0]);
+   std::next_permutation(vS.begin(), vS.end());
    //cout << "kFALSE: " << vM << vS << endl;
 
    return equals;
@@ -74,8 +76,8 @@ bool checkPermute()
 
 void permuteTime(const int n, double* tTMath, double* tStd)
 {
-   Int_t original[n];
-   Int_t vector[n];
+   vector<Int_t> original(n);
+   vector<Int_t> v(n);
 
    TStopwatch t; 
 
@@ -84,28 +86,48 @@ void permuteTime(const int n, double* tTMath, double* tStd)
    // TMATH
    t.Start(); 
    for (int j = 0; j < npass; ++j) { 
-      copy(original, original + n, vector);
-      while ( TMath::Permute(n, vector) );
+      copy(original.begin(), original.end(), v.begin());
+      while ( TMath::Permute(n, &v[0]) );
    }
    t.Stop(); 
    *tTMath =  t.RealTime();
-   cout << "TMath::Permute time :\t " << t.RealTime() << endl;
+   cout << "TMath::Permute time :\t " << t.RealTime();
 
    // STD
    t.Start();
    for (int j = 0; j < npass; ++j) { 
-      copy(original, original + n, vector);
-      while ( std::next_permutation(vector, vector + n) );
+      copy(original.begin(), original.end(), v.begin());
+      while ( std::next_permutation(&v[0], &v[n]) );
    }
    t.Stop();
    *tStd = t.RealTime();
-   cout << "std::next_permutation time :\t " << t.RealTime() << endl;
+   cout << "  std::next_permutation time :\t " << t.RealTime();
+}
+
+void testGSLPermute(Int_t n, double* tGSL)
+{
+   TStopwatch t; 
+
+   gsl_permutation *p = gsl_permutation_alloc(n);
+
+   gsl_permutation_init(p);
+
+   t.Start();
+   while ( gsl_permutation_next(p) == GSL_SUCCESS )
+   {
+//       gsl_permutation_fprintf(stdout, p, " %u");
+//       fprintf(stdout, "\n");
+   }
+   t.Stop();
+   *tGSL = t.RealTime();
+   cout << "  gsl_permutation_next time :\t " << t.RealTime();
 }
 
 void testPermute() 
 {
    vector<double> tM( arraysize );
    vector<double> tS( arraysize );
+   vector<double> tG( arraysize );
    vector<double> index( arraysize );
 
    cout << "checkPermute()...." 
@@ -114,12 +136,14 @@ void testPermute()
 
    for ( int i = minsize; i <= maxsize; i += 1)
    {
-      permuteTime(i, &tM[i - minsize ], &tS[ i -minsize ]);
+      permuteTime(i, &tM[ i - minsize ], &tS[ i -minsize ]);
+      testGSLPermute(i, &tG[ i - minsize ]);
       index[ i - minsize ] = i;
+      cout << endl;
    }
 
    for ( int i = minsize; i <= maxsize; i += 1)
-      cout << tM[ i - minsize ] << ' ' << tS[ i - minsize ] << endl;
+      cout << tM[ i - minsize ] << ' ' << tS[ i - minsize ] << ' ' << tG[ i - minsize ] << endl;
 
    TCanvas* c1 = new TCanvas("c1", "Comparision of Permutation Time", 600, 400);
    TH2F* hpx = new TH2F("hpx", "Comparision of Permutation Time", arraysize, minsize, maxsize, arraysize, tM[0],tS[arraysize-1]);
@@ -138,9 +162,16 @@ void testPermute()
    gS->SetTitle("std::next_permutation()");
    gS->Draw("SAME");
 
+   TGraph* gG = new TGraph(arraysize, &index[0], &tG[0]);
+   gG->SetLineColor(4);
+   gG->SetLineWidth(3);
+   gG->SetTitle("gsl_permutation_next()");
+   gG->Draw("SAME");
+
    TLegend* legend = new TLegend(0.15,0.72,0.4,0.86);
    legend->AddEntry(gM, "TMath::Permute()");
    legend->AddEntry(gS, "std::next_permutation()");
+   legend->AddEntry(gG, "gsl_permutation_next()");
    legend->Draw();
 
    hpx->GetXaxis()->SetTitle("Array Size");
