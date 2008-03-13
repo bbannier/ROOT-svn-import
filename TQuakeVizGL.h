2b38914fc5efb8e9a11dadef10216bdecdb91cef
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-03-13 18:12:20 +0000

    New tutorial for visualization of earthquake data.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/matevz@22645 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ TQuakeVizGL.h
@@ -0,0 +1,47 @@
+// @(#)root/eve:$Id$
+// Author: Matevz Tadel 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TQuakeVizGL
+#define ROOT_TQuakeVizGL
+
+#include "TGLObject.h"
+
+class TGLViewer;
+class TGLScene;
+
+class TQuakeViz;
+
+class TQuakeVizGL : public TGLObject
+{
+private:
+   TQuakeVizGL(const TQuakeVizGL&);            // Not implemented
+   TQuakeVizGL& operator=(const TQuakeVizGL&); // Not implemented
+
+protected:
+   TQuakeViz             *fM;  // Model object.
+
+public:
+   TQuakeVizGL();
+   virtual ~TQuakeVizGL() {}
+
+   virtual Bool_t SetModel(TObject* obj, const Option_t* opt=0);
+   virtual void   SetBBox();
+
+   virtual void DirectDraw(TGLRnrCtx & rnrCtx) const;
+
+   // To support two-level selection
+   // virtual Bool_t SupportsSecondarySelect() const { return kTRUE; }
+   // virtual void ProcessSelection(TGLRnrCtx & rnrCtx, TGLSelectRecord & rec);
+
+   ClassDef(TQuakeVizGL, 0); // GL renderer class for TQuakeViz.
+};
+
+#endif
