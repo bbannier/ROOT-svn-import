c325abc913860df0aaa977e626abdc2177cd926d
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-03-20 09:00:52 +0000

    Create dev branch for prooflite
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/gerri@22765 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ prooflite/build/unix/makechangelog.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+SVN2CL=build/unix/svn2cl.sh
+
+echo ""
+echo "Generating README/ChangeLog from SVN logs..."
+echo ""
+
+# Generate ChangeLog from version v5-12-00 till now
+$SVN2CL -i -f README/ChangeLog -r HEAD:15807
+
+exit 0
