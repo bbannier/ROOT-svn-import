78fa2452da310116e9cc65ee1097699db4040839
dmgonzal <dmgonzal@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-04-01 09:03:22 +0000

    streesTMath modified with a few improvements, i.e. choosing the size and type of the array
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/mathDev@22919 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/mathcore/test/stressTMath.cxx
+++ math/mathcore/test/stressTMath.cxx
@@ -5,31 +5,31 @@
 #include <TStopwatch.h>
 #include <TMath.h>
 
-using namespace std;
-using namespace TMath;
+using std::cout;
+using std::endl;
 
-const unsigned int SIZE = 100000;
 const unsigned int NUMTEST = 500;
 
-//#define DEBUG
+// #define DEBUG
 
 template <typename T> T randD() {
    static TRandom2 r( time( 0 ) );
    return (T) r.Uniform(-500,500);
 }
 
-template <typename T> double stressVector(const char* type)
+template <typename T> double stressVector(unsigned int size, const char* type)
 {
    cout << "Generating random vector of '" 
         << type << "'..." << endl;
 
    double totalTime = 0;
+   double totalUnitTime = 0;
 
-   T *vector = new T[SIZE];
-   generate(vector, &vector[SIZE], randD<T>);
+   T *vector = new T[size];
+   std::generate(vector, &vector[size], randD<T>);
 
 #ifdef DEBUG
-   for ( unsigned int i = 0; i < SIZE; ++i )
+   for ( unsigned int i = 0; i < size; ++i )
       cout << vector[i] << " " << endl;
 #endif
 
@@ -37,80 +37,110 @@ template <typename T> double stressVector(const char* type)
 
    w.Start( kTRUE );
    for ( unsigned int i = 0; i < NUMTEST; ++i )
-      MinElement(SIZE, vector);
+      TMath::MinElement(size, vector);
    w.Stop();
-   cout << "MinMaxElement() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "MinMaxElement() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/NUMTEST << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
    w.Start( kTRUE );
    for ( unsigned int i = 0; i < NUMTEST; ++i )
-      LocMin(SIZE, vector);
+      TMath::LocMin(size, vector);
    w.Stop();
-   cout << "LocMin/Max() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "LocMin/Max() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/NUMTEST << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
    w.Start( kTRUE );
    for ( unsigned int i = 0; i < NUMTEST; ++i )
-      Mean(SIZE, vector);
+      TMath::Mean(size, vector);
    w.Stop();
-   cout << "Mean() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "Mean() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/NUMTEST << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
+
+   w.Start( kTRUE );
+   for ( unsigned int i = 0; i < NUMTEST/2; ++i )
+      TMath::Median(size, vector);
+   w.Stop();
+   cout << "Median() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/(NUMTEST/2) << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
    w.Start( kTRUE );
    for ( unsigned int i = 0; i < NUMTEST; ++i )
-      Median(SIZE, vector);
+      TMath::RMS(size, vector);
    w.Stop();
-   cout << "Median() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "RMS() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/NUMTEST << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
    w.Start( kTRUE );
-   for ( unsigned int i = 0; i < NUMTEST; ++i )
-      RMS(SIZE, vector);
+   for ( unsigned int i = 0; i < NUMTEST/4; ++i )
+      TMath::GeomMean(size, vector);
    w.Stop();
-   cout << "RMS() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "GeomMean() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/(NUMTEST/4) << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
+   Int_t index[size];
    w.Start( kTRUE );
-   for ( unsigned int i = 0; i < NUMTEST; ++i )
-      GeomMean(SIZE, vector);
+   for ( unsigned int i = 0; i < NUMTEST/5; ++i )
+      TMath::Sort(size, vector, index, kFALSE);
    w.Stop();
-   cout << "GeomMean() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "Sort() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/(NUMTEST/5) << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
-   Int_t index[SIZE];
+   std::sort(vector, vector + size);
+#ifdef DEBUG
+   for ( unsigned int i = 0; i < size; ++i )
+      cout << vector[i] << " " << endl;
+#endif
    w.Start( kTRUE );
-   for ( unsigned int i = 0; i < NUMTEST; ++i )
-      Sort(SIZE, vector, index, kFALSE);
+   for ( unsigned int i = 0; i < 100*NUMTEST; ++i )
+      TMath::BinarySearch(size, vector, vector[ i % size ]);
    w.Stop();
-   cout << "Sort() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "BinarySearch() Total Time: " << w.CpuTime() 
+        << " Time: " << w.CpuTime()/(100*NUMTEST) << endl;
+   totalUnitTime += w.CpuTime()/NUMTEST;
+   totalTime += w.CpuTime();
 
-   w.Start( kTRUE );
-   for ( unsigned int i = 0; i < NUMTEST; ++i )
-      BinarySearch(SIZE, vector, vector[1]);
-   w.Stop();
-   cout << "BinarySearch() Time: " << w.CpuTime()/NUMTEST << endl;
-   totalTime += w.CpuTime()/NUMTEST;
+   cout << "\nTotal Time: "      << totalTime << "\n"
+        << "Total Unit Time: " << totalUnitTime << "\n" << endl;
 
-   cout << "Total Time: " << totalTime << "\n" << endl;
+   delete vector;
 
-   return totalTime;
+   return totalUnitTime;
 }
 
-void stressTMath() 
+void stressTMath(unsigned int size, char * type) 
 {
    double totalTime = 0;
    
    cout << "Stress Test Start..." << endl;
 
-   totalTime += stressVector<Short_t>("Short_t");
-   totalTime += stressVector<Int_t>("Int_t");
-   totalTime += stressVector<Float_t>("Float_t");
-   totalTime += stressVector<Double_t>("Double_t");
-   totalTime += stressVector<Long_t>("Long_t");
-   totalTime += stressVector<Long64_t>("Long64_t");
+   if ( strcmp(type, "Short_t") == 0 )
+      totalTime += stressVector<Short_t>(size, type);
+   else if ( strcmp(type, "Int_t") == 0 )
+      totalTime += stressVector<Int_t>(size, type);
+   else if ( strcmp(type, "Float_t") == 0 )
+      totalTime += stressVector<Float_t>(size, type);
+   else if ( strcmp(type, "Long_t") == 0 )
+      totalTime += stressVector<Long_t>(size, type);
+   else if ( strcmp(type, "Long64_t") == 0 )
+      totalTime += stressVector<Long64_t>(size, type);
+   else
+      totalTime += stressVector<Double_t>(size, "Double_t");
    
-   cout << "Total Test Time: " << totalTime << "\n" << endl;
+   //cout << "Total Test Time: " << totalTime << "\n" << endl;
 
    cout << "End of Stress Test..." << endl;
 
@@ -118,9 +148,33 @@ void stressTMath()
 }
 
 
-int main()
+int main(int argc, char* argv[])
 {
-   stressTMath();
+   // Default size and data type
+   unsigned int size = 100000;
+   char* type = "Double_t";
+      
+   if ( argc == 1 )
+   {
+      cout << "Usage: " << argv[0]
+           << " [SIZE OF ARRAY] [TYPE OF ARRAY]\n\n"
+           << "where [TYPE OF ARRAY] is one of the following:\n"
+           << "\t\"Short_t\"\n"
+           << "\t\"Int_t\"\n"
+           << "\t\"Float_t\"\n"
+           << "\t\"Long_t\"\n"
+           << "\t\"Long64_t\"\n"
+           << "\t \"Double_t\"\n"
+           << endl;
+   }
+
+   if ( argc > 1 )
+      size = (unsigned int) atoi(argv[1]);
+
+   if ( argc > 2 )
+      type = argv[2];
+
+   stressTMath(size, type);
 
    return 0;
 }
