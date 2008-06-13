b7265285a2da4790f8314b43e7088e897f566444
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-06-13 17:30:44 +0000

    Merge rev 24270 and 24271 from trunk.
    ==========================================
    
    From Alja.
    
    Fix  font rendering bug on mac os.
    
    Move build of TEveCaloData::CellId_t cache from render classes to the base
    classes. Rename fCache to fCellIdCache.
    
    
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/fireworks@24272 27541ba8-7e3a-0410-8455-c3a389f83636

--- eve/src/TEveCaloLegoGL.cxx
+++ eve/src/TEveCaloLegoGL.cxx
@@ -625,16 +625,8 @@ void TEveCaloLegoGL::DrawXYScales(TGLRnrCtx & rnrCtx,
    
    Float_t yOff  =  0.03*TMath::Sign(y1-y0, axY);
    Float_t xOff  =  0.03*TMath::Sign(x1-x0, ayX);
-
    Float_t rxy = (fPhiAxis->GetXmax()-fPhiAxis->GetXmin())/(fEtaAxis->GetXmax()-fEtaAxis->GetXmin());
-   if (rxy>1)
-   {
-      yOff /= rxy;
-   }
-   else
-   {
-      xOff *=rxy;
-   }
+   (rxy>1) ? yOff /= rxy : xOff *=rxy;
 
    Float_t xOff2 =  xOff*0.5;
    Float_t yOff2 =  yOff*0.5;
@@ -1130,7 +1122,6 @@ void TEveCaloLegoGL::DirectDraw(TGLRnrCtx & rnrCtx) const
    else
       fDataMax   = fM->fData->GetMaxVal(fM->fPlotEt);
 
-   //   printf("Direct draw max %f\n", fDataMax);
    Int_t ondiv;
    Double_t omin, omax;
    THLimitsFinder::Optimize(0, fDataMax, fM->fNZSteps, omin, omax, ondiv,  fZAxisStep);
@@ -1140,12 +1131,10 @@ void TEveCaloLegoGL::DirectDraw(TGLRnrCtx & rnrCtx) const
 
 
    // cache
-   if (fM->fCacheOK == kFALSE)
+   if (fM->fCellIdCacheOK == kFALSE)
    {
       fDLCacheOK = kFALSE;
-      fM->ClearCache();
-      fM->fData->GetCellList(fM->GetEta(), fM->GetEtaRng(),fM->GetPhi(), fM->GetPhiRng(), fM->fCellList);
-      fM->fCacheOK = kTRUE;
+      fM->BuildCellIdCache();
    }
    if (cells3D && fDLCacheOK == kFALSE) MakeDisplayList();
 
@@ -1164,22 +1153,19 @@ void TEveCaloLegoGL::DirectDraw(TGLRnrCtx & rnrCtx) const
    Float_t sx = (eM-em)/fM->GetEtaRng();
    Float_t sy = (pM-pm)/fM->GetPhiRng();
    glScalef(sx/unit, sy/unit, fM->GetValToHeight());
-
    glTranslatef(-fM->GetEta(), -fM->fPhi, 0);
 
-
-   glPushAttrib(GL_ENABLE_BIT | GL_LINE_BIT | GL_POLYGON_BIT);
+   // draw cells
+   glPushAttrib(GL_LINE_BIT | GL_POLYGON_BIT);
    glLineWidth(1);
    glDisable(GL_LIGHTING);
-   glDisable(GL_CULL_FACE);
    glEnable(GL_NORMALIZE);
    glEnable(GL_POLYGON_OFFSET_FILL);
-
-   // draw cells
    glPushName(0);
    glPolygonOffset(0.8, 1);
    cells3D ? DrawCells3D(rnrCtx):DrawCells2D(rnrCtx);
    glPopName();
+   glPopAttrib();
 
    // draw histogram base
    if (rnrCtx.Selection() == kFALSE && rnrCtx.Highlight() == kFALSE)
@@ -1187,9 +1173,11 @@ void TEveCaloLegoGL::DirectDraw(TGLRnrCtx & rnrCtx) const
       DrawHistBase(rnrCtx);
       if (fM->fDrawHPlane)
       {
+         glPushAttrib(GL_ENABLE_BIT | GL_POLYGON_BIT);
          glEnable(GL_BLEND);
          glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
          glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+         glDisable(GL_CULL_FACE);
          TGLUtil::ColorTransparency(fM->fPlaneColor, fM->fPlaneTransparency);
          Float_t zhp = fM->fHPlaneVal*fZAxisMax;
          glBegin(GL_POLYGON);
@@ -1198,11 +1186,11 @@ void TEveCaloLegoGL::DirectDraw(TGLRnrCtx & rnrCtx) const
          glVertex3f(fM->fEtaMax, fM->GetPhiMax(), zhp);
          glVertex3f(fM->fEtaMin, fM->GetPhiMax(), zhp);
          glEnd();
+	 glPopAttrib();
       }
    }
 
    glPopMatrix();
-   glPopAttrib();
 }
 
 //______________________________________________________________________________
