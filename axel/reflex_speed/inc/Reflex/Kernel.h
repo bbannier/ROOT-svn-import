e9a4f5aac5278278021bba91d7ca51dcfb6b9f56
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-07-03 14:16:09 +0000

    Keyworks need trailing '$'
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@24646 27541ba8-7e3a-0410-8455-c3a389f83636

--- axel/reflex_speed/inc/Reflex/Kernel.h
+++ axel/reflex_speed/inc/Reflex/Kernel.h
@@ -17,13 +17,14 @@
 #define REFLEX_API_VERSION 100001
 
 // Keywords corresponding to the above revision
-#define REFLEX_API_REVISION_RAW "$Revision: 1"
-#define REFLEX_API_DATE_RAW     "$Date: "
-#define REFLEX_API_HEADURL_RAW  "$HeadURL: "
-#define REFLEX_API_ID_RAW       "$Id: "
+#define REFLEX_API_REVISION_RAW "$Revision$"
+#define REFLEX_API_DATE_RAW     "$Date$"
+#define REFLEX_API_HEADURL_RAW  "$HeadURL$"
+#define REFLEX_API_ID_RAW       "$Id$"
 
 // User readable version of the keywords:
 // they do not include the tags themselves but only their values
+// (and a trailing '$')
 #define REFLEX_API_REVISION     (REFLEX_API_REVISION_RAW + 11)
 #define REFLEX_API_DATE         (REFLEX_API_DATE_RAW + 7)
 #define REFLEX_API_HEADURL      (REFLEX_API_HEADURL_RAW + 10)
