ae4e594a4cc1834039f4bd799c1a545d06c6af8f
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-07-03 13:42:07 +0000

    Add a header file forward declaring all of Reflex's API types.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@24638 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ axel/reflex_speed/inc/Reflex/Reflex_fwd.h
@@ -0,0 +1,29 @@
+// @(#)root/reflex:$Id$
+// Author: Axel Naumann, 2008-07-03
+
+// Copyright CERN, CH-1211 Geneva 23, 2004-2006, All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose is hereby granted without fee, provided that this copyright and
+// permissions notice appear in all copies and derivatives.
+//
+// This software is provided "as is" without express or implied warranty.
+
+#ifndef Reflex_Reflex_fwd
+#define Reflex_Reflex_fwd
+
+namespace Reflex {
+   class Any;
+   class Base;
+   class Member;
+   class MemberTemplate;
+   class Object;
+   class ValueObject;
+   class PropertyList;
+   class Scope;
+   class Type;
+   class TypeTemplate;
+   class ICallback;
+}
+
+#endif // Reflex_Reflex
