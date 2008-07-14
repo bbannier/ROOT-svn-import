d27f04a208c2e01fc433165f23fb2e0ada474793
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-07-14 16:17:46 +0000

    Add API class Catalog
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@24815 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ axel/reflex_speed/inc/Reflex/Catalog.h
@@ -0,0 +1,49 @@
+// @(#)root/reflex:$Name:  $:$Id$
+// Author: Axel Naumann, 2008
+
+// Copyright CERN, CH-1211 Geneva 23, 2004-2008, All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose is hereby granted without fee, provided that this copyright and
+// permissions notice appear in all copies and derivatives.
+//
+// This software is provided "as is" without express or implied warranty.
+
+#ifndef Reflex_Catalog
+#define Reflex_Catalog
+
+#include <string>
+#include <typeinfo>
+#include "Reflex/Kernel.h"
+#include "Reflex/Type.h"
+#include "Reflex/Scope.h"
+
+namespace Reflex {
+
+   namespace Internal {
+      class CatalogImpl;
+   }
+
+   class RFLX_API Catalog {
+   public:
+      Catalog(const Internal::CatalogImpl* catalog = 0): fImpl(catalog) {}
+      ~Catalog() {}
+
+      Type ByName(const std::string& name) const;
+      Type ByTypeInfo(const std::type_info & ti) const;
+      Scope ScopeByName(const std::string& name) const;
+
+      // shortcut access:
+      static Scope GlobalScope();
+      static Type Get_int() {return Get(kINT);}
+      static Type Get_float() {return Get(kFLOAT);}
+      static Type Get(EFUNDAMENTALTYPE);
+
+
+   private:
+      const Internal::Catalog*    fCatalog;
+   };
+
+} // namespace Reflex
+
+#endif
