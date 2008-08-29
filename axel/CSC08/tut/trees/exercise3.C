7e5e3dc151e460f6d56add735c097e5a65f01b75
bellenot <bellenot@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-08-29 16:43:12 +0000

    filter Landau distribution when filling tree
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@25310 27541ba8-7e3a-0410-8455-c3a389f83636

--- axel/CSC08/tut/trees/exercise3.C
+++ axel/CSC08/tut/trees/exercise3.C
@@ -4,6 +4,16 @@
 #include "TClonesArray.h"
 #include "myparticle.h"
 
+Float_t MyLandau(Double_t mean = 0, Double_t sigma = 1)
+{
+   Float_t land = 1.0e9;
+   while (land > 3.0 * mean) {
+      land = gRandom->Landau(mean, sigma);
+   }
+   return land;
+   
+}
+
 void exercise3(Int_t nevents = 1000)
 {
    Int_t i, j;
@@ -38,12 +48,12 @@ void exercise3(Int_t nevents = 1000)
          Float_t x = gRandom->Gaus(1.2);
          Float_t y = gRandom->Gaus(0.9);
          Float_t z = gRandom->Gaus();
-         Float_t e_e = gRandom->Landau(510,25.0);
-         Float_t e_u = gRandom->Landau(1056.6,10.0);
-         Float_t e_p = gRandom->Landau(1349.8,13.0);
-         Float_t p_e = gRandom->Landau(1.0, 0.1);
-         Float_t p_u = gRandom->Landau(2.0, 0.2);
-         Float_t p_p = gRandom->Landau(2.7, 0.27);
+         Float_t e_e = MyLandau(510,25.0);
+         Float_t e_u = MyLandau(1056.6,10.0);
+         Float_t e_p = MyLandau(1349.8,13.0);
+         Float_t p_e = MyLandau(1.0, 0.1);
+         Float_t p_u = MyLandau(2.0, 0.2);
+         Float_t p_p = MyLandau(2.7, 0.27);
          // create a new particle in the vector
          Electron *e = new(el[j]) Electron(x, y, z, e_e, p_e);
          Muon     *u = new(mu[j]) Muon(x, y, z, e_u, p_u);
