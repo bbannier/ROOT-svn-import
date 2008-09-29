375506d11aa107c735a4c81a8816f605ffb81786
bellenot <bellenot@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-09-29 14:05:12 +0000

    Create new GUI development branch
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@25584 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ gui_bb/graf3d/eve/inc/TEveProjectionAxesGL.h
@@ -0,0 +1,58 @@
+// @(#)root/eve:$Id$
+// Author: Matevz Tadel 2007
+
+/*************************************************************************
+ * Copyright (C) 1995-2007, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TEveProjectionAxesGL
+#define ROOT_TEveProjectionAxesGL
+
+#include "TGLObject.h"
+#include <list>
+
+class TEveProjectionAxes;
+class TEveProjection;
+class TGLFont;
+
+class TEveProjectionAxesGL : public TGLObject
+{
+private:
+   TEveProjectionAxesGL(const TEveProjectionAxesGL&);            // Not implemented
+   TEveProjectionAxesGL& operator=(const TEveProjectionAxesGL&); // Not implemented
+
+
+   typedef std::pair<Float_t, Float_t>  TM_t; // tick-mark <pos, value> pair
+   typedef std::list<TM_t>              TMList_t;
+
+   mutable TMList_t   fTMList;    // list of tick-mark position-value pairs
+
+   void               DrawTickMarks(Bool_t horizontal, Float_t tms) const;
+
+   void               SplitInterval(Float_t x1, Float_t x2, Int_t axis) const;
+   void               SplitIntervalByPos(Float_t min, Float_t max, Int_t axis)const;
+   void               SplitIntervalByVal(Float_t min, Float_t max, Int_t axis)const;
+
+protected:
+   TEveProjectionAxes     *fM;  // model object.
+   mutable TEveProjection *fProjection; // cached model projection
+
+public:
+   TEveProjectionAxesGL();
+   virtual ~TEveProjectionAxesGL() {}
+
+   virtual Bool_t  SetModel(TObject* obj, const Option_t* opt=0);
+   virtual void    SetBBox();
+   virtual void    DirectDraw(TGLRnrCtx & rnrCtx) const;
+
+   Bool_t IgnoreSizeForOfInterest() const { return kTRUE;}
+
+
+   ClassDef(TEveProjectionAxesGL, 0); // GL renderer class for TEveProjectionAxes.
+};
+
+#endif
---
+++ gui_bb/net/xrootd/src/xrootd/src/XrdOss/XrdOssCache.hh
@@ -0,0 +1,145 @@
+#ifndef __XRDOSS_CACHE_H__
+#define __XRDOSS_CACHE_H__
+/******************************************************************************/
+/*                                                                            */
+/*                        X r d O s s C a c h e . h h                         */
+/*                                                                            */
+/* (c) 2003 by the Board of Trustees of the Leland Stanford, Jr., University  */
+/*                            All Rights Reserved                             */
+/*   Produced by Andrew Hanushevsky for Stanford University under contract    */
+/*              DE-AC03-76-SFO0515 with the Department of Energy              */
+/******************************************************************************/
+
+//         $Id$
+
+#include <time.h>
+#include <sys/stat.h>
+#include "XrdOuc/XrdOucDLlist.hh"
+#include "XrdSys/XrdSysError.hh"
+
+/******************************************************************************/
+/*              O S   D e p e n d e n t   D e f i n i t i o n s               */
+/******************************************************************************/
+
+#ifdef __solaris__
+#include <sys/statvfs.h>
+#define STATFS_t struct statvfs
+#define FS_Stat(a,b) statvfs(a,b)
+#define FS_BLKSZ f_frsize
+#endif
+#ifdef __linux__
+#include <sys/vfs.h>
+#define FS_Stat(a,b) statfs(a,b)
+#define STATFS_t struct statfs
+#define FS_BLKSZ f_bsize
+#endif
+#ifdef AIX
+#include <sys/statfs.h>
+#define STATFS_t struct statfs
+#define FS_Stat(a,b) statfs(a,b)
+#define FS_BLKSZ f_bsize
+#endif
+#ifdef __macos__
+#include <sys/param.h>
+#include <sys/mount.h>
+#define STATFS_t struct statfs
+#define FS_Stat(a,b) statfs(a,b)
+#define FS_BLKSZ f_bsize
+#endif
+#ifdef __FreeBSD__
+#include <sys/param.h>
+#include <sys/mount.h>
+#define STATFS_t struct statfs
+#define FS_Stat(a,b) statfs(a,b)
+/* XXX */
+#define FS_BLKSZ f_bsize
+#endif
+
+/******************************************************************************/
+/*                    X r d O s s C a c h e _ F S D a t a                     */
+/******************************************************************************/
+  
+// Flags values for FSData
+//
+#define XrdOssFSData_OFFLINE  0x0001
+#define XrdOssFSData_ADJUSTED 0x0002
+#define XrdOssFSData_REFRESH  0x0004
+
+class XrdOssCache_FSData
+{
+public:
+
+XrdOssCache_FSData *next;
+long long           size;
+long long           frsz;
+dev_t               fsid;
+const char         *path;
+time_t              updt;
+int                 stat;
+
+       XrdOssCache_FSData(const char *, STATFS_t &, dev_t);
+      ~XrdOssCache_FSData() {if (path) free((void *)path);}
+};
+
+/******************************************************************************/
+/*                        X r d O s s C a c h e _ F S                         */
+/******************************************************************************/
+
+class XrdOssCache_Group;
+  
+class XrdOssCache_FS
+{
+public:
+
+enum FSOpts {None = 0, isXA = 1};
+
+XrdOssCache_FS     *next;
+const   char       *group;
+const   char       *path;
+int                 plen;
+FSOpts              opts;
+        char        suffix[4];  // Corresponds to OssPath::sfxLen
+XrdOssCache_FSData *fsdata;
+XrdOssCache_Group  *fsgroup;
+
+static long long    freeSpace(long long &Size, const char *path=0);
+
+       XrdOssCache_FS(      int  &retc,
+                      const char *fsg,
+                      const char *fsp,
+                      FSOpts      opt);
+      ~XrdOssCache_FS() {if (group) free((void *)group);
+                         if (path)  free((void *)path);
+                        }
+};
+
+/******************************************************************************/
+/*                     X r d O s s C a c h e _ G r o u p                      */
+/******************************************************************************/
+  
+// Eventually we will have management information associated with cache groups
+//
+class XrdOssCache_Group
+{
+public:
+
+XrdOssCache_Group *next;
+char              *group;
+XrdOssCache_FS    *curr;
+long long          Usage;
+long long          Quota;
+int                GRPid;
+static long long   PubQuota;
+
+static XrdOssCache_Group *fsgroups;
+
+       XrdOssCache_Group(const char *grp, XrdOssCache_FS *fsp=0) 
+                        : next(0), group(strdup(grp)), curr(fsp), Usage(0),
+                          Quota(-1), GRPid(-1) {}
+      ~XrdOssCache_Group() {if (group) free((void *)group);}
+};
+
+// Suffixes to filenames that may exist in the cache
+//
+#define XRDOSS_SFX_LIST  (char *)".anew", (char *)".fail", (char *)".lock", (char *)".map", (char *)".stage"
+#endif
