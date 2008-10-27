fa291df486fd66ef8659695f749ea6f63b7aed11
cranmer <cranmer@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-10-27 19:13:54 +0000

    adding HypoTestCalculator interface, CombinedCalculator base class and ProfileLikelihoodCalculator concrete class
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@25984 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ roostats/roofit/roostats/inc/HypoTestCalculator.h
@@ -0,0 +1,44 @@
+// @(#)root/roostats:$Id: HypoTestCalculator.h 24970 2008-10-10  $
+// Author: Kyle Cranmer, Lorenzo Moneta, Gregory Schott, Wouter Verkerke
+/*************************************************************************
+ * Copyright (C) 1995-2008, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOSTATS_HypoTestCalculator
+#define ROOSTATS_HypoTestCalculator
+
+#include "TNamed.h"
+#include "RooStats/HypoTestResult.h"
+#include "RooAbsPdf.h"
+#include "RooArgSet.h"
+
+namespace RooStats {
+  class HypoTestCalculator {
+  public:
+    HypoTestCalculator();
+    virtual ~HypoTestCalculator();
+    
+    // main interface, pure virtual
+    virtual HypoTestResult* GetHypoTest() const = 0;   
+
+    // set the PDF for the null hypothesis
+    virtual void SetNullPdf(const char* name) = 0;
+    // set the PDF for the alternate hypothesis
+    virtual void SetAlternatePdf(const char* name) = 0;
+    // set a common PDF for both the null and alternate hypotheses
+    virtual void SetCommonPdf(const char* name) = 0;
+    // set parameter values for the null if using a common PDF
+    virtual void SetNullParameters(RooArgSet&) = 0;
+    // set parameter values for the alternate if using a common PDF
+    virtual void SetAlternateParameters(RooArgSet&) = 0;
+    
+
+  protected:
+    //    ClassDef(HypoTestCalculator,1)        
+  };
+}
+#endif
