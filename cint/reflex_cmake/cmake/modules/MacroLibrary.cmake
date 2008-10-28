0504dce17cbb9b02fea39aec605f2d6c556ed36d
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-10-28 15:11:35 +0000

    From Hady Zalek (Murex): prototype of CMake-based test suite for Reflex, including the legacy tests (aka check-reflex) and a first set of new ones.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@26000 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex_cmake/cmake/modules/MacroLibrary.cmake
+++ cint/reflex_cmake/cmake/modules/MacroLibrary.cmake
@@ -1,8 +1,9 @@
 # MacroLibrary
 #   Offers a collection of macros which extend the built-in cmake commands
 
-INCLUDE(MacroAdditionalCleanFiles)
 INCLUDE(MacroAddLinkFlags)
+INCLUDE(MacroAddSubdirectories)
+INCLUDE(MacroAdditionalCleanFiles)
 INCLUDE(MacroEnsureOutOfSourceBuild)
 INCLUDE(MacroFindPackageIf)
 INCLUDE(MacroGetTargetDirectory)
