dfa265f583c323af130ebe1ed335e4f713487212
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2008-12-05 14:59:16 +0000

    Tag for PROOF tests
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/proof-tags@26680 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ v5.21.07-PROOF.00/tutorials/eve/test_windows.C
@@ -0,0 +1,72 @@
+#include "TEveWindow.h"
+#include "TEveViewer.h"
+#include "TEveManager.h"
+#include "TEveBrowser.h"
+#include "TEveGedEditor.h"
+#include "TGLEmbeddedViewer.h"
+#include "TCanvas.h"
+#include "TGTab.h"
+
+void test_windows()
+{
+   TEveManager::Create();
+
+   TEveUtil::Macro("pointset_test.C");
+
+   TEveWindowSlot  *slot = 0;
+   TEveWindowFrame *evef = 0;
+
+   TEveViewer *v = 0;
+
+   // ----------------------------------------------------------------
+
+   slot = TEveWindow::CreateWindowInTab(gEve->GetBrowser()->GetTabRight());
+
+   TEveWindowPack* pack1 = slot->MakePack();
+
+   slot = pack1->NewSlot();
+   // Embedded viewer.
+   v = new TEveViewer("BarViewer");
+   TGLEmbeddedViewer* xx = new TGLEmbeddedViewer(0, 0, 0);
+   v->SetGLViewer(xx);
+   evef = slot->MakeFrame(xx->GetFrame());
+   evef->SetElementName("Bar Embedded Viewer");
+
+   gEve->GetViewers()->AddElement(v);
+   v->AddScene(gEve->GetEventScene());
+
+   slot = pack1->NewSlot();   
+   TEveWindowPack* pack2 = slot->MakePack();
+   pack2->FlipOrientation();
+
+   slot = pack2->NewSlot();
+   slot->StartEmbedding();
+   new TCanvas(); // Sometimes crashes on destroy - should use embedded canvas?
+   slot->StopEmbedding();
+
+   slot = pack2->NewSlot();
+   // SA viewer.
+   v = new TEveViewer("FooViewer");
+   slot->StartEmbedding();
+   v->SpawnGLViewer(gClient->GetRoot(), gEve->GetEditor());
+   slot->StopEmbedding("Foo StandAlone Viewer");
+
+   gEve->GetViewers()->AddElement(v);
+   v->AddScene(gEve->GetEventScene());   
+
+   // ----------------------------------------------------------------
+
+   slot = TEveWindow::CreateWindowInTab(gEve->GetBrowser()->GetTabRight());
+
+   TEveWindowTab* tab1 = slot->MakeTab();
+   tab1->NewSlot();
+   slot = tab1->NewSlot();
+
+   TEveWindowTab* tab2 = slot->MakeTab();
+   tab2->NewSlot();
+   tab2->NewSlot();
+
+   // ----------------------------------------------------------------
+
+   gEve->GetBrowser()->GetTabRight()->SetTab(1);
+}
