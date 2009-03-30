82d44d1d6aab2e0652acff603756a47aa7870292
cranmer <cranmer@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-03-30 02:41:55 +0000

    fixed minor bug in ProfileLikelihoodCalculator
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@27984 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roostats/src/FeldmanCousins.cxx
+++ roostats/roofit/roostats/src/FeldmanCousins.cxx
@@ -117,7 +117,7 @@ void FeldmanCousins::CreateParameterPoints() const{
     RooRealVar *myarg; 
     while ((myarg = (RooRealVar *)it.Next())) { 
       if(!myarg) continue;
-      myarg->setBins(10);
+      myarg->setBins(30);
     }
 
     fPointsToTest= new RooDataHist("parameterScan", "", *parameters);
