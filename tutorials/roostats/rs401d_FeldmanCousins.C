6dca814de49f93682c0a95fbe6611841190c9157
cranmer <cranmer@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-04-06 15:07:46 +0000

    Improved documentation
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/roostats@28111 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/roostats/rs401d_FeldmanCousins.C
+++ tutorials/roostats/rs401d_FeldmanCousins.C
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////////////////////////
 //
-// 'Neutrino Oscillation Example from Feldman & Cousins' RooStats tutorial macro #401
+// 'Neutrino Oscillation Example from Feldman & Cousins'
 // author: Kyle Cranmer
 // date March 2009
 //
@@ -8,6 +8,10 @@
 // to create a confidence interval for a toy neutrino oscillation experiment.
 // The example attempts to faithfully reproduce the toy example described in Feldman & Cousins' 
 // original paper, Phys.Rev.D57:3873-3889,1998. 
+//
+// to run it:
+// .L tutorials/roostats/NuMuToNuE_Oscillation.cxx+
+// .x tutorials/roostats/rs401d_FeldmanCousins.C+
 /////////////////////////////////////////////////////////////////////////
 
 #ifndef __CINT__
@@ -26,7 +30,6 @@
 #include "RooAddPdf.h"
 
 #include "TROOT.h"
-#include "NuMuToNuE_Oscillation.h"
 #include "RooPolynomial.h"
 #include "RooRandom.h"
 
@@ -43,6 +46,9 @@
 
 #include <iostream>
 
+// PDF class created for this macro
+#include "NuMuToNuE_Oscillation.h"
+
 // use this order for safety on library loading
 using namespace RooFit ;
 using namespace RooStats ;
@@ -55,20 +61,26 @@ void rs401d_FeldmanCousins()
   TStopwatch t;
   t.Start();
 
-  gROOT->ProcessLine(".L tutorials/roostats/NuMuToNuE_Oscillation.cxx+");
+  //  gROOT->ProcessLine(".L tutorials/roostats/NuMuToNuE_Oscillation.cxx+");
 
   /*
-Taken from Feldman & Cousins paper, Phys.Rev.D57:3873-3889,1998. 
-e-Print: physics/9711021 (see page 13.)
-To demonstrate how this works in practice, and how it compares to alternative approaches 
-that have been used, we consider a toy model of a typical neutrino oscillation experiment. 
-The toy model is deﬁned by the following parameters: Mesons are assumed to decay to 
-neutrinos uniformly in a region 600 m to 1000 m from the detector. The expected background 
-from conventional νe interactions and misidentiﬁed νµ interactions is assumed to be 100 
-events in each of 5 energy bins which span the region from 10 to 60 GeV. We assume that 
-the νµ ﬂux is such that if P (νµ → ν e ) = 0.01 averaged over any bin, then that bin would 
-have an expected additional contribution of 100 events due to ν→ ν 
-oscillations. 
+    Taken from Feldman & Cousins paper, Phys.Rev.D57:3873-3889,1998. 
+    e-Print: physics/9711021 (see page 13.)
+
+    Quantum mechanics dictates that the probability of such a transformation is given by the formula 
+    P (νµ → ν e ) = sin^2 (2θ) sin^2 (1.27 ∆m^2 L /E )
+    where P is the probability for a νµ to transform into a νe , L is the distance in km between 
+    the creation of the neutrino from meson decay and its interaction in the detector, E is the 
+    neutrino energy in GeV, and ∆m^2 = |m^2− m^2 | in (eV/c^2 )^2 . 
+
+    To demonstrate how this works in practice, and how it compares to alternative approaches 
+    that have been used, we consider a toy model of a typical neutrino oscillation experiment. 
+    The toy model is deﬁned by the following parameters: Mesons are assumed to decay to 
+    neutrinos uniformly in a region 600 m to 1000 m from the detector. The expected background 
+    from conventional νe interactions and misidentiﬁed νµ interactions is assumed to be 100 
+    events in each of 5 energy bins which span the region from 10 to 60 GeV. We assume that 
+    the νµ ﬂux is such that if P (νµ → ν e ) = 0.01 averaged over any bin, then that bin would 
+    have an expected additional contribution of 100 events due to νµ → ν e oscillations. 
    */
 
   // Make signal model model
@@ -77,18 +89,22 @@ oscillations.
   RooRealVar deltaMSq("deltaMSq","#Delta m^{2}",40,1,300,"eV/c^{2}");
   RooRealVar sinSq2theta("sinSq2theta","sin^{2}(2#theta)", .006,.0,.02);
   // PDF for oscillation only describes deltaMSq dependence, sinSq2theta goes into sigNorm
+  // 1) The code for this PDF was created by issuing these commands
+  //    root [0] RooClassFactory x
+  //    root [1] x.makePdf("NuMuToNuE_Oscillation","L,E,deltaMSq","","pow(sin(1.27*deltaMSq*L/E),2)")
   NuMuToNuE_Oscillation PnmuTone("PnmuTone","P(#nu_{#mu} #rightarrow #nu_{e}",L,E,deltaMSq);
 
   // only E is observable, so create the signal model by integrating out L 
   RooAbsPdf* sigModel = PnmuTone.createProjection(L);
 
-  //\int dE' dL' P(E',L' | \Delta m^2). 
+  // create   \int dE' dL' P(E',L' | \Delta m^2). 
   // Given RooFit will renormalize the PDF in the range of the observables, 
-  // the total probability to oscillate in the experiment's acceptance
+  // the average probability to oscillate in the experiment's acceptance
   // needs to be incorporated into the extended term in the likelihood.
-  // Do this by creating a RooAbsReal representing the integral.
+  // Do this by creating a RooAbsReal representing the integral and divide by 
+  // the area in the E-L plane.
   // The integral should be over "primed" observables, so we need
-  // an independent copy of PnmuTone
+  // an independent copy of PnmuTone not to interfere with the original.
 
   // Independent copy for Integral
   RooRealVar EPrime("EPrime","", 15,10,60,"GeV");
@@ -103,8 +119,7 @@ oscillations.
   // let maxEventsInBin = flux * cross-section for nu_e interaction in E bin 
   // maxEventsInBin * 1% chance per bin =  100 events / bin
   // therefore maxEventsInBin = 10,000.
-  // for 5 bins, this means maxEventsTot = 50,000
-   
+  // for 5 bins, this means maxEventsTot = 50,000   
   RooConstVar maxEventsTot("maxEventsTot","maximum number of sinal events",50000);
   RooConstVar inverseArea("inverseArea","1/(#Delta E #Delta L)",
 			   1./(EPrime.getMax()-EPrime.getMin())/(LPrime.getMax()-LPrime.getMin()));
@@ -114,7 +129,7 @@ oscillations.
   // bkg = 5 bins * 100 events / bin
   RooConstVar bkgNorm("bkgNorm","normalization for background",500);
 
-  // flat background
+  // flat background (0th order polynomial, so no arguments for coefficients)
   RooPolynomial bkgEShape("bkgEShape","flat bkg shape", E);
 
   // total model
@@ -122,8 +137,8 @@ oscillations.
 		  RooArgList(sigNorm,bkgNorm));
 
   // for debugging, check model tree
-  model.printCompactTree();
-  model.graphVizTree("model.dot");
+  //  model.printCompactTree();
+  //  model.graphVizTree("model.dot");
 
 
   // turn off some messages
@@ -147,12 +162,15 @@ oscillations.
   // make some plots
   TCanvas* dataCanvas = new TCanvas("dataCanvas");
   dataCanvas->Divide(2,2);
+
+  // plot the PDF
   dataCanvas->cd(1);
-  TH1* hh = PnmuTone.createHistogram("hh",E,Binning(40),YVar(L,Binning(40))) ;
+  TH1* hh = PnmuTone.createHistogram("hh",E,Binning(40),YVar(L,Binning(40)),Scaling(kFALSE)) ;
   hh->SetLineColor(kBlue) ;
   hh->SetTitle("True Signal Model");
   hh->Draw("surf");
 
+  // plot the data with the best fit
   dataCanvas->cd(2);
   RooPlot* Eframe = E.frame();
   data->plotOn(Eframe);
@@ -164,12 +182,12 @@ oscillations.
   Eframe->SetTitle("toy data with best fit model (and sig+bkg components)");
   Eframe->Draw();
 
+  // plot the likelihood function
   dataCanvas->cd(3);
-  //  TH1* hhh = PnmuTone.createHistogram("hhh",E,Binning(40),YVar(L,Binning(40))) ;
   RooNLLVar nll("nll", "nll", model, *data, Extended());
   RooProfileLL pll("pll", "", nll, RooArgSet(deltaMSq, sinSq2theta));
   //  TH1* hhh = nll.createHistogram("hhh",sinSq2theta,Binning(40),YVar(deltaMSq,Binning(40))) ;
-  TH1* hhh = pll.createHistogram("hhh",sinSq2theta,Binning(40),YVar(deltaMSq,Binning(40))) ;
+  TH1* hhh = pll.createHistogram("hhh",sinSq2theta,Binning(40),YVar(deltaMSq,Binning(40)),Scaling(kFALSE)) ;
   hhh->SetLineColor(kBlue) ;
   hhh->SetTitle("Likelihood Function");
   hhh->Draw("surf");
@@ -178,7 +196,7 @@ oscillations.
 
 
 
-  ///////////////////////////////////////
+  //////////////////////////////////////////////////////////
   //////// show use of Feldman-Cousins utility in RooStats
   RooStats::FeldmanCousins fc;
   // set the distribution creator, which encodes the test statistic
@@ -188,14 +206,14 @@ oscillations.
   fc.SetTestSize(.1); // set size of test
   fc.SetData(*data);
   fc.UseAdaptiveSampling(true);
-  fc.SetNBins(25); 
+  fc.SetNBins(25); // number of points to test per parameter
 
   // use the Feldman-Cousins tool
   ConfInterval* interval = fc.GetInterval();
   //ConfInterval* interval = 0;
 
 
-  /////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////
   ///////// show use of ProfileLikeihoodCalculator utility in RooStats
   RooStats::ProfileLikelihoodCalculator plc;
   plc.SetPdf(model);
@@ -210,15 +228,11 @@ oscillations.
   // make plot of resulting interval
   dataCanvas->cd(4);
   
-  if(interval){
-    std::cout << "is this point in the interval? " << 
-      interval->IsInInterval(parameters) << std::endl;
-  }
-
+  // first plot a small dot for every point tested
   RooDataHist* parameterScan = (RooDataHist*) fc.GetPointsToScan();
   parameterScan->Draw("deltaMSq:sinSq2theta");
 
- 
+  // now loop through the points and put a marker if it's in the interval
   RooArgSet* tmpPoint;
   // loop over points to test
   for(Int_t i=0; i<parameterScan->numEntries(); ++i){
@@ -231,8 +245,6 @@ oscillations.
 	mark->SetMarkerColor(kBlue);
 	mark->Draw("s");
       }
-      //      else
-      //	mark->SetMarkerColor(kRed);
     }
     
 
@@ -245,12 +257,8 @@ oscillations.
 	plcMark->SetMarkerColor(kGreen);
 	plcMark->Draw("s");
       }
-      //      else
-      //plcMark->SetMarkerColor(kMagenta);
-
     }
-    //delete tmpPoint;
-    //    delete mark;
+    delete tmpPoint;
   }
 
 
@@ -260,3 +268,4 @@ oscillations.
     
 
 }
+
