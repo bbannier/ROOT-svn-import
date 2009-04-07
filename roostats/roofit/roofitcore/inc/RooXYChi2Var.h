0f9387bcdbe3095aa6487c1ef6d93127a2949a4f
wouter <wouter@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-04-07 14:51:37 +0000

    o  RooXYChi2Var
            - New implementation of RooAbsOptTestStatistic that allows evaluation of 'free-form' chi^2 terms
              for datasets (xi +/- exi, y +/- ey) represent by a RooDataSet of xi with the event weights
              being interpreted as y. xi may be a vector of observables or a single observable.
              Symmetric or asymmetric errors can be specified on both x and y components
              
              The input function can be either a RooAbsReal, or an extended RooAbsPdf that is evaluated as the
              value of the p.d.f. times the number of expected events. Optionally the function is integrated
              over the region specified by the 'exi' errors, rather than evaluated at the specified point.
    
    
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@28141 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roofitcore/inc/RooXYChi2Var.h
+++ roostats/roofit/roofitcore/inc/RooXYChi2Var.h
@@ -21,14 +21,15 @@
 #include "RooCmdArg.h"
 #include "RooDataSet.h"
 #include "RooAbsPdf.h"
+#include "RooNumIntConfig.h"
 
 class RooXYChi2Var : public RooAbsOptTestStatistic {
 public:
 
   // Constructors, assignment etc
-  RooXYChi2Var() {} ;
-  RooXYChi2Var(const char *name, const char* title, RooAbsReal& func, RooDataSet& data) ;
-  RooXYChi2Var(const char *name, const char* title, RooAbsPdf& extPdf, RooDataSet& data) ;
+  RooXYChi2Var() ;
+  RooXYChi2Var(const char *name, const char* title, RooAbsReal& func, RooDataSet& data, Bool_t integrate=kFALSE) ;
+  RooXYChi2Var(const char *name, const char* title, RooAbsPdf& extPdf, RooDataSet& data, Bool_t integrate=kFALSE) ;
   
   RooXYChi2Var(const RooXYChi2Var& other, const char* name=0);
   virtual TObject* clone(const char* newname) const { return new RooXYChi2Var(*this,newname); }
@@ -46,11 +47,34 @@ public:
     return 1.0 ; 
   }
 
+  RooNumIntConfig& binIntegratorConfig() { return _intConfig ; }
+  const RooNumIntConfig& binIntegratorConfig() const { return _intConfig ; }
+
 protected:
 
-  Bool_t _extended ;
+  Bool_t allowFunctionCache() { 
+    // Disable function (component) caching if integration is requested as the function
+    // will be evaluated at coordinates other than the points in the dataset
+    return !_integrate ; 
+  }
+
+  Double_t fy() const ; 
+
+  Bool_t _extended ; // Is the input function and extended p.d.f.
+  Bool_t _integrate ; // Is integration over the bin volume requested
+ 
+  RooArgSet _rrvArgs ; // Set of real-valued observables
+  TIterator* _rrvIter ; //! Iterator over set of real-valued observables
+
+  void initialize() ;
+  void initIntegrator() ;
+  Double_t xErrorContribution(Double_t ydata) const ;
 
   virtual Double_t evaluatePartition(Int_t firstEvent, Int_t lastEvent, Int_t stepSize) const ;
+
+  RooNumIntConfig   _intConfig ; // Numeric integrator configuration for integration of function over bin
+  RooAbsFunc*       _funcBinding ; //! Integrator binding of fit function
+  RooAbsIntegrator* _funcIntegrator ; //! Numeric integrator engine over fit function
   
   ClassDef(RooXYChi2Var,1) // Chi^2 function of p.d.f w.r.t a unbinned dataset with X and Y values
 };
