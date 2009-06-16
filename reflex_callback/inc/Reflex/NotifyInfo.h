ac8e360badbb51796f695de9db15ac0192cf8cca
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-06-16 10:26:53 +0000

    Add new file forgotten in previous commit.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/axel@29023 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ reflex_callback/inc/Reflex/NotifyInfo.h
@@ -0,0 +1,110 @@
+// @(#)root/reflex:$Id$
+// Author: Axel Naumann, 2009
+
+// Copyright CERN, CH-1211 Geneva 23, 2004-2006, All rights reserved.
+//
+// Permission to use, copy, modify, and distribute this software for any
+// purpose is hereby granted without fee, provided that this copyright and
+// permissions notice appear in all copies and derivatives.
+//
+// This software is provided "as is" without express or implied warranty.
+
+#ifndef Reflex_NotifyInfo
+#define Reflex_NotifyInfo
+
+namespace Reflex {
+
+   /** What to call the callback for
+    */
+   enum ENotifyWhat {
+      kNotifyType = 1,
+      kNotifyScope = 2,
+      kNotifyMember = 4,
+      kNotifyEverything = 7
+   };
+
+   /**
+    * Enum for bit mask defining what a callback should be called for.
+    * The states of types / scopes / members are
+    *   1) unknown
+    *   2) only name known (e.g. TypeName, but no TypeBase)
+    *   3) forward declaration exists (TypeBase without type's data, e.g. "class" but no scope yet)
+    *   4) type fully declared (sizeof etc)
+    *   5) fully declared (incl. scope)
+    */
+   enum ENotifyTransition {
+      kNotifyNameCreated = 0x01, // transition 1 -> 2
+      kNotifyNameResolved = 0x02, // transition 2 -> 3
+      kNotifyTypeDeclared = 0x04, // transition 3 -> 4
+      kNotifyFullyDeclared = 0x08, // transition 4 -> 5
+      kNotifyAllLoad = 0x0f, // any transition in -> direction
+
+      kNotifyPartiallyUndeclared = 0x80, // transition 5 -> 4
+      kNotifyTypeUndeclared = 0x40, // transition 4 -> 3
+      kNotifyUnresolved = 0x20, // transition 3 -> 2
+      kNotifyUnloaded = 0x10, // transition 2 -> 1
+      kNotifyAllUnload = 0xf0, // any transition in <- direction
+
+      kNotifyAllTransitions = kNotifyAllLoad | kNotifyAllUnload
+   };
+
+   /**
+    * When to call the callback: before or after the transition has occurred.
+    */
+   enum ENotifyWhen {
+      kNotifyBefore = 1, // called before the transition happens
+      kNotifyAfter = 2, // called after the transition happens
+      kNotifyBoth = kNotifyBefore | kNotifyAfter,
+
+      kNotifyDisabled = 0x10, // never called; bit combined with kNotifyBefore and / or kNotifyAfter
+   };
+
+   /**
+    * Callback messages to the callback handler; return values of the callback function
+    */
+   enum ECallbackReturn {
+      kCallbackReturnNothing = 0, // default
+      kCallbackReturnHandled = 1, // stop processing other callbacks
+      kCallbackReturnVeto    = 2, // for kNotifyBefore: veto the action that should follow (e.g. unloading)
+   };
+
+   class NotifySelection {
+   public:
+      NotifySelection():
+         fWhat(kNotifyEverything),
+         fWhen(kNotifyAfter),
+         fTransition(kNotifyFullyDeclared | kNotifyUnloaded)
+      {}
+
+      char What() const { return fWhat; }
+      char When() const { return fWhen; }
+      char Transition() const { return fTransition; }
+
+   protected:
+      char fWhat; // (combination of) ENotifyWhat
+      char fWhen; // (combination of) ENotifyWhen
+      char fTransition; // (combination of) ENotifyTransition
+   };
+
+   struct NotifyInfo {
+      NotifyInfo(const std::string& name, ENotifyWhat what, ENotifyWhen when, ENotifyTransition trans):
+         fName(name), fWhat(what), fWhen(when), fTransition(trans) {}
+
+      const std::string& fName;
+      ENotifyWhat fWhat;
+      ENotifyWhen fWhen;
+      ENotifyTransition fTransition;
+   };
+
+   template <class ELEM> // Type, Scope, or Member
+   struct NotifyInfoT: NotifyInfo {
+      NotifyInfoT(ELEM elem, ENotifyWhat what, ENotifyWhen when, ENotifyTransition trans):
+         NotifyInfo(elem.Name(), what, when, trans),
+         fElem(elem) {}
+
+      ELEM fElem;
+   };
+
+} // namespace Reflex
+
+#endif // Reflex_NotifyInfo
