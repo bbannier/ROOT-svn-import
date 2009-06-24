f5b232f54adcf31ae60677e38d25f65698a916b2
cranmer <cranmer@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-06-24 02:39:35 +0000

    include protections for CINT
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@29183 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roostats/inc/ToyMCSampler.h
+++ roostats/roofit/roostats/inc/ToyMCSampler.h
@@ -46,7 +46,9 @@ END_HTML
 #include "RooStats/TestStatistic.h"
 #include "RooStats/RooStatsUtils.h"
 
+#ifndef __CINT__
 #include "RooGlobalFunc.h"
+#endif
 #include "RooWorkspace.h"
 #include "RooMsgService.h"
 #include "RooAbsPdf.h"
