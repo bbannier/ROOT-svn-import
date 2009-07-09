cb8b945fb301166a06966d621f2d694cc85d7174
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-07-09 18:02:57 +0000

    Apply CMS patch root-5.22-00a-Reflex-gendict_selclass.patch
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@29430 27541ba8-7e3a-0410-8455-c3a389f83636

--- fireworks/cint/reflex/python/genreflex/gendict.py
+++ fireworks/cint/reflex/python/genreflex/gendict.py
@@ -373,6 +373,20 @@ class genDictionary(object) :
                 print '--->> genreflex: INFO: Using typedef %s to select class %s' % (t['fullname'], self.genTypeName(catt['id']))
               selec.append(catt)
               self.typedefs_for_usr.append(t)
+              if match[0].has_key('fields') :
+                # copy all fields selection attrs over to the underlying class, see sav #49472
+                # this part is needed for selfields which checks the selection, not the class's attrs
+                clsname = self.genTypeName(catt['id'])
+                newselattrs = {'name': clsname, 'n_name': self.selector.genNName(clsname)}
+                self.selector.sel_classes.append({'fields': match[0]['fields'], 'attrs': newselattrs, 'used': 1, 'methods':[]})
+            elif match[0].has_key('fields') :
+              # copy all fields selection attrs over to the underlying class, see sav #49472
+              clname = self.genTypeName(catt['id'])
+              for c in self.selector.sel_classes :
+                attrs = c['attrs']
+                if 'n_name' in attrs and attrs['n_name'] == clname \
+                      or 'n_pattern' in attrs and self.selector.matchpattern(clname, attrs['n_pattern']) :
+                  c['fields'] += match[0]['fields']
       if self.resolvettd :
         newselector = self.resolveSelectorTypedefs( self.selector.sel_classes )
         if newselector:
@@ -1114,7 +1128,7 @@ class genDictionary(object) :
             % (cls, typeidtype, cls, mod, typ)
     if 'extra' in attrs :
       for pname, pval in attrs['extra'].items() :
-        if pname not in ('name','pattern','n_name','file_name','file_pattern') :
+        if pname not in ('name','pattern','n_name','file_name','file_pattern','fields') :
           if pname == 'id' : pname = 'ClassID'
           sc += '\n  .AddProperty("%s", "%s")' % (pname, pval)
 
