fd61e82518d22a6bce731f53d3e5f6e3390925a6
matevz <matevz@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-07-09 18:02:25 +0000

    Apply CMS patch root-5.22-00a-smatrix.patch
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/fireworks@29429 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/smatrix/inc/Math/MatrixRepresentationsStatic.h
+++ math/smatrix/inc/Math/MatrixRepresentationsStatic.h
@@ -122,24 +122,33 @@ namespace Math {
       symmetric matrix
    */
 
+   struct RowOffsetsBase 
+   {
+     protected:
+       static void init(int *v, int *offsets, unsigned int D);
+   };
+
    template<unsigned int D>
-   struct RowOffsets {
+   struct RowOffsets : RowOffsetsBase {
       RowOffsets() {
          int v[D];
-         v[0]=0;
-         for (unsigned int i=1; i<D; ++i)
-            v[i]=v[i-1]+i;
-         for (unsigned int i=0; i<D; ++i) { 
-            for (unsigned int j=0; j<=i; ++j)
-               fOff[i*D+j] = v[i]+j; 
-            for (unsigned int j=i+1; j<D; ++j)
-               fOff[i*D+j] = v[j]+i ;
-         }
+         this->init(v, fOff, D);
       }
       int operator()(unsigned int i, unsigned int j) const { return fOff[i*D+j]; }
       int apply(unsigned int i) const { return fOff[i]; }
       int fOff[D*D];
    };
+
+   template <unsigned int D> struct SymMatrixOffsets
+   {
+    protected:
+      static RowOffsets<D> offsets;
+   };
+
+   template <unsigned int D>
+   RowOffsets<D>
+   SymMatrixOffsets<D>::offsets;
+
 
 //_________________________________________________________________________________
    /**
@@ -161,11 +170,11 @@ namespace Math {
       @ingroup MatRep 
    */
    template <class T, unsigned int D>
-   class MatRepSym {
+   class MatRepSym : SymMatrixOffsets<D> {
 
    public: 
 
-      MatRepSym() :fOff(0) { CreateOffsets(); } 
+      MatRepSym() :fOff(&SymMatrixOffsets<D>::offsets) { } 
 
       typedef T  value_type;
 
