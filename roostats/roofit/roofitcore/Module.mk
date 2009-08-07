4c54554d2e5ba4a8c920eb48f6fc49f7fce81dc8
schott <schott@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-08-07 17:13:38 +0000

    for compilation for the new toy-MC modules
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@29727 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roofitcore/Module.mk
+++ roostats/roofit/roofitcore/Module.mk
@@ -43,8 +43,8 @@ ROOFITCOREH1   := Roo1DTable.h RooAbsArg.h RooAbsBinning.h RooAbsCategory.h \
                   RooCmdConfig.h RooComplex.h RooConstVar.h RooConvCoefVar.h \
                   RooConvGenContext.h RooConvIntegrandBinding.h RooCurve.h \
                   RooCustomizer.h RooDataHist.h RooDataProjBinding.h RooDataSet.h \
-                  RooDirItem.h RooDLLSignificanceMCSModule.h RooAbsAnaConvPdf.h \
-                  RooAddPdf.h RooEfficiency.h RooEffProd.h RooExtendPdf.h
+                  RooDirItem.h RooUpperLimitMCSModule.h RooDLLSignificanceMCSModule.h RooAbsAnaConvPdf.h \
+                  RooAddPdf.h RooEfficiency.h RooEffProd.h RooExtendPdf.h RooNuisanceGenerationMCSModule.h
 
 ROOFITCOREH2   := RooDouble.h RooEffGenContext.h RooEllipse.h RooErrorHandler.h \
                   RooErrorVar.h RooFit.h RooFitResult.h RooFormula.h \
