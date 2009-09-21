309f2a83732442f0f0c4d07a9359818843b3159f
dmgonzal <dmgonzal@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-09-21 14:41:50 +0000

    Added the documentation to the SparseData class
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/mathDev@30332 27541ba8-7e3a-0410-8455-c3a389f83636

--- math/mathcore/inc/Fit/SparseData.h
+++ math/mathcore/inc/Fit/SparseData.h
@@ -28,24 +28,37 @@ namespace ROOT {
 
    namespace Fit { 
 
+      // This is a proxy to a list<Box>
       class ProxyListBox;
 
       class SparseData : public FitData  { 
       public:
+         //Constructor with a vector
          SparseData(vector<double>& min, vector<double>& max);
+         
+         //Constructor with a dimension and two arrays
          SparseData(const unsigned int dim, double min[], double max[]);
+
+         //Destructor
          ~SparseData();
 
+         //Returns the number of points stored
          unsigned int NPoints() const;
+         //Returns the dimension of the object (bins)
          unsigned int NDim() const;
 
+         // Adds a new bin specified by the vectors
          void Add(std::vector<double>& min, std::vector<double>& max, 
                   const double content, const double error = 1.0);
          
+         // Debug method to print the list of bins stored
          void PrintList() const;
 
+         // Transforms the data into a ROOT::Fit::BinData structure
          void GetBinData(BinData&) const;
+         // Same as before, but with integral format
          void GetBinDataIntegral(BinData&) const;
+         // Same as before, but including zero content bins
          void GetBinDataNoZeros(BinData&) const;
          
       private : 
