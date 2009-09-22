0236e38aa1bf22e4c13faca4c2d14417b62648ec
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-09-22 10:51:02 +0000

    Remove Inuit; replaced by EditLine.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@30352 27541ba8-7e3a-0410-8455-c3a389f83636

--- lib/Inuit/include/Inuit/Enums/EWindow.h
+++
@@ -1,7 +0,0 @@
-ADD_ENUMCONST(kWindowResize,),
-ADD_ENUMCONST(kWindowMove,),
-ADD_ENUMCONST(kWindowHide,),
-ADD_ENUMCONST(kWindowShow,),
-ADD_ENUMCONST(kWindowMinimize,),
-ADD_ENUMCONST(kWindowMaximize,),
-ADD_ENUMCONST(kNumWindow,)
--- lib/Inuit/test/clear/clear.cxx
+++
@@ -1,9 +0,0 @@
-#include <string>
-#include <iostream>
-#include "inuit/drivers/TerminalDriver.hxx"
-
-int main(int, char*[]) {
-   using namespace Inuit;
-   TerminalDriver::Instance().Clear();
-   return 0;
-}
