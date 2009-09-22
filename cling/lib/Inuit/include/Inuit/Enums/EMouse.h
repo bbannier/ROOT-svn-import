6ca5158e8c829ded4cc5ec2711f04690fcf49015
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-09-22 10:51:02 +0000

    Remove Inuit; replaced by EditLine.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@30352 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/lib/Inuit/include/Inuit/Enums/EMouse.h
+++
@@ -1,4 +0,0 @@
-ADD_ENUMCONST(kMouseButtonLeft,),
-ADD_ENUMCONST(kMouseButtonRight,),
-ADD_ENUMCONST(kMouseButtonMiddle,),
-ADD_ENUMCONST(kMouseMove,)
--- cling/lib/Inuit/include/Inuit/Enums/EType.h
+++
@@ -1,5 +0,0 @@
-ADD_ENUMCONST(kTypePrintable,), // printable
-ADD_ENUMCONST(kTypeNonPrintable,), // nonprintable
-ADD_ENUMCONST(kTypeMouse,), // mouse input
-ADD_ENUMCONST(kTypeWindow,), // window input
-ADD_ENUMCONST(kNumTypes,)
