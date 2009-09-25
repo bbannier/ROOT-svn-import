cc1b7814e647ae36e9e803785edb8dcd2652e331
moneta <moneta@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-09-25 14:19:46 +0000

    - some quick cleanup of the class
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@30453 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roostats/inc/UpperLimitMCSModule.h
+++ roostats/roofit/roostats/inc/UpperLimitMCSModule.h
@@ -1,59 +1,59 @@
-/*****************************************************************************
- * Project: RooFit                                                           *
- * Package: RooFitCore                                                       *
- *    File: $Id: 
- * Authors:                                                                  *
- *   WV, Wouter Verkerke, UC Santa Barbara, verkerke@slac.stanford.edu       *
- *   DK, David Kirkby,    UC Irvine,         dkirkby@uci.edu                 *
- *                                                                           *
- * Copyright (c) 2000-2005, Regents of the University of California          *
- *                          and Stanford University. All rights reserved.    *
- *                                                                           *
- * Redistribution and use in source and binary forms,                        *
- * with or without modification, are permitted according to the terms        *
- * listed in LICENSE (http://roofit.sourceforge.net/license.txt)             *
- *****************************************************************************/
+// @(#)root/roostats:$Id: CombinedCalculator.h 30450 2009-09-25 13:30:32Z moneta $
+// Author: Kyle Cranmer, Lorenzo Moneta, Gregory Schott, Wouter Verkerke, Nils Ruthmann
+/*************************************************************************
+ * Copyright (C) 1995-2008, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
 
 #ifndef UPPER_LIMIT_MCS_MODULE
 #define UPPER_LIMIT_MCS_MODULE
 
 #include "RooAbsMCStudyModule.h"
-#include "RooStats/ProfileLikelihoodCalculator.h"
-#include "RooRealVar.h"
-#include "RooFit.h"
 #include <string>
 
+class RooArgSet; 
+class RooDataSet; 
+class RooRealVar; 
+class RooAbsPdf; 
+
 namespace RooStats {
+
+   class ProfileLikelihoodCalculator; 
+
 
 class UpperLimitMCSModule : public RooAbsMCStudyModule {
 public:
 
 
-  UpperLimitMCSModule(RooArgSet* poi, Double_t CL=0.95) ;
-  UpperLimitMCSModule(const UpperLimitMCSModule& other) ;
-  virtual ~UpperLimitMCSModule() ;
+   UpperLimitMCSModule(const RooArgSet* poi, Double_t CL=0.95) ;
+   UpperLimitMCSModule(const UpperLimitMCSModule& other) ;
+   virtual ~UpperLimitMCSModule() ;
 
-  Bool_t initializeInstance() ; 
+   Bool_t initializeInstance() ; 
 
-  Bool_t initializeRun(Int_t /*numSamples*/) ; 
-  RooDataSet* finalizeRun() ;
+   Bool_t initializeRun(Int_t /*numSamples*/) ; 
+   RooDataSet* finalizeRun() ;
 
-  //Bool_t processAfterFit(Int_t /*sampleNum*/)  ;
-  Bool_t processBetweenGenAndFit(Int_t /*sampleNum*/) ;
+   //Bool_t processAfterFit(Int_t /*sampleNum*/)  ;
+   Bool_t processBetweenGenAndFit(Int_t /*sampleNum*/) ;
 	
 private:
 
-  std::string _parName ;  // Name of Nsignal parameter
-  RooStats::ProfileLikelihoodCalculator* _plc;
-  RooRealVar* _ul ;
+   std::string _parName ;  // Name of Nsignal parameter
+   RooStats::ProfileLikelihoodCalculator* _plc;
+   RooRealVar* _ul ;
 
-  RooArgSet* _poi;
-  RooDataSet* _data ;     // Summary dataset to store results
-  Double_t _cl;
-  RooAbsPdf* _model;
+   const RooArgSet* _poi;  // parameters of interest
+   RooDataSet* _data ;     // Summary dataset to store results
+   Double_t _cl;
+   RooAbsPdf* _model;
 
-  ClassDef(UpperLimitMCSModule,0) // MCStudy module to calculate upperlimit of a given poi
-} ;
+   ClassDef(UpperLimitMCSModule,0) // MCStudy module to calculate upperlimit of a given poi
+};
 
 }
 
