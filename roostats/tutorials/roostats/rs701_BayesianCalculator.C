c583961d6d17861933a35fcf142ca572be114b86
schott <schott@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-09-29 17:18:45 +0000

    finalised and improved plot for this tutorial
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@30533 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/tutorials/roostats/rs701_BayesianCalculator.C
+++ roostats/tutorials/roostats/rs701_BayesianCalculator.C
@@ -25,14 +25,14 @@ void rs701_BayesianCalculator()
 {
 
   RooWorkspace* w = new RooWorkspace("w",true);
-  w->factory("SUM::pdf(s[0.001,15]*Uniform(x[0,1]),b[0.001,0,2]*Uniform(x))");
+  w->factory("SUM::pdf(s[0.001,10]*Uniform(x[0,1]),b[0.001,0,2]*Uniform(x))");
   w->factory("Gaussian::prior_b(b,0.001,1)");
   w->factory("PROD::model(pdf,prior_b)");
   RooAbsPdf* model = w->pdf("model");  // pdf*priorNuisance
   const RooArgSet nuisanceParameters(*(w->var("b")));
 
   w->factory("Uniform::priorPOI(s)");
-  RooAbsRealLValue* POI = w->var("s");
+  RooRealVar* POI = w->var("s");
   RooAbsPdf* priorPOI = w->pdf("priorPOI");
 
   w->factory("n[1]"); // observed number of events
@@ -44,8 +44,24 @@ void rs701_BayesianCalculator()
 
   SimpleInterval* interval = bcalc.GetInterval();
   std::cout << "90% CL interval: [ " << interval->LowerLimit() << " - " << interval->UpperLimit() << " ] or 95% CL limits\n";
-  bcalc.GetPosteriorPlot()->Draw();
-  
+
   // observe one event while expecting one background event -> the 95% CL upper limit on s is 4.10
   // observe one event while expecting zero background event -> the 95% CL upper limit on s is 4.74
+
+  // The plotting code below can be replace by the single following line in the ROOT > 5.25.00 future releases
+  //bcalc.GetPosteriorPlot()->Draw();
+
+  // bcalc.PlotPosterior(); // for the moment this is the current plot
+
+  // the commented code below produce a much nicer plot
+
+  RooAbsPdf* fPosteriorPdf = bcalc.GetPosteriorPdf();
+
+  RooPlot* plot = POI->frame();
+  plot->SetTitle(TString("Posterior probability of parameter \"")+TString(POI->GetName())+TString("\""));  
+  fPosteriorPdf->plotOn(plot,RooFit::Range(interval->LowerLimit(),interval->UpperLimit(),kFALSE),RooFit::VLines(),RooFit::DrawOption("F"),RooFit::MoveToBack(),RooFit::FillColor(kGray));
+  fPosteriorPdf->plotOn(plot);
+  plot->GetYaxis()->SetTitle("posterior probability");
+  plot->Draw();
+  
 }
