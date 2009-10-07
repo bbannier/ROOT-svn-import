f110d1f9f9eb3aa6ce0ea4c5d5c4f0bafa1de002
pcanal <pcanal@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-10-07 14:03:22 +0000

    Implement continuation lines
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@30603 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ lib/Interpreter/Diagnostics.h
@@ -0,0 +1,80 @@
+#ifndef CLING_DIAGNOSTICS_H
+#define CLING_DIAGNOSTICS_H
+
+/*************************************************************************
+ * Copyright (C) 2009-2010, Cling team.                                  *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see LICENSE.                                  *
+ * For the list of contributors see CREDITS.                             *
+ *************************************************************************/
+
+// version: $Id$
+// author:  Alexei Svitkine
+
+#include <set>
+#include <map>
+
+#include <llvm/Support/raw_os_ostream.h>
+
+#include <clang/Basic/LangOptions.h>
+#include <clang/Basic/Diagnostic.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
+
+namespace cling {
+   
+   //
+   // DiagnosticsProvider provides a re-usable clang::Diagnostic object
+   // that can be used for multiple parse operations.
+   //
+   
+   class DiagnosticsProvider : public clang::DiagnosticClient {
+      
+   public:
+      
+      DiagnosticsProvider(llvm::raw_os_ostream& out, const clang::LangOptions& opts);
+      
+      void HandleDiagnostic(clang::Diagnostic::Level DiagLevel,
+                            const clang::DiagnosticInfo &Info);
+      
+      void setOffset(unsigned offset);
+      
+      clang::Diagnostic * getDiagnostic();
+      
+   private:
+      
+      unsigned _offs;
+      clang::TextDiagnosticPrinter _tdp;
+      clang::Diagnostic _diag;
+      std::set<std::pair<clang::diag::kind, unsigned> > _memory;
+      
+   };
+   
+   
+   //
+   // ProxyDiagnosticClient can act as a proxy to another diagnostic client.
+   //
+   
+   class ProxyDiagnosticClient : public clang::DiagnosticClient {
+      
+   public:
+      
+      ProxyDiagnosticClient(clang::DiagnosticClient *DC);
+      
+      void HandleDiagnostic(clang::Diagnostic::Level DiagLevel,
+                            const clang::DiagnosticInfo &Info);
+      
+      bool hadError(clang::diag::kind Kind) const;
+      bool hadErrors() const;
+      
+   private:
+      
+      clang::DiagnosticClient *_DC;
+      std::multimap<clang::diag::kind, const clang::DiagnosticInfo> _errors;
+      
+   };
+   
+   
+} // namespace cling
+
+#endif // CLING_DIAGNOSTICS_H
---
+++ lib/Interpreter/ParseEnvironment.cpp
@@ -0,0 +1,92 @@
+/*************************************************************************
+ * Copyright (C) 2009-2010, Cling team.                                  *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see LICENSE.                                  *
+ * For the list of contributors see CREDITS.                             *
+ *************************************************************************/
+
+// version: $Id: ParseOperation.h 30397 2009-09-24 13:08:16Z axel $
+// author:  Alexei Svitkine
+
+#include "ParseEnvironment.h"
+
+#include <clang/Basic/Version.h>
+#include <llvm/System/Path.h>
+
+#include <clang/Basic/LangOptions.h>
+#include <clang/Basic/FileManager.h>
+#include <clang/Lex/HeaderSearch.h>
+#include <clang/Basic/TargetInfo.h>
+#include <clang/Lex/LexDiagnostic.h>
+#include <clang/Lex/Preprocessor.h>
+#include "clang/Frontend/InitPreprocessor.h"
+#include <clang/Frontend/InitHeaderSearch.h>
+#include <clang/AST/ASTContext.h>
+
+namespace cling {
+   
+   //
+   // ParseEnvironment
+   //
+   
+   ParseEnvironment::ParseEnvironment(const clang::LangOptions& options,
+                                      clang::TargetInfo& target,
+                                      clang::Diagnostic *diag,
+                                      clang::FileManager *fileMgr,
+                                      clang::PPCallbacks *callbacks) :
+	m_srcMgr(new clang::SourceManager),
+	m_fileMgr(fileMgr ? 0 : new clang::FileManager), // create a file manager only if we have to
+	m_headerInfo(new clang::HeaderSearch(fileMgr ? *fileMgr : *m_fileMgr))
+   {
+      //------------------------------------------------------------------------
+      // Fill the header database
+      //------------------------------------------------------------------------
+      
+      clang::InitHeaderSearch hiInit(*m_headerInfo);
+      hiInit.AddDefaultEnvVarPaths(options);
+      hiInit.AddDefaultSystemIncludePaths(options);
+      llvm::sys::Path clangIncl(LLVM_LIBDIR, strlen(LLVM_LIBDIR));
+      clangIncl.appendComponent("clang");
+      clangIncl.appendComponent(CLANG_VERSION_STRING);
+      clangIncl.appendComponent("include");
+      hiInit.AddPath( clangIncl.c_str(), clang::InitHeaderSearch::System,
+                     true, false, false, true /*ignore sysroot*/);
+      hiInit.Realize();
+      
+      //-------------------------------------------------------------------------
+      // Create the preprocessor
+      //-------------------------------------------------------------------------      
+      m_preproc.reset(new clang::Preprocessor(*diag, options, target, *m_srcMgr, *m_headerInfo));
+      m_preproc->setPPCallbacks(callbacks);
+      clang::PreprocessorInitOptions ppOptions;
+      InitializePreprocessor(*m_preproc, ppOptions);
+      m_preproc->getBuiltinInfo().InitializeBuiltins(m_preproc->getIdentifierTable(),
+                                                     m_preproc->getLangOptions().NoBuiltin);
+      
+      
+      
+      m_ast.reset(new clang::ASTContext(options,
+                                       *m_srcMgr,
+                                       target,
+                                       m_preproc->getIdentifierTable(),
+                                       m_preproc->getSelectorTable(),
+                                       m_preproc->getBuiltinInfo()));
+   }
+   
+   clang::ASTContext * ParseEnvironment::getASTContext() const
+   {
+      return m_ast.get();
+   }
+   
+   clang::Preprocessor * ParseEnvironment::getPreprocessor() const
+   {
+      return m_preproc.get();
+   }
+   
+   clang::SourceManager * ParseEnvironment::getSourceManager() const
+   {
+      return m_srcMgr.get();
+   }
+
+}
