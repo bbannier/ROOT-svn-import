12c5894e026aa387e9335dc35554d0efa902417d
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-10-16 13:43:13 +0000

    Enable C++
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@30766 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/tools/driver/cling.cpp
+++ cling/tools/driver/cling.cpp
@@ -46,7 +46,7 @@ int main( int argc, char **argv )
    langInfo.HexFloats   = 1;
    langInfo.BCPLComment = 1; // Only for C99/C++.
    langInfo.Digraphs    = 1; // C94, C99, C++.
-   //langInfo.CPlusPlus   = 1;
+   langInfo.CPlusPlus   = 1;
    langInfo.CPlusPlus0x = 1;
    langInfo.CXXOperatorNames = 1;
    langInfo.Bool = 1;
