c41680a13004e88220f7d9771169b77d47f5d517
schott <schott@27541ba8-7e3a-0410-8455-c3a389f83636>
2009-11-18 15:06:15 +0000

    removed linear interpolation for Upper limit estimation
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@31275 27541ba8-7e3a-0410-8455-c3a389f83636

--- roostats/roofit/roostats/src/HypoTestInverterResult.cxx
+++ roostats/roofit/roostats/src/HypoTestInverterResult.cxx
@@ -95,35 +95,51 @@ double HypoTestInverterResult::GetYError( int index ) const
 
 void HypoTestInverterResult::CalculateLimits()
 { 
-  // find the 2 objects the closer to the limit and make a linear extrapolation to the target
-
   double cl = 1-ConfidenceLevel();
 
-  if (Size()<2) {
-    std::cout << "not enough points to get the inverted interval\n";
-  }
+  // find the 2 objects the closer to the limit and make a linear extrapolation to the target
+
+//   if (Size()<2) {
+//     std::cout << "not enough points to get the inverted interval\n";
+//   }
+
+//   double v1 = fabs(GetYValue(0)-cl);
+//   int i1 = 0;
+//   double v2 = fabs(GetYValue(1)-cl);
+//   int i2 = 1;
+
+//   if (Size()>2)
+//     for (int i=2; i<Size(); i++) {
+//       double vt = fabs(GetYValue(i)-cl);
+//       if ( vt<v1 || vt<v2 ) {
+// 	if ( v1<v2 ) {
+// 	  v2 = vt;
+// 	  i2 = i;
+// 	} else {
+// 	  v1 = vt;
+// 	  i1 = i;
+// 	}
+//       }
+//     }
+
+//   fLowerLimit = ((RooRealVar*)fParameters.first())->getMin();
+//   fUpperLimit = GetXValue(i1)+(cl-GetYValue(i1))*(GetXValue(i2)-GetXValue(i1))/(GetYValue(i2)-GetYValue(i1));
+
+  // find the object the closest to the target and take it as the upper limit
 
   double v1 = fabs(GetYValue(0)-cl);
   int i1 = 0;
-  double v2 = fabs(GetYValue(1)-cl);
-  int i2 = 1;
-
-  if (Size()>2)
-    for (int i=2; i<Size(); i++) {
-      double vt = fabs(GetYValue(i)-cl);
-      if ( vt<v1 || vt<v2 ) {
-	if ( v1<v2 ) {
-	  v2 = vt;
-	  i2 = i;
-	} else {
-	  v1 = vt;
-	  i1 = i;
-	}
-      }
+  for (int i=1; i<Size(); i++) {
+    double vt = fabs(GetYValue(i)-cl);
+    if ( vt<v1 ) {
+      v1 = vt;
+      i1 = i;
     }
+  }
 
   fLowerLimit = ((RooRealVar*)fParameters.first())->getMin();
-  fUpperLimit = GetXValue(i1)+(cl-GetYValue(i1))*(GetXValue(i2)-GetXValue(i1))/(GetYValue(i2)-GetYValue(i1)); // MAYBE TOO MANY GETYVALUE CALLS!
+  fUpperLimit = GetXValue(i1);
+
 
   return;
 }
