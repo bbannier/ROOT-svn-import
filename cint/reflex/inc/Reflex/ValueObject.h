204fa3883bcd75ea69e967a4d0d1660a6093edf6
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-01-14 15:23:00 +0000

    Change author name of new files to Philippe Bourgau (Murex). Update the copyright year for files changes this year.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@32016 27541ba8-7e3a-0410-8455-c3a389f83636

--- cint/reflex/inc/Reflex/ValueObject.h
+++ cint/reflex/inc/Reflex/ValueObject.h
@@ -1,7 +1,7 @@
 // @(#)root/reflex:$Id$
 // Author: Pere Mato 2006
 
-// Copyright CERN, CH-1211 Geneva 23, 2004-2005, All rights reserved.
+// Copyright CERN, CH-1211 Geneva 23, 2004-2010, All rights reserved.
 //
 // Permission to use, copy, modify, and distribute this software for any
 // purpose is hereby granted without fee, provided that this copyright and
