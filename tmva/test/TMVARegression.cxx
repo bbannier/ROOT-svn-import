a515151eeaafc00fbdb51087aed8ec6f56c9d3af
speckmay <speckmay@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-02-08 16:17:55 +0000

    bugfixes in MethodMLP in TMVA; fixed bug in regulator computation and estimator computation
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@32285 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/test/TMVARegression.cxx
+++ tmva/test/TMVARegression.cxx
@@ -241,7 +241,7 @@ int main( int argc, char** argv )
 
    // Neural network (MLP)
    if (Use["MLP"])
-      factory->BookMethod( TMVA::Types::kMLP, "MLP", "!H:!V:VarTransform=Norm:NeuronType=tanh:NCycles=20000:HiddenLayers=N+20,N+15:TestRate=6:TrainingMethod=BP:Sampling=0.3:SamplingEpoch=0.8:ConvergenceImprove=1e-6:ConvergenceTests=15" );
+      factory->BookMethod( TMVA::Types::kMLP, "MLP", "!H:!V:VarTransform=Norm:NeuronType=tanh:NCycles=20000:HiddenLayers=N+20:TestRate=6:TrainingMethod=BFGS:Sampling=0.3:SamplingEpoch=0.8:ConvergenceImprove=1e-6:ConvergenceTests=15:!UseRegulator" );
 
    // Support Vector Machine
    if (Use["SVM"])
