a4498474d6a3572e9f1e78d712f5dd619ec3a667
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-03-07 07:30:30 +0000

    Synchronize to trunk 32486
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/proofbench@32487 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/eve/src/TEvePlot3D.cxx
+++ graf3d/eve/src/TEvePlot3D.cxx
@@ -12,11 +12,6 @@
 #include "TEvePlot3D.h"
 #include "TEveTrans.h"
 
-#include "TVirtualPad.h"
-#include "TVirtualViewer3D.h"
-#include "TBuffer3D.h"
-#include "TBuffer3DTypes.h"
-
 
 //______________________________________________________________________________
 // Description of TEvePlot3D
@@ -35,44 +30,10 @@ TEvePlot3D::TEvePlot3D(const char* n, const char* t) :
    InitMainTrans();
 }
 
-
-/******************************************************************************/
-
-/*
-// For now use true sizes of plots.
 //______________________________________________________________________________
-void TEvePlot3D::ComputeBBox()
+void TEvePlot3D::Paint(Option_t* )
 {
-   // Compute bounding-box of the data.
+   // Paint this object. Only direct rendering is supported.
 
-   if (fPlot)
-      BBoxZero(); // should be BBoxZero(0.5); once the plots are stuffed into unit box.
-   else
-      BBoxZero();
-}
-*/
-
-//______________________________________________________________________________
-void TEvePlot3D::Paint(Option_t*)
-{
-   // Paint object.
-   // This is for direct rendering (using TEvePlot3DGL class).
-
-   static const TEveException eh("TEvePlot3D::Paint ");
-
-   if (fRnrSelf == kFALSE) return;
-
-   TBuffer3D buff(TBuffer3DTypes::kGeneric);
-
-   // Section kCore
-   buff.fID           = this;
-   buff.fColor        = GetMainColor();
-   buff.fTransparency = GetMainTransparency();
-   if (HasMainTrans())
-      RefMainTrans().SetBuffer3D(buff);
-   buff.SetSectionsValid(TBuffer3D::kCore);
-
-   Int_t reqSections = gPad->GetViewer3D()->AddObject(buff);
-   if (reqSections != TBuffer3D::kNone)
-      Error(eh, "only direct GL rendering supported.");
+   PaintStandard(this);
 }
