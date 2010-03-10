3a88b19c612eee25b35de6bfc68c00268a7139ef
russo <russo@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-03-10 18:49:08 +0000

    Merge in changes for new command line implementation based on copying global mappings.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@32536 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/lib/Interpreter/Visitors.cpp
+++ cling/lib/Interpreter/Visitors.cpp
@@ -11,89 +11,123 @@
 
 #include "Visitors.h"
 
-#include <clang/Basic/SourceManager.h>
-#include <clang/Lex/Lexer.h>
-#include <clang/Sema/SemaDiagnostic.h>
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/DeclGroup.h"
+#include "clang/AST/Decl.h"
+#include "clang/AST/ExprCXX.h"
+#include "clang/AST/Expr.h"
+#include "clang/AST/Stmt.h"
+#include "clang/AST/StmtVisitor.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/FileManager.h"
+#include "clang/Basic/IdentifierTable.h"
+#include "clang/Basic/LangOptions.h"
+#include "clang/Basic/SourceLocation.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Basic/TargetInfo.h"
+#include "clang/Basic/Version.h"
+#include "clang/CodeGen/ModuleBuilder.h"
+#include "clang/Driver/Arg.h"
+#include "clang/Driver/ArgList.h"
+#include "clang/Driver/CC1Options.h"
+#include "clang/Driver/DriverDiagnostic.h"
+#include "clang/Driver/OptTable.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/CompilerInvocation.h"
+#include "clang/Frontend/FrontendAction.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/FrontendDiagnostic.h"
+#include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/Frontend/HeaderSearchOptions.h"
+#include "clang/Frontend/TextDiagnosticBuffer.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Lex/HeaderSearch.h"
+#include "clang/Lex/MacroInfo.h"
+#include "clang/Lex/PPCallbacks.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/lib/Sema/Sema.h"
+#include "clang/Parse/Parser.h"
+#include "clang/Sema/ParseAST.h"
+#include "llvm/ADT/OwningPtr.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/ExecutionEngine/ExecutionEngine.h"
+#include "llvm/Linker.h"
+#include "llvm/LLVMContext.h"
+#include "llvm/Module.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/ManagedStatic.h"
+#include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/PrettyStackTrace.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/System/DynamicLibrary.h"
+#include "llvm/System/Path.h"
+#include "llvm/System/Process.h"
+#include "llvm/System/Signals.h"
+#include "llvm/Target/TargetSelect.h"
+#include "llvm/Transforms/Utils/Cloning.h"
 
-using std::string;
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <limits.h>
+#include <stdint.h>
 
 namespace cling {
    
-   //
-   // StmtFinder
-   //
-   
-   StmtFinder::StmtFinder(unsigned pos, const clang::SourceManager& sm)
-	: m_pos(pos), m_srcMgr(sm), m_stmt(NULL)
-   {
-   }
-   
-   StmtFinder::~StmtFinder()
-   {
-   }
-   
-   void StmtFinder::VisitChildren(clang::Stmt *S)
-   {
-      for (clang::Stmt::child_iterator I = S->child_begin(), E = S->child_end();
-           I != E; ++I) {
-         if (*I) {
-            Visit(*I);
-         }
+//
+//  StmtSplitter
+//
+
+StmtSplitter::~StmtSplitter()
+{
+}
+
+void StmtSplitter::VisitChildren(clang::Stmt* S)
+{
+  clang::Stmt::child_iterator I = S->child_begin();
+  clang::Stmt::child_iterator E = S->child_end();
+  for (; I != E; ++I) {
+    if (*I) {
+      Visit(*I);
+    }
+  }
+}
+
+//
+//  FunctionBodyConsumer
+//
+
+FunctionBodyConsumer::FunctionBodyConsumer(StmtSplitter& visitor,
+   const char* funcName)
+   : clang::ASTConsumer(), m_visitor(visitor), m_funcName(funcName)
+{
+}
+
+FunctionBodyConsumer::~FunctionBodyConsumer()
+{
+}
+
+void FunctionBodyConsumer::HandleTopLevelDecl(clang::DeclGroupRef D)
+{
+  clang::DeclGroupRef::iterator I = D.begin();
+  clang::DeclGroupRef::iterator E = D.end();
+  for (; I != E; ++I) {
+    clang::FunctionDecl* FD = dyn_cast<clang::FunctionDecl>(*I);
+    if (FD) {
+      if (FD->getNameAsCString() == m_funcName) {
+	clang::Stmt* S = FD->getBody();
+	if (S) {
+	  m_visitor.VisitChildren(S);
+	}
       }
-   }
-   
-   void StmtFinder::VisitStmt(clang::Stmt *S)
-   {
-      clang::SourceLocation Loc = S->getLocStart();
-      unsigned offs = m_srcMgr.getFileOffset(m_srcMgr.getInstantiationLoc(Loc));
-      if (offs == m_pos) {
-         m_stmt = S;
-      }
-   }
-   
-   clang::Stmt * StmtFinder::getStmt() const
-   {
-      return m_stmt;
-   }
-   
-   //
-   // StmtSplitter
-   //
-   
-   StmtSplitter::StmtSplitter(const string& src,
-                              const clang::SourceManager& sm,
-                              const clang::LangOptions& options,
-                              std::vector<clang::Stmt*> *stmts)
-   : _src(src), m_srcMgr(sm), _options(options), m_vec_stmts(stmts)
-   {
-   }
-   
-   StmtSplitter::~StmtSplitter()
-   {
-   }
-   
-   void StmtSplitter::VisitChildren(clang::Stmt *S)
-   {
-      for (clang::Stmt::child_iterator I = S->child_begin(), E = S->child_end();
-           I != E; ++I) {
-         if (*I) {
-            Visit(*I);
-         }
-      }
-   }
-   
-   void StmtSplitter::VisitStmt(clang::Stmt *S)
-   {
-//      fprintf(stderr,"VisitStmt\n");
-//      S->dump();
-      m_vec_stmts->push_back(S);
-   }
-   
-   void StmtSplitter::VisitDeclStmt(clang::DeclStmt *S)
-   {
-//      fprintf(stderr,"VisitDeclStmt\n");
-//      S->dump();
-      m_vec_stmts->push_back(S);      
-   }
-   
-} // namespace ccons
+    }
+  }
+}
+
+} // namespace cling
