ae76f102367f1b235de4afd532bf65c6578bc7c1
russo <russo@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-03-17 21:16:57 +0000

    Remove all the commented out old code which was there for reference purposes only and is now not needed.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@32661 27541ba8-7e3a-0410-8455-c3a389f83636

--- include/cling/Interpreter/Interpreter.h
+++ include/cling/Interpreter/Interpreter.h
@@ -43,7 +43,6 @@
 #include "clang/Lex/MacroInfo.h"
 #include "clang/Lex/PPCallbacks.h"
 #include "clang/Lex/Preprocessor.h"
-//#include "clang/lib/Sema/Sema.h"
 #include "clang/Parse/Parser.h"
 #include "clang/Sema/ParseAST.h"
 #include "llvm/ADT/OwningPtr.h"
@@ -133,112 +132,10 @@ public:
    //---------------------------------------------------------------------
    virtual ~Interpreter();
 
-#if 0
-   //---------------------------------------------------------------------
-   //! Add a translation unit
-   //!
-   //! @param  fileName path of the file to be parsed
-   //! @return true if the file was successfuly processed, false otherwise
-   //---------------------------------------------------------------------
-   virtual bool addUnit(const std::string& fileName);
-
-   //---------------------------------------------------------------------
-   //! Add a translation unit
-   //!
-   //! @param  buffer a buffer to be processed
-   //! @param  id     the ID that was assigned to the translation unit
-   //! @return true if the buffer was successfuly processed, false
-   //!         otherwise
-   //---------------------------------------------------------------------
-   virtual bool addUnit(const llvm::MemoryBuffer* buffer,
-                        UnitID_t& id);
-
-   //---------------------------------------------------------------------
-   //! Remove the translation unit
-   //!
-   //! @param id id of the translation unit to be removed
-   //! @return true if the unit was removed, false otherwise
-   //---------------------------------------------------------------------
-   virtual bool removeUnit(const UnitID_t& id);
-#endif // 0
-
-   //---------------------------------------------------------------------
-   //! Check if the input is complete or not.
-   //!
-   //! @param context code to prefix to the input
-   //! @param line code to analyze
-   //! @param indentLevel keep track of the block nesting
-   //! @param return function declaration found in the code
-   //! @return true if the unit was removed, false otherwise
-   //---------------------------------------------------------------------
    InputType analyzeInput(const std::string& contextSource,
                           const std::string& line,
                           int& indentLevel,
                           std::vector<clang::FunctionDecl*> *fds);
-
-#if 0
-   //---------------------------------------------------------------------
-   //! Get a compiled  module linking together all translation units
-   //!
-   //! @return the module or 0, consult the diagnostics if a 0 pointer
-   //!         is returned
-   //---------------------------------------------------------------------
-   virtual llvm::Module* getModule();
-
-   //---------------------------------------------------------------------
-   //! Compile the filename and link it to all the modules known to the
-   //! compiler but do not add it to the list
-   //!
-   //! @return the resulting module or 0, consult the diagnostics if a 0
-   //!         pointer is returned
-   //---------------------------------------------------------------------
-   virtual llvm::Module* linkFile(const std::string& fileName,
-                                  std::string* errMsg = 0);
-
-   //---------------------------------------------------------------------
-   //! Compile the buffer and link it to all the modules known to the
-   //! compiler but do not add it to the list
-   //!
-   //! @param  buff the input buffer, the compiler takes the ownership
-   //!         over it
-   //! @return the resulting module or 0, consult the diagnostics if a 0
-   //!         pointer is returned
-   //---------------------------------------------------------------------
-   virtual llvm::Module* linkSource(const std::string& src,
-                                    std::string* errMsg = 0);
-
-   //---------------------------------------------------------------------
-   //! Link the module to all the modules known to the compiler but do
-   //! not add it to the list
-   //!
-   //! @return the resulting module or 0, consult the diagnostics if a 0
-   //!         pointer is returned
-   //---------------------------------------------------------------------
-   virtual llvm::Module* linkModule(llvm::Module *module,
-                                    std::string* errMsg = 0);
-
-   //---------------------------------------------------------------------
-   //! Load a module making sure to execute the global variable initialization.
-   //!
-   //! @return true if the module was loaded.
-   bool loadModule(llvm::Module *module);
-
-   //---------------------------------------------------------------------
-   //! Execute a module containing a function funcname.
-   //!
-   //! @return funcname()'s return value
-   int executeModuleMain(llvm::Module *module,
-                         const std::string& funcname = "main");
-
-   //---------------------------------------------------------------------
-   //! Execute a file containing a function funcname.
-   //! If funcname == "()", funcname is the stem (no extension) of
-   //! filename.
-   //!
-   //! @return funcname()'s return value
-   int executeFile(const std::string& filename,
-                   const std::string& funcname = "()");
-#endif // 0
 
    void processLine(const std::string& input_line);
    int loadFile(const std::string& filename);
@@ -272,31 +169,6 @@ private:
    void printModule(llvm::Module* m);
    void executeCommandLine();
    void executeFunction(const std::string& funcname);
-
-#if 0
-   ParseEnvironment* parseSource(const std::string& source);
-   ParseEnvironment* parseFile(const std::string& fileName);
-   llvm::Module*     compile(clang::TranslationUnitDecl* tu);
-   bool              addUnit(const UnitID_t& id,
-                             clang::TranslationUnitDecl* tu);
-
-   std::vector<clang::Decl*>
-   extractDeclarations(clang::TranslationUnitDecl* tu);
-   void insertDeclarations(clang::TranslationUnitDecl* tu, clang::Sema* sema);
-   void dumpTU(clang::DeclContext* dc);
-   clang::QualType typeCopy(clang::QualType source,
-                            const clang::ASTContext& sourceContext,
-                            clang::ASTContext& targetContext);
-   llvm::Module* copyModule(const llvm::Module* src);
-#endif // 0
-
-#if 0
-   std::string splitInput(const std::string& input, std::vector<std::string>& statements);
-   bool handleDeclStmt(const clang::DeclStmt *DS, ParseEnvironment *pEnv,
-                       const std::string& src,
-                       std::string& globalDecls,
-                       std::string& processedCode);
-#endif // 0
 
 };
 
