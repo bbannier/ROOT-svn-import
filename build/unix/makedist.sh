bbannier/ROOT-svn-import

commit 0414e59634fa26bc32321f9329b020cc13e8d36b

Don't pass the filenames to makemsi, the file containing them is sufficient.
git-svn-id: https://root.cern.ch/svn/root/trunk@31897 27541ba8-7e3a-0410-8455-c3a389f83636

build/unix/makedist.sh
@@ -67,11 +67,7 @@ fi
 
 ${pwd}/build/unix/distfilelist.sh $dir > ${TARFILE}.filelist
 rm -f ${TARFILE}
-if [ "x${TAR}" != "x" ]; then
-   $TARCMD || exit 1
-else
-   $TARCMD `cat ${TARFILE}.filelist` || exit 1
-fi
+$TARCMD || exit 1
 rm ${TARFILE}.filelist 
 
 if [ "x$DOGZIP" = "xy" ]; then

commit deb0a9206c1be1c89d166e6a56c408c76cb8ab0a

fix regression introduced in r31897.
git-svn-id: https://root.cern.ch/svn/root/trunk@32911 27541ba8-7e3a-0410-8455-c3a389f83636

build/unix/makedist.sh
@@ -67,7 +67,11 @@ fi
 
 ${pwd}/build/unix/distfilelist.sh $dir > ${TARFILE}.filelist
 rm -f ${TARFILE}
-$TARCMD || exit 1
+if [ "x${TAR}" != "x" ] || [ "x$MSI" = "x1" ]; then
+   $TARCMD || exit 1
+else
+   $TARCMD `cat ${TARFILE}.filelist` || exit 1
+fi
 rm ${TARFILE}.filelist 
 
 if [ "x$DOGZIP" = "xy" ]; then