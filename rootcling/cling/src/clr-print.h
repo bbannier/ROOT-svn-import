a02a1631b632719a6f0eb34bfd0ed67a0cd82f33
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-04-21 10:04:09 +0000

    From Zdenek: * remove unused functions from TCling * coding conventions * put clr functions into classes
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@33116 27541ba8-7e3a-0410-8455-c3a389f83636

--- rootcling/cling/src/clr-print.h
+++ rootcling/cling/src/clr-print.h
@@ -1,20 +1,58 @@
-
-/* clr-print.h */
+// @(#)root/cint:$Id$
+// Author: Zdenek Culik   16/04/2010
 
 #ifndef __CLR_PRINT_H__
 #define __CLR_PRINT_H__
 
-#include <string>
+#include "Reflex/Reflex.h"
+#include "TString.h"
+#include "clr-util.h" // class THtmlHelper
 
-#include <Reflex/Reflex.h>
+/* -------------------------------------------------------------------------- */
 
-/* pseudo code from Reflex dictionaty */
+class TReflexPrinter : public THtmlHelper {
+private:
+   void PrintLine(TString key, TString name);
 
-void ClrPrint (std::string file_name,
-               std::string style_file_name = "",
-               bool use_html = false);
+   void PrintTypeName(Reflex::Type t);
+   void PrintTypeNotes(Reflex::Type t);
+   void PrintType(Reflex::Type t);
+   void PrintSrcType(Reflex::Type t);
 
-std::string ClrPrintScope (Reflex::Scope scope,
-                           std::string style_file_name = "");
+   void PrintNamespace(Reflex::Scope s);
+   void PrintClass(Reflex::Scope s);
+   void PrintEnum(Reflex::Scope s);
+   void PrintVariable(Reflex::Member m);
+   void PrintFunction(Reflex::Member m);
+
+   void PrintMember(Reflex::Member m);
+   void PrintAccess(Reflex::Member m, int& acs);
+   void PrintScope(Reflex::Scope s, bool show_access = false);
+   void PrintSrcScope(TString text, Reflex::Scope s);
+   #if 0
+   void PrintTypeTemplate(Reflex::TypeTemplate s);
+   void PrintMemberTemplate(Reflex::MemberTemplate s);
+   #endif
+
+public:
+   TReflexPrinter () { }
+   virtual ~ TReflexPrinter () { }
+
+   void Print(Reflex::Scope scope);
+};
+
+/* -------------------------------------------------------------------------- */
+
+/* pseudo code from Reflex dictionary */
+
+void ClrPrint(TString file_name,
+              TString style_file_name = "",
+              bool useHtml = false);
+
+TString ClrPrintScope (Reflex::Scope scope,
+                       TString style_file_name = "",
+                       bool useHtml = false);
+
+/* -------------------------------------------------------------------------- */
 
 #endif /* __CLR_PRINT_H__ */
--- rootcling/cling/src/clr-scan.h
+++ rootcling/cling/src/clr-scan.h
@@ -1,5 +1,5 @@
-
-/* clr-scan.h */
+// @(#)root/cint:$Id$
+// Author: Zdenek Culik   16/04/2010
 
 #ifndef __CLR_SCAN_H__
 #define __CLR_SCAN_H__
@@ -9,38 +9,115 @@
 #include "clang/AST/DeclGroup.h"
 #include "clang/AST/DeclFriend.h"
 
-#include <string>
+#include "Reflex/Reflex.h"
+#include "Reflex/Builder/ReflexBuilder.h"
 
-/* ---------------------------------------------------------------------- */
+#include "TString.h"
 
-extern const char * clang_key;
+/* -------------------------------------------------------------------------- */
 
-void ClrInit (clang::ASTContext * C,
-              std::string print_file_name,
-              std::string style_file_name,
-              std::string dict_file_name);
+class TScanner
+{
+private:
+   clang::ASTContext* fCtx;
+   TObject * fReporter;
 
-void ClrScan (clang::Decl * D);
+private:
+   static int unknown_class_counter;
+   static int unknown_enum_counter;
 
-void ClrOutput ();
+private:
+   void ShowInfo(const TString msg, const TString location = "");
+   void ShowWarning(const TString msg, const TString location = "");
+   void ShowError(const TString msg, const TString location = "");
 
-/* ---------------------------------------------------------------------- */
+   TString GetLocation(clang::Decl* D);
+   TString GetName(clang::Decl* D);
 
-void ClrStore (clang::ASTContext * C,
-               clang::Decl * D);
+   void ScanInfo(clang::Decl* D);
+   void ScanUnknown(clang::Decl* D, TString txt = "");
+   void ScanUnexpected(clang::Decl* D, TString txt = "");
+   void ScanUnsupported(clang::Decl* D, TString txt = "");
+   void ScanUnimplemented(clang::Decl* D, TString txt = "");
 
-void ClrInfo ();
+   void ScanUnknownType(clang::QualType qual_type);
+   void ScanUnsupportedType(clang::QualType qual_type);
+   void ScanUnimplementedType(clang::QualType qual_type);
+   void ScanUnimplementedType (clang::Type* T);
 
-void ClrTest ();
+   Reflex::ClassBuilder* GetClassBuilder(clang::RecordDecl* D, TString template_params = "");
 
-/* ---------------------------------------------------------------------- */
+   Reflex::Type ScanPointerType(clang::PointerType* T);
+   Reflex::Type ScanLValueReferenceType(clang::LValueReferenceType* T);
+   Reflex::Type ScanMemberPointerType(clang::MemberPointerType* T);
+   Reflex::Type ScanArrayType(clang::ArrayType* T);
+   Reflex::Type ScanFunctionType(clang::FunctionType* T);
+   Reflex::Type ScanEnumType(clang::EnumType* T);
+   Reflex::Type ScanRecordType(clang::RecordType* T);
+   Reflex::Type ScanElaboratedType(clang::ElaboratedType* T);
+   Reflex::Type ScanTypedefType(clang::TypedefType* T);
+   Reflex::Type ScanBuiltinType(clang::BuiltinType* T);
+   Reflex::Type ScanQualifiedNameType(clang::QualifiedNameType* T);
+   Reflex::Type ScanTemplateTypeParmType(clang::TemplateTypeParmType* T);
+   Reflex::Type ScanSubstTemplateTypeParmType(clang::SubstTemplateTypeParmType* T);
+   TString ConvTemplateName(clang::TemplateName& N);
+   TString ExprToStr(clang::Expr* expr);
+   TString ConvTemplateArguments(const clang::TemplateArgumentList& list);
+   Reflex::Type ScanTemplateSpecializationType(clang::TemplateSpecializationType* T);
+   Reflex::Type ScanQualType(clang::QualType qual_type);
 
-typedef const char * const_char_ptr;
+   TString ConvTemplateParameterList(clang::TemplateParameterList* list);
+   TString ScanTemplateParams(clang::TemplateDecl* D);
 
-void ClrOptions (int & param_argc, const_char_ptr * & param_argv);
+   void ScanClassTemplate(clang::ClassTemplateDecl* D);
+   void ScanFunctionTemplate(clang::FunctionTemplateDecl* D);
+   void ScanTemplateParm(clang::TemplateTemplateParmDecl* D);
+   void ScanTemplateTypeParmDecl(clang::TemplateTypeParmDecl *D);
 
-int driver_main (int argc, const_char_ptr * argv);
+   TString FuncParameters(clang::FunctionDecl* D);
+   Reflex::Type FuncType(clang::FunctionDecl* D);
+   unsigned int FuncModifiers(clang::FunctionDecl* D);
+   unsigned int VisibilityModifiers(clang::AccessSpecifier access);
+   unsigned int Visibility(clang::Decl* D);
+   unsigned int VarModifiers(clang::VarDecl* D);
 
-/* ---------------------------------------------------------------------- */
+   void ScanFieldMember(clang::FieldDecl* D, Reflex::ClassBuilder* builder);
+   void ScanVarMember(clang::VarDecl* D, Reflex::ClassBuilder* builder);
+   void ScanMethod(clang::CXXMethodDecl* D, Reflex::ClassBuilder* builder, TString template_params = "");
+   void ScanEnumMember(clang::EnumDecl* D, Reflex::ClassBuilder* builder);
+   void ScanTypedefMember(clang::TypedefDecl* D, Reflex::ClassBuilder* builder);
+   void ScanFunctionTemplateMember(clang::FunctionTemplateDecl* D, Reflex::ClassBuilder* builder);
+   void ScanFriendMember(clang::FriendDecl *D, Reflex::ClassBuilder* builder);
+   void ScanMember(clang::Decl* D, Reflex::ClassBuilder* builder);
+   void ScanClass(clang::RecordDecl* D, TString template_params = "");
+
+   void ScanEnum(clang::EnumDecl* D);
+
+   void ScanUsingDecl(clang::UsingDecl* D, Reflex::Scope Outer);
+   void ScanUsingShadowDecl(clang::UsingShadowDecl* D, Reflex::Scope Outer);
+
+   void ScanTranslationUnit(clang::TranslationUnitDecl* D, Reflex::Scope Outer);
+   void ScanNamespace(clang::NamespaceDecl* D);
+   void ScanLinkageSpec(clang::LinkageSpecDecl* D, Reflex::Scope Outer);
+   void ScanTypedef(clang::TypedefDecl* D);
+   void ScanVariable(clang::VarDecl* D);
+   void ScanFunction(clang::FunctionDecl* D, TString template_params = "");
+   void ScanDecl(clang::Decl* D, Reflex::Scope Outer);
+
+public:
+   TScanner ();
+   virtual ~ TScanner ();
+
+   void Scan (clang::ASTContext* C, clang::Decl* D);
+};
+
+/* -------------------------------------------------------------------------- */
+
+extern const char* kClangKey; // property key used for connection with Clang objects
+
+void ClrScan(clang::ASTContext* C, clang::Decl* D);
+void ClrStore(clang::ASTContext* C, clang::Decl* D); // old function name, equal to ClrScan
+
+/* -------------------------------------------------------------------------- */
 
 #endif /* __CLR_SCAN_H__ */
