e67cdab3a4ee19d995f21391c18a70afd27ae52c
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-04-28 10:44:52 +0000

    Re-enable non-interactive use of cling. Fix first test so all cling tests now pass!
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@33272 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/test/SourceCall/test_01.c
+++ cling/test/SourceCall/test_01.c
@@ -2,7 +2,9 @@
 
 #include <stdio.h>
 
-int test_01(int argc=12, char** argv= "A default argument")
+const char* defaultArgV[] = {"A default argument", "", 0};
+
+int test_01(int argc=12, const char** argv = defaultArgV)
 {
   int i;
   for( i = 0; i < 5; ++i )
