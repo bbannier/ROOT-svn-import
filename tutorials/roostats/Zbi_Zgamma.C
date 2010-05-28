ec31010d7c620defb9bbfcffc230bcb0d0e65b90
cranmer <cranmer@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-05-28 15:03:58 +0000

    added tutorial demonstraiting RooFit Z_gamma = analytic Z_Bi
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/roostats@33647 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/roostats/Zbi_Zgamma.C
+++ tutorials/roostats/Zbi_Zgamma.C
@@ -22,49 +22,47 @@ using namespace RooStats;
 
 void Zbi_Zgamma() {
 
+  // Make model for prototype on/off problem
+  // Pois(x | s+b) * Pois(y | tau b )
+  // for Z_Gamma, use uniform prior on b.
   RooWorkspace* w = new RooWorkspace("w",true);
-  RooAbsPdf* px = (RooAbsPdf*)   w->factory("Poisson::px(x[0,500],sum::splusb(s[0,0,100],b[100,0,300]))");
-  RooAbsPdf* py = (RooAbsPdf*)   w->factory("Poisson::py(y[100,0,500],prod::taub(tau[1.],b))");	     
-  RooRealVar* x = w->var("x");
-  RooRealVar* y =  w->var("y");
-  RooRealVar* b =  w->var("b");
-  RooAbsPdf* pdf = (RooAbsPdf*) w->factory("PROD::pdf(px,py)");
-  RooAbsPdf* prior = (RooAbsPdf*) w->factory("Uniform::prior_b(b)");
+  w->factory("Poisson::px(x[150,0,500],sum::splusb(s[0,0,100],b[100,0,300]))");
+  w->factory("Poisson::py(y[100,0,500],prod::taub(tau[1.],b))");	     
+  w->factory("Uniform::prior_b(b)");
 
-  // construct the Bayesian averaged model:
+  // construct the Bayesian-averaged model (eg. a projection pdf)
   // p'(x|s) = \int db p(x|s+b) * [ p(y|b) * prior(b) ]
-  w->factory("PROJ::ifoo(PROD::foo(px|b,py,prior_b),b)") ;
+  w->factory("PROJ::averagedModel(PROD::foo(px|b,py,prior_b),b)") ;
 
-  // plot it, blue is smeared model, red is b known exactly
+  // plot it, blue is averaged model, red is b known exactly
   RooPlot* frame = w->var("x")->frame() ;
-  w->pdf("ifoo")->plotOn(frame) ;
+  w->pdf("averagedModel")->plotOn(frame) ;
   w->pdf("px")->plotOn(frame,LineColor(kRed)) ;
   frame->Draw() ;
 
 
-  // compare Z_Bi analytic calculation with the numerical
-  // RooFit implementation of Z_Gamma
-  // for an example with x = 144, y = 100
-
+  // compare analytic calculation of Z_Bi
+  // with the numerical RooFit implementation of Z_Gamma
+  // for an example with x = 150, y = 100
    
   // numeric RooFit Z_Gamma
   w->var("y")->setVal(100);
-  w->var("x")->setVal(144);
-  RooAbsReal* cdf = w->pdf("ifoo")->createCdf(*w->var("x"));
+  w->var("x")->setVal(150);
+  RooAbsReal* cdf = w->pdf("averagedModel")->createCdf(*w->var("x"));
   cdf->getVal(); // get ugly print messages out of the way
 
   cout << "Hybrid p-value = " << cdf->getVal() << endl;
   cout << "Z_Gamma Significance  = " << 
-    RooStats::PValueToSignificance(1-cdf->getVal()) << endl;
+    PValueToSignificance(1-cdf->getVal()) << endl;
 
   // analytic Z_Bi
-  double Z_Bi = RooStats::NumberCountingUtils::BinomialWithTauObsZ(144, 100, 1);
+  double Z_Bi = NumberCountingUtils::BinomialWithTauObsZ(150, 100, 1);
   std::cout << "Z_Bi significance estimation: " << Z_Bi << std::endl;
 
   // OUTPUT
-  //Hybrid p-value = 0.997101
-  //Z_Gamma Significance  = 2.75905
-  //Z_Bi significance estimation: 2.75905
+  // Hybrid p-value = 0.999058
+  // Z_Gamma Significance  = 3.10804
+  // Z_Bi significance estimation: 3.10804
 
 
 }
