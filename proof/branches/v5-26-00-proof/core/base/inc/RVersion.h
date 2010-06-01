8be63997d0e59f2563b44287f851727e6abe88b4
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-06-01 11:36:21 +0000

    Restore after tag (dev cycle 5-26-00-proof-03)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@33697 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/branches/v5-26-00-proof/core/base/inc/RVersion.h
+++ proof/branches/v5-26-00-proof/core/base/inc/RVersion.h
@@ -14,12 +14,12 @@
  *
 */
 
-#define ROOT_RELEASE "5.26/00-proof-02"
+#define ROOT_RELEASE "5.26/00-proof"
 #define ROOT_RELEASE_DATE "Jun 1 2010"
 #define ROOT_RELEASE_TIME "00:00:56"
 #define ROOT_SVN_REVISION 33694
-#define ROOT_SVN_BRANCH "branches/dev/proof/tags/v5-26-00-proof-02"
-#define ROOT_VERSION_CODE 334388
+#define ROOT_SVN_BRANCH "branches/dev/proof/branches/v5-26-00-proof"
+#define ROOT_VERSION_CODE 334389
 #define ROOT_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + (c))
 
 #endif
