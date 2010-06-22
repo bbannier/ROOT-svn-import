84ac68f7463bd981b78180d22ff1528b1feec7da
alja <alja@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-06-22 11:43:49 +0000

    Add a possibility to draw cells in 2D mode at fixed height.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/fireworks@34053 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf3d/eve/src/TEveCaloLegoGL.cxx
+++ graf3d/eve/src/TEveCaloLegoGL.cxx
@@ -900,6 +900,8 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
    Float_t bws    = -1; //smallest bin
    Float_t logMax = -1;
 
+   Float_t baseOffset = fM->GetFixedHeightValIn2DMode()*fMaxVal;
+
    if (fM->f2DMode == TEveCaloLego::kValColor)
    {
       fM->AssertPalette();
@@ -909,14 +911,14 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
       {
          if (rnrCtx.SecSelection()) glLoadName(i->fId);
          glBegin(GL_POLYGON);
-         Float_t val = i->fSumVal;
-         fM->fPalette->ColorFromValue(TMath::FloorNint(val), col);
+         fM->fPalette->ColorFromValue(TMath::FloorNint(i->fSumVal), col);
          col[3] = fM->GetData()->GetSliceTransparency(i->fMaxSlice);
          TGLUtil::Color4ubv(col);
-         glVertex3f(i->fX0, i->fY0, val);
-         glVertex3f(i->fX1, i->fY0, val);
-         glVertex3f(i->fX1, i->fY1, val);
-         glVertex3f(i->fX0, i->fY1, val);
+         Float_t z = fM->GetHasFixedHeightIn2DMode() ? baseOffset : i->fSumVal;
+         glVertex3f(i->fX0, i->fY0, z);
+         glVertex3f(i->fX1, i->fY0, z);
+         glVertex3f(i->fX1, i->fY1, z);
+         glVertex3f(i->fX0, i->fY1, z);
          glEnd();
       }
    }
@@ -946,10 +948,11 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
             glPushName(i->fId);
 
             glBegin(GL_QUADS);
-            glVertex3f(i->fX0, i->fY0,  i->fSumVal);
-            glVertex3f(i->fX1, i->fY0,  i->fSumVal);
-            glVertex3f(i->fX1, i->fY1,  i->fSumVal);
-            glVertex3f(i->fX0, i->fY1,  i->fSumVal);
+            Float_t z = fM->GetHasFixedHeightIn2DMode() ? baseOffset : i->fSumVal;
+            glVertex3f(i->fX0, i->fY0, z);
+            glVertex3f(i->fX1, i->fY0, z);
+            glVertex3f(i->fX1, i->fY1, z);
+            glVertex3f(i->fX0, i->fY1, z);
             glEnd();
 
             glPopName();
@@ -975,10 +978,11 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
             Float_t bw = fValToPixel*TMath::Log10(i->fSumVal+1);
             x = i->X();
             y = i->Y();
-            glVertex3f(x - bw, y - bw, i->fSumVal);
-            glVertex3f(x + bw, y - bw, i->fSumVal);
-            glVertex3f(x + bw, y + bw, i->fSumVal);
-            glVertex3f(x - bw, y + bw, i->fSumVal);
+            Float_t z = fM->GetHasFixedHeightIn2DMode() ? baseOffset : i->fSumVal;
+            glVertex3f(x - bw, y - bw, z);
+            glVertex3f(x + bw, y - bw, z);
+            glVertex3f(x + bw, y + bw, z);
+            glVertex3f(x - bw, y + bw, z);
          }
          glEnd();
 
@@ -986,12 +990,13 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
          { 
             glPushAttrib(GL_ENABLE_BIT | GL_POLYGON_BIT);
             Float_t z    = 0;
-            Float_t zOff = fDataMax*0.1 ;
+            Float_t zOff = fDataMax*0.001 ; // avoid polygon stpiling
             glBegin(GL_QUADS);
             for ( vCell2D_i i = cells2D.begin(); i != cells2D.end(); ++i) {
                Char_t transp = TMath::Min(100, 80 + fM->fData->GetSliceTransparency(i->fMaxSlice) / 5);
                TGLUtil::ColorTransparency(fM->fData->GetSliceColor(i->fMaxSlice), transp);
-               z = i->fSumVal - zOff;
+               z = fM->GetHasFixedHeightIn2DMode() ? baseOffset : i->fSumVal;
+               z -=  zOff;
                glVertex3f(i->fX0, i->fY0, z);
                glVertex3f(i->fX1, i->fY0, z);
                glVertex3f(i->fX1, i->fY1, z);
@@ -1003,7 +1008,8 @@ void TEveCaloLegoGL::DrawCells2D(TGLRnrCtx &rnrCtx, vCell2D_t& cells2D) const
             glBegin(GL_QUADS);
             for ( vCell2D_i i = cells2D.begin(); i != cells2D.end(); ++i) {
                TGLUtil::ColorTransparency(fM->fData->GetSliceColor(i->fMaxSlice), 60);
-               z = i->fSumVal + zOff;
+               z = fM->GetHasFixedHeightIn2DMode() ? baseOffset : i->fSumVal;
+               z -=  zOff;
                glVertex3f(i->fX0, i->fY0, z);
                glVertex3f(i->fX1, i->fY0, z);
                glVertex3f(i->fX1, i->fY1, z);
