225fb1b1d40b82b30bdaa6c1af987220c95e7ddb
hvoss <hvoss@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-07-02 21:21:22 +0000

    now also with minuit
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@34306 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmvaValidation/parOptimizer/src/Optimizer.cxx
+++ tmvaValidation/parOptimizer/src/Optimizer.cxx
@@ -31,6 +31,7 @@ ClassImp(TMVA::Optimizer)
 #include "TMVA/MethodBase.h"   
 #include "TMVA/MethodBDT.h"   
 #include "TMVA/GeneticFitter.h"
+#include "TMVA/MinuitFitter.h"
 #include "TMVA/Interval.h"
 //_______________________________________________________________________
 TMVA::Optimizer::Optimizer(MethodBase * const method, TString fomType) 
@@ -70,7 +71,7 @@ TMVA::Optimizer::~Optimizer()
       std::cout << "for "<<i<<" fom = " << y[i] << std::endl;
    }
 
-   TH2D   *h=new TH2D(fFOM->GetMethod()->GetMethodName()+"_FOMvsIterHist","",2,0,n,2,ymin,ymax);
+   TH2D   *h=new TH2D(fFOM->GetMethod()->GetMethodName()+"_FOMvsIterHist","",2,0,n,2,ymin*0.95,ymax*1.05);
    TGraph *gFOMvsIter = new TGraph(n,x,y);
    gFOMvsIter->SetName((fFOM->GetMethod()->GetMethodName()+"_FOMvsIter").Data());
    gFOMvsIter->Write();
@@ -85,8 +86,8 @@ TMVA::Optimizer::~Optimizer()
 //_______________________________________________________________________
 void TMVA::Optimizer::optimize(TString optimizationFitType)
 {
-   if      (optimizationFitType == "Scan") this->optimizeScan();
-   else if (optimizationFitType == "GA"  ) this->optimizeGA();
+   if      (optimizationFitType == "Scan"    ) this->optimizeScan();
+   else if (optimizationFitType == "GA" || optimizationFitType == "Minuit" ) this->optimizeFit(optimizationFitType);
    else {
       std::cout << "You have chosen as optimization type " << optimizationFitType
                 << " that is not (yet) coded --> exit()" << std::endl;
@@ -111,79 +112,47 @@ void TMVA::Optimizer::optimizeScan()
    //   too much freedom for the user just gives us bad publicity when being
    //   compared with Neurobayes!!
    
-   std::vector< std::pair<Int_t,Int_t > >  loopVariable;
+   std::vector< std::vector<Int_t>* >  loopVariable;
    
    if (fFOM->GetMethod()->GetMethodType() == TMVA::Types::kBDT){
-      //      for (UInt_t i=0;i<9;i++){loopVariable.push_back(i+1);}
-      //      for (UInt_t i=0;i<9;i++){loopVariable.push_back(9-i);}
-      loopVariable.push_back(std::make_pair(2,10));
-      loopVariable.push_back(std::make_pair(3,10));
-      loopVariable.push_back(std::make_pair(4,10));
-      loopVariable.push_back(std::make_pair(5,10));
-      loopVariable.push_back(std::make_pair(10,10));
-      loopVariable.push_back(std::make_pair(15,10));
-      loopVariable.push_back(std::make_pair(20,10));
-
-      loopVariable.push_back(std::make_pair(2,50));
-      loopVariable.push_back(std::make_pair(3,50));
-      loopVariable.push_back(std::make_pair(4,50));
-      loopVariable.push_back(std::make_pair(5,50));
-      loopVariable.push_back(std::make_pair(10,50));
-      loopVariable.push_back(std::make_pair(15,50));
-      loopVariable.push_back(std::make_pair(20,50));
-
-      loopVariable.push_back(std::make_pair(2,100));
-      loopVariable.push_back(std::make_pair(3,100));
-      loopVariable.push_back(std::make_pair(4,100));
-      loopVariable.push_back(std::make_pair(5,100));
-      loopVariable.push_back(std::make_pair(10,100));
-      loopVariable.push_back(std::make_pair(15,100));
-      loopVariable.push_back(std::make_pair(20,100));
-
-      loopVariable.push_back(std::make_pair(2,300));
-      loopVariable.push_back(std::make_pair(3,300));
-      loopVariable.push_back(std::make_pair(4,300));
-      loopVariable.push_back(std::make_pair(5,300));
-      loopVariable.push_back(std::make_pair(10,300));
-      loopVariable.push_back(std::make_pair(15,300));
-      loopVariable.push_back(std::make_pair(20,300));
-
-      loopVariable.push_back(std::make_pair(2,400));
-      loopVariable.push_back(std::make_pair(3,400));
-      loopVariable.push_back(std::make_pair(4,400));
-      loopVariable.push_back(std::make_pair(5,400));
-      loopVariable.push_back(std::make_pair(10,400));
-      loopVariable.push_back(std::make_pair(15,400));
-      loopVariable.push_back(std::make_pair(20,400));
-
       loopVariable.clear();
-      
-      loopVariable.push_back(std::make_pair(3,400));
-      loopVariable.push_back(std::make_pair(2,50));
-      loopVariable.push_back(std::make_pair(4,300));
-
-
+      std::vector<Int_t> *tmp = new std::vector<Int_t>; 
+      tmp->push_back(3); tmp->push_back(400); tmp->push_back(400); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(2); tmp->push_back(50); tmp->push_back(400); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(4); tmp->push_back(300); tmp->push_back(400); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(3); tmp->push_back(400); tmp->push_back(200); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(2); tmp->push_back(50); tmp->push_back(200); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(4); tmp->push_back(300); tmp->push_back(200); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(3); tmp->push_back(400); tmp->push_back(50); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(2); tmp->push_back(50); tmp->push_back(50); loopVariable.push_back(tmp);
+      tmp->clear();
+      tmp->push_back(4); tmp->push_back(300); tmp->push_back(50); loopVariable.push_back(tmp);
    }
    
    for (UInt_t i=0;i<loopVariable.size();i++){    
       
       
-      if(i!=0)fFOM->GetMethod()->Reset();
-      std::cout << "Set new parameter " << loopVariable[i].first << " / " << loopVariable[i].second << std::endl;
-      ((MethodBDT*)(fFOM->GetMethod()))->SetMaxDepth(loopVariable[i].first);     
-      ((MethodBDT*)(fFOM->GetMethod()))->SetNodeMinEvents(loopVariable[i].second);     
-      
-      std::cout << "Set BaseDir " << std::endl;
-      
-      
-      fFOM->GetMethod()->BaseDir()->cd();
-      std::cout << "Get Transformation events " << std::endl;
-      fFOM->GetMethod()->GetTransformationHandler().CalcTransformations(
-                                                                        fFOM->GetMethod()->Data()->GetEventCollection());
-      std::cout << "train in optimizer with " << fFOM->GetMethod()->Data()->GetNEvents() << std::endl;
-      fFOM->GetMethod()->Train();
-      currentFOM = fFOM->GetFOM(); 
-      std::cout << "With variable:"  << loopVariable[i].first << " / " << loopVariable[i].second << " we get GetFOM = " << currentFOM << std::endl;
+     if(i!=0)fFOM->GetMethod()->Reset();
+     ((MethodBDT*)(fFOM->GetMethod()))->SetMaxDepth((*(loopVariable[i]))[0]);     
+     ((MethodBDT*)(fFOM->GetMethod()))->SetNodeMinEvents((*(loopVariable[i]))[1]);     
+     ((MethodBDT*)(fFOM->GetMethod()))->SetNTrees((*(loopVariable[i]))[2]);     
+     
+     fFOM->GetMethod()->BaseDir()->cd();
+     fFOM->GetMethod()->GetTransformationHandler().CalcTransformations(
+                                                                       fFOM->GetMethod()->Data()->GetEventCollection());
+     fFOM->GetMethod()->Train();
+     currentFOM = fFOM->GetFOM(); 
+     
+     std::cout << "With variable:";
+     for (UInt_t iv=0; iv<<(*(loopVariable[i])).size(); iv++) std::cout << (*(loopVariable[i]))[iv]<< "  ";
+     std::cout << " we get GetFOM = " << currentFOM << std::endl;
       if (currentFOM > bestFOM) {
          bestFOM = currentFOM;
          ibest   = i;
@@ -191,30 +160,44 @@ void TMVA::Optimizer::optimizeScan()
 
    }
    
-   fFOM->GetMethod()->Reset();
-   ((MethodBDT*)(fFOM->GetMethod()))->SetMaxDepth(loopVariable[ibest].first);
-   ((MethodBDT*)(fFOM->GetMethod()))->SetNodeMinEvents(loopVariable[ibest].second);
-   
-   std::cout << "And the winner is : MaxDepth = " << loopVariable[ibest].first << " MinEv="<<  loopVariable[ibest].second  << std::endl;
+   std::cout << "And the winner is : MaxDepth = " << (*(loopVariable[ibest]))[0] 
+             << " and NNodeMinEvents = " << (*(loopVariable[ibest]))[1]
+             << " and NTrees = " << (*(loopVariable[ibest]))[2]
+             << std::endl;
 
+   fFOM->GetMethod()->Reset();
+   ((MethodBDT*)(fFOM->GetMethod()))->SetMaxDepth((*(loopVariable[ibest]))[0]);
+   ((MethodBDT*)(fFOM->GetMethod()))->SetNodeMinEvents((*(loopVariable[ibest]))[1]);
+   ((MethodBDT*)(fFOM->GetMethod()))->SetNTrees((*(loopVariable[ibest]))[2]);
+
+   
 }
 
-void TMVA::Optimizer::optimizeGA()
+void TMVA::Optimizer::optimizeFit(TString optimizationFitType)
 {
    // ranges in which the fit varies the parameters (currently hardcoded for BDTs only)
    std::vector<Interval*> ranges;
 
    ranges.push_back(new Interval(2,20,20-2+1)); // MaxDepth
    ranges.push_back(new Interval(50,500,(500-50)/10+1)); // NNodeMinEvents
+   ranges.push_back(new Interval(50,800,(800-50)/10+1)); // NTrees
 
 
    // create the fitter
-   TString opt="PopSize=20:Steps=30:Cycles=3:ConvCrit=0.01:SaveBestCycle=5";
 
-   FitterBase* fitter = new GeneticFitter( *this, 
-                                           "FitterGA_BDTOptimize", 
-                                           ranges, opt );
+   FitterBase* fitter = NULL;
 
+   if ( optimizationFitType == "Minuit"  ) {
+     TString opt="";
+     fitter = new MinuitFitter(  *this, 
+                                 "FitterMinuit_BDTOptimize", 
+                                 ranges, opt );
+   }else if ( optimizationFitType == "GA"  ) {
+     TString opt="PopSize=20:Steps=30:Cycles=3:ConvCrit=0.01:SaveBestCycle=5";
+     fitter = new GeneticFitter( *this, 
+                                 "FitterGA_BDTOptimize", 
+                                 ranges, opt );
+   }
 
    std::vector<Double_t> pars;
    for (std::vector<Interval*>::const_iterator parIt = ranges.begin(); parIt != ranges.end(); parIt++) {
@@ -231,12 +214,15 @@ void TMVA::Optimizer::optimizeGA()
 
 
    std::cout << "And the winner is : MaxDepth = " << pars[0] 
-             << " and NNodeMinEvents = " << pars[1] << std::endl;
+             << " and NNodeMinEvents = " << pars[1] 
+             << " and NTrees = " << pars[2] 
+             << std::endl;
 
    fFOM->GetMethod()->Reset();
    ((MethodBDT*)(fFOM->GetMethod()))->SetMaxDepth(Int_t(pars[0]));
    ((MethodBDT*)(fFOM->GetMethod()))->SetNodeMinEvents(Int_t(pars[1]));
-
+   ((MethodBDT*)(fFOM->GetMethod()))->SetNTrees(Int_t(pars[2]));
+   
 }
 //_______________________________________________________________________
 Double_t TMVA::Optimizer::EstimatorFunction( std::vector<Double_t> & pars)
