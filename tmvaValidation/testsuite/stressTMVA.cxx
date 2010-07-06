c71216ae9be1f3fb0aafb208960b3bb471062177
evt <evt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-07-06 07:05:24 +0000

    improved first version of ROC curve / factory test
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@34331 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmvaValidation/testsuite/stressTMVA.cxx
+++ tmvaValidation/testsuite/stressTMVA.cxx
@@ -1,30 +1,34 @@
-
-// Author: Christoph Rosemann   Dec. 2009
+// Authors: Christoph Rosemann, Eckhard von Toerne   July 2010
 // TMVA unit tests
 
 #include <iostream>
 #include "tmvaut/UnitTestSuite.h"
+
 #include "tmvaut/utEvent.h"
 #include "tmvaut/utVariableInfo.h"
 #include "tmvaut/utDataSetInfo.h"
 #include "tmvaut/utDataSet.h"
+#include "tmvaut/MethodUnitTestWithROCLimits.h"
+
 using namespace UnitTesting;
 using namespace std;
 
 int main()
 {
-   UnitTestSuite TMVA_test("TMVA unit testing");
-
-   TMVA_test.intro();
-
-   TMVA_test.addTest(new utEvent);
-   TMVA_test.addTest(new utVariableInfo);
-   TMVA_test.addTest(new utDataSetInfo);
-   TMVA_test.addTest(new utDataSet);
-   TMVA_test.run();
-
-   long int nFail = TMVA_test.report();
-   cout << "Total number of failures: " << nFail << endl;
-   cout << "************************************************************************************************" << endl;
+  UnitTestSuite TMVA_test("TMVA unit testing");
+  
+  TMVA_test.intro();
+  
+  TMVA_test.addTest(new utEvent);
+  TMVA_test.addTest(new utVariableInfo);
+  TMVA_test.addTest(new utDataSetInfo);
+  TMVA_test.addTest(new utDataSet);
+  TMVA_test.addTest(new MethodUnitTestWithROCLimits); 
+   
+  TMVA_test.run();
+  
+  long int nFail = TMVA_test.report();
+  cout << "Total number of failures: " << nFail << endl;
+  cout << "************************************************************************************************" << endl;
    //  return eventTest.report();
 }
