ccc0af0e06d477071a42e8de7dbfc81e505a60e8
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-07-08 14:10:07 +0000

    Add a bit useless data to Particle so it eats up more space :-) Add a macro to create a tree from EventData
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@34352 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ introtutorials/createTree.C
@@ -0,0 +1,50 @@
+// Creates the ROOT file for the introductory tutorials at
+// http://root.cern.ch/drupal/
+//
+// Axel, 2010-07-08
+
+// Prefer compiled:
+#include "EventData.h+"
+
+void createTree(ULong64_t numEvents = 100000) {
+   TFile* f = new TFile("eventdata.root", "RECREATE");
+   TTree* tree = new TTree("EventData", "Tutorial tree");
+   tree->SetAutoSave(0); // to not confuse people with "EventData;2" keys
+
+   EventData* event = new EventData();
+   tree->Branch("event", &event);
+
+   Particle p;
+
+   for (ULong64_t i = 0; i < numEvents; ++i) {
+      event->clear();
+      int nParticles = 10 * gRandom->Exp(10.);
+      for (int ip = 0; ip < nParticles; ++ip) {
+         do {
+            p.fPosX = gRandom->Gaus(gRandom->PoissonD(0.1), 1.)
+               + gRandom->BreitWigner(0.1, 0.1);;
+         } while (fabs(p.fPosX) > 10.);
+         p.fPosY = gRandom->Gaus(gRandom->PoissonD(0.01), .7);
+         p.fPosZ = gRandom->Gaus(gRandom->PoissonD(10), 19.);
+
+         p.fMomentum = gRandom->Exp(12);
+         p.fMomentumPhi = gRandom->Uniform(2*TMath::Pi());
+         do {
+            p.fMomentumEta = gRandom->BreitWigner(0.01, 10.);
+         } while (fabs(p.fMomentumEta) > 12.);
+
+         event->AddParticle(p);
+      }
+      event->setSize();
+
+      tree->Fill();
+
+      if (i % (numEvents/50) == 0) {
+         printf("*");
+         fflush(stdout);
+      }
+   }
+   printf("\n");
+   tree->Write();
+   delete f;
+}
