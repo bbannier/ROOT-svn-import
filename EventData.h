07092c9e5b016fbfb5e1f330f3f9087dea14cff0
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-07-08 14:10:07 +0000

    Add a bit useless data to Particle so it eats up more space :-) Add a macro to create a tree from EventData
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/introtutorials@34352 27541ba8-7e3a-0410-8455-c3a389f83636

--- EventData.h
+++ EventData.h
@@ -3,16 +3,26 @@
 
 class Particle {
 public:
+   Particle() {memset(fTags, 0, sizeof(fTags)); }
    double fPosX,fPosY,fPosZ; // particle position nearest to interaction point
    double fMomentum; // particle momentum
    double fMomentumPhi; // particle direction (phi)
    double fMomentumEta; // particle direction (eta)
+   Long64_t fTags[128]; // particle tags
 };
 
 class EventData {
 public:
    std::vector<Particle> fParticles; // particles of the event
    int fEventSize; // size (in bytes) of the event
+
+   void setSize() {
+      fEventSize = sizeof(EventData) + fParticles.size() * sizeof(Particle);
+   }
+   void clear() {
+      fParticles.clear();
+   }
+   void AddParticle(const Particle& p) { fParticles.push_back(p); }
 
    ClassDef(EventData,1); // Data for an event
 };
