8ae86ac210ebf34f788d84096fca101f7b45eabf
evt <evt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-07-18 09:03:25 +0000

    changes from Christoph Rosemann, unit test for TMVA methods
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@34468 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmvaValidation/testsuite/tmvaut/MethodUnitTestWithROCLimits.cxx
+++ tmvaValidation/testsuite/tmvaut/MethodUnitTestWithROCLimits.cxx
@@ -2,35 +2,14 @@
 #include "TFile.h"
 #include "TMVA/MethodBase.h"
 
-/*
-#include "TMVA/Tools.h"
-
-
-#include <cstdlib>
-#include <iostream>
-#include <map>
-#include <string>
-
-#include "TChain.h"
-#include "TFile.h"
-#include "TTree.h"
-#include "TString.h"
-#include "TObjString.h"
-#include "TSystem.h"
-#include "TROOT.h"
-#include "TPluginManager.h"
-
-#include "TMVA/Factory.h"
-#include "TMVA/Tools.h"
-*/
-
 using namespace std;
 using namespace UnitTesting;
 using namespace TMVA;
 
-MethodUnitTestWithROCLimits::MethodUnitTestWithROCLimits(double upLimit, double lowLimit,
-			       const std::string & name,const std::string & filename, std::ostream* sptr) :
-  UnitTest(name,filename,sptr), _upROCLimit(upLimit), _lowROCLimit(lowLimit)
+MethodUnitTestWithROCLimits::MethodUnitTestWithROCLimits(const Types::EMVA& theMethod, const TString& methodTitle, const TString& theOption,
+														double lowLimit, double upLimit,
+														const std::string & name,const std::string & filename, std::ostream* sptr) :
+ UnitTest((string)methodTitle, __FILE__), _methodType(theMethod) , _methodTitle(methodTitle), _methodOption(theOption), _upROCLimit(upLimit), _lowROCLimit(lowLimit)
 {
 }
 
@@ -46,39 +25,30 @@ bool MethodUnitTestWithROCLimits::ROCIntegralWithinInterval()
 
 void MethodUnitTestWithROCLimits::run()
 {
-   //cout << " it's running " << endl;
+	// FIXME:: create _this_ file or rather somewhere else?
   TString outfileName( "TMVA.root" );
-
   TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
 
-  if(!outputFile) // file already exists
+// FIXME:: if file can't be created do something more?
+  if(!outputFile)
     return;    
 
+// FIXME:: make the factory option mutable?
 // absolute silence options:
   string factoryOptions( "!V:Silent:Transformations=I;D;P;G,D:AnalysisType=Classification:!Color:!DrawProgressBar" );
-//   std::string factoryOptions( "!V:!Silent:Transformations=I;D;P;G,D:AnalysisType=Classification" );
 
-  Factory* factory = new Factory( "TMVAClassification", outputFile, factoryOptions );
+  Factory* factory = new Factory( "TMVAUnitTesting", outputFile, factoryOptions );
   
   factory->AddVariable( "myvar1 := var1+var2", 'F' );
   factory->AddVariable( "myvar2 := var1-var2", "Expression 2", "", 'F' );
   factory->AddVariable( "var3",                "Variable 3", "units", 'F' );
   factory->AddVariable( "var4",                "Variable 4", "units", 'F' );
   
-  //   factory->AddSpectator( "spec1:=var1*2",  "Spectator 1", "units" );
-  //   factory->AddSpectator( "spec2:=var1*3",  "Spectator 2", "units" );
-  
-  
-  TFile *input(0);
+  TFile* input(0);
+// FIXME:: give the filename of the sample somewhere else?
   TString fname = "../../tmva/test/tmva_example.root"; 
   input = TFile::Open( fname );
   
-  if (input == NULL) 
-    {
-        fname = "../tmva/test/tmva_example.root"; 
-        input = TFile::Open( fname );
-    }
-
   if (input == NULL) 
     {
       cerr << "broken/inaccessible input file" << endl;
@@ -87,31 +57,29 @@ void MethodUnitTestWithROCLimits::run()
   TTree *signal     = (TTree*)input->Get("TreeS");
   TTree *background = (TTree*)input->Get("TreeB");
   
-  Double_t signalWeight     = 1.0;
-  Double_t backgroundWeight = 1.0;
-  
-  factory->AddSignalTree    ( signal,     signalWeight     );
-  factory->AddBackgroundTree( background, backgroundWeight );
+  factory->AddSignalTree(signal);  
+  factory->AddBackgroundTree(background);
   
   factory->SetBackgroundWeightExpression("weight");
   
   TCut mycuts = "";
   TCut mycutb = "";
   
-  
+  // FIXME:: make options string mutable?
   factory->PrepareTrainingAndTestTree( mycuts, mycutb,
 				       "nTrain_Signal=0:nTrain_Background=0:SplitMode=Random:NormMode=NumEvents:!V" );
  
- 
-   // _theMethod = factory->BookMethod( Types::kCuts, "Cuts","!H:!V:FitMethod=MC:EffSel:SampleSize=2000:VarProp=FSmart");
-  factory->BookMethod( Types::kLD, "LD","!H:!V");
- 
+  factory->BookMethod(_methodType, _methodTitle, _methodOption);
+
   factory->TrainAllMethods();
   factory->TestAllMethods();
   factory->EvaluateAllMethods();
-  _theMethod = dynamic_cast<TMVA::MethodBase*> (factory->GetMethod("LD"));
-  //_theMethod->TestClassification();
-  cout << " the roc integral is " << _theMethod->GetROCIntegral() << endl;  
+
+	_theMethod = dynamic_cast<TMVA::MethodBase*> (factory->GetMethod(_methodTitle));
+	_ROCValue = _theMethod->GetROCIntegral();
+
+ test_(ROCIntegralWithinInterval());
+
   outputFile->Close();
   delete factory;
 }
