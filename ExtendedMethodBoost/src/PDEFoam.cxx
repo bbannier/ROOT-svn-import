3b6883729e34165953a51c50131621089a807476
avoigt <avoigt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-08-20 17:25:32 +0000

    Bugfix: avoid log(0) in case of CrossEntropy and zero number of signal or background events
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tmvaValidation@34905 27541ba8-7e3a-0410-8455-c3a389f83636

--- ExtendedMethodBoost/src/PDEFoam.cxx
+++ ExtendedMethodBoost/src/PDEFoam.cxx
@@ -615,7 +615,7 @@ Float_t TMVA::PDEFoam::GetSeparation(Float_t s, Float_t b)
    case kMisClassificationError: // 1 - max(p,1-p)
       return 1 - TMath::Max(p, 1-p);
    case kCrossEntropy: // -p*log(p) - (1-p)*log(1-p)
-      return -p*TMath::Log(p) - (1-p)*TMath::Log(1-p);
+      return (p<=0 || p >=1 ? 0 : -p*TMath::Log(p) - (1-p)*TMath::Log(1-p));
    default:
       Log() << kFATAL << "Unknown separation type" << Endl;
       break;
