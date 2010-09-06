3da244f9603fb2fc082a94a4abf9711470aec55c
moneta <moneta@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-09-06 07:26:25 +0000

    add new efficiency class from Christian Gumpert
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/mathDev@35149 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ hist/hist/inc/TEfficiency.h
@@ -0,0 +1,156 @@
+#ifndef ROOT_TEfficiency
+#define ROOT_TEfficiency
+
+//standard header
+#include <vector>
+
+//ROOT header
+#ifndef ROOT_TAttLine
+#include "TAttLine.h"
+#endif
+
+#ifndef ROOT_TAttFill
+#include "TAttFill.h"
+#endif
+
+#ifndef ROOT_TAttMarker
+#include "TAttMarker.h"
+#endif
+
+class TCollection;
+class TF1;
+class TGraphAsymmErrors;
+class TH1;
+class TList;
+
+//|TEfficiency
+//------------------------
+
+class TEfficiency: public TNamed, public TAttLine, public TAttFill, public TAttMarker
+{
+public:
+   //function pointer to methods calculating confidence intervals
+   #ifdef __CINT__
+      typedef void* pBoundFunc;
+   #else
+      typedef Double_t(*pBoundFunc)(Int_t,Int_t,double);
+   #endif
+   
+      //enumaration type for different statistic options for calculating confidence intervals
+      //kF* ... frequentist methods; kB* ... bayesian methods      
+      enum {
+	 kFCP,                             //Clopper-Pearson interval (recommended by PDG)
+	 kFNormal,                         //normal approximation
+	 kFWilson,                         //Wilson interval
+	 kFAC,                             //Agresti-Coull interval
+	 kBJeffrey,                        //Jeffrey interval (Prior ~ Beta(0.5,0.5)
+	 kBUniform,                        //Prior ~ Uniform = Beta(1,1)
+	 kBBayesian                        //user specified Prior ~ Beta(fBeta_alpha,fBeta_beta)
+      };
+
+protected:
+
+      Double_t      fBeta_alpha;             //parameter for prior beta distribution (default = 1)
+      Double_t      fBeta_beta;              //parameter for prior beta distribution (default = 1)
+      Double_t      fConfLevel;              //confidence level (default = 0.95)
+      TDirectory*   fDirectory;              //!pointer to directory holding this TEfficiency object
+      TList*        fFunctions;              //pointer to list of functions
+      pBoundFunc    fLowerBound;             //!pointer to a method calculating the lower bound
+      TGraphAsymmErrors* fPaintGraph;        //!temporary graph for painting
+      TH1*          fPaintHisto;             //!temporary histogram for painting      
+      TH1*          fPassedHistogram;        //histogram for events which passed certain criteria
+      Int_t         fStatisticOption;        //defines how the confidence intervals are determined
+      TH1*          fTotalHistogram;         //histogram for total number of events
+      pBoundFunc    fUpperBound;             //!pointer to a method calculating the upper bound
+      Double_t      fWeight;                 //weight for all events (default = 1)
+
+      enum{
+	 kIsBayesian = BIT(14)               //bayesian statistics are used
+      };
+
+      void          Build(const char* name,const char* title);      
+      static Bool_t CheckBinning(const TH1& pass,const TH1& total);
+      static Bool_t CheckConsistency(const TH1& pass,const TH1& total);
+      static Bool_t CheckEntries(const TH1& pass,const TH1& total);
+      
+public:
+      TEfficiency();   
+      TEfficiency(const TH1& passed,const TH1& total);
+      TEfficiency(const char* name,const char* title,Int_t nbins,
+		   const Double_t* xbins);
+      TEfficiency(const char* name,const char* title,Int_t nbins,Double_t xlow,
+		   Double_t xup);
+      TEfficiency(const char* name,const char* title,Int_t nbinsx,
+		   Double_t xlow,Double_t xup,Int_t nbinsy,Double_t ylow,
+		   Double_t yup);
+      TEfficiency(const char* name,const char* title,Int_t nbinsx,
+		   const Double_t* xbins,Int_t nbinsy,const Double_t* ybins);
+      TEfficiency(const char* name,const char* title,Int_t nbinsx,
+		   Double_t xlow,Double_t xup,Int_t nbinsy,Double_t ylow,
+		   Double_t yup,Int_t nbinsz,Double_t zlow,Double_t zup);
+      TEfficiency(const char* name,const char* title,Int_t nbinsx,
+		   const Double_t* xbins,Int_t nbinsy,const Double_t* ybins,
+		   Int_t nbinsz,const Double_t* zbins);      
+      TEfficiency(const TEfficiency& heff);
+      ~TEfficiency();
+      
+      void          Add(const TEfficiency& rEff) {*this += rEff;}
+      void          Draw(const Option_t* opt="");
+      void          Fill(Bool_t bPassed,Double_t x,Double_t y=0,Double_t z=0);
+      Int_t         FindFixBin(Double_t x,Double_t y=0,Double_t z=0) const;
+      Int_t         Fit(TF1* f1,Option_t* opt="");
+      Double_t      GetBetaAlpha() const {return fBeta_alpha;}
+      Double_t      GetBetaBeta() const {return fBeta_beta;}
+      Double_t      GetConfidenceLevel() const {return fConfLevel;}
+      TH1*          GetCopyPassedHisto() const;
+      TH1*          GetCopyTotalHisto() const;
+      Int_t         GetDimension() const;
+      TDirectory*   GetDirectory() const {return fDirectory;}
+      Double_t      GetEfficiency(Int_t bin) const;
+      Double_t      GetEfficiencyErrorLow(Int_t bin) const;
+      Double_t      GetEfficiencyErrorUp(Int_t bin) const;
+      Int_t         GetGlobalBin(Int_t binx,Int_t biny=0,Int_t binz=0) const;
+      TList*        GetListOfFunctions() const {return fFunctions;}
+      Int_t         GetPassedEvents(Int_t bin) const;
+      Int_t         GetStatisticOption() const {return fStatisticOption;}
+      Int_t         GetTotalEvents(Int_t bin) const;
+      Double_t      GetWeight() const {return fWeight;}
+      void          Merge(TCollection* list);      
+      TEfficiency& operator+=(const TEfficiency& rhs);
+      TEfficiency& operator=(const TEfficiency& rhs);
+      void          Paint(const Option_t* opt);
+      void          SetBetaAlpha(Double_t alpha);
+      void          SetBetaBeta(Double_t beta);      
+      void          SetConfidenceLevel(Double_t level);
+      void          SetDirectory(TDirectory* dir);
+      void          SetName(const char* name);
+      void          SetPassedEvents(Int_t bin,Int_t events);
+      Bool_t        SetPassedHistogram(const TH1& rPassed,Option_t* opt);
+      void          SetStatisticOption(Int_t option);
+      void          SetTitle(const char* title);
+      void          SetTotalEvents(Int_t bin,Int_t events);
+      Bool_t        SetTotalHistogram(const TH1& rTotal,Option_t* opt);
+      void          SetWeight(Double_t weight);    
+      Bool_t        UsesBayesianStat() const {return TestBit(kIsBayesian);}
+      
+      static TGraphAsymmErrors* Combine(TCollection* pList,Option_t* opt="",
+					Int_t n=0,Double_t* p=0);
+      
+      //calculating boundaries of confidence intervals
+      static Double_t NormalUp(Int_t total,Int_t passed,Double_t level);
+      static Double_t NormalLow(Int_t total,Int_t passed,Double_t level);
+      static Double_t WilsonUp(Int_t total,Int_t passed,Double_t level);
+      static Double_t WilsonLow(Int_t total,Int_t passed,Double_t level);
+      static Double_t AgrestiCoullUp(Int_t total,Int_t passed,Double_t level);
+      static Double_t AgrestiCoullLow(Int_t total,Int_t passed,Double_t level);
+      static Double_t ClopperPearsonUp(Int_t total,Int_t passed,Double_t level);
+      static Double_t ClopperPearsonLow(Int_t total,Int_t passed,Double_t level);
+      static Double_t BayesianUp(Int_t total,Int_t passed,Double_t level,Double_t alpha=1,Double_t beta=1);
+      static Double_t BayesianLow(Int_t total,Int_t passed,Double_t level,Double_t alpha=1,Double_t beta=1);
+      
+      ClassDef(TEfficiency,1)     //calculating efficiencies
+};
+
+const TEfficiency operator+(const TEfficiency& lhs,const TEfficiency& rhs);
+
+#endif
