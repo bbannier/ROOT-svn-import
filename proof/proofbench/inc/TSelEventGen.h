41fb6da33cbfbe344709b2eebf7ab972a7f791d6
saryu <saryu@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-09-13 20:20:36 +0000

    Cleaning up unused data members.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/proofbench@35258 27541ba8-7e3a-0410-8455-c3a389f83636

--- proof/proofbench/inc/TSelEventGen.h
+++ proof/proofbench/inc/TSelEventGen.h
@@ -26,7 +26,6 @@
 #ifndef ROOT_TSelector
 #include <TSelector.h>
 #endif
-#include <test/Event.h>
 #ifndef ROOT_TTree
 #include <TTree.h>
 #endif
@@ -37,9 +36,7 @@
 #include <TProofBenchMode.h>
 #endif
 
-class TH1F;
-class TH1I;
-class TSortedList;
+class Event;
 
 R__EXTERN TProofServ *gProofServ;
 
@@ -66,53 +63,6 @@ protected:
 public :
 
    TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-
-   // Declaration of leave types
-   Event           *event;
-   Char_t          fType[20];
-   Char_t          *fEventName;
-   Int_t           fNtrack;
-   Int_t           fNseg;
-   Int_t           fNvertex;
-   UInt_t          fFlag;
-   Double32_t      fTemperature;
-   Int_t           fMeasures[10];
-   Double32_t      fMatrix[4][4];
-   Double32_t      fClosestDistance[21];   //[fNvertex]
-   EventHeader     fEvtHdr;
-   TClonesArray    *fTracks;
-   TRefArray       *fHighPt;
-   TRefArray       *fMuons;
-   TRef            fLastTrack;
-   TRef            fWebHistogram;
-   TH1F            *fH;
-   TBits           fTriggerBits;
-   Bool_t          fIsValid;
-
-   //Output hist
-   TH1F* fPtHist;
-   TH1I* fNTracksHist;
-
-   // List of branches
-   TBranch        *b_event_fType;   //!
-   TBranch        *b_fEventName;   //!
-   TBranch        *b_event_fNtrack;   //!
-   TBranch        *b_event_fNseg;   //!
-   TBranch        *b_event_fNvertex;   //!
-   TBranch        *b_event_fFlag;   //!
-   TBranch        *b_event_fTemperature;   //!
-   TBranch        *b_event_fMeasures;   //!
-   TBranch        *b_event_fMatrix;   //!
-   TBranch        *b_fClosestDistance;   //!
-   TBranch        *b_event_fEvtHdr;   //!
-   TBranch        *b_fTracks;   //!
-   TBranch        *b_fHighPt;   //!
-   TBranch        *b_fMuons;   //!
-   TBranch        *b_event_fLastTrack;   //!
-   TBranch        *b_event_fWebHistogram;   //!
-   TBranch        *b_fH;   //!
-   TBranch        *b_event_fTriggerBits;   //!
-   TBranch        *b_event_fIsValid;   //!
 
    TSelEventGen(TTree *);
    TSelEventGen();
@@ -146,36 +96,9 @@ void TSelEventGen::Init(TTree *tree)
    // generated code, but the routine can be extended by the user if needed.
    // Init() will be called many times when running with PROOF.
 
-   // Set branch addresses
-   fEventName=0;
-   fTracks=0;
-   fHighPt=0;
-   fMuons=0;
-   fH=0;
-
    if (tree == 0) return;
    fChain = tree;
    fChain->SetMakeClass(1);
-
-   fChain->SetBranchAddress("fType[20]",fType);
-   fChain->SetBranchAddress("fEventName",fEventName);
-   fChain->SetBranchAddress("fNtrack",&fNtrack);
-   fChain->SetBranchAddress("fNseg",&fNseg);
-   fChain->SetBranchAddress("fNvertex",&fNvertex);
-   fChain->SetBranchAddress("fFlag",&fFlag);
-   fChain->SetBranchAddress("fTemperature",&fTemperature);
-   fChain->SetBranchAddress("fMeasures[10]",fMeasures);
-   fChain->SetBranchAddress("fMatrix[4][4]",fMatrix);
-   fChain->SetBranchAddress("fClosestDistance",fClosestDistance);
-   fChain->SetBranchAddress("fEvtHdr",&fEvtHdr);
-   fChain->SetBranchAddress("fTracks",&fTracks);
-   fChain->SetBranchAddress("fHighPt",&fHighPt);
-   fChain->SetBranchAddress("fMuons",&fMuons);
-   fChain->SetBranchAddress("fLastTrack",&fLastTrack);
-   fChain->SetBranchAddress("fWebHistogram",&fWebHistogram);
-   fChain->SetBranchAddress("fH",&fH);
-   fChain->SetBranchAddress("fTriggerBits",&fTriggerBits);
-   fChain->SetBranchAddress("fIsValid",&fIsValid);
 }
 
 Bool_t TSelEventGen::Notify()
@@ -186,27 +109,6 @@ Bool_t TSelEventGen::Notify()
    // will be retrieved. It is normaly not necessary to make changes
    // to the generated code, but the routine can be extended by the
    // user if needed.
-
-   // Get branch pointers
-   b_event_fType = fChain->GetBranch("fType[20]");
-   b_fEventName = fChain->GetBranch("fEventName");
-   b_event_fNtrack = fChain->GetBranch("fNtrack");
-   b_event_fNseg = fChain->GetBranch("fNseg");
-   b_event_fNvertex = fChain->GetBranch("fNvertex");
-   b_event_fFlag = fChain->GetBranch("fFlag");
-   b_event_fTemperature = fChain->GetBranch("fTemperature");
-   b_event_fMeasures = fChain->GetBranch("fMeasures[10]");
-   b_event_fMatrix = fChain->GetBranch("fMatrix[4][4]");
-   b_fClosestDistance = fChain->GetBranch("fClosestDistance");
-   b_event_fEvtHdr = fChain->GetBranch("fEvtHdr");
-   b_fTracks = fChain->GetBranch("fTracks");
-   b_fHighPt = fChain->GetBranch("fHighPt");
-   b_fMuons = fChain->GetBranch("fMuons");
-   b_event_fLastTrack = fChain->GetBranch("fLastTrack");
-   b_event_fWebHistogram = fChain->GetBranch("fWebHistogram");
-   b_fH = fChain->GetBranch("fH");
-   b_event_fTriggerBits = fChain->GetBranch("fTriggerBits");
-   b_event_fIsValid = fChain->GetBranch("fIsValid");
 
    return kTRUE;
 }
