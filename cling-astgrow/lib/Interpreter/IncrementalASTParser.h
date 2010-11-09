df045db1467739cd5126558963b8b718d51f7ead
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-09 00:19:51 +0000

    Major surgery: move the whole growing AST (memory buffer, AST context and incremental parser) into IncrementalASTParser. Remove the InputValidator: this will soon be done differently, using a #pragma end of input and checking the size of the parser node stack. Next: use the IncrementalASTVisitor in the decl vs statement analysis.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/axel@36549 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cling-astgrow/lib/Interpreter/IncrementalASTParser.h
@@ -0,0 +1,43 @@
+//------------------------------------------------------------------------------
+// CLING - the C++ LLVM-based InterpreterG :)
+// version: $Id$
+// author:  Axel Naumann <axel@cern.ch>
+//------------------------------------------------------------------------------
+
+#ifndef CLING_INCREMENTAL_AST_PARSER_H
+#define CLING_INCREMENTAL_AST_PARSER_H
+
+#include "llvm/ADT/OwningPtr.h"
+#include "llvm/ADT/StringRef.h"
+
+namespace clang {
+  class CompilerInstance;
+  class Parser;
+  class Sema;
+  class ASTConsumer;
+}
+
+namespace cling {
+  class MutableMemoryBuffer;
+
+  class IncrementalASTParser {
+  public:
+    IncrementalASTParser(clang::CompilerInstance* CI,
+                         clang::ASTConsumer* Consumer);
+    ~IncrementalASTParser();
+    
+    clang::CompilerInstance* getCI() const { return m_CI.get(); }
+    clang::CompilerInstance* parse(llvm::StringRef src,
+                                   int nTopLevelDecls = 1,
+                                   clang::ASTConsumer* Consumer = 0);
+    
+  private:
+    llvm::OwningPtr<clang::CompilerInstance> m_CI; // compiler instance.
+    llvm::OwningPtr<clang::Parser> m_Parser; // parser (incremental)
+    llvm::OwningPtr<clang::Sema> m_Sema; // sema used for parsing
+    llvm::OwningPtr<MutableMemoryBuffer> m_MemoryBuffer; // compiler instance.
+    clang::ASTConsumer* m_Consumer;
+    bool m_InterruptAtNextTopLevelDecl; // whether to stop parsing at the next top level decl 
+  };
+}
+#endif // CLING_INCREMENTAL_AST_PARSER_H
