1bddc27a5cdd7eb23fc5093afb458638e5a711ae
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-09 00:19:51 +0000

    Major surgery: move the whole growing AST (memory buffer, AST context and incremental parser) into IncrementalASTParser. Remove the InputValidator: this will soon be done differently, using a #pragma end of input and checking the size of the parser node stack. Next: use the IncrementalASTVisitor in the decl vs statement analysis.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@36549 27541ba8-7e3a-0410-8455-c3a389f83636

--- axel/cling-astgrow/include/cling/Interpreter/Interpreter.h
+++ axel/cling-astgrow/include/cling/Interpreter/Interpreter.h
@@ -4,23 +4,13 @@
 // author:  Lukasz Janyst <ljanyst@cern.ch>
 //------------------------------------------------------------------------------
 
-#ifndef CLING_COMPILER_H
-#define CLING_COMPILER_H
+#ifndef CLING_INTERPRETER_H
+#define CLING_INTERPRETER_H
 
-#include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ADT/OwningPtr.h"
 
 #include <string>
 #include <vector>
-
-//------------------------------------------------------------------------------
-// Forward declarations from LLVM
-//------------------------------------------------------------------------------
-namespace llvm {
-class Module;
-class LLVMContext;
-class ExecutionEngine;
-}
 
 //------------------------------------------------------------------------------
 // Forward declarations from CLANG
@@ -28,16 +18,13 @@ class ExecutionEngine;
 namespace clang {
 class ASTConsumer;
 class CompilerInstance;
-class FunctionDecl;
-class Preprocessor;
-class Token;
-class Stmt;
 }
 
 namespace cling {
 
 class CIBuilder;
 class ExecutionContext;
+class IncrementalASTParser;
 
 //---------------------------------------------------------------------------
 //! Class for managing many translation units supporting automatic
@@ -71,15 +58,15 @@ public:
       return prev;
    }
 
-   clang::CompilerInstance* getCI() const { return m_CI.get(); }
-   clang::CompilerInstance* createCI();
+   clang::CompilerInstance* getCI() const;
+   clang::CompilerInstance* createCI() const;
    CIBuilder& getCIBuilder() const { return *m_CIBuilder.get(); }
 
 private:
    llvm::OwningPtr<cling::CIBuilder> m_CIBuilder; // our compiler intsance builder
-   llvm::OwningPtr<clang::CompilerInstance> m_CI; // compiler instance.
    llvm::OwningPtr<ExecutionContext> m_ExecutionContext; // compiler instance.
-   unsigned long long m_UniqueCounter; // number of generated call wrappers
+  llvm::OwningPtr<IncrementalASTParser> m_IncrASTParser; // incremental AST and its parser
+  unsigned long long m_UniqueCounter; // number of generated call wrappers
    bool m_printAST; // whether to print the AST to be processed
 
 private:
@@ -89,16 +76,14 @@ private:
 
    std::string createUniqueName();
 
-   clang::CompilerInstance* createStatementList(const std::string& srcCode,
-         std::vector<clang::Stmt*>& stmts);
-
    clang::ASTConsumer* maybeGenerateASTPrinter() const;
    clang::CompilerInstance* compileString(const std::string& srcCode);
    clang::CompilerInstance* compileFile(const std::string& filename,
                                         const std::string* trailcode = 0);
+  
 
 };
 
 } // namespace cling
 
-#endif // CLING_COMPILER_H
+#endif // CLING_INTERPRETER_H
