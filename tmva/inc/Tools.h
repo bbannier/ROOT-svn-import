ec2c1ad0a1e0693d1d12cfcbda953cb811817621
speckmay <speckmay@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-09 02:31:39 +0000

    add writing out of citation at the end of the evaluation of the methods
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@36551 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/inc/Tools.h
+++ tmva/inc/Tools.h
@@ -200,7 +200,8 @@ namespace TMVA {
 
       // print TMVA citation (to be called from, eg, .TMVAlogon)
       enum ECitation { kPlainText = 1,
-		       kBibTeX };
+		       kBibTeX,
+		       kLaTeX };
 
       void TMVAWelcomeMessage();
       void TMVAWelcomeMessage( MsgLogger& logger, EWelcomeMessage m = kStandardWelcomeMsg );
