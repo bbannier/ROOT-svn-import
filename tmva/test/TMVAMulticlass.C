b9c2d7a988cd729a0a3d653299b4a779bb8906d5
therhaag <therhaag@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-16 10:20:14 +0000

    improve multiclass example
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@36678 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/test/TMVAMulticlass.C
+++ tmva/test/TMVAMulticlass.C
@@ -27,38 +27,46 @@ void TMVAMulticlass(){
    TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
    TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
                                                "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=multiclass" );
-   factory->AddVariable( "var0", 'F' );
    factory->AddVariable( "var1", 'F' );
+   factory->AddVariable( "var2", "Variable 2", "", 'F' );
+   factory->AddVariable( "var3", "Variable 3", "units", 'F' );
+   factory->AddVariable( "var4", "Variable 4", "units", 'F' );
    TFile *input(0);
-   TString fname = "./data.root";
+   TString fname = "./tmva_example_multiple_background.root";
    if (!gSystem->AccessPathName( fname )) {
-      // first we try to find data.root in the local directory
+      // first we try to find the file in the local directory
       std::cout << "--- TMVAMulticlass   : Accessing " << fname << std::endl;
       input = TFile::Open( fname );
    }
    else {
-      gROOT->LoadMacro( "./createData.C");
-      create_multiclassdata(20000);
-      cout << " created data.root for tests of the multiclass features"<<endl;
+      cout << "Creating testdata...." << std::endl;
+      gROOT->ProcessLine(".L createData.C+");
+      gROOT->ProcessLine("create_MultipleBackground(2000)");
+      cout << " created tmva_example_multiple_background.root for tests of the multiclass features"<<endl;
       input = TFile::Open( fname );
    }
    if (!input) {
       std::cout << "ERROR: could not open data file" << std::endl;
       exit(1);
    }
-   TTree *tree     = (TTree*)input->Get("TreeR");
+   //TTree *tree     = (TTree*)input->Get("TreeR");
+   TTree *signal      = (TTree*)input->Get("TreeS");
+   TTree *background0 = (TTree*)input->Get("TreeB0");
+   TTree *background1 = (TTree*)input->Get("TreeB1");
+   TTree *background2 = (TTree*)input->Get("TreeB2");
    
    gROOT->cd( outfileName+TString(":/") );
-   factory->AddTree    ( tree, "Signal1",    1. , "cls==0"   );
-   factory->AddTree    ( tree, "Signal2",    1. , "cls==1"   );
-   factory->AddTree    ( tree, "Background",    1., "cls==2" );
+   factory->AddTree    (signal,"Signal");
+   factory->AddTree    (background0,"bg0");
+   factory->AddTree    (background1,"bg1");
+   factory->AddTree    (background2,"bg2");
+   
    factory->PrepareTrainingAndTestTree( "", "SplitMode=Random:NormMode=NumEvents:!V" );
 
-   //factory->BookMethod( TMVA::Types::kBDT, "BDT", "!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.30:UseBaggedGrad:GradBaggingFraction=0.6:SeparationType=GiniIndex:nCuts=20:NNodesMax=5");
-   factory->BookMethod( TMVA::Types::kMLP, "MLP", "!H:!V:NeuronType=tanh:VarTransform=N:NCycles=100:HiddenLayers=N+5,3:TestRate=5"); // testing vartransforms
-   //factory->BookMethod( TMVA::Types::kMLP, "MLP2", "!H:!V:NeuronType=tanh:NCycles=100:HiddenLayers=N+5,3:TestRate=5");
-   //factory->BookMethod( TMVA::Types::kFDA, "FDA_GA",
-   //                     "H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x0*x1:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1" );
+   factory->BookMethod( TMVA::Types::kBDT, "BDTG", "!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.30:UseBaggedGrad:GradBaggingFraction=0.50:SeparationType=GiniIndex:nCuts=20:NNodesMax=5");
+   factory->BookMethod( TMVA::Types::kMLP, "MLP2", "!H:!V:NeuronType=tanh:NCycles=100:HiddenLayers=N+5,3:TestRate=5");
+   factory->BookMethod( TMVA::Types::kFDA, "FDA_GA",
+                        "H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1" );
    
   // Train MVAs using the set of training events
    factory->TrainAllMethods();
