51c5390865c7e660b665e868bdba816e678aab65
therhaag <therhaag@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-16 10:40:35 +0000

    improve multiclass example
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@36679 27541ba8-7e3a-0410-8455-c3a389f83636

--- tmva/test/TMVAMulticlass.C
+++ tmva/test/TMVAMulticlass.C
@@ -1,3 +1,11 @@
+/**********************************************************************************
+ * Project   : TMVA - a Root-integrated toolkit for multivariate data analysis    *
+ * Package   : TMVA                                                               *
+ * Root Macro: TMVAMulticlass                                                     *
+ *                                                                                *
+ * This macro provides a simple example for the training and testing of the TMVA  *
+ * multiclass classification                                                      *
+ **********************************************************************************/
 
 #include <cstdlib>
 #include <iostream>
@@ -22,15 +30,48 @@
 
 using namespace TMVA;
 
-void TMVAMulticlass(){
+void TMVAMulticlass( TString myMethodList = "" )
+{
+
+   //---------------------------------------------------------------
+   // default MVA methods to be trained + tested
+   std::map<std::string,int> Use;
+   Use["MLP"]             = 1;
+   Use["BDTG"]            = 1;
+   Use["FDA_GA"]          = 0;
+   //---------------------------------------------------------------
+
+   std::cout << std::endl;
+   std::cout << "==> Start TMVAMulticlass" << std::endl;
+
+   if (myMethodList != "") {
+      for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) it->second = 0;
+
+      std::vector<TString> mlist = TMVA::gTools().SplitString( myMethodList, ',' );
+      for (UInt_t i=0; i<mlist.size(); i++) {
+         std::string regMethod(mlist[i]);
+
+         if (Use.find(regMethod) == Use.end()) {
+            std::cout << "Method \"" << regMethod << "\" not known in TMVA under this name. Choose among the following:" << std::endl;
+            for (std::map<std::string,int>::iterator it = Use.begin(); it != Use.end(); it++) std::cout << it->first << " ";
+            std::cout << std::endl;
+            return;
+         }
+         Use[regMethod] = 1;
+      }
+   }
+
+   // Create a new root output file.
    TString outfileName = "TMVAMulticlass.root";
    TFile* outputFile = TFile::Open( outfileName, "RECREATE" );
+   
    TMVA::Factory *factory = new TMVA::Factory( "TMVAClassification", outputFile,
                                                "!V:!Silent:Color:DrawProgressBar:Transformations=I;D;P;G,D:AnalysisType=multiclass" );
    factory->AddVariable( "var1", 'F' );
    factory->AddVariable( "var2", "Variable 2", "", 'F' );
    factory->AddVariable( "var3", "Variable 3", "units", 'F' );
    factory->AddVariable( "var4", "Variable 4", "units", 'F' );
+
    TFile *input(0);
    TString fname = "./tmva_example_multiple_background.root";
    if (!gSystem->AccessPathName( fname )) {
@@ -49,7 +90,7 @@ void TMVAMulticlass(){
       std::cout << "ERROR: could not open data file" << std::endl;
       exit(1);
    }
-   //TTree *tree     = (TTree*)input->Get("TreeR");
+
    TTree *signal      = (TTree*)input->Get("TreeS");
    TTree *background0 = (TTree*)input->Get("TreeB0");
    TTree *background1 = (TTree*)input->Get("TreeB1");
@@ -63,10 +104,15 @@ void TMVAMulticlass(){
    
    factory->PrepareTrainingAndTestTree( "", "SplitMode=Random:NormMode=NumEvents:!V" );
 
-   factory->BookMethod( TMVA::Types::kBDT, "BDTG", "!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.30:UseBaggedGrad:GradBaggingFraction=0.50:SeparationType=GiniIndex:nCuts=20:NNodesMax=5");
-   factory->BookMethod( TMVA::Types::kMLP, "MLP2", "!H:!V:NeuronType=tanh:NCycles=100:HiddenLayers=N+5,3:TestRate=5");
-   factory->BookMethod( TMVA::Types::kFDA, "FDA_GA",
-                        "H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1" );
+   if (Use["BDTG"]) // gradient boosted decision trees
+      factory->BookMethod( TMVA::Types::kBDT, "BDTG", "!H:!V:NTrees=1000:BoostType=Grad:Shrinkage=0.30:UseBaggedGrad:GradBaggingFraction=0.50:SeparationType=GiniIndex:nCuts=20:NNodesMax=5");
+   
+   if (Use["MLP"]) // neural network
+      factory->BookMethod( TMVA::Types::kMLP, "MLP2", "!H:!V:NeuronType=tanh:NCycles=100:HiddenLayers=N+5,3:TestRate=5");
+   
+   if (Use["FDA_GA"]) // functional discriminant with GA minimizer
+      factory->BookMethod( TMVA::Types::kFDA, "FDA_GA",
+                           "H:!V:Formula=(0)+(1)*x0+(2)*x1+(3)*x2+(4)*x3:ParRanges=(-1,1);(-10,10);(-10,10);(-10,10);(-10,10):FitMethod=GA:PopSize=300:Cycles=3:Steps=20:Trim=True:SaveBestGen=1" );
    
   // Train MVAs using the set of training events
    factory->TrainAllMethods();
