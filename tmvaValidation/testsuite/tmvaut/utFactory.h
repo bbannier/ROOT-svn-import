d05cdda9ffdc7da808ca5c38442666a50005085c
evt <evt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-28 10:58:39 +0000

    adding new tmva/factory unit test
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@37031 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tmvaValidation/testsuite/tmvaut/utFactory.h
@@ -0,0 +1,40 @@
+#ifndef UTFACTORY_H
+#define UTFACTORY_H
+
+// Author: E. v. Toerne,  Nov 2011, implementing unit tests by C. Rosemann
+// TMVA unit tests
+//
+// this class acts as interface to several factory applications
+
+#include <string>
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+#include "TTree.h"
+#include "TString.h"
+
+#include "TMVA/Factory.h"
+#include "TMVA/Types.h"
+
+#include "UnitTest.h"
+
+namespace UnitTesting
+{
+   class utFactory : public UnitTest
+   {
+   public:
+      utFactory(const char* theOption="");
+      virtual ~utFactory();
+      virtual void run();
+      
+   protected:
+      virtual TTree* create_Tree(const char* opt="");
+      virtual void operateSingleFactory(const char* opt="");
+   private:
+      // disallow copy constructor and assignment
+      utFactory(const utFactory&);
+      utFactory& operator=(const utFactory&);
+   };
+} // namespace UnitTesting
+#endif // 
