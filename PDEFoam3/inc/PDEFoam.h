e55ed44d06b8318bbc9b295772b6c0eed91fb180
avoigt <avoigt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-29 20:32:44 +0000

    Naively implement kernel in PDEFoam projection functions Project2() and Draw1Dim().  This implementatin works fine with 1- and 2-dimensional foams (reproduces the behaviour of the old messy PDEFoam implementation).  However, for higher dimensions (>2) the projection algorithms simply sums the (kernel estimated) cell values over the other dimensions.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tmvaValidation@37080 27541ba8-7e3a-0410-8455-c3a389f83636

--- PDEFoam3/inc/PDEFoam.h
+++ PDEFoam3/inc/PDEFoam.h
@@ -283,11 +283,11 @@ namespace TMVA {
       // ---------- Foam projection methods
 
       // project foam to two-dimensional histogram
-      TH2D* Project2(Int_t idim1, Int_t idim2, const char *opt="cell_value", 
+      TH2D* Project2(Int_t idim1, Int_t idim2, ECellValue cell_value=kValue, 
                      PDEFoamKernel *kernel=NULL, UInt_t maxbins=50);
 
       // Project one-dimensional foam to a 1-dim histogram
-      TH1D* Draw1Dim(const char *opt, Int_t nbin);
+      TH1D* Draw1Dim(ECellValue cell_value, Int_t nbin, PDEFoamKernel *kernel=NULL);
 
       // Generates C++ code (root macro) for drawing foam with boxes (only 2-dim!)
       void RootPlot2dim( const TString& filename, TString opt,
