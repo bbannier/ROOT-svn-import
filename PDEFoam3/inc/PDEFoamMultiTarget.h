23bff667ba3e1e809b30d3b38483984af85ef07a
avoigt <avoigt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-11-30 20:35:04 +0000

    Override GetCellValue(map, ...) in PDEFoamMultiTarget to return the targets given an incomplete event vector.  Now no casting is needed in MethodPDEFoam, since it is sufficient to use the PDEFoam interface.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tmvaValidation@37123 27541ba8-7e3a-0410-8455-c3a389f83636

--- PDEFoam3/inc/PDEFoamMultiTarget.h
+++ PDEFoam3/inc/PDEFoamMultiTarget.h
@@ -8,9 +8,9 @@
  * Description:                                                                   *
  *      Concrete PDEFoamEvent sub-class.  This foam stores the number             *
  *      of events with every cell, as well as the statistical error on            *
- *      the event number.  In addition to PDEFoamEvent it adds a                  *
- *      public function for projecting the target values given an                 *
- *      event with N_variables < dimension of foam                                *
+ *      the event number.  It overrides GetCellValue() for projecting             *
+ *      the target values given an incomplete event map with                      *
+ *      N_variables < dimension of foam.                                          *
  *                                                                                *
  * Authors (alphabetical):                                                        *
  *      S. Jadach        - Institute of Nuclear Physics, Cracow, Poland           *
@@ -18,7 +18,7 @@
  *      Dominik Dannheim - CERN, Switzerland                                      *
  *      Alexander Voigt  - CERN, Switzerland                                      *
  *                                                                                *
- * Copyright (c) 2008:                                                            *
+ * Copyright (c) 2010:                                                            *
  *      CERN, Switzerland                                                         *
  *      MPI-K Heidelberg, Germany                                                 *
  *                                                                                *
@@ -39,17 +39,19 @@ namespace TMVA {
    class PDEFoamMultiTarget : public PDEFoamEvent {
 
    protected:
+      ETargetSelection fTargetSelection; // the target selection method
       
       PDEFoamMultiTarget(const PDEFoamMultiTarget&); // Copy Constructor  NOT USED
 
       // ---------- Public functions ----------------------------------
    public:
       PDEFoamMultiTarget();                  // Default constructor (used only by ROOT streamer)
-      PDEFoamMultiTarget(const TString&);    // Principal user-defined constructor
+      PDEFoamMultiTarget(const TString&, ETargetSelection); // Principal user-defined constructor
       virtual ~PDEFoamMultiTarget(){};       // Default destructor
 
-      // extract the targets from the foam
-      std::vector<Float_t> GetTargets( std::vector<Float_t> &vals, ETargetSelection ts );
+      // overridden from PDEFoam: extract the targets from the foam
+      virtual std::vector<Float_t> GetCellValue( std::map<Int_t,Float_t>&, ECellValue );
+      using PDEFoam::GetCellValue;
 
       // ---------- ROOT class definition
       ClassDef(PDEFoamMultiTarget,1) // Tree of PDEFoamCells
