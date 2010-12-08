72fe70094e1bbe53e5e19ee60f87eaa0d8e10d2a
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-12-08 10:27:18 +0000

    Add printer of typed values that can be re-implemented (e.g. by users) for any type to be printed. It will be used by prompt to print the value of the last expression. (Replacement of CINT G__ateval)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@37386 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cling/include/cling/Interpreter/ValuePrinter.h
@@ -0,0 +1,92 @@
+//--------------------------------------------------------------------*- C++ -*-
+// CLING - the C++ LLVM-based InterpreterG :)
+// version: $Id$
+// author:  Axel Naumann <axel@cern.ch>
+//------------------------------------------------------------------------------
+
+#ifndef CLING_VALUEPRINTER_H
+#define CLING_VALUEPRINTER_H
+
+
+#include <string>
+
+namespace clang {
+  class raw_ostream
+}
+
+namespace cling {
+  
+  // Can be re-implemented to print type-specific details, e.g. as
+  //   template <typename ACTUAL>
+  //   void dumpPtr(clang::raw_ostream& o, const clang::Decl* a, ACTUAL* ac,
+  //                int flags, const char* tname);
+  template <typename TY>
+  void printValue(clang::raw_ostream& o, const void* const t, TY* u, int flags,
+                  const char* tname) {
+    if (flags & kIsConst) {
+      o << "const ";
+    }
+    o << tname;
+    if (!(flags & kIsObj)) {
+      o << "*";
+    }
+    o << " @ " << t << '\n';
+  }
+  
+  class ValuePrinter {
+  public:
+    
+    enum DumperFlags {
+      kIsObj = 1,
+      kIsConst = 2
+    };
+
+  protected:
+    template <typename TY>
+    struct TypedPrinter {
+      void operator()(clang::raw_ostream& o, const TY& t) {
+        std::string n;
+        printValue(o, &t, &t, kIsObj | kIsConst, getTypeName(&t, n));
+      }
+      void operator()(clang::raw_ostream& o, TY& t) {
+        std::string n;
+        printValue(o, &t, &t, kIsObj, getTypeName(&t, n));
+      }
+    };
+    
+    template <typename TY>
+    struct TypedPrinter<TY*> {
+      void operator()(clang::raw_ostream& o, TY* t) {
+        std::string n;
+        printValue(o, t, t, 0, getTypeName(t, n));
+      }
+    };
+
+  public:
+    template <typename T>
+    void print(clang::raw_ostream& o, const T& t) {
+      // give temporaries a defined lifetime
+      TypedPrinter<T> d; d(o, t);
+    }
+    
+    template <typename T>
+    void print(clang::raw_ostream& o, T& t) {
+      // give temporaries a defined lifetime
+      TypedPrinter<T> d; d(o, t);
+    }
+    
+    template <typename T>
+    const char* getTypeName(T*,std::string& n) {
+      // Get the type name. This function is always compiled with clang,
+      // thus the layout of its name is fixed.
+      n = __PRETTY_FUNCTION__;
+      n.erase(0,24);
+      n.erase(n.length() - 17, std::string::npos);
+      size_t l = n.length();
+      if (n[l - 1] == ' ') n.erase(l - 1);
+      return n.c_str();
+    }
+  };
+}
+
+#endif // CLING_VALUEPRINTER_H
