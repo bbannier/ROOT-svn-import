da39c32b8030f1146c025a7f9d938ff2655fac74
avoigt <avoigt@27541ba8-7e3a-0410-8455-c3a389f83636>
2010-12-09 19:14:41 +0000

    Add macro, which plots the tree structure of PDEFoam
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tmvaValidation@37446 27541ba8-7e3a-0410-8455-c3a389f83636

--- PDEFoam3/test/PlotFoams.C
+++ PDEFoam3/test/PlotFoams.C
@@ -43,6 +43,9 @@ void PlotFoams( TString fin = "weights/TMVAClassification_PDEFoam.weights_foams.
    TString macro_rms_ov_mean = Form( "Plot(\"%s\", TMVA::kRmsOvMean, \"RMS/Mean\", %s)", 
 				     fin.Data(), (useTMVAStyle ? "kTRUE" : "kFALSE") );
    cbar->AddButton( "RMS over Mean", macro_rms_ov_mean, "Plot RMS over Mean", "button" );
+   TString macro_cell_tree = Form( "PlotCellTree(\"%s\", \"Cell tree\", %s)",
+				   fin.Data(), (useTMVAStyle ? "kTRUE" : "kFALSE") );
+   cbar->AddButton( "Cell tree", macro_cell_tree, "Plot cell tree", "button" );
 
    cbar->Show();
    file->Close();
@@ -162,4 +165,98 @@ void Plot( TString fin = "weights/TMVAClassification_PDEFoam.weights_foams.root"
    } // if dimension > 1
 
    file->Close();
+}
+
+
+void PlotCellTree( TString fin = "weights/TMVAClassification_PDEFoam.weights_foams.root", 
+		   TString cv_long, bool useTMVAStyle=kTRUE )
+{
+   // Draw the PDEFoam cell tree
+
+   cout << "read file: " << fin << endl;
+   TFile *file = TFile::Open(fin);
+
+   if (useTMVAStyle)  TMVAGlob::SetTMVAStyle();
+
+   // find foams
+   TListIter foamIter(gDirectory->GetListOfKeys());
+   TKey *foam_key = NULL; // the foam key
+   TCanvas *canv = NULL;  // the canvas
+   while (foam_key = (TKey*) foamIter()) {
+      TString name(foam_key->GetName());
+      TString class_name(foam_key->GetClassName());
+      if (!class_name.Contains("PDEFoam"))
+	 continue;
+      cout << "PDEFoam found: " << class_name 
+	   << " " << name << endl;
+
+      // read the foam
+      TMVA::PDEFoam *foam = (TMVA::PDEFoam*) foam_key->ReadObj();
+      canv = new TCanvas(Form("canvas_%s",name.Data()),
+			 Form("%s of %s",cv_long.Data(),name.Data()), 640, 480);
+
+      // get cell tree depth
+      UInt_t   depth = foam->GetRootCell()->GetTreeDepth();
+      Double_t ystep = 1.0/(depth + 1.0);
+      DrawCell(foam->GetRootCell(), foam, 0.5, 1.-0.5*ystep, 0.25, ystep);
+   }
+
+   file->Close();
+}
+
+void DrawCell( TMVA::PDEFoamCell *cell, TMVA::PDEFoam *foam,
+	       Double_t x, Double_t y,
+	       Double_t xscale,  Double_t yscale )
+{
+   // recursively draw cell and it's daughters
+
+   Float_t xsize = xscale*1.5;
+   Float_t ysize = yscale/3;
+   if (xsize > 0.15) xsize=0.1; //xscale/2;
+   if (cell->GetDau0() != NULL) {
+      TLine *a1 = new TLine(x-xscale/4, y-ysize, x-xscale, y-ysize*2);
+      a1->SetLineWidth(2);
+      a1->Draw();
+      DrawCell(cell->GetDau0(), foam, x-xscale, y-yscale, xscale/2, yscale);
+   }
+   if (cell->GetDau1() != NULL){
+      TLine *a1 = new TLine(x+xscale/4, y-ysize, x+xscale, y-ysize*2);
+      a1->SetLineWidth(2);
+      a1->Draw();
+      DrawCell(cell->GetDau1(), foam, x+xscale, y-yscale, xscale/2, yscale);
+   }
+
+   TPaveText *t = new TPaveText(x-xsize, y-ysize, x+xsize, y+ysize, "NDC");
+
+   t->SetBorderSize(1);
+   t->SetFillStyle(1);
+
+   // draw all cell elements
+   TVectorF *vec = (TVectorF*) cell->GetElement();
+   if (vec != NULL){
+      for (Int_t i = 0; i < vec->GetNrows(); ++i) {
+	 t->AddText( Form("V[%i]=%.5f", i, vec(i)) );
+      }
+   }
+
+   if (cell->GetStat() != 1) {
+      // cell is inactive --> draw split point
+      t->SetFillColor( TColor::GetColor("#BBBBBB") );
+      t->SetTextColor( TColor::GetColor("#000000") );
+
+      // cell position and size
+      TMVA::PDEFoamVect cellPosi(foam->GetTotDim()), cellSize(foam->GetTotDim());
+      cell->GetHcub(cellPosi, cellSize);
+      Int_t    kBest = cell->GetBest(); // best division variable
+      Double_t xBest = cell->GetXdiv(); // best division point
+      t->AddText( Form("dim=%i", kBest) );
+      t->AddText( Form("cut=%.5g", foam->VarTransformInvers(kBest,cellPosi[kBest] + xBest*cellSize[kBest])) );
+   } else {
+      t->SetFillColor( TColor::GetColor("#DD0033") );
+      t->SetTextColor( TColor::GetColor("#FFFFFF") );
+   }
+
+   t->Draw();
+
+   return;
 }
