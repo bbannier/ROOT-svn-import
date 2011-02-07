77efa450b02de844b83781ff9a0b2834f7a0af33
rdm <rdm@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-02-07 15:35:54 +0000

    in rootcinttmp_pch.sh we now generate the *_dicthdr.h and the lib/*.pch files. This allows for smooth integration in the build system. Pch's for all libraries are now generated.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@37997 27541ba8-7e3a-0410-8455-c3a389f83636

--- rootcling/cling/src/rootcinttmp_pch.sh
+++ rootcling/cling/src/rootcinttmp_pch.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
+
+ret=0
+
 if "$@"; then
     rootsys=$PWD
     shift 3 # rootcint -cint -f
-    dict=cint/cling/src/`basename $1 | sed 's,.cxx,_dicthdr.h,'`
+    dict=`echo $1 | sed 's,.cxx,_dicthdr.h,'`
+    pch=`echo lib/$(basename $1) | sed 's,.cxx,.pch,'`
     echo "" > $dict
     shift
     incl=""
@@ -10,9 +14,17 @@ if "$@"; then
         case $1 in
             -I*) incl="$incl $1" ;;
             -*) ;;
-            *) if test "x$incl" != "x"; then echo "//" $incl > $dict;  incl=""; fi
+            *) if test "x$incl" != "x"; then
+                   echo "//" $incl > $dict
+                   incl=""
+               fi
                islinkdef=""
                if ! (echo $1 | grep -i linkdef > /dev/null 2>&1 ); then
+                   # skip this specif file
+                   if (echo $1 | grep VectorUtil_Cint.h > /dev/null 2>&1 ); then
+                      shift
+                      continue
+                   fi
                    hdr=`echo $1 | sed 's,^'$rootsys',..,'`
                    hdr=`echo $hdr | sed 's,^include/,,'`
                    echo '#include "'$hdr'"' >> $dict
@@ -21,4 +33,25 @@ if "$@"; then
         esac
         shift
     done
+
+    CC1OPTS=`echo "" | clang -c -o /dev/null -v -x c++ - 2>&1 | \
+             grep [-]cc1 | head -n1 | sed -e 's,-emit-obj ,,' \
+                -e 's,-main-file-name - ,,' -e 's,-v ,,' \
+                -e 's,-o /dev/null ,,' -e 's,c++ -,c++,' \
+                -e 's,",,g'`
+
+    echo "$CC1OPTS `grep '^// -I' $dict | sed 's,^//,,'` \
+       -I. -Iinclude -emit-pch -relocatable-pch $dict -o $pch"
+    $CC1OPTS `grep '^// -I' $dict | sed 's,^//,,'` \
+       -I. -Iinclude -emit-pch -relocatable-pch $dict -o $pch
+    ret=$?
+    if [ $ret -ne 0 ]; then
+       # force re-running of script in case of pch generation error
+       rm -f $1
+    else
+       # by default created with 600
+       chmod 644 $pch
+    fi
 fi
+
+exit $ret
