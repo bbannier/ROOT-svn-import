bbannier/ROOT-svn-import

commit 417e89a4f871eda0fb110470e34868be45124b94

Re-implementation of failed r38286, r38289 (i.e. partial "un-revert" of the revert r38290): If lookup for class A<std::B> fails, try recursively for A<B>. Fixes report by Jacek M. Holeczek with algebra3 and aux::vec3< std::complex<double> > which was only known as aux::vec3< complex<double> >. Also fix up template arguments for functions declared within templated classes, fixes std::vector<std::string>() and A<std::B>::f().
git-svn-id: https://root.cern.ch/svn/root/trunk@38293 27541ba8-7e3a-0410-8455-c3a389f83636

cint/cint/src/func.cxx
@@ -950,8 +950,12 @@ char* G__rename_templatefunc(G__FastAllocString& funcname)
          *ptmplt = 0;
       }
       else {
-         *ptmplt = '<';
-         ptmplt = (char*)0;
+         if (G__defined_templateclass(funcname)) {
+            *ptmplt = 0;
+         } else {
+            *ptmplt = '<';
+            ptmplt = (char*)0;
+         }
       }
    }
    if (ptmplt) {
@@ -1000,6 +1004,7 @@ char* G__rename_templatefunc(G__FastAllocString& funcname)
          funcname2 += buf2;
       }
       while (c != '>');
+      funcname2 += ptmplt + ip;
       funcname = funcname2;
    }
    return funcname;

cint/cint/src/struct.cxx
@@ -1718,26 +1718,55 @@ try_again:
       }
    }
    // If tagname not found, try instantiating class template.
-   len = strlen(tagname);
-   if ((tagname[len-1] == '>') && (noerror < 2) && ((len < 2) || (tagname[len-2] != '-'))) {
-      if (G__loadingDLL) {
-         G__fprinterr(G__serr, "Error: '%s' Incomplete template resolution in shared library", tagname);
-         G__genericerror(0);
-         G__fprinterr(G__serr, "Add following line in header for making dictionary\n");
-         G__fprinterr(G__serr, "   #pragma link C++ class %s;\n", tagname);
-         return -1;
+   if (noerror < 2) {
+      len = strlen(tagname);
+      bool tryInstantiation = false;
+      if ((tagname[len-1] == '>') && ((len < 2) || (tagname[len-2] != '-'))) {
+         if (G__loadingDLL) {
+            G__fprinterr(G__serr, "Error: '%s' Incomplete template resolution in shared library", tagname);
+            G__genericerror(0);
+            G__fprinterr(G__serr, "Add following line in header for making dictionary\n");
+            G__fprinterr(G__serr, "   #pragma link C++ class %s;\n", tagname);
+            return -1;
+         }
+         tryInstantiation = true;
+      } else {
+         G__Definedtemplateclass* deftmplt =G__defined_templateclass((char*) tagname);
+         tryInstantiation = (deftmplt && deftmplt->def_para && deftmplt->def_para->default_parameter);
       }
-      // CAUTION: tagname may be modified in following function.
-      i = G__instantiate_templateclass((char*) tagname, noerror);
-      return i;
-   }
-   else if (noerror < 2) {
-      G__Definedtemplateclass* deftmplt = G__defined_templateclass((char*) tagname);
-      if (deftmplt && deftmplt->def_para && deftmplt->def_para->default_parameter) {
+      if (tryInstantiation) {
+         // remove all std:: and try recursively.
+         G__FastAllocString noStd(tagname);
+         char* posstd = strstr((char*)noStd, "std::");
+         if (posstd) {
+            while (posstd) {
+               // shift right of std:: to left (aka remove "std::")
+               do {
+                  *posstd = posstd[5];
+               } while (*(posstd++));
+               posstd = strstr(posstd, "std::");
+            }
+            int nostdTy = G__defined_typename_noerror(noStd, 1);
+            if (nostdTy >= 0) {
+               i = G__newtype.tagnum[nostdTy];
+               if (i >= 0) {
+                  return i;
+               }
+            } else {
+               int subnoerr = noerror;
+               if (subnoerr == 4) subnoerr = 3;
+               if (enclosing) subnoerr |= 0x1000;
+               i = G__defined_tagname(noStd, subnoerr);
+               if (i >= 0) {
+                  return i;
+               }
+            }
+         }
+         // CAUTION: tagname may be modified in following function.
          i = G__instantiate_templateclass((char*) tagname, noerror);
          return i;
       }
-   }
+   } // if noerror < 2
 
    if (noerror == 4)
       return -1;

commit 5c0e7b91c99d1d1239f638eacc4ef298d373082b

Don't autoload! Don't instantiate templates! All we need is the name.
Fixes problem with stressInterpreter introduced by r38293.


git-svn-id: https://root.cern.ch/svn/root/trunk@38306 27541ba8-7e3a-0410-8455-c3a389f83636

cint/cint/src/func.cxx
@@ -985,7 +985,7 @@ char* G__rename_templatefunc(G__FastAllocString& funcname)
             buf = G__fulltypename(typenum);
          }
          else {
-            tagnum = G__defined_tagname(buf, 1);
+            tagnum = G__defined_tagname(buf, 4);
             if (-1 != tagnum) {
                buf = G__fulltagname(tagnum, 1);
             }