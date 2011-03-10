7d9a8267340a7f24c37c53f0b9d4b91a043c97c8
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-03-10 14:27:07 +0000

    Move EvalInfo.h into DynamicLookup.h, because the class is used only by the DynamicExprTransformer
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@38364 27541ba8-7e3a-0410-8455-c3a389f83636

--- lib/Interpreter/DynamicLookup.h
+++ lib/Interpreter/DynamicLookup.h
@@ -16,7 +16,6 @@
 
 namespace clang {
   class Sema;
-  
 }
 
 namespace cling {
@@ -40,6 +39,31 @@ namespace cling {
     clang::Sema* m_Sema;
   };
 } // end namespace cling
+
+namespace cling {
+  // The DynamicExprTransformer needs to have information about the nodes
+  // it visits in order to escape properly the unknown symbols. Walking up 
+  // it needs to know not only the node, which is being returned from the 
+  // visited subnode, but it needs information from its subnode if the 
+  // subnode can handle the dependent symbol itself or it wants delegate it
+  // to its parent.
+  // Ideally when given subnode has enough information to handle the unknown
+  // symbol it should do it instead of delegating to the parent. This limits
+  // the size of the expressions/statements being escaped.
+  class EvalInfo {
+  private:      
+    clang::Stmt *m_newStmt; // the new/old node
+  public:
+    bool IsEvalNeeded; // whether to emit the Eval call or not
+    
+    EvalInfo(clang::Stmt *S, bool needed)
+      :m_newStmt(S), IsEvalNeeded(needed) {};
+    
+    clang::Stmt *getNewStmt() const { return m_newStmt; }
+    void setNewStmt(clang::Stmt *S) { m_newStmt = S; } 
+  };
+  
+} //end namespace cling
 
 namespace cling {
   class Interpreter;
