97bd3411143887997c52f8bf521d394cdda03507
mato <mato@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-03-18 14:37:55 +0000

    merging again with trunk :-(
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@38508 27541ba8-7e3a-0410-8455-c3a389f83636

--- cmake/tmva/inc/VariableRearrangeTransform.h
+++ cmake/tmva/inc/VariableRearrangeTransform.h
@@ -4,16 +4,14 @@
 /**********************************************************************************
  * Project: TMVA - a Root-integrated toolkit for multivariate data analysis       *
  * Package: TMVA                                                                  *
- * Class  : VariableDecorrTransform                                               *
+ * Class  : VariableRearrangeTransform                                            *
  * Web    : http://tmva.sourceforge.net                                           *
  *                                                                                *
  * Description:                                                                   *
- *      Decorrelation of input variables                                          *
+ *      rearrangement of input variables                                          *
  *                                                                                *
  * Authors (alphabetical):                                                        *
- *      Andreas Hoecker <Andreas.Hocker@cern.ch> - CERN, Switzerland              *
- *      Joerg Stelzer   <Joerg.Stelzer@cern.ch>  - CERN, Switzerland              *
- *      Helge Voss      <Helge.Voss@cern.ch>     - MPI-K Heidelberg, Germany      *
+ *      Peter Speckmayer <Peter.Speckmayer@cern.ch> - CERN, Switzerland           *
  *                                                                                *
  * Copyright (c) 2005:                                                            *
  *      CERN, Switzerland                                                         *
@@ -25,24 +23,16 @@
  * (http://tmva.sourceforge.net/LICENSE)                                          *
  **********************************************************************************/
 
-#ifndef ROOT_TMVA_VariableDecorrTransform
-#define ROOT_TMVA_VariableDecorrTransform
+#ifndef ROOT_TMVA_VariableRearrangeTransform
+#define ROOT_TMVA_VariableRearrangeTransform
 
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
-// VariableDecorrTransform                                              //
+// VariableRearrangeTransform                                           //
 //                                                                      //
-// Linear interpolation class                                           //
+// rearrangement of input variables                                     //
 //                                                                      //
 //////////////////////////////////////////////////////////////////////////
-
-#ifndef ROOT_TMatrixDfwd
-#include "TMatrixDfwd.h"
-#endif
-
-#ifndef ROOT_TMatrixDSymfwd
-#include "TMatrixDSymfwd.h"
-#endif
 
 #ifndef ROOT_TMVA_VariableTransformBase
 #include "TMVA/VariableTransformBase.h"
@@ -50,22 +40,23 @@
 
 namespace TMVA {
 
-   class VariableDecorrTransform : public VariableTransformBase {
+   class VariableRearrangeTransform : public VariableTransformBase {
 
    public:
 
-      VariableDecorrTransform( DataSetInfo& dsi );
-      virtual ~VariableDecorrTransform( void );
+      typedef std::vector<Float_t>       FloatVector;
+
+      VariableRearrangeTransform( DataSetInfo& dsi );
+      virtual ~VariableRearrangeTransform( void );
 
       void   Initialize();
       Bool_t PrepareTransformation( const std::vector<Event*>& );
 
-      //      virtual const Event* Transform(const Event* const, Types::ESBType type = Types::kMaxSBType) const;
       virtual const Event* Transform(const Event* const, Int_t cls ) const;
-      virtual const Event* InverseTransform(const Event* const, Int_t cls ) const;
+      virtual const Event* InverseTransform( const Event* const, Int_t cls ) const;
 
-      void WriteTransformationToStream ( std::ostream& ) const;
-      void ReadTransformationFromStream( std::istream&, const TString& );
+      void WriteTransformationToStream ( std::ostream& ) const {}
+      void ReadTransformationFromStream( std::istream&, const TString& ) { SetCreated(); }
 
       virtual void AttachXMLTo(void* parent);
       virtual void ReadFromXML( void* trfnode );
@@ -80,16 +71,9 @@ namespace TMVA {
 
    private:
 
-      //      mutable Event*          fTransformedEvent;   //! local event copy
-      std::vector<TMatrixD*>  fDecorrMatrices;     //! Decorrelation matrix [class0/class1/.../all classes]
-
-      void CalcSQRMats( const std::vector<Event*>&, Int_t maxCls );
-      std::vector<TMatrixDSym*>* CalcCovarianceMatrices( const std::vector<Event*>& events, Int_t maxCls );
-
-      ClassDef(VariableDecorrTransform,0) // Variable transformation: decorrelation
+      ClassDef(VariableRearrangeTransform,0) // Variable transformation: normalization
    };
 
 } // namespace TMVA
 
-#endif
-
+#endif 
