d4c17c9865c4379b3c4bc47da5dbd966aaa6189c
mato <mato@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-04-13 08:19:25 +0000

    Made few changes to simply the building standalone packages (TVMA, RooFit)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cmake@38815 27541ba8-7e3a-0410-8455-c3a389f83636

--- cmake/modules/StandaloneBuild.cmake
+++ cmake/modules/StandaloneBuild.cmake
@@ -1,25 +1,34 @@
+##################################################################################################
+# --- StandaloneBuild.cmake module----------------------------------------------------------------
+# This module provides the sufficent environemnt to be able to build ROOT as standalone projects
+# It only assumes a valid ROOTSYS that can be provided directly with the command line
+#              cmake -DROOTSYS=<ROOT Installation> <package source> 
+##################################################################################################
+ 
+#---Set the CMake module path to locate the needed modules ---------------------------------------
+set(CMAKE_MODULE_PATH ${ROOTSYS}/cmake/modules)
 find_package(ROOT)
 
 #---Checking for the existing installation of ROOT------------------------------------------------
-message(STATUS "----Configuring ${name} as build standalone build package using an installation of ROOT")
+message(STATUS "Configuring ${CMAKE_PROJECT_NAME} as standalone build ROOT package")
 if(ROOT_FOUND)
   message(STATUS "Found ROOT installation at ${ROOTSYS}")
 else()
   message(ERROR "ROOT installation not found")
 endif()  
 
-set(CMAKE_MODULE_PATH ${ROOTSYS}/cmake/modules)
 
+#---Minimal environment---------------------------------------------------------------------------
 include(ROOTExports)
 include(RootNewMacros)
 include(CheckCompiler)
 
 #---Initialize project----------------------------------------------------------------------------
-#project(${name})
 set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
 set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
 
 include_directories(${ROOT_INCLUDE_DIRS})
 link_directories(${ROOT_LIBRARY_DIRS})
+
 
