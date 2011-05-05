4cf3a36fed20cadc415ef4e23e4016af62d5d03f
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-05-05 15:46:08 +0000

    * Have ChainedASTConsumer derive from SemaConsumer (yes, just like clang::MultiplexConsumer) * Teach ChainedASTConsumer about the consumers we use: there is no point in being generic, esp when there's only controlled use cases and it costs time (de-/re-registering) * Implement a startup PCH to considerably speed up cling's startup - once it works. Involves:   - PCH generator if the PCH doesn't exist. That seems to work; it's the driver's job to tell the interpreter at which point to create the PCH (i.e. when the startup has finished) and to define the name / location of the startup PCH.   - loading the PCH when available. That's the part that crashes cling because of invalid source files / locations.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@39114 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/lib/Interpreter/ChainedASTConsumer.h
+++ cling/lib/Interpreter/ChainedASTConsumer.h
@@ -8,7 +8,7 @@
 #define CLING_CHAINED_AST_CONSUMER_H
 
 #include "llvm/ADT/SmallVector.h"
-#include "clang/AST/ASTConsumer.h"
+#include "clang/Sema/SemaConsumer.h"
 #include "clang/AST/DeclGroup.h"
 
 namespace clang {
@@ -16,20 +16,25 @@ namespace clang {
 }
 
 namespace cling {
-  class ChainedASTConsumer: public clang::ASTConsumer {
+  class ChainedASTConsumer: public clang::SemaConsumer {
   public:
-    ChainedASTConsumer() {}
+    // Copy of IncrementalParser::EConsumerIndex!
+    enum EConsumerIndex {
+      kCodeGenerator,
+      kASTDumper,
+      kPCHGenerator,
+      kFunctionBodyConsumer,
+      kNConsumers
+    };
+    ChainedASTConsumer(): Consumers() {}
     virtual ~ChainedASTConsumer();
     
 #define CAC_DECL(WHAT, ARGS, PARAM) \
-void WHAT ARGS { \
-for (llvm::SmallVector<clang::ASTConsumer*,2>::iterator i = Consumers.begin(), \
-e = Consumers.end(); i != e; ++i) (*i)->WHAT PARAM; \
-}
+    void WHAT ARGS { for (int i = 0; i < kNConsumers; ++i) \
+      if (Consumers[i]) Consumers[i]->WHAT PARAM; }
     CAC_DECL(Initialize,(clang::ASTContext &Context),(Context));
     CAC_DECL(HandleTopLevelDecl,(clang::DeclGroupRef D),(D));
     CAC_DECL(HandleInterestingDecl,(clang::DeclGroupRef D),(D));
-    CAC_DECL(HandleTranslationUnit,(clang::ASTContext &Ctx),(Ctx));
     CAC_DECL(HandleTagDeclDefinition,(clang::TagDecl *D),(D));
     CAC_DECL(CompleteTentativeDefinition,(clang::VarDecl *D),(D));
     CAC_DECL(HandleVTable,(clang::CXXRecordDecl *RD, bool DefinitionRequired), \
@@ -37,14 +42,45 @@ e = Consumers.end(); i != e; ++i) (*i)->WHAT PARAM; \
     CAC_DECL(PrintStats,(),());
 #undef CAC_DECL
 
-    clang::ASTMutationListener *GetASTMutationListener() {
-      return Consumers.empty() ? 0 : Consumers[0]->GetASTMutationListener();
-    }
-    clang::ASTDeserializationListener *GetASTDeserializationListener() {
-      return Consumers.empty() ? 0 : Consumers[0]->GetASTDeserializationListener();
+    void HandleTranslationUnit(clang::ASTContext &Ctx) {
+      for (int i = 0; i < kNConsumers; ++i)
+        if (i != kPCHGenerator && Consumers[i]) Consumers[i]->HandleTranslationUnit(Ctx);
     }
 
-    llvm::SmallVector<clang::ASTConsumer*,2> Consumers;
+#define CSC_DECL(WHAT, ARGS, PARAM)                                     \
+    void WHAT ARGS {                                                    \
+      for (int i = 0; i < kNConsumers; ++i) {                           \
+        if (Consumers[i]) {                                             \
+          clang::SemaConsumer* SC =dyn_cast<clang::SemaConsumer>(Consumers[i]);\
+          if (SC) SC->WHAT PARAM;                                       \
+      } } }
+    CSC_DECL(InitializeSema,(clang::Sema &S),(S));
+    CSC_DECL(ForgetSema,(),());
+#undef CSC_DECL
+
+    clang::ASTMutationListener *GetASTMutationListener() {
+      for (int i = 0; i < kNConsumers; ++i) {
+        if (Consumers[i] && Consumers[i]->GetASTMutationListener()) {
+          return Consumers[i]->GetASTMutationListener();
+        }
+      }
+      return 0;
+    }
+    clang::ASTDeserializationListener *GetASTDeserializationListener() {
+      for (int i = 0; i < kNConsumers; ++i) {
+        if (Consumers[i] && Consumers[i]->GetASTDeserializationListener()) {
+          return Consumers[i]->GetASTDeserializationListener();
+        }
+      }
+      return 0;
+    }
+
+    void add(EConsumerIndex I, clang::ASTConsumer* C) {
+      assert(!Consumers[I] && "Consumer already registered at this index!");
+      Consumers[I] = C;
+    }
+
+    clang::ASTConsumer* Consumers[kNConsumers];
   };
 } // namespace cling
 
