fa386643b08b53f62f74c71a34fef5c4f153fd00
rdm <rdm@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-05-16 23:32:55 +0000

    generalize make scripts and rename to makexxxx.sh.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/docbook@39206 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ docbook/users-guide/makehtmlchunk.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+docbookdirs="/usr/share/xml/docbook/stylesheet/docbook-xsl \
+             /sw/share/xml/xsl/docbook-xsl"
+
+docbook=
+for d in $docbookdirs; do
+   if [ -d $d ]; then
+      docbook=$d
+   fi
+done
+
+if [ -z $docbook ]; then
+   echo "No docbook installation found"
+   exit 1
+fi
+
+if [ -d html ]; then
+   rm -rf html
+fi
+
+mkdir html
+cd html
+
+xsltproc --xinclude --output ROOTUsersGuide.html \
+   $docbook/html/chunk.xsl \
+   ../ROOTUsersGuide.xml
---
+++ docbook/users-guide/makepdf.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+docbookdirs="/usr/share/xml/docbook/stylesheet/docbook-xsl \
+             /sw/share/xml/xsl/docbook-xsl"
+
+fopjars="/usr/share/java/fop.jar \
+         /sw/share/java/fop/fop.jar"
+
+docbook=
+for d in $docbookdirs; do
+   if [ -d $d ]; then
+      docbook=$d
+   fi
+done
+
+if [ -z $docbook ]; then
+   echo "No docbook installation found"
+   exit 1
+fi
+
+fopjar=
+for f in $fopjars; do
+   if [ -f $f ]; then
+      fopjar=$f
+   fi
+done
+
+if [ -z $fopjar ]; then
+   echo "No fop.jar file found"
+   exit 1
+fi
+
+xsltproc --xinclude --output ROOTUsersGuide.fo \
+   $docbook/fo/docbook.xsl \
+   ROOTUsersGuide.xml
+
+java -Xmx1024m -jar $fopjar ROOTUsersGuide.fo ROOTUsersGuide.pdf
+rm ROOTUsersGuide.fo
+
+exit 0
