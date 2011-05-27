16b57d69177b23cf57b2b8bca4d18066c1ba6a77
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-05-27 15:28:10 +0000

    Add tests for the dynamic lookup
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@39461 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cling/test/Extensions/Lookup/Simple.C
@@ -0,0 +1,7 @@
+// RUN: cat %s | %cling -I%p
+// RUN: cat %s | %cling -I%p | FileCheck %s
+
+#include "ResolveAlwaysAsGCling.h"
+
+gCling->setCallbacks(new cling::test::ResolveAlwaysAsGClingCallback(gCling));
+jksghdgsjdf->getVersion() // CHECK: {{.*Interpreter.*}}
