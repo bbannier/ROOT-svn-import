73641cda2d57469f4b8293da59d219ab9ade5397
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-05-27 13:00:31 +0000

    Add test for the value printer.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@39450 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cling/test/Prompt/ValuePrinter/Assignments.C
@@ -0,0 +1,11 @@
+// RUN: cat %s | %cling
+// RUN: cat %s | %cling | FileCheck %s
+
+int a = 12;
+a // CHECK: (int) 12
+
+const char* b = "b" // CHECK: (const char *) "b"
+
+struct C {int d;} E = {22};
+E // CHECK: {{\(C\) @0x[0-9A-Fa-f].*}}
+E.d // CHECK: (int) 22
