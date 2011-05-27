59e6ba16cb091d1f774578ec7b12140d66d52600
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-05-27 20:41:20 +0000

    Use proper naming
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@39468 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/test/Extensions/Lookup/Simple.C
+++ cling/test/Extensions/Lookup/Simple.C
@@ -1,8 +1,8 @@
 // RUN: cat %s | %cling -I%p
 // RUN: cat %s | %cling -I%p | FileCheck %s
 
-#include "ResolveAlwaysAsGCling.h"
+#include "SymbolResolverCallback.h"
 
-gCling->setCallbacks(new cling::test::ResolveAlwaysAsGClingCallback(gCling));
+gCling->setCallbacks(new cling::test::SymbolResolverCallback(gCling));
 jksghdgsjdf->getVersion() // CHECK: {{.*Interpreter.*}}
 hsdghfjagsp->Draw() // CHECK: (int const) 12
