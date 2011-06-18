83e370bfd84701e3b35d09c5d53f125980c6698c
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-06-18 11:04:59 +0000

    Don't print the result of printf itself.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@39815 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/test/Extensions/Lookup/Simple.C
+++ cling/test/Extensions/Lookup/Simple.C
@@ -8,6 +8,7 @@ jksghdgsjdf->getVersion() // CHECK: {{.*Interpreter.*}}
 hsdghfjagsp->Draw() // CHECK: (int const) 12
 
 h->Add10(h->Add10(h->Add10(0))) // CHECK: (int const) 30
-h->PrintString(std::string("test")) // CHECK: test
+h->PrintString(std::string("test")); // CHECK: test
 int a[5] = {1,2,3,4,5};
-h->PrintArray(a, 5) // CHECK: 12345
+h->PrintArray(a, 5); // CHECK: 12345
+.q
