3c14cedd4b1f4e482fdbb3b475c705cccaf2d4ca
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-06-25 19:52:42 +0000

    Tweak the tests to match the new, more verbose value printer
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@39963 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Prompt/ValuePrinter/Assignments.C
+++ test/Prompt/ValuePrinter/Assignments.C
@@ -7,11 +7,11 @@ a // CHECK: (int) 12
 const char* b = "b" // CHECK: (const char *) "b"
 
 struct C {int d;} E = {22};
-E // CHECK: {{\(C\) @0x[0-9A-Fa-f].*}}
+E // CHECK: {{\(struct C\) @0x[0-9A-Fa-f].*}}
 E.d // CHECK: (int) 22
 
 #include <string>
-std::string s("abc") // CHECK: {{\(std::basic_string<char>\) @0x[0-9A-Fa-f].*}} 
+std::string s("abc") // CHECK: {{\(std::string\) @0x[0-9A-Fa-f].*}} 
 s.c_str() // CHECK: (const char * const) "abc"
 
 .q
