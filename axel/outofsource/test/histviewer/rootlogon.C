75217e2a9fbe919f4dc1dbf0741a4414933a072d
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-07-07 13:41:11 +0000

    Not needed anymore.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40160 27541ba8-7e3a-0410-8455-c3a389f83636

--- axel/outofsource/test/histviewer/rootlogon.C
+++
@@ -1,9 +0,0 @@
-{
-   printf("\n");
-   if (gSystem->Load("./libHistViewer.so")==0) {
-      printf("Shared library libHistViewer.so loaded\n");
-      gui = new HistAction(gClient->GetRoot(),1,1);
-   } else {
-      printf("Unable to load libHistViewer.so\n");
-   } 
-}
