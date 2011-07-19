2aa7527a1725558877e63ee9c85204867d91d17b
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-07-19 18:36:35 +0000

    This test should pass when Axel fixes the mutable memory buffers.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@40303 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/Prompt/Redeclarations.C
+++ test/Prompt/Redeclarations.C
@@ -1,17 +1,14 @@
 // RUN: cat %s | %cling
-// XFAIL: *
 
-// TODO: When we can dyn_cast the clients
-//#include "clang/Basic/Diagnostic.h"
-//#include "clang/Frontend/CompilerInstance.h"
-//#include "clang/Frontend/VerifyDiagnosticsClient.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/VerifyDiagnosticsClient.h"
 
-//#include "cling/Interpreter/Interpreter.h"
+#include "cling/Interpreter/Interpreter.h"
 
-
-//clang::Diagnostic& Diags = gCling->getCI()->getDiagnostics();
-//clang::DiagnosticClient* Client = new clang::VerifyDiagnosticsClient(Diags, Diags.takeClient());
-//Diags.setClient(Client);
+clang::Diagnostic& Diags = gCling->getCI()->getDiagnostics();
+clang::DiagnosticClient* Client = new clang::VerifyDiagnosticsClient(Diags, Diags.takeClient());
+Diags.setClient(Client);
 
 #include <string>
 std::string s;
