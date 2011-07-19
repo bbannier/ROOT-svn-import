4f727171d49d5420bea60205ab3ee5a0bd13d3a6
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-07-19 17:17:51 +0000

    Prefer not to have optimizations on the module, yet. The pro is that we can see the pure SSA streaming from CodeGen in the module.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cling@40302 27541ba8-7e3a-0410-8455-c3a389f83636

--- lib/Interpreter/CIFactory.cpp
+++ lib/Interpreter/CIFactory.cpp
@@ -154,10 +154,10 @@ namespace cling {
     CI->createSema(CompleteTranslationUnit, CCC);
 
     // Set CodeGen options
-    //CI->getCodeGenOpts().CXAAtExit = 0; // don't use atexit() for d'tors - needs explicit call to runStaticInitializers(d'tors)
+    // CI->getCodeGenOpts().CXAAtExit = 0; // don't use atexit() for d'tors - needs explicit call to runStaticInitializers(d'tors)
     // CI->getCodeGenOpts().DebugInfo = 1; // want debug info
     // CI->getCodeGenOpts().EmitDeclMetadata = 1; // For unloading, for later
-    CI->getCodeGenOpts().OptimizationLevel = 1; // just a bit
+    CI->getCodeGenOpts().OptimizationLevel = 0; // see pure SSA, that come out
     assert((CI->getCodeGenOpts().VerifyModule = 1) && "When asserts are on, let's also assert the module");
     return CI;
   }
