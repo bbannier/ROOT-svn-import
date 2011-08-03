ace0dbb05d82ee1623e6e397e6fbbd7ab8cab37c
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-03 13:57:04 +0000

    More fill patterns added.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tpdev@40440 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/ios/src/FillPatterns.cxx
+++ graf2d/ios/src/FillPatterns.cxx
@@ -332,6 +332,240 @@ CGPatternRef create_pattern_10()
    return CGPatternCreate(0, CGRectMake(0.f, 0.f, 8.f, 8.f), CGAffineTransformIdentity, 8.f, 8.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
 }
 
+//______________________________________________________________________________
+void draw_pattern_11(void *, CGContextRef ctx)
+{
+   Float_t red = 0.f, green = 0.f, blue = 0.f, alpha = 1.f;
+   GetColorForIndex(gVirtualX->GetFillColor(), red, green, blue);
+   CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+
+   CGContextFillRect(ctx, CGRectMake(0.f, 4.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 3.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 5.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 6.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 6.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 5.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 3.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 4.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 1.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 3.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 4.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 5.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 6.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 7.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(0.f, 10.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 14.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 11.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 13.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 12.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 9.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 10.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 11.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 12.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 13.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 14.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 15.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 12.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 11.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 13.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 10.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 14.f, 1.f, 1.f));
+
+}
+
+//______________________________________________________________________________
+CGPatternRef create_pattern_11()
+{
+   CGPatternCallbacks patternCallbacks;
+   patternCallbacks.version = 0;
+   patternCallbacks.drawPattern = draw_pattern_11;
+   patternCallbacks.releaseInfo = 0;
+   
+   return CGPatternCreate(0, CGRectMake(0.f, 0.f, 8.f, 16.f), CGAffineTransformIdentity, 8.f, 16.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
+}
+
+//______________________________________________________________________________
+void draw_pattern_12(void *, CGContextRef ctx)
+{
+   Float_t red = 0.f, green = 0.f, blue = 0.f, alpha = 1.f;
+   GetColorForIndex(gVirtualX->GetFillColor(), red, green, blue);
+   CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+
+   CGContextFillRect(ctx, CGRectMake(0.f, 1.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 7.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 11.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 12.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 13.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(1.f, 2.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 6.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 10.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 14.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(2.f, 3.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 4.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 5.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 9.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 15.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(3.f, 9.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 15.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(4.f, 3.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 4.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 5.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 9.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 15.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(5.f, 2.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 6.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 10.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 14.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(6.f, 1.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 7.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 11.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 12.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 13.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(7.f, 1.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 7.f, 1.f, 1.f));
+}
+
+//______________________________________________________________________________
+CGPatternRef create_pattern_12()
+{
+   CGPatternCallbacks patternCallbacks;
+   patternCallbacks.version = 0;
+   patternCallbacks.drawPattern = draw_pattern_12;
+   patternCallbacks.releaseInfo = 0;
+   
+   return CGPatternCreate(0, CGRectMake(0.f, 0.f, 8.f, 16.f), CGAffineTransformIdentity, 8.f, 16.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
+}
+
+//______________________________________________________________________________
+void draw_pattern_13(void *, CGContextRef ctx)
+{
+   Float_t red = 0.f, green = 0.f, blue = 0.f, alpha = 1.f;
+   GetColorForIndex(gVirtualX->GetFillColor(), red, green, blue);
+   CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+
+   CGContextFillRect(ctx, CGRectMake(0.f, 7.f,  1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(1.f, 0.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 6.f,  1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(2.f, 1.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 5.f,  1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(3.f, 2.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 4.f,  1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(4.f, 3.f,  1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(5.f, 2.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 4.f,  1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(6.f, 1.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 5.f,  1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(7.f, 0.f,  1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 6.f,  1.f, 1.f));
+}
+
+//______________________________________________________________________________
+CGPatternRef create_pattern_13()
+{
+   CGPatternCallbacks patternCallbacks;
+   patternCallbacks.version = 0;
+   patternCallbacks.drawPattern = draw_pattern_13;
+   patternCallbacks.releaseInfo = 0;
+   
+   return CGPatternCreate(0, CGRectMake(0.f, 0.f, 8.f, 8.f), CGAffineTransformIdentity, 8.f, 8.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
+}
+
+//______________________________________________________________________________
+void draw_pattern_14(void *, CGContextRef ctx)
+{
+   Float_t red = 0.f, green = 0.f, blue = 0.f, alpha = 1.f;
+   GetColorForIndex(gVirtualX->GetFillColor(), red, green, blue);
+   CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+
+   for (UInt_t i = 0; i < 16; ++i) {
+      CGContextFillRect(ctx, CGRectMake(0.f, i, 1.f, 1.f));
+      CGContextFillRect(ctx, CGRectMake(12.f, i, 1.f, 1.f));
+      
+      CGContextFillRect(ctx, CGRectMake(i, 3.f, 1.f, 1.f));
+      CGContextFillRect(ctx, CGRectMake(i, 15.f, 1.f, 1.f));
+   }
+   
+   for (UInt_t i = 0; i < 8; ++i) {
+      CGContextFillRect(ctx, CGRectMake(i, 7.f, 1.f, 1.f));
+      CGContextFillRect(ctx, CGRectMake(i + 4, 11.f, 1.f, 1.f));
+      CGContextFillRect(ctx, CGRectMake(4.f, 8.f + i, 1.f, 1.f));
+      CGContextFillRect(ctx, CGRectMake(8.f, 4.f + i, 1.f, 1.f));
+   }
+   
+}
+
+//______________________________________________________________________________
+CGPatternRef create_pattern_14()
+{
+   CGPatternCallbacks patternCallbacks;
+   patternCallbacks.version = 0;
+   patternCallbacks.drawPattern = draw_pattern_14;
+   patternCallbacks.releaseInfo = 0;
+   
+   return CGPatternCreate(0, CGRectMake(0.f, 0.f, 16.f, 16.f), CGAffineTransformIdentity, 16.f, 16.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
+}
+
+//______________________________________________________________________________
+void draw_pattern_15(void *, CGContextRef ctx)
+{
+   Float_t red = 0.f, green = 0.f, blue = 0.f, alpha = 1.f;
+   GetColorForIndex(gVirtualX->GetFillColor(), red, green, blue);
+   CGContextSetRGBFillColor(ctx, red, green, blue, alpha);
+
+   CGContextFillRect(ctx, CGRectMake(0.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(0.f, 6.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(1.f, 5.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(1.f, 7.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(2.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(2.f, 6.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(3.f, 1.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(3.f, 3.f, 1.f, 1.f));
+   
+   CGContextFillRect(ctx, CGRectMake(4.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(4.f, 6.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(5.f, 5.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(5.f, 7.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(6.f, 2.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(6.f, 6.f, 1.f, 1.f));
+
+   CGContextFillRect(ctx, CGRectMake(7.f, 1.f, 1.f, 1.f));
+   CGContextFillRect(ctx, CGRectMake(7.f, 3.f, 1.f, 1.f));
+}
+
+//______________________________________________________________________________
+CGPatternRef create_pattern_15()
+{
+   CGPatternCallbacks patternCallbacks;
+   patternCallbacks.version = 0;
+   patternCallbacks.drawPattern = draw_pattern_15;
+   patternCallbacks.releaseInfo = 0;
+   
+   return CGPatternCreate(0, CGRectMake(0.f, 0.f, 8.f, 8.f), CGAffineTransformIdentity, 8.f, 8.f, kCGPatternTilingConstantSpacingMinimalDistortion, 1, &patternCallbacks);
+}
+
 PatternGenerator_t gPatternGenerators[kPredefinedFillPatterns] = {create_pattern_1,
                                                                   create_pattern_2,
                                                                   create_pattern_3,
@@ -341,7 +575,12 @@ PatternGenerator_t gPatternGenerators[kPredefinedFillPatterns] = {create_pattern
                                                                   create_pattern_7,
                                                                   create_pattern_8,
                                                                   create_pattern_9,
-                                                                  create_pattern_10};
+                                                                  create_pattern_10,
+                                                                  create_pattern_11,
+                                                                  create_pattern_12,
+                                                                  create_pattern_13,
+                                                                  create_pattern_14,
+                                                                  create_pattern_15};
 
 }
 }
