17a4dba03d772c1fa250a5fed3ec1a45e7cdeb6b
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-03 07:00:37 +0000

    Test commit :)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40426 27541ba8-7e3a-0410-8455-c3a389f83636

--- tpdev/core/base/src/TVirtualX.cxx
+++ tpdev/core/base/src/TVirtualX.cxx
@@ -1,6 +1,7 @@
 // @(#)root/base:$Id$
 // Author: Fons Rademakers   3/12/95
 
+
 /*************************************************************************
  * Copyright (C) 1995-2000, Rene Brun and Fons Rademakers.               *
  * All rights reserved.                                                  *
@@ -8,6 +9,7 @@
  * For the licensing terms see $ROOTSYS/LICENSE.                         *
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
+
 
 //////////////////////////////////////////////////////////////////////////
 //                                                                      //
