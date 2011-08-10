77eba5bce27c62d421459add5f5620a4d9b6b1e7
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-10 15:12:59 +0000

    Pad editor's code is in a separated class now, GUI is in a separated nib file.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tpdev@40535 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ test/ios/Tutorials/PadEditorController.m
@@ -0,0 +1,19 @@
+//
+//  PadEditorController.m
+//  Tutorials
+//
+//  Created by Timur Pocheptsov on 8/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "PadEditorController.h"
+
+
+@implementation PadEditorController
+
+- (void) switchActivated : (id) control
+{
+
+}
+
+@end
