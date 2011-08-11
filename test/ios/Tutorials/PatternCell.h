a0353ff9c8d5c3dd24adaeb477e6c2ee6de08a61
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-11 13:23:40 +0000

    Pattern selector is also implemented as picker view now.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tpdev@40558 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/ios/Tutorials/PatternCell.h
+++ test/ios/Tutorials/PatternCell.h
@@ -1,25 +1,25 @@
 //
-//  ColorCell.h
+//  PatternCell.h
 //  Tutorials
 //
 //  Created by Timur Pocheptsov on 8/11/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
 //
 
+#import <CoreGraphics/CGPattern.h>
 #import <UIKit/UIKit.h>
 
-//
-//This is a small and simple custom view, used inside color picker (UIPickerView).
-//
 
-@interface ColorCell : UIView {
-   float rgb[3];
+@interface PatternCell : UIView {
+   CGPatternRef fillPattern;
+   BOOL darkBackground;
 }
 
 - (id) initWithFrame : (CGRect) frame;
 - (void) dealloc;
 
-- (void) setRGB : (const double *) rgb;
+- (void) setFillPattern : (CGPatternRef) pattern;
+- (void) setDarkBackground;
 - (void) drawRect : (CGRect) rect;
 
 @end
