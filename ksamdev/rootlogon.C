2e02036adb69cdb6937125374492149c1024f3ad
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-11 13:03:45 +0000

    Load relevant macros
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40554 27541ba8-7e3a-0410-8455-c3a389f83636

--- ksamdev/rootlogon.C
+++ ksamdev/rootlogon.C
@@ -1,8 +1,16 @@
 {
-  // Add local dirs to macro path
+   // Remove old link to package 'RTEvent'
+   if (!gSystem->AccessPathName("RTEvent")) gSystem->Unlink("RTEvent");
+   // Add local dirs to macro path
    TString mp(TROOT::GetMacroPath());
    Ssiz_t icol = mp.Index(".:");
    if (icol == kNPOS) icol = -2;
    mp.Insert(icol+2, TString::Format("%s/macros:%s/src:", gSystem->Getenv("KSDIR"), gSystem->Getenv("KSDIR")));
    TROOT::SetMacroPath(mp);
+   // Load most common macros
+   gROOT->ProcessLine(".L loadPackage.C+");
+   gROOT->ProcessLine("loadPackage(\"RTEvent\")");
+   gROOT->ProcessLine(".L releaseCache.C+");
+   gROOT->ProcessLine(".L WriteTest.C+");
+   gROOT->ProcessLine(".L ReadTest.C+");
 }
