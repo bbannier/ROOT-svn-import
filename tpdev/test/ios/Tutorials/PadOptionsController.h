d8b52e600bf4b0b31c124c6c513d6cd14cc30f2a
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-11 09:32:00 +0000

    New idead - editor in a popover, not hiding pad's content when visible.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40546 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tpdev/test/ios/Tutorials/PadOptionsController.h
@@ -0,0 +1,37 @@
+//
+//  PadOptionsController.h
+//  Tutorials
+//
+//  Created by Timur Pocheptsov on 8/11/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface PadOptionsController : UIViewController {
+   IBOutlet UISwitch *tickX_;
+   IBOutlet UISwitch *tickY_;
+
+   IBOutlet UISwitch *gridX_;
+   IBOutlet UISwitch *gridY_;
+
+   IBOutlet UISwitch *logX_;
+   IBOutlet UISwitch *logY_;
+   IBOutlet UISwitch *logZ_;
+   
+   IBOutlet UIPickerView *colorPicker_;
+   IBOutlet UIPickerView *patternPicker_;
+}
+
+@property (nonatomic, retain) UISwitch *tickX;
+@property (nonatomic, retain) UISwitch *tickY;
+@property (nonatomic, retain) UISwitch *gridX;
+@property (nonatomic, retain) UISwitch *gridY;
+@property (nonatomic, retain) UISwitch *logX;
+@property (nonatomic, retain) UISwitch *logY;
+@property (nonatomic, retain) UISwitch *logZ;
+@property (nonatomic, retain) UIPickerView *colorPicker;
+@property (nonatomic, retain) UIPickerView *patternPicker;
+
+@end
