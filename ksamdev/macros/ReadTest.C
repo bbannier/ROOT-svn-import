74e34a4954a57e361231552a33236019a4e5312d
ganis <ganis@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-11 12:48:38 +0000

    First working version of ReadTest
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40552 27541ba8-7e3a-0410-8455-c3a389f83636

--- ksamdev/macros/ReadTest.C
+++ ksamdev/macros/ReadTest.C
@@ -1,10 +1,17 @@
+#include "TChain.h"
+#include "TFile.h"
+#include "TFileCollection.h"
+#include "THashList.h"
+#include "TProof.h"
+#include "TQueryResult.h"
 #include "TStopwatch.h"
 #include "TString.h"
 #include "TSystem.h"
 #include "TROOT.h"
 
+void ParseTag(const char *tag, Int_t &nf, Int_t &comp, Int_t &split, TString &fnlist);
 
-void ReadTest(const char *basedir)
+void ReadTest(const char *basedir, Bool_t cold = kTRUE, const char *proof = 0, Bool_t rebuild = kFALSE)
 {
 
    // Get test tag
@@ -12,121 +19,130 @@ void ReadTest(const char *basedir)
    
    // Parse test tag to get relevant config parameters
    TString fnlist;
-   Int_f nf, comp, split;
+   Int_t nf, comp, split;
    ParseTag(testtag, nf, comp, split, fnlist);
 
    // Read selector
-   TString sel = (split) ? "ReadSelSplit.cxx+" : "ReadSel.cxx+";
+   TString sel = (split) ? "ReadSelSplit" : "ReadSel";
+   const char *aclicc = (rebuild) ? "++" : "+";
+   TString selload = TString::Format("src/%s.cxx%s", sel.Data(), aclicc);
    
-   if (gSystem->AccessPathName("results")) gSystem->MakeDirectory("results");
-   TString fnout = TString::Format("results/readtest_%s.txt", testtag.Data());
-   FILE *fout = fopen(fnout.Data(), "w");
-   
-   // Local run
-   TFileCollection *fc = new TFileCollection("","",fnlist.Data());
-   TChain *c = new TChain("rt");
-   c->AddFileInfoList(fc->GetList());
-   
-   Long64_t ref_rb = TFile::GetFileBytesRead();
-   
-   TStopwatch swtc;
-   c->Process(sel);
-   swtc.Stop();
+   TString resdir(gROOT->GetVersion());
+   resdir.ReplaceAll("/", "-"); resdir.ReplaceAll(".", "-");
+   if (TString(gSystem->GetMakeExe()).Contains(gSystem->GetFlagsDebug())) resdir += "_dbg"; 
+   resdir.Insert(0, "results/");
+   if (gSystem->AccessPathName(resdir)) gSystem->mkdir(resdir, kTRUE);
+   TString fnout = TString::Format("%s/readtest-%s_%s.txt", resdir.Data(), gSystem->HostName(), testtag.Data());
+   FILE *fout = fopen(fnout.Data(), "a");
 
-   Long64_t totsz = TFile::GetFileBytesRead() - ref_rb;
-   Long64_t ref_rb = TFile::GetFileBytesRead();
+   TStopwatch swtc;
+   swtc.Stop();
+   Long64_t ref_rb, totsz = -1;
+   if (!proof || (proof && strlen(proof) <= 0)) {
+      // Local run
+      TFileCollection *fc = new TFileCollection("","",fnlist.Data());
+      TChain *c = new TChain("rt");
+      c->AddFileInfoList(fc->GetList());
+
+      gROOT->ProcessLine(TString::Format(".L %s", selload.Data()));
+      
+      ref_rb = TFile::GetFileBytesRead();
+      
+      swtc.Start();
+      c->Process(sel);
+      swtc.Stop();
+
+      totsz = TFile::GetFileBytesRead() - ref_rb;
+      ref_rb = TFile::GetFileBytesRead();
+   } else {
+      // Proof run
+      TProof *p = gProof;
+      if (!p) {
+         p = TProof::Open(proof);
+      } else {
+         TString sp(proof);
+         sp.ReplaceAll("workers=", "");
+         Printf("sp: '%s' (%s)", sp.Data(), proof);
+         if (sp.IsDigit()) p->SetParallel(sp.Atoi());
+      }
+      if (p && p->IsValid()) {
+         p->UploadPackage("packages/RTEvent.par");
+         p->EnablePackage("RTEvent", kTRUE);
+         p->Load(selload.Data());
+         swtc.Start();
+         p->Process(testtag.Data(), sel);
+         swtc.Stop();
+         if (p->GetQueryResult())
+            totsz = p->GetQueryResult()->GetBytes();
+         p->Close();
+         delete p;
+      }
+   }
    
    Double_t rate = (swtc.RealTime() > 0) ? totsz/1024/1024/swtc.RealTime() : -1.;
-   Printf("ReadTest: '%s'   rt: %.3f s ct: %.3f s (%lld bytes, %.2f MB/s)",
-          gSystem->BaseName(basedir), swtc.RealTime(), swtc.CpuTime(), totsz, rate);
+   Printf("ReadTest: '%s'   rt: %.3f s ct: %.3f s (%lld bytes, %.2f MB/s) %d %s",
+          gSystem->BaseName(basedir), swtc.RealTime(), swtc.CpuTime(), totsz, rate, cold, proof ? proof : "");
    if (fout)
-      fprintf(fout, "%s  %.3f  %.3f  %lld  %.2f\n",
-                    testtag.Data(), swtc.RealTime(), swtc.CpuTime(), totsz, rate);
+      fprintf(fout, "%s  %.3f  %.3f  %lld  %.2f %d %s\n",
+                    testtag.Data(), swtc.RealTime(), swtc.CpuTime(), totsz, rate, cold, proof ? proof : "");
 
    if (fout) fclose(fout);
 }
 
+void ReadTests(const char *basedir, Bool_t dowarm = kFALSE, Bool_t rebuild = kFALSE)
+{
+
+   // Basic ROOT cold read
+   gROOT->ProcessLine(TString::Format("releaseCache(\"%s\")", basedir));
+   ReadTest(basedir, kFALSE, rebuild);
+   // Basic ROOT warm read
+   if (dowarm) ReadTest(basedir, kTRUE);
+   // Basic PROOF cold reads
+   TProof *p = TProof::Open("lite://");
+   if (!p || (p && !p->IsValid())) return;
+   Int_t nw = p->GetParallel();
+   Int_t i = 0;
+   for (i = 2; i <= nw; i++) { 
+      gROOT->ProcessLine(TString::Format("releaseCache(\"%s\")", basedir));
+      ReadTest(basedir, kFALSE, TString::Format("workers=%d",i), rebuild);
+      rebuild = kFALSE;
+   }
+   // Basic PROOF warm reads
+   if (dowarm) {
+      for (i = 2; i <= nw; i++) { 
+         ReadTest(basedir, kTRUE, TString::Format("workers=%d",i));
+      }
+   }   
+}
+
+
+void ParseTag(const char *tag, Int_t &nf, Int_t &comp, Int_t &split, TString &fnlist)
+{
+   // Init outputs
+   nf = -1;
+   comp = -1;
+   split = -1;
+   fnlist = "";
    
-
-
-void WriteTest(const char *basedir = "", Int_t nf = 1, Long64_t ne = 1000,
-               Int_t comp_level = 0, Int_t split_level = 99, FILE *fout = 0);
-
-void WriteTests(const char *basedir = "")
-{
-   if (gSystem->AccessPathName("results")) gSystem->MakeDirectory("results");
-   FILE *fout = fopen("results/writetest.txt", "w");
-
-   TString dir;
-   // Write Test 1: no compression, 20 files, 100000 evts/file, splitted
-   dir.Form("%s/data_20_c0_s99", basedir);
-   WriteTest(dir, 20, 100000, 0, 99, fout);
-   // Write Test 2: no compression, 200 files, 10000 evts/file, splitted
-   dir.Form("%s/data_200_c0_s99", basedir);
-   WriteTest(dir, 200, 10000, 0, 99, fout);
-   // Write Test 3: no compression, 20 files, 100000 evts/file, unsplitted
-   dir.Form("%s/data_20_c0_s1", basedir);
-   WriteTest(dir, 20, 100000, 0, 1, fout);
-   // Write Test 4: no compression, 200 files, 10000 evts/file, unsplitted
-   dir.Form("%s/data_200_c0_s1", basedir);
-   WriteTest(dir, 200, 10000, 0, 1, fout);
-   // Write Test 5: compression, 20 files, 100000 evts/file, splitted
-   dir.Form("%s/data_20_c1_s99", basedir);
-   WriteTest(dir, 20, 100000, 1, 99, fout);
-   // Write Test 6: compression, 200 files, 10000 evts/file, splitted
-   dir.Form("%s/data_200_c1_s99", basedir);
-   WriteTest(dir, 200, 10000, 1, 99, fout);
-   // Write Test 7: compression, 20 files, 100000 evts/file, unsplitted
-   dir.Form("%s/data_20_c1_s1", basedir);
-   WriteTest(dir, 20, 100000, 1, 1, fout);
-   // Write Test 8: compression, 200 files, 10000 evts/file, unsplitted
-   dir.Form("%s/data_200_c1_s1", basedir);
-   WriteTest(dir, 200, 10000, 1, 1, fout);
-
-   if (fout) fclose(fout);
+   TString tg(tag), t;
+   // Get rid of 'data_'
+   tg.ReplaceAll("data_", "");
+   // Tokenize over '_'
+   Ssiz_t from = 0;
+   while (tg.Tokenize(t, from, "_")) {
+      if (t.BeginsWith("c")) {
+         t.Remove(0,1);
+         if (t.IsDigit()) comp = t.Atoi();
+      } else if (t.BeginsWith("s")) {
+         t.Remove(0,1);
+         if (t.IsDigit()) split = t.Atoi();
+      } else {
+         if (t.IsDigit()) nf = t.Atoi();
+      }
+   }
+   fnlist.Form("filelists/%s.txt", tag);
+   // Done
+   Printf("ParseTag: '%s' -> nf: %d, comp: %d, split: %d, fnlist: '%s'", tag, nf, comp, split, fnlist.Data());
 }
 
 
-void WriteTest(const char *basedir, Int_t nf, Long64_t ne,
-               Int_t comp_level, Int_t split_level, FILE *fout)
-{
-
-   fprintf(stderr, "Test: '%s'\r", gSystem->BaseName(basedir));
-
-   if (gSystem->AccessPathName("filelists")) gSystem->MakeDirectory("filelists");
-   TString fntxt = TString::Format("filelists/%s.txt", gSystem->BaseName(basedir));
-
-   FILE *ftxt = fopen(fntxt.Data(), "w");
-  
-   if (gSystem->AccessPathName(basedir)) gSystem->MakeDirectory(basedir);
-
-   TStopwatch swtc;
-   TString fn;
-   for (Int_t i = 1; i <= nf; i++) {
-      fn.Form("%s/event-%s-%d.root", basedir,gSystem->HostName(), i);
-      fprintf(stderr, "Writing '%s' ... %d to go   \r", fn.Data(), nf-i+1);
-      generateFile(fn, ne, comp_level, split_level, 10, 5);
-      if (ftxt) fprintf(ftxt, "%s\n", fn.Data());
-   }
-   swtc.Stop();
-
-   if (ftxt) fclose(ftxt);
-
-   // Get sizes
-   Long64_t totsz = 0;
-   FileStat_t st;
-   for (Int_t i = 1; i <= nf; i++) {
-      fn.Form("%s/event-%s-%d.root", basedir,gSystem->HostName(), i);
-      if (gSystem->GetPathInfo(fn, st) == 0)
-	 totsz += st.fSize;
-      else
- 	 Printf("WARNING: file '%s' cannor be stated", fn.Data());      
-   }
-
-   Double_t rate = (swtc.RealTime() > 0) ? totsz/1024/1024/swtc.RealTime() : -1.;
-   Printf("WriteTest: '%s'   rt: %.3f s ct: %.3f s (%lld bytes, %.2f MB/s)",
-          gSystem->BaseName(basedir), swtc.RealTime(), swtc.CpuTime(), totsz, rate);
-   if (fout)
-      fprintf(fout, "%s  %.3f  %.3f  %lld  %.2f\n",
-              gSystem->BaseName(basedir), swtc.RealTime(), swtc.CpuTime(), totsz, rate);
-}
