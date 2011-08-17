971bdbfe8cd14bc85055ad649e26513d238c0f9f
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-17 15:14:56 +0000

    Line style are in a separate file now (to use them later in a GUI of line-attrib. editors)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40619 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tpdev/graf2d/ios/inc/IOSLineStyles.h
@@ -0,0 +1,13 @@
+#ifndef ROOT_IOSLineStyles
+#define ROOT_IOSLineStyles
+
+namespace ROOT_iOS {
+namespace GraphicUtils {
+
+extern const unsigned linePatternLengths[10];
+extern const float dashLinePatterns[10][8];
+
+}
+}
+
+#endif
