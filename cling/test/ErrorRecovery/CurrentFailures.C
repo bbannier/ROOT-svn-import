bd58e86ac633884eea6d9d0529ce227e6e96d96b
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-20 11:40:01 +0000

    Add initial version of the error recovery test suite. There are few known issues that need to be taken care of and it is better to have sort-of regressions for them.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40647 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cling/test/ErrorRecovery/CurrentFailures.C
@@ -0,0 +1,7 @@
+// RUN: cat %s | %cling -I%p | FileCheck %s
+// XFAIL: *
+dep->getVersion();
+a
+
+extern "C" int printf // expected-error {{expected ';' after top level declarator}}
+extern "C" int printf(const char* fmt, ...); // expected-error {{redefinition of 'printf' as different kind of symbol}} expected-note {{previous definition is here}}
