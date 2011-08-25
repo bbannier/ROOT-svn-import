7c121610c171647e0368b280b8b6e47d9b26fd92
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-25 10:36:34 +0000

    Add more examples in which error recovery couldn't recover
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40700 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/test/ErrorRecovery/CurrentFailures.C
+++ cling/test/ErrorRecovery/CurrentFailures.C
@@ -1,5 +1,12 @@
 // RUN: cat %s | %cling -I%p | FileCheck %s
 // XFAIL: *
+
+.x CurrentFailures.h
+
+ // First one is okay and the redefinition errors are not handled properly
+.L CurrentFailures.h
+.L CurrentFailures.h
+
 dep->getVersion();
 a
 
