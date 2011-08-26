25ee95fc9b03ffbffc0b469b4705eeb00ade7764
vvassilev <vvassilev@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-26 11:39:24 +0000

    Narrow down one of the still unresolved issues we have with the error recovery.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40722 27541ba8-7e3a-0410-8455-c3a389f83636

--- cling/test/ErrorRecovery/CurrentFailures.C
+++ cling/test/ErrorRecovery/CurrentFailures.C
@@ -1,8 +1,12 @@
 // RUN: cat %s | %cling -I%p | FileCheck %s
 // XFAIL: *
 
-dep->getVersion();
-a
-
+// the issue is that when we don't have semicolon in the end of the input
+// in the case of cling where we have incremental compilation the parser 
+// encounters an error and tries to recover by looking for so called safe tokens
+// or anchors (e.g ';' or EOF). The input doesn't have semicolon and we play 
+// with Lexer's EOF tokens. My assumption is that while trying to recover
+// and continue parsing the Lexer enters sort-of error state and it breaks with // assertion
 extern "C" int printf // expected-error {{expected ';' after top level declarator}}
-extern "C" int printf(const char* fmt, ...); // expected-error {{redefinition of 'printf' as different kind of symbol}} expected-note {{previous definition is here}}
+
+a;
