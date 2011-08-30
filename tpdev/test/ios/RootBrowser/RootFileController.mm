14f012b74a92c7fb4d41fd36a8762d78d54f9ade
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-08-30 08:13:14 +0000

    "Open File" view was added and the code to show/hide virtual keyboard, enter the file name and open a file using TWebFile.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40754 27541ba8-7e3a-0410-8455-c3a389f83636

--- tpdev/test/ios/RootBrowser/RootFileController.mm
+++ tpdev/test/ios/RootBrowser/RootFileController.mm
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/NSEnumerator.h>
+#import <QuartzCore/QuartzCore.h>
 
 #import "FileContentController.h"
 #import "RootFileController.h"
@@ -28,10 +29,18 @@
       self.navigationItem.title = @"ROOT files";
       UIBarButtonItem *backButton = [[UIBarButtonItem alloc] initWithTitle : @"Back to ROOT files" style:UIBarButtonItemStylePlain target : nil action : nil];
       self.navigationItem.backBarButtonItem = backButton;
-      UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle : @"Open file" style:UIBarButtonItemStylePlain target : self action : @selector(openFile)];
+      UIBarButtonItem *leftButton = [[UIBarButtonItem alloc] initWithTitle : @"Open file" style:UIBarButtonItemStylePlain target : self action : @selector(showFileOpenView)];
       self.navigationItem.leftBarButtonItem = leftButton;
       [backButton release];
       scrollView.bounces = NO;
+      
+      [self.view bringSubviewToFront : fileOpenView];
+      
+      fileNameField.clearButtonMode = UITextFieldViewModeAlways;
+      
+      UITapGestureRecognizer *tap = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(hideFileOpenView)];
+      [self.view addGestureRecognizer : tap];
+      [tap release];
    }
 
    return self;
@@ -67,18 +76,22 @@
 {
    CGRect mainFrame;
    CGRect scrollFrame;
+   CGRect fileViewFrame;
 
-//   const UIInterfaceOrientation orientation = self.interfaceOrientation;
-   if (orientation == UIInterfaceOrientationPortrait || orientation == UIInterfaceOrientationPortraitUpsideDown) {
+   if (UIInterfaceOrientationIsPortrait(orientation)) {
       mainFrame = CGRectMake(0.f, 0.f, 768.f, 1004.f);
       scrollFrame = CGRectMake(0.f, 44.f, 768.f, 960.f);
+      fileViewFrame = CGRectMake(0.f, 44.f, 768.f, 120.f);
    } else {
       mainFrame = CGRectMake(0.f, 0.f, 1024.f, 748.f);
       scrollFrame = CGRectMake(0.f, 44.f, 1024.f, 704.f);   
+      fileViewFrame = CGRectMake(0.f, 44.f, 1024.f, 120.f);
    }
    
    self.view.frame = mainFrame;
    self.scrollView.frame = scrollFrame;
+   
+   fileOpenView.frame = fileViewFrame;
    
    [self placeFileShortcuts];
 }
@@ -87,6 +100,7 @@
 - (void) viewWillAppear:(BOOL)animated
 {
    [self correctFramesForOrientation : self.interfaceOrientation];
+   [fileNameField resignFirstResponder];
    //Check if I have to call [super viewWillAppear];
 }
 
@@ -136,7 +150,7 @@
       fileContainers = [[NSMutableArray alloc] init];
    
    const CGRect shortcutFrame = CGRectMake(0.f, 0.f, [FileShortcut iconWidth], [FileShortcut iconHeight]);
-   FileShortcut *newShortcut = [[FileShortcut alloc] initWithFrame:shortcutFrame controller:self filePath : fileName];
+   FileShortcut *newShortcut = [[FileShortcut alloc] initWithFrame : shortcutFrame controller : self filePath : fileName];
 
    if (![newShortcut getFileContainer]) {
       UIAlertView *alert = [[UIAlertView alloc] initWithTitle : @"File Open Error:"
@@ -153,6 +167,7 @@
 
    [fileContainers addObject : newShortcut];
    [scrollView addSubview : newShortcut];
+        
    [newShortcut release];
    
    [self placeFileShortcuts];
@@ -169,10 +184,63 @@
 }
 
 //____________________________________________________________________________________________________
-- (void) openFile
+- (void) animateFileOpenView
 {
-//   fileOpenView.hidden = !fileOpenView.hidden;
+   //Do animation.
+   // First create a CATransition object to describe the transition
+   CATransition *transition = [CATransition animation];
+   // Animate over 3/4 of a second
+   transition.duration = 0.15;
+   // using the ease in/out timing function
+   transition.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+   // Now to set the type of transition.
+   transition.type = kCATransitionPush;
+   
+   if (!fileOpenView.hidden)
+      transition.subtype = kCATransitionFromBottom;
+   else
+      transition.subtype = kCATransitionFromTop;
+   transition.delegate = self;
+   // Next add it to the containerView's layer. This will perform the transition based on how we change its contents.
+   [fileOpenView.layer addAnimation : transition forKey : nil];
 }
 
+//____________________________________________________________________________________________________
+- (void) showFileOpenView
+{
+   fileOpenView.hidden = !fileOpenView.hidden;
+   //
+   [self animateFileOpenView];
+
+   if (!fileOpenView.hidden)
+      [fileNameField becomeFirstResponder];
+   else
+      [fileNameField resignFirstResponder];
+}
+
+//____________________________________________________________________________________________________
+- (IBAction) textFieldDidEndOnExit : (id) sender
+{
+   NSString *filePath = fileNameField.text;
+   if (filePath) {//TODO - do I need this check?
+      [self addFileShortcut : filePath];
+   }
+}
+
+//____________________________________________________________________________________________________
+- (IBAction) textFieldEditingDidEnd : (id) sender
+{
+   [sender resignFirstResponder];
+   fileOpenView.hidden = YES;
+   [self animateFileOpenView];
+}
+
+//____________________________________________________________________________________________________
+- (void) hideFileOpenView
+{
+   [fileNameField resignFirstResponder];
+   fileOpenView.hidden = YES;
+   [self animateFileOpenView];
+}
 
 @end
