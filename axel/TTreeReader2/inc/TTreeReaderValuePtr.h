56f12ca8ccde50cce21928667ff7e1ea1d992107
axel <axel@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-09-21 13:31:02 +0000

    Split array and pointer access.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@40966 27541ba8-7e3a-0410-8455-c3a389f83636

--- axel/TTreeReader2/inc/TTreeReaderValuePtr.h
+++ axel/TTreeReader2/inc/TTreeReaderValuePtr.h
@@ -9,13 +9,13 @@
  * For the list of contributors see $ROOTSYS/README/CREDITS.             *
  *************************************************************************/
 
-#ifndef ROOT_TTreeReaderValue
-#define ROOT_TTreeReaderValue
+#ifndef ROOT_TTreeReaderValuePtr
+#define ROOT_TTreeReaderValuePtr
 
 
 ////////////////////////////////////////////////////////////////////////////
 //                                                                        //
-// TTreeReaderValue                                                       //
+// TTreeReaderValuePtr                                                    //
 //                                                                        //
 // A simple interface for reading data from trees or chains.              //
 //                                                                        //
@@ -25,14 +25,8 @@
 #ifndef ROOT_TString
 #include "TString.h"
 #endif
-#ifndef ROOT_TClass
-#include "TClass.h"
-#endif
-#ifndef ROOT_TDataType
-#include "TDataType.h"
-#endif
-#ifndef ROOT_TLeaf
-#include "TLeaf.h"
+#ifndef ROOT_TDictionary
+#include "TDictionary.h"
 #endif
 
 class TBranch;
@@ -91,17 +85,16 @@ namespace ROOT {
 
       const char* GetName() const { return fBranchName; }
 
-      TTreeReaderValuePtrBase(TTreeReader& tr, const char* branchname, TClass* cl, EDataType dt);
+      TTreeReaderValuePtrBase(TTreeReader& tr, const char* branchname, TDictionary* dict);
 
-      void Init(TTreeReader& tr, TLeaf* leaf, TClass* cl, EDataType dt);
-      void Init(TTreeReader& tr, TBranch* branch, TClass* cl, EDataType dt);
+      void Init(TTreeReader& tr, TLeaf* leaf, TDictionary* dict);
+      void Init(TTreeReader& tr, TBranch* branch, TDictionary* dict);
 
       void UpdateAddresses();
       EReadStatus LoadEntry();
       TLeaf* GetCounterLeaf(TBranch* branch);
       void* GetAddress();
-      TClass* GetValueClass() const;
-      TDataType* GetValueDataType() const;
+      TDictionary* GetValueType() const;
 
       void SetBranchName();
 
@@ -118,23 +111,6 @@ namespace ROOT {
       ClassDef(TTreeReaderValuePtrBase, 0);//Base class for accessors to data via TTreeReader
    };
 
-   class TTreeReaderArrayBase: public TTreeReaderValuePtrBase {
-   public:
-      TTreeReaderArrayBase(TTreeReader& tr, const char* branchname,
-                           TClass* cl, EDataType dt);
-
-      Int_t GetSize();
-      Bool_t IsEmpty() { return !GetSize(); }
-
-   protected:
-      void* Get();
-
-   private:
-      TTreeReaderValuePtrBase fSizeValue; // value reader accessing the size
-
-      ClassDefT(TTreeReaderArrayBase, 0);//Accessor to member of an object stored in a collection
-   };
-
 } // namespace ROOT
 
 
@@ -143,8 +119,7 @@ class TTreeReaderValuePtr: public ROOT::TTreeReaderValuePtrBase {
 public:
    TTreeReaderValuePtr() {}
    TTreeReaderValuePtr(TTreeReader& tr, const char* branchname):
-      TTreeReaderValuePtrBase(tr, branchname, TClass::GetClass(typeid(T)),
-                              TDataType::GetType(typeid(T))) {}
+      TTreeReaderValuePtrBase(tr, branchname, TDictionary::GetType(typeid(T))) {}
 
    T* Get() { return *(T**)GetAddress(); }
    T* operator->() { return Get(); }
@@ -153,21 +128,4 @@ public:
    ClassDefT(TTreeReaderValuePtr, 0);//Accessor to data via TTreeReader
 };
 
-
-template <typename T>
-class TTreeReaderArray: public ROOT::TTreeReaderArrayBase {
-public:
-   TTreeReaderArray(TTreeReader& tr, const char* branchname):
-      TTreeReaderArrayBase(tr, branchname, TClass::GetClass(typeid(T*)),
-                              TDataType::GetType(typeid(T*)))
-   {
-      // Create an array reader of branch "branchname" for TTreeReader "tr".
-   }
-
-   T& At(Int_t idx) { return ((T*)Get())[idx]; }
-   T& operator[](Int_t idx) { return At(idx); }
-
-   ClassDefT(TTreeReaderArray, 0);//Accessor to member of an object stored in a collection
-};
-
-#endif // ROOT_TTreeReaderValue
+#endif // ROOT_TTreeReaderValuePtr
