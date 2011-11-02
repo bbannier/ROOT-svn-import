1d44cf00d3be499bfe21480488b09cbc71c52477
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-11-02 16:38:43 +0000

    Initially, read only keys for objects and identify their types.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tp_gl_es_ios@41727 27541ba8-7e3a-0410-8455-c3a389f83636

--- test/ios/LazyLoader/LazyLoader/ViewController.h
+++ test/ios/LazyLoader/LazyLoader/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  LazyLoader
-//
-//  Created by Timur Pocheptsov on 11/2/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
