8dfb23b195329b5b064890a723b3e74a2b912daf
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-11-02 13:59:38 +0000

    LazyLoader - test application to implement loading of ROOT's object from file by demand: instead of reading all "visible" objects, read their keys first, use generic pictograms to show the type of object in a browser, load range of objects visible in a browser's view and generate thumbnails. Required: 1. "infinite" scrooll. 2. asynchronous image generation.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/tp_gl_es_ios@41715 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ test/ios/LazyLoader/LazyLoader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LazyLoader
+//
+//  Created by Timur Pocheptsov on 11/2/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
