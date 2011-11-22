44762499cacd0f5110875f3027ddca3b15507137
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-11-22 14:34:02 +0000

    Non-gui part of TVirtualX for Mac OSX
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42165 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ macosx/graf2d/macosx/inc/TGQuartz.h
@@ -0,0 +1,39 @@
+#ifndef ROOT_TGQuartz
+#define ROOT_TGQuartz
+
+#ifndef ROOT_TGCocoa
+#include "TGCocoa.h"
+#endif
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// This is non-GUI part of TVirtualX interface, implemented for         //
+// MacOS X, using CoreGraphics (Quartz).                                //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+class TGQuartz : public TGCocoa {
+public:
+   TGQuartz();
+   
+   //Final-overriders for TVirtualX.
+   virtual void      DrawBox(Int_t x1, Int_t y1, Int_t x2, Int_t y2, EBoxMode mode);
+   virtual void      DrawCellArray(Int_t x1, Int_t y1, Int_t x2, Int_t y2,
+                                   Int_t nx, Int_t ny, Int_t *ic);
+   virtual void      DrawFillArea(Int_t n, TPoint *xy);
+   
+   using TGCocoa::DrawLine;
+   
+   virtual void      DrawLine(Int_t x1, Int_t y1, Int_t x2, Int_t y2);
+   virtual void      DrawPolyLine(Int_t n, TPoint *xy);
+   virtual void      DrawPolyMarker(Int_t n, TPoint *xy);
+   virtual void      DrawText(Int_t x, Int_t y, Float_t angle, Float_t mgn, const char *text,
+                              ETextMode mode);
+
+private:
+   TGQuartz(const TGQuartz &rhs);
+   TGQuartz &operator = (const TGQuartz &rhs);
+   
+   ClassDef(TGQuartz, 0);//2D graphics for Mac OSX.
+};
+
+#endif
