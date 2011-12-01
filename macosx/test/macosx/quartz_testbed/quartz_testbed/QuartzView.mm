9fafd9d8ed4f0ccaf23d68aaa937fe153ad2b952
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-01 18:12:28 +0000

    Save and restore context on top level.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42366 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzView.mm
+++ macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzView.mm
@@ -6,6 +6,7 @@
    QuartzTest *test;
 }
 
+//______________________________________________________________________________
 - (id)initWithFrame:(NSRect)frame
 {
    if (self = [super initWithFrame : frame]) {
@@ -16,16 +17,20 @@
    return self;
 }
 
+//______________________________________________________________________________
 - (void) awakeFromNib
 {
    test = new QuartzTest(300, 300);
 }
 
+//______________________________________________________________________________
 - (void) drawRect : (NSRect)dirtyRect
 {
    // Drawing code here.
    NSGraphicsContext *nsContext = [NSGraphicsContext currentContext];
    CGContextRef cgContext = (CGContextRef)[nsContext graphicsPort];
+   
+   CGContextSaveGState(cgContext);
    
    CGContextTranslateCTM(cgContext, 0.f, dirtyRect.size.height);
    CGContextScaleCTM(cgContext, 1.f, -1.f);
@@ -33,8 +38,11 @@
    test->SetContext(cgContext);
    test->SetPadSizes(dirtyRect.size.width, dirtyRect.size.height);
    test->Draw();
+   
+   CGContextRestoreGState(cgContext);
 }
 
+//______________________________________________________________________________
 - (void) dealloc
 {
    delete test;
