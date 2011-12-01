a6b5e0b7dc959d1533d45a8377fd58ab865946da
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-01 11:47:28 +0000

    Settings for test application to link against ROOT's dynamic libraries.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42343 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzView.h
+++ macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzView.h
@@ -1,11 +1,3 @@
-//
-//  QuartzView.h
-//  quartz_testbed
-//
-//  Created by Timur Pocheptsov on 12/1/11.
-//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
-//
-
 #import <Cocoa/Cocoa.h>
 
 @interface QuartzView : NSView
