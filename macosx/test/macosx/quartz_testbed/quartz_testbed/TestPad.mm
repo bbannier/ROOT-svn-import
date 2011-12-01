27eed1fd4276d5022fa9f208e8b8b69913e3844c
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-01 15:28:31 +0000

    Test app is ready.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42358 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/quartz_testbed/quartz_testbed/TestPad.mm
+++ macosx/test/macosx/quartz_testbed/quartz_testbed/TestPad.mm
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <algorithm>
 #include <stdexcept>
 
@@ -38,7 +39,7 @@ TestPad::TestPad(UInt_t w, UInt_t h)
    fPhi = 50;
 
    fAspectRatio = 0.;
-
+std::cout<<"gStyle: "<<gStyle<<std::endl;
    fTickx = gStyle->GetPadTickX();
    fTicky = gStyle->GetPadTickY();
 
