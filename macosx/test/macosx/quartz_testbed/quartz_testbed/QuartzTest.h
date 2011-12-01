a0d417ea75ae447c0726c84151646701d8119f0f
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-01 13:00:20 +0000

    Because of some subtle bug in Module.mk, libGMACOSX.so was not compiled/linked correctly.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42345 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzTest.h
+++ macosx/test/macosx/quartz_testbed/quartz_testbed/QuartzTest.h
@@ -1,14 +1,17 @@
 #ifndef QUARTZTEST_INCLUDED
 #define QUARTZTEST_INCLUDED
 
-/*
+#include "TGQuartz.h"
+
+//#include "Test"
 
 class QuartzTest {
 public:
+   QuartzTest();
 
 private:
-
+   TGQuartz fQuartzSingleton;
 };
-*/
+
 
 #endif
