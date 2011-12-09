7886c54a19e581ed36b7bf0ea4bc31bebc6bd513
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-09 16:37:33 +0000

    Make test app more complex - add nested frame + button. Hehe, crash!
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42456 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/test0/main.cpp
+++ macosx/test/macosx/test0/main.cpp
@@ -1,11 +1,21 @@
 #include "TApplication.h"
+#include "TGButton.h"
 #include "TGFrame.h"
 
 int main(int argc, char ** argv)
 {
    TApplication app("test_app", &argc, argv);
-   TGFrame frame(0, 400, 400);
-   
-   frame.MapRaised();
+
+   TGMainFrame *frame = new TGMainFrame(0, 400, 400);
+
+   TGHorizontalFrame *nestedFrame = new TGHorizontalFrame(frame, 400, 400);
+   TGTextButton *btn = new TGTextButton(nestedFrame, "Try to press me");
+   nestedFrame->AddFrame(btn, new TGLayoutHints(kLHintsCenterX | kLHintsCenterY, 2, 2, 2, 2));
+   frame->AddFrame(nestedFrame, new TGLayoutHints(kLHintsCenterX | kLHintsCenterY, 2, 2, 2, 2));
+   frame->MapSubwindows();
+   frame->MapRaised();
+
    app.Run();
+   
+   //Prosti menia, gospodi, no udaliat' ia nichego tut ne budu!!! :)
 }
