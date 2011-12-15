ce277448337abfe4c4c0cc409581dee3f83c1c5f
evt <evt@27541ba8-7e3a-0410-8455-c3a389f83636>
2011-12-15 13:30:09 +0000

    unit test for reader applications of old-version weightfiles
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42499 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ tmvaValidation/testsuite/tmvaut/ReaderTest.h
@@ -0,0 +1,61 @@
+#ifndef READERTEST_H
+#define READERTEST_H
+// author Eckhard von Toerne, U. of Bonn 2011
+
+#include "TFile.h"
+#include "TString.h"
+#include "TSystem.h"
+#include "TMVA/MethodBase.h"
+#include "TMVA/Reader.h"
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <iostream>
+#include <cassert>
+
+#include "TTree.h"
+#include "TString.h"
+
+#include "TMVA/Factory.h"
+#include "TMVA/MethodBase.h"
+#include "TMVA/Types.h"
+
+#include "UnitTest.h"
+
+namespace UnitTesting
+{
+
+// usage
+//ReaderTest(weightfile, tmvafile, treename, varlist)
+
+class ReaderTest : public UnitTest {
+public:
+   ReaderTest(TString methodtitle, 
+                    TString weightfile, 
+                    TString tmvafile, 
+                    TString treename ,
+              const std::vector<TString>* varlist, 
+              bool isRegression=false);
+
+   ReaderTest(TString methodtitle, 
+              TString weightdir,
+              bool isRegression=false);
+ 
+   virtual ~ReaderTest();
+   virtual void run();
+private:
+   bool fIsRegression;
+   TString fWeightFile, fTreeFilename,fTreeName,fMethodTitle;
+   const std::vector<TString>* fVariableNames;
+   const std::vector<TString>* fBranchNames;
+   const std::vector<TString>* fSpecNames;
+   TFile* fTestFile;
+   TTree* fTestTree;
+   int fNVar;
+   int fNSpecs;
+   static const int fMaxEvt=50;
+};
+
+} // namespace UnitTesting
+
+#endif
