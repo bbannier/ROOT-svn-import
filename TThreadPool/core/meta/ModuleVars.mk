cdcd83831c8960117e9c08585fdf70a2efa5c49a
anar <anar@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-01-30 12:20:39 +0000

    Removing obsolete branch from the repository
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/anar@42821 27541ba8-7e3a-0410-8455-c3a389f83636

--- TThreadPool/core/meta/ModuleVars.mk
+++
@@ -1,4 +0,0 @@
-ifeq ($(BUILDCLING),yes)
-MAKECLINGPCH := $(ROOT_SRCDIR)/build/unix/makeclingpch.sh
-ROOTCINTTMP  := $(MAKECLINGPCH) "$(LLVMCONFIG:llvm-config=clang++) $(CXXFLAGS:-fPIC=)" $(call stripsrc,$(UTILSDIRS)/rootcint_tmp$(EXEEXT))
-endif
