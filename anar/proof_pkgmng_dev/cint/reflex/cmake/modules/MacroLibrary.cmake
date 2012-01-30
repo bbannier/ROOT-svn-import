c930d8b6c223d48b4883ff6a950e690cf0bbb347
anar <anar@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-01-30 12:21:24 +0000

    Removing obsolete branch from the repository
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@42824 27541ba8-7e3a-0410-8455-c3a389f83636

--- anar/proof_pkgmng_dev/cint/reflex/cmake/modules/MacroLibrary.cmake
+++
@@ -1,9 +0,0 @@
-# MacroLibrary
-#   Offers a collection of macros which extend the built-in cmake commands
-
-INCLUDE(MacroAdditionalCleanFiles)
-INCLUDE(MacroAddLinkFlags)
-INCLUDE(MacroEnsureOutOfSourceBuild)
-INCLUDE(MacroFindPackageIf)
-INCLUDE(MacroGetTargetDirectory)
-INCLUDE(MacroParseArguments)
