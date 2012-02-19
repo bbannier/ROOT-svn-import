bbannier/ROOT-svn-import

commit 4ae815e788dc8196ff7978b70f7debacbbf8fec9

From Sangsu Ryu and me.     Add support for paralell dataset verification. This is implemented via a dedicated     TSelector (TSelVerifyDataSet) which is run over the list of files in the dataset via     TPacketizerFile. The file order is preserved using the recently introduced index in     TFileInfo.     Parallel verification is the now the default. For test and comparison purposes it is     still possible to verify datasets serially by specifying 'S' in the option field of     VerifyDataSet and RegisterDataSet .
    To avoid/reduce code duplication some of the static functions in TDataSetManager have
    refactorized. The overall behavior should be unchanged.


git-svn-id: https://root.cern.ch/svn/root/trunk@42882 27541ba8-7e3a-0410-8455-c3a389f83636

proof/proof/src/TProofServ.cxx
@@ -3604,6 +3604,66 @@ void TProofServ::HandleArchive(TMessage *mess, TString *slb)
 }
 
 //______________________________________________________________________________
+TMap *TProofServ::GetDataSetNodeMap(const char *dsn, TString &emsg)
+{
+   // Get a map {server-name, list-of-files} for the daset dsn to be used in
+   // TPacketizerFile. Returns a pointer to the map (ownership of the caller).
+   // Or (TMap *)0 and an error message in emsg.
+
+   TMap *fcmap = 0;
+   emsg = "";
+   
+   // Make sure we have something in input and a dataset manager
+   if (!fDataSetManager) {
+      emsg.Form("dataset manager not initialized!");
+      return fcmap;
+   }
+   if (!dsn || (dsn && strlen(dsn) <= 0)) {
+      emsg.Form("dataset name undefined!");
+      return fcmap;
+   }
+
+   TFileCollection *fc = 0;
+   // Get the dataset
+   if (!(fc = fDataSetManager->GetDataSet(dsn))) {
+      emsg.Form("requested dataset '%s' does not exists", dsn);
+      return fcmap;
+   }
+    
+   // Prepare data set map
+   fcmap = new TMap();
+
+   TIter nxf(fc->GetList());
+   TFileInfo *fiind = 0;
+   TString key;
+   while ((fiind = (TFileInfo *)nxf())) {
+      TUrl *xurl = fiind->GetCurrentUrl();
+      // Find the key for this server
+      key.Form("%s://%s", xurl->GetProtocol(), xurl->GetHostFQDN());
+      if (xurl->GetPort() > 0) 
+         key += TString::Format(":%d", xurl->GetPort());
+         // Get the map entry for this key
+      TPair *ent = 0;
+      THashList* l = 0;
+      if ((ent = (TPair *) fcmap->FindObject(key.Data()))) {
+         // Attach to the list
+         l = (THashList *) ent->Value();
+      } else {
+         // Create list 
+         l = new THashList;
+         l->SetOwner(kTRUE);
+         // Add it to the map
+         fcmap->Add(new TObjString(key.Data()), l);
+      }
+      // Add fileinfo with index to list
+      l->Add(fiind);
+   }
+  
+   // Done
+   return fcmap;
+}
+
+//______________________________________________________________________________
 void TProofServ::HandleProcess(TMessage *mess, TString *slb)
 {
    // Handle processing request.
@@ -3639,9 +3699,9 @@ void TProofServ::HandleProcess(TMessage *mess, TString *slb)
 
    if (IsTopMaster()) {
 
+      TString emsg;
       // Make sure the dataset contains the information needed
       if ((!hasNoData) && dset->GetListOfElements()->GetSize() == 0) {
-         TString emsg;
          if (TProof::AssertDataSet(dset, input, fDataSetManager, emsg) != 0) {
             SendAsynMessage(TString::Format("AssertDataSet on %s: %s",
                                  fPrefix.Data(), emsg.Data()));
@@ -3649,6 +3709,30 @@ void TProofServ::HandleProcess(TMessage *mess, TString *slb)
             // To terminate collection
             if (sync) SendLogFile();
             return;
+         }
+      } else if (hasNoData) {
+         // Check if we are required to process with TPacketizerFile a registered dataset
+         TNamed *ftp = dynamic_cast<TNamed *>(input->FindObject("PROOF_FilesToProcess"));
+         if (ftp) {
+            TString dsn(ftp->GetTitle());
+            if (!dsn.Contains(":") || dsn.BeginsWith("dataset:")) {
+               dsn.ReplaceAll("dataset:", "");
+               // Get the map for TPacketizerFile
+               TMap *fcmap = GetDataSetNodeMap(dsn, emsg);
+               if (!fcmap) {
+                  SendAsynMessage(TString::Format("HandleProcess on %s: %s",
+                                                  fPrefix.Data(), emsg.Data()));
+                  Error("HandleProcess", "%s", emsg.Data());
+                  // To terminate collection
+                  if (sync) SendLogFile();
+                  return;
+               }
+               input->Remove(ftp);
+               delete ftp;
+               fcmap->SetOwner(kTRUE);
+               fcmap->SetName("PROOF_FilesToProcess");
+               input->Add(fcmap);
+            }
          }
       }
 
@@ -3668,7 +3752,6 @@ void TProofServ::HandleProcess(TMessage *mess, TString *slb)
       SafeDelete(input);
 
       // Save input data, if any
-      TString emsg;
       if (TProof::SaveInputData(pq, fCacheDir.Data(), emsg) != 0)
          Warning("HandleProcess", "could not save input data: %s", emsg.Data());
 
@@ -4300,8 +4383,10 @@ void TProofServ::ProcessNext(TString *slb)
       if (psr) {
          if (RegisterDataSets(input, fPlayer->GetOutputList()) != 0)
             Warning("ProcessNext", "problems registering produced datasets");
-         fPlayer->GetOutputList()->Remove(psr);
-         delete psr;
+         do {
+            fPlayer->GetOutputList()->Remove(psr);
+            delete psr;
+         } while ((psr = (TNamed *) fPlayer->GetOutputList()->FindObject("PROOFSERV_RegisterDataSet")));
       }
    }
 
@@ -4384,6 +4469,11 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
          if (!(fcn = (TNamed *) out->FindObject(tag))) continue;
          // Register option
          TString regopt(fcn->GetTitle());
+         // Sort according to the internal index, if required
+         if (regopt.Contains(":sortidx:")) {
+            ds->Sort(kTRUE);
+            regopt.ReplaceAll(":sortidx:", "");
+         }
          // Register this dataset
          if (fDataSetManager) {
             if (fDataSetManager->TestBit(TDataSetManager::kAllowRegister)) {
@@ -4429,9 +4519,11 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
             Error("RegisterDataSets", "dataset manager is undefined!");
             return -1;
          }
-         // Cleanup temporary stuff
-         out->Remove(fcn);
-         SafeDelete(fcn);
+         // Cleanup all temporary stuff (there may be more objects with the same name, created by each worker)
+         do {
+            out->Remove(fcn);
+            SafeDelete(fcn);
+         } while ((fcn = (TNamed *) out->FindObject(tag)));
       }
    }
 

proof/proof/inc/LinkDef.h
@@ -56,6 +56,7 @@
 
 #pragma link C++ class TDataSetManager;
 #pragma link C++ class TDataSetManagerFile;
+#pragma link C++ class TSelVerifyDataSet+;
 
 // For backward compatibility with old client / masters
 #pragma link C++ class std::pair<TDSetElement*, TString>;

proof/proof/inc/TDataSetManager.h
@@ -41,7 +41,7 @@ class TFileInfo;
 class TMD5;
 class TUrl;
 class TVirtualMonitoringWriter;
-
+class TFileStager;
 
 class TDataSetManager : public TObject {
 
@@ -152,6 +152,14 @@ public:
                                         Int_t *touched = 0, Int_t *opened = 0, Int_t *disappeared = 0,
                                         TList *flist = 0, Long64_t avgsz = -1, const char *mss = 0,
                                         Int_t maxfiles = -1, const char *stageopts = 0);
+   static Bool_t            CheckStagedStatus(TFileInfo *fileInfo, Int_t fopt, Int_t maxfiles, 
+                                              Int_t newstagedfiles, TFileStager* stager, Bool_t createStager,
+                                              Bool_t dbg, Bool_t &changed, Bool_t& touched,
+                                              Bool_t& disappeared);
+   static void              ProcessFile(TFileInfo *fileInfo, Int_t sopt, Bool_t checkstg, Bool_t doall,
+                                        TFileStager* stager, Bool_t createStager, const char* stageopts,
+                                        Bool_t dbg, Bool_t &changed, Bool_t& opened);
+
    static Int_t             ScanFile(TFileInfo *fileinfo, Bool_t notify);
    static Int_t             FillMetaData(TFileInfo *fi, TDirectory *d, const char *rdir = "/");
 

proof/proof/inc/TProofServ.h
@@ -43,25 +43,26 @@
 #include "TProofQueryResult.h"
 #endif
 
+class TDataSetManager;
 class TDSet;
+class TDSetElement;
+class TFileCollection;
+class TFileHandler;
+class THashList;
+class TIdleTOTimer;
+class TList;
+class TMap;
+class TMessage;
+class TMonitor;
+class TMutex;
 class TProof;
-class TVirtualProofPlayer;
 class TProofLockPath;
 class TQueryResultManager;
-class TSocket;
-class THashList;
-class TList;
-class TDSetElement;
-class TMessage;
-class TShutdownTimer;
 class TReaperTimer;
-class TIdleTOTimer;
-class TMutex;
-class TFileCollection;
-class TDataSetManager;
-class TFileHandler;
-class TMonitor;
 class TServerSocket;
+class TShutdownTimer;
+class TSocket;
+class TVirtualProofPlayer;
 
 // Hook to external function setting up authentication related stuff
 // for old versions.
@@ -200,6 +201,7 @@ private:
    Int_t         SendResults(TSocket *sock, TList *outlist = 0, TQueryResult *pq = 0);
    Bool_t        AcceptResults(Int_t connections, TVirtualProofPlayer *mergerPlayer);
    
+   TMap         *GetDataSetNodeMap(const char *dsn, TString &emsg);
    Int_t         RegisterDataSets(TList *in, TList *out);
 
    // Waiting queries handlers

proof/proof/inc/TSelVerifyDataSet.h
@@ -1,0 +1,127 @@
+// Author: Sangsu Ryu 28/06/2011
+
+/*************************************************************************
+ * Copyright (C) 1995-2005, Rene Brun and Fons Rademakers.               *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+#ifndef ROOT_TSelVerifyDataSet
+#define ROOT_TSelVerifyDataSet
+
+//////////////////////////////////////////////////////////////////////////
+//                                                                      //
+// TSelVerifyDataSet                                                    //
+//                                                                      //
+// PROOF selector to parallel-process dataset on workers                //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifndef ROOT_TSelector
+#include <TSelector.h>
+#endif
+#ifndef ROOT_TString
+#include <TString.h>
+#endif
+
+class TTree;
+class TFileCollection;
+
+class TSelVerifyDataSet : public TSelector {
+
+private:
+
+   Int_t fFopt;
+   Int_t fSopt;
+   Int_t fRopt;
+
+   // File selection, Reopen and Touch options
+   Bool_t fAllf;
+   Bool_t fCheckstg;
+   Bool_t fNonStgf;
+   Bool_t fReopen;
+   Bool_t fTouch;
+   Bool_t fStgf;
+
+   // File processing options
+   Bool_t fNoaction;
+   Bool_t fFullproc;
+   Bool_t fLocateonly;
+   Bool_t fStageonly;
+
+   // Run options
+   Bool_t fDoall;
+   Bool_t fGetlistonly;
+   Bool_t fScanlist;
+
+   Bool_t fDbg;
+
+   TString fMss;
+   TString fStageopts;
+
+   Bool_t fChangedDs;
+   Int_t fTouched;
+   Int_t fOpened;
+   Int_t fDisappeared;
+
+   TFileCollection *fSubDataSet; // Sub-dataset being verified
+
+public :
+
+   TSelVerifyDataSet(TTree *);
+   TSelVerifyDataSet();
+   virtual ~TSelVerifyDataSet() {}
+   virtual Int_t   Version() const {return 1;}
+   virtual void    Begin(TTree *);
+   virtual void    SlaveBegin(TTree *tree);
+   virtual void    Init(TTree *tree);
+   virtual Bool_t  Notify();
+   virtual Bool_t  Process(Long64_t entry);
+   virtual void    SetOption(const char *option) { fOption = option; }
+   virtual void    SetObject(TObject *obj) { fObject = obj; }
+   virtual void    SetInputList(TList *input) {fInput = input;}
+   virtual TList  *GetOutputList() const { return fOutput; }
+   virtual void    SlaveTerminate();
+   virtual void    Terminate();
+
+   ClassDef(TSelVerifyDataSet,0) //PROOF selector for parallel dataset verification
+};
+
+#endif
+
+#ifdef TSelVerifyDataSet_cxx
+void TSelVerifyDataSet::Init(TTree *)
+{
+   fFopt = -1;
+   fSopt = 0;
+   fRopt = 0;
+
+   fAllf = 0;
+   fCheckstg = 0;
+   fNonStgf = 0;
+   fReopen = 0;
+   fTouch = 0;
+   fStgf = 0;
+   fNoaction = 0;
+   fFullproc = 0;
+   fLocateonly = 0;
+   fStageonly = 0;
+   fDoall       = 0;
+   fGetlistonly = 0;
+   fScanlist    = 0;
+   fDbg = 0;
+
+   fChangedDs = kFALSE;
+   fTouched = 0;
+   fOpened = 0;
+   fDisappeared = 0;
+   fSubDataSet = 0;
+}
+
+Bool_t TSelVerifyDataSet::Notify()
+{
+   return kTRUE;
+}
+#endif // #ifdef TSelVerifyDataSet_cxx

proof/proof/src/TProof.cxx
@@ -9695,7 +9695,7 @@ Int_t TProof::UploadDataSetFromFile(const char *, const char *, const char *, In
 
 //______________________________________________________________________________
 Bool_t TProof::RegisterDataSet(const char *dataSetName,
-                               TFileCollection *dataSet, const char* optStr)
+                               TFileCollection *dataSet, const char *optStr)
 {
    // Register the 'dataSet' on the cluster under the current
    // user, group and the given 'dataSetName'.
@@ -9707,6 +9707,8 @@ Bool_t TProof::RegisterDataSet(const char *dataSetName,
    // is configured to allow so). By default the dataset is not verified.
    // If 'opts' contains 'T' the in the dataset object (status bits, meta,...)
    // is trusted, i.e. not reset (if the dataset manager is configured to allow so).
+   // If 'opts' contains 'S' validation would be run serially (meaningful only if
+   // validation is required).
    // Returns kTRUE on success.
 
    // Check TFileInfo compatibility
@@ -9721,10 +9723,20 @@ Bool_t TProof::RegisterDataSet(const char *dataSetName,
       return kFALSE;
    }
 
+   Bool_t parallelverify = kFALSE;
+   TString sopt(optStr);
+   if (sopt.Contains("V") && fProtocol >= 34 && !sopt.Contains("S")) {
+      // We do verification in parallel later on; just register for now
+      parallelverify = kTRUE;
+      sopt.ReplaceAll("V", "");
+   }
+   // This would screw up things remotely, make sure is not there
+   sopt.ReplaceAll("S", "");
+
    TMessage mess(kPROOF_DATASETS);
    mess << Int_t(kRegisterDataSet);
    mess << TString(dataSetName);
-   mess << TString(optStr);
+   mess << sopt;
    mess.WriteObject(dataSet);
    Broadcast(mess);
 
@@ -9733,8 +9745,21 @@ Bool_t TProof::RegisterDataSet(const char *dataSetName,
    if (fStatus != 0) {
       Error("RegisterDataSet", "dataset was not saved");
       result = kFALSE;
+      return result;
    }
-   return result;
+
+   // If old server or not verifying in parallel we are done
+   if (!parallelverify) return result;
+   
+   // If we are here it means that we will verify in parallel
+   sopt += "V";
+   if (VerifyDataSet(dataSetName, sopt) < 0){
+      Error("RegisterDataSet", "problems verifying dataset '%s'", dataSetName);
+      return kFALSE;
+   }
+
+   // We are done
+   return kTRUE; 
 }
 
 //______________________________________________________________________________
@@ -9993,32 +10018,132 @@ TList* TProof::FindDataSets(const char* /*searchString*/, const char* /*optStr*/
 }
 
 //______________________________________________________________________________
-Int_t TProof::VerifyDataSet(const char *uri, const char* optStr)
+Int_t TProof::VerifyDataSet(const char *uri, const char *optStr)
 {
    // Verify if all files in the specified dataset are available.
    // Print a list and return the number of missing files.
+   // Returns -1 in case of error.
 
    if (fProtocol < 15) {
       Info("VerifyDataSet", "functionality not available: the server has an"
                             " incompatible version of TFileInfo");
-      return kError;
+      return -1;
    }
 
-   Int_t nMissingFiles = 0;
-   TMessage nameMess(kPROOF_DATASETS);
-   nameMess << Int_t(kVerifyDataSet);
-   nameMess << TString(uri ? uri : "");
-   nameMess << TString(optStr ? optStr : "");
-   Broadcast(nameMess);
+   // Sanity check
+   if (!uri || (uri && strlen(uri) <= 0)) {
+      Error("VerifyDataSet", "dataset name is is mandatory");
+      return -1;
+   }
 
-   Collect(kActive, fCollectTimeout);
+   Int_t nmissingfiles = 0;
 
-   if (fStatus < 0) {
-      Info("VerifyDataSet", "no such dataset %s", uri);
-      return  -1;
-   } else
-      nMissingFiles = fStatus;
-   return nMissingFiles;
+   TString sopt(optStr);
+   if (fProtocol < 34 || sopt.Contains("S")) {
+      sopt.ReplaceAll("S", "");
+      Info("VerifyDataSet", "Master-only verification");
+      TMessage nameMess(kPROOF_DATASETS);
+      nameMess << Int_t(kVerifyDataSet);
+      nameMess << TString(uri ? uri : "");
+      nameMess << sopt;
+      Broadcast(nameMess);
+
+      Collect(kActive, fCollectTimeout);
+
+      if (fStatus < 0) {
+         Info("VerifyDataSet", "no such dataset %s", uri);
+         return  -1;
+      } else
+         nmissingfiles = fStatus;
+      return nmissingfiles;
+   }
+
+   //Let PROOF master prepare node-files map
+   SetParameter("PROOF_FilesToProcess", Form("dataset:%s", uri));
+
+   // Use TPacketizerFile
+   TString oldpack;
+   if (TProof::GetParameter(GetInputList(), "PROOF_Packetizer", oldpack) != 0) oldpack = "";
+   SetParameter("PROOF_Packetizer", "TPacketizerFile");
+
+   // Add dataset name
+   SetParameter("PROOF_VerifyDataSet", uri);
+   // Add options
+   SetParameter("PROOF_VerifyDataSetOption", optStr);
+   Int_t oldifiip = -1;
+   if (TProof::GetParameter(GetInputList(), "PROOF_IncludeFileInfoInPacket", oldifiip) != 0) oldifiip = -1;
+   SetParameter("PROOF_IncludeFileInfoInPacket", (Int_t)1);
+
+   // TO DO : figure out mss and stageoption
+   const char* mss="";
+   SetParameter("PROOF_MSS", mss);
+   const char* stageoption="";
+   SetParameter("PROOF_StageOption", stageoption);
+
+   GetInputList()->Print();
+
+   // Process verification in parallel
+   Process("TSelVerifyDataSet", (Long64_t) 1);
+
+   // Restore packetizer
+   if (!oldpack.IsNull())
+      SetParameter("PROOF_Packetizer", oldpack);
+   else
+      DeleteParameters("PROOF_Packetizer");
+
+   // Delete or restore parameters
+   DeleteParameters("PROOF_FilesToProcess");
+   DeleteParameters("PROOF_VerifyDataSet");
+   DeleteParameters("PROOF_VerifyDataSetOption");
+   DeleteParameters("PROOF_MSS");
+   DeleteParameters("PROOF_StageOption");
+   if (oldifiip > -1) {
+      SetParameter("PROOF_IncludeFileInfoInPacket", oldifiip);
+   } else {
+      DeleteParameters("PROOF_IncludeFileInfoInPacket");
+   }
+
+   // Merge outputs
+   Int_t nopened = 0;
+   Int_t ntouched = 0;
+   Bool_t changed_ds = kFALSE;
+
+   TIter nxtout(GetOutputList());
+   TObject* obj;
+   TList *lfiindout = new TList;
+   while ((obj = nxtout())) {
+      TList *l = dynamic_cast<TList *>(obj);
+      if (l && TString(l->GetName()).BeginsWith("PROOF_ListFileInfos_")) {
+         TIter nxt(l);
+         TFileInfo *fiindout = 0;
+         while ((fiindout = (TFileInfo*) nxt())) {
+            lfiindout->Add(fiindout);
+         }
+      }
+      // Add up number of disppeared files
+      TParameter<Int_t>* pdisappeared = dynamic_cast<TParameter<Int_t>*>(obj);
+      if ( pdisappeared && TString(pdisappeared->GetName()).BeginsWith("PROOF_NoFilesDisppeared_")) {
+         nmissingfiles += pdisappeared->GetVal();
+      }
+      TParameter<Int_t>* pnopened = dynamic_cast<TParameter<Int_t>*>(obj);
+      if (pnopened && TString(pnopened->GetName()).BeginsWith("PROOF_NoFilesOpened_")) {
+         nopened += pnopened->GetVal();
+      }
+      TParameter<Int_t>* pntouched = dynamic_cast<TParameter<Int_t>*>(obj);
+      if (pntouched && TString(pntouched->GetName()).BeginsWith("PROOF_NoFilesTouched_")) {
+         ntouched += pntouched->GetVal();
+      }
+      TParameter<Bool_t>* pchanged_ds = dynamic_cast<TParameter<Bool_t>*>(obj);
+      if (pchanged_ds && TString(pchanged_ds->GetName()).BeginsWith("PROOF_DataSetChanged_")) {
+         if (pchanged_ds->GetVal() == kTRUE) changed_ds = kTRUE;
+      }
+   }
+
+   Info("VerifyDataset", "%s: changed? %d (# files opened = %d, # files touched = %d,"
+                         " # missing files = %d)",
+                         uri, changed_ds, nopened, ntouched, nmissingfiles);
+
+   return nmissingfiles;
 }
 
 //______________________________________________________________________________

commit 0dad4de019ceae3ee9d5d852f956863ba0d8afa1

Fix issue with in-loop list modification introduced by r42882
git-svn-id: https://root.cern.ch/svn/root/trunk@43055 27541ba8-7e3a-0410-8455-c3a389f83636

proof/proof/src/TProofServ.cxx
@@ -4503,10 +4503,13 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
 {
    // Register TFileCollections in 'out' as datasets according to the rules in 'in'
 
-   PDB(kDataset, 1) Info("RegisterDataSets", "enter");
+   PDB(kDataset, 1)
+      Info("RegisterDataSets", "enter: %d objs in the output list", (out ? out->GetSize() : -1));
 
    if (!in || !out) return 0;
 
+   THashList tags;
+   TList torm;
    TString msg;
    TIter nxo(out);
    TObject *o = 0;
@@ -4518,6 +4521,11 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
          TNamed *fcn = 0;
          TString tag = TString::Format("DATASET_%s", ds->GetName());
          if (!(fcn = (TNamed *) out->FindObject(tag))) continue;
+         // If this tag is in the list of processed tags, flag it for removal
+         if (tags.FindObject(tag)) {
+            torm.Add(o);
+            continue;
+         }
          // Register option
          TString regopt(fcn->GetTitle());
          // Sort according to the internal index, if required
@@ -4552,6 +4560,9 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
                   } else {
                      Info("RegisterDataSets", "dataset '%s' successfully registered", ds->GetName());
                      msg.Form("Registering and verifying dataset '%s' ... OK", ds->GetName());
+                     // Add tag to the list of processed tags to avoid double processing (there may be more objects with
+                     // the same name, created by each worker)
+                     tags.Add(new TObjString(tag));
                   }
                   SendAsynMessage(msg.Data(), kTRUE);
                   // Notify
@@ -4570,13 +4581,13 @@ Int_t TProofServ::RegisterDataSets(TList *in, TList *out)
             Error("RegisterDataSets", "dataset manager is undefined!");
             return -1;
          }
-         // Cleanup all temporary stuff (there may be more objects with the same name, created by each worker)
-         do {
-            out->Remove(fcn);
-            SafeDelete(fcn);
-         } while ((fcn = (TNamed *) out->FindObject(tag)));
       }
    }
+   // Cleanup all temporary stuff possibly created by each worker
+   TIter nxrm(&torm);
+   while ((o = nxrm())) out->Remove(o);
+   tags.SetOwner(kTRUE);
+   torm.SetOwner(kTRUE);
 
    PDB(kDataset, 1) Info("RegisterDataSets", "exit");
    // Done