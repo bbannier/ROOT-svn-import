bd55f20fed0941636fcf687920d09942a0026a14
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-02-29 13:16:06 +0000

    Move command buffer into namespace X11 (as, for example, event translator). Initialize command buffer in TGCocoa's ctor.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@43174 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/graf2d/macosx/inc/X11Buffer.h
+++ macosx/graf2d/macosx/inc/X11Buffer.h
@@ -23,94 +23,95 @@
 namespace ROOT {
 namespace MacOSX {
 
-class X11Command {
+namespace Details {
+class CocoaPrivate;
+}
+
+namespace X11 {
+
+class Command {
 protected:
    const Drawable_t fID;
    const GContext_t fGC;
    
 public:
-   X11Command(Drawable_t wid, GContext_t gc);
-   virtual ~X11Command();
+   Command(Drawable_t wid, GContext_t gc);
+   virtual ~Command();
 
    virtual void Execute()const = 0;
    
-   X11Command(const X11Command &rhs) = delete;
-   X11Command &operator = (const X11Command &rhs) = delete;
+   Command(const Command &rhs) = delete;
+   Command &operator = (const Command &rhs) = delete;
 };
 
-class X11DrawLine : public X11Command {
+class DrawLine : public Command {
 private:
    const Point_t fP1;
    const Point_t fP2;
 
 public:
-   X11DrawLine(Drawable_t wid, GContext_t gc, const Point_t &p1, const Point_t &p2);
+   DrawLine(Drawable_t wid, GContext_t gc, const Point_t &p1, const Point_t &p2);
    void Execute()const;
 };
 
-class X11ClearArea : public X11Command {
+class ClearArea : public Command {
 private:
    const Rectangle_t fArea;
 
 public:
-   X11ClearArea(Window_t wid, const Rectangle_t &area);
+   ClearArea(Window_t wid, const Rectangle_t &area);
    void Execute()const;
 };
 
-class X11CopyArea : public X11Command {
+class CopyArea : public Command {
 private:
    const Drawable_t  fSrc;
    const Rectangle_t fArea;
    const Point_t     fDstPoint;
 
 public:
-   X11CopyArea(Drawable_t src, Drawable_t dst, GContext_t gc, const Rectangle_t &area, const Point_t &dstPoint);
+   CopyArea(Drawable_t src, Drawable_t dst, GContext_t gc, const Rectangle_t &area, const Point_t &dstPoint);
    void Execute()const;
 };
 
-class X11DrawString : public X11Command {
+class DrawString : public Command {
 private:
    const Point_t     fPoint;
    const std::string fText;
 
 public:
-   X11DrawString(Drawable_t wid, GContext_t gc, const Point_t &point, const std::string &text);
+   DrawString(Drawable_t wid, GContext_t gc, const Point_t &point, const std::string &text);
    void Execute()const;
 };
 
-class X11FillRectangle : public X11Command {
+class FillRectangle : public Command {
 private:
    const Rectangle_t fRectangle;
 
 public:
-   X11FillRectangle(Drawable_t wid, GContext_t gc, const Rectangle_t &rectangle);
+   FillRectangle(Drawable_t wid, GContext_t gc, const Rectangle_t &rectangle);
    void Execute()const;
 };
 
-class X11DrawRectangle : public X11Command {
+class DrawRectangle : public Command {
 private:
    Rectangle_t fRectangle;
 
 public:
-   X11DrawRectangle(Drawable_t wid, GContext_t gc, const Rectangle_t &rectangle);
+   DrawRectangle(Drawable_t wid, GContext_t gc, const Rectangle_t &rectangle);
    void Execute()const;
 };
 
-namespace Details {
-
-class CocoaPrivate;
-
-}
-
-class X11CommandBuffer {
+class CommandBuffer {
 private:
-   X11CommandBuffer(const X11CommandBuffer &rhs) = delete;
-   X11CommandBuffer &operator = (const X11CommandBuffer &rhs) = delete;
+   CommandBuffer(const CommandBuffer &rhs) = delete;
+   CommandBuffer &operator = (const CommandBuffer &rhs) = delete;
    
-   std::vector<X11Command *> fCommands;
+   std::vector<Command *> fCommands;
 
 public:
-   ~X11CommandBuffer();
+   CommandBuffer();
+   ~CommandBuffer();
 
    void AddDrawLine(Drawable_t wid, GContext_t gc, Int_t x1, Int_t y1, Int_t x2, Int_t y2);
    void AddClearArea(Window_t wid, Int_t x, Int_t y, UInt_t w, UInt_t h);
@@ -125,7 +126,8 @@ private:
 
 };
 
-}
-}
+}//X11
+}//MacOSX
+}//ROOT
 
 #endif
