08d9f8cb48f7963cb531e26cfcf016faeb67a648
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-03-21 09:40:11 +0000

    Update gradient examples.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@43429 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/test/macosx/test2/grad2.C
+++ macosx/test/macosx/test2/grad2.C
@@ -8,13 +8,13 @@ void grad2()
    const Double_t locations[] = {0., 1.};
    new TColor(1001, 1., 0., 0., "red", 0.5);
    const Color_t idx1[] = {kOrange, 1001};
-   new TColorGradient(1002, TColorGradient::kGDVertical, 2, locations, idx1, kTRUE);
+   new TColorGradient(1002, TColorGradient::kGDVertical, 2, locations, idx1);
    
    hist->SetFillColor(1002);
    
    new TColor(1003, 0., 1., 0., "green", 0.5);
    const Color_t idx2[] = {kBlue, 1003};
-   new TColorGradient(1004, TColorGradient::kGDVertical, 2, locations, idx2, kTRUE);
+   new TColorGradient(1004, TColorGradient::kGDVertical, 2, locations, idx2);
    
    hist2->SetFillColor(1004);
    
