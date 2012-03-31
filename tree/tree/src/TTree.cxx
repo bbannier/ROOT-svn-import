bbannier/ROOT-svn-import

commit 1605706319bf45e728c5a66e79519bb3ea8bbe35

Rework TTree::ReadStream() / ReadFile(), mainly to fix delimited reading of string columns: TLeaf::ReadValue() now takes an optional delimiter argument that is ignored for all but TLeafC. Here, input stops when reading this character, instead of at the first whitespace. Use that in TTree::ReadStream() to delimit reading of TLeafC. TTree::ReadStream() now tokenizes the row itself, and passes a stringstream containing nothing but the current column to TLeaf::ReadValue(). Improve error handling, improve debug output. Clearly state which lines are skipped due to errors and why. Fix windows files leaving '\n' in branch names when reading them from the file. Separate concepts of number of input line (for communication with user) and number of good lines (as returned). Simplify EOF-without-newline case. Add error message for TLeaf::ReadValue(), i.e. if ReadValue() is called on a derived class that doesn't implement it.
git-svn-id: https://root.cern.ch/svn/root/trunk@43597 27541ba8-7e3a-0410-8455-c3a389f83636

tree/tree/src/TTree.cxx
@@ -6270,9 +6270,6 @@ Long64_t TTree::ReadFile(const char* filename, const char* branchDescriptor, cha
    //      x:y:z      (all variables are assumed of type "F"
    //      x/D:y:z    (all variables are of type "D"
    //      x:y/D:z    (x is type "F", y and z of type "D"
-   //  -If the type is a string of characters. This will read
-   //  subsequent characters until a whitespace is found (whitespace
-   //  characters are considered to be blank, newline and tab).
    //
    //  delimiter allows for the use of another delimiter besides whitespace.
    //    This provides support for direct import of common data file formats
@@ -6285,9 +6282,6 @@ Long64_t TTree::ReadFile(const char* filename, const char* branchDescriptor, cha
    //    not specified (besides ' '), the delimiter is automatically set to ','.
    //
    // Lines in the input file starting with "#" are ignored.
-   // This function will read and ignore any whitespace characters
-   // (this includes blank spaces and the newline and tab characters).
-   // Handles newlines specified with '\n', '\r', or "\r\n".
    //
    // A TBranch object is created for each variable in the expression.
    // The total number of rows read from the file is returned.
@@ -6316,6 +6310,7 @@ Long64_t TTree::ReadFile(const char* filename, const char* branchDescriptor, cha
 char TTree::GetNewlineValue(istream &inputStream)
 {
    // Determine which newline this file is using.
+   // Return '\r' for Windows '\r\n' as that already terminates.
 
    Long_t inPos = inputStream.tellg();
    char newline = '\n';
@@ -6328,11 +6323,8 @@ char TTree::GetNewlineValue(istream &inputStream)
       }
       if(c == newline) break;
       if(c == '\r') { 
-         if(inputStream.get() == newline) break;
-         else {
-            newline = '\r';
-            break;
-         }
+         newline = '\r';
+         break;
       }
    }
    inputStream.clear();
@@ -6350,6 +6342,8 @@ Long64_t TTree::ReadStream(istream& inputStream, const char *branchDescriptor, c
    gTree = this;
    char newline = GetNewlineValue(inputStream);
    std::istream& in = inputStream;
+   Long64_t nlines = 0;
+
    TBranch *branch;
    Int_t nbranches = fBranches.GetEntries();
    if (nbranches == 0) {
@@ -6366,6 +6360,7 @@ Long64_t TTree::ReadStream(istream& inputStream, const char *branchDescriptor, c
             Error("ReadStream","Error reading stream");
             return 0;
          }
+         ++nlines;
          nch = strlen(bd);
       } else {
          strlcpy(bd,branchDescriptor,100000);
@@ -6409,54 +6404,118 @@ Long64_t TTree::ReadStream(istream& inputStream, const char *branchDescriptor, c
       delete [] bd;
    }
 
-   //loop on all lines in the file
    nbranches = fBranches.GetEntries();
-   Bool_t status = kTRUE;
-   Long64_t nlines = 0;
-   while(1) {
 
-      while (isspace(in.peek())) {
-         in.get();
+   if (gDebug > 1) {
+      Info("ReadStream", "Will use branches:");
+      for (int i = 0 ; i < nbranches; ++i) {
+         TBranch* br = (TBranch*) fBranches.At(i);
+         Info("ReadStream", "  %s: %s [%s]", br->GetName(),
+              br->GetTitle(), br->GetListOfLeaves()->At(0)->IsA()->GetName());
       }
-      if ( in.peek() != '#' ) {
-         //loop on branches and read the branch values into their buffer
-         for (Int_t i=0;i<nbranches;i++) {
-            branch = (TBranch*)fBranches.At(i);
-            TLeaf *leaf = (TLeaf*)branch->GetListOfLeaves()->At(0);
-            leaf->ReadValue(in);
-            if (in.eof()) {
-               if(i == nbranches-1) {
-                  // handle no newline char on last line
-                  Fill();
-                  nlines++;
-               }
-               return nlines;
-            }
-            status = in.good();
-            if (!status) {
-               if(in.fail() && ! in.bad()) { // just couldn't interpret formatted data
-                  Warning("ReadStream","Couldn't read formatted data for branch %s on line %lld",branch->GetName(),nlines+1);
-                  in.clear();
-                  status = kTRUE;
-               }
-               else {
-                  Warning("ReadStream","Illegal value after line %lld",nlines);
-                  in.clear();
-                  break;
-               }
-            }
-            if(in.peek() == delimiter) in.get();
-         }
-         //we are now ready to fill the tree
-         if (status) {
-            Fill();
-            nlines++;
-         }
+      if (gDebug > 3) {
+         Info("ReadStream", "Dumping read tokens, format:");
+         Info("ReadStream", "LLLLL:BBB:gfbe:GFBE:T");
+         Info("ReadStream", "   L: line number");
+         Info("ReadStream", "   B: branch number");
+         Info("ReadStream", "   gfbe: good / fail / bad / eof of token");
+         Info("ReadStream", "   GFBE: good / fail / bad / eof of file");
+         Info("ReadStream", "   T: Token being read");
       }
-      in.ignore(8192,newline);
    }
 
-   return nlines;
+   //loop on all lines in the file
+   Long64_t nGoodLines = 0;
+   std::string line;
+   const char sDelim[2] = { delimiter, 0 };
+   while(in.good()) {
+      if (newline == '\r' && in.peek() == '\n') {
+         // Windows, skip '\n':
+         in.get();
+      }
+      std::getline(in, line, newline);
+      ++nlines;
+
+      if (line[0] == '#') {
+         if (gDebug > 2) {
+            Info("ReadStream", "Skipping comment line '%s'", line.c_str());
+         }
+         continue;
+      }
+      if (gDebug > 2) {
+         Info("ReadStream", "Parsing line '%s'", line.c_str());
+      }
+
+      // Loop on branches and read the branch values into their buffer
+      std::stringstream sToken;
+      TString sLine(line);
+      TString tok;
+      Ssiz_t pos = 0;
+      Int_t iBranch = 0;
+      Bool_t goodLine = kTRUE;
+      while (goodLine && iBranch < nbranches
+             && sLine.Tokenize(tok, pos, sDelim)) {
+         branch = (TBranch*)fBranches.At(iBranch++);
+         TLeaf *leaf = (TLeaf*)branch->GetListOfLeaves()->At(0);
+         sToken.clear();
+         sToken.seekp(0, std::ios_base::beg);
+         sToken.seekg(0, std::ios_base::beg);
+         sToken.str(tok.Data());
+         leaf->ReadValue(sToken, 0 /* 0 = "all" */);
+         if (gDebug > 3) {
+            Info("ReadStream", "%5lld:%3d:%d%d%d%d:%d%d%d%d:%s",
+                 nlines, iBranch,
+                 (int)sToken.good(), (int)sToken.fail(),
+                 (int)sToken.bad(), (int)sToken.eof(),
+                 (int)in.good(), (int)in.fail(),
+                 (int)in.bad(), (int)in.eof(),
+                 sToken.str().c_str());
+         }
+
+         // Error handling
+         if (sToken.bad()) {
+            // How could that happen for a stringstream?
+            Warning("ReadStream",
+                    "Buffer error while reading data for branch %s on line %lld",
+                    branch->GetName(), nlines);
+         } else if (!sToken.eof()) {
+            if (sToken.fail()) {
+               Warning("ReadStream",
+                       "Couldn't read formatted data in \"%s\" for branch %s on line %lld; ignoring line",
+                       tok.Data(), branch->GetName(), nlines);
+               goodLine = kFALSE;
+            } else {
+               std::string remainder;
+               std::getline(sToken, remainder, newline);
+               if (!remainder.empty()) {
+                  Warning("ReadStream",
+                          "Ignoring trailing \"%s\" while reading data for branch %s on line %lld",
+                          remainder.c_str(), branch->GetName(), nlines);
+               }
+            }
+         }
+      } // tokenizer loop
+
+      if (iBranch < nbranches) {
+         Warning("ReadStream",
+                 "Read too few columns (%d < %d) in line %lld; ignoring line",
+                 iBranch, nbranches, nlines);
+         goodLine = kFALSE;
+      } else if (pos != kNPOS) {
+         Warning("ReadStream",
+                 "Ignoring trailing \"%s\" while reading line %lld",
+                 sLine.Data() + pos - 1 /* also print delimiter */,
+                 nlines);
+      }
+
+      //we are now ready to fill the tree
+      if (goodLine) {
+         Fill();
+         ++nGoodLines;
+      }
+   }
+
+   return nGoodLines;
 }
 
 //______________________________________________________________________________

tree/tree/inc/TLeaf.h
@@ -83,7 +83,9 @@ public:
    virtual void     PrintValue(Int_t i = 0) const;
    virtual void     ReadBasket(TBuffer&) {}
    virtual void     ReadBasketExport(TBuffer&, TClonesArray*, Int_t) {}
-   virtual void     ReadValue(istream& /*s*/) {}
+   virtual void     ReadValue(istream& /*s*/, Char_t /*delim*/ = ' ') {
+      Error("ReadValue", "Not implemented!");
+   }
            Int_t    ResetAddress(void* add, Bool_t destructor = kFALSE);
    virtual void     SetAddress(void* add = 0);
    virtual void     SetBranch(TBranch* branch) { fBranch = branch; }

tree/tree/inc/TLeafB.h
@@ -49,7 +49,7 @@ public:
    virtual void    PrintValue(Int_t i = 0) const;
    virtual void    ReadBasket(TBuffer&);
    virtual void    ReadBasketExport(TBuffer&, TClonesArray* list, Int_t n);
-   virtual void    ReadValue(istream&);
+   virtual void    ReadValue(istream &s, Char_t delim = ' ');
    virtual void    SetAddress(void* addr = 0);
    virtual void    SetMaximum(Char_t max) { fMaximum = max; }
    virtual void    SetMinimum(Char_t min) { fMinimum = min; }

tree/tree/inc/TLeafC.h
@@ -50,7 +50,7 @@ public:
    virtual void    PrintValue(Int_t i=0) const;
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
-   virtual void    ReadValue(istream & s);
+   virtual void    ReadValue(istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    virtual void    SetMaximum(Int_t max) {fMaximum = max;}
    virtual void    SetMinimum(Int_t min) {fMinimum = min;}

tree/tree/inc/TLeafD.h
@@ -47,7 +47,7 @@ public:
    virtual void    PrintValue(Int_t i=0) const;
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
-   virtual void    ReadValue(istream & s);
+   virtual void    ReadValue(istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    
    ClassDef(TLeafD,1);  //A TLeaf for a 64 bit floating point data type.

tree/tree/inc/TLeafF.h
@@ -47,7 +47,7 @@ public:
    virtual void    PrintValue(Int_t i=0) const;
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
-   virtual void    ReadValue(istream & s);
+   virtual void    ReadValue(istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    
    ClassDef(TLeafF,1);  //A TLeaf for a 32 bit floating point data type.

tree/tree/inc/TLeafI.h
@@ -50,7 +50,7 @@ public:
    virtual void    PrintValue(Int_t i=0) const;
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
-   virtual void    ReadValue(istream & s);
+   virtual void    ReadValue(istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    virtual void    SetMaximum(Int_t max) {fMaximum = max;}
    virtual void    SetMinimum(Int_t min) {fMinimum = min;}

tree/tree/inc/TLeafL.h
@@ -50,7 +50,7 @@ public:
    virtual void    PrintValue(Int_t i=0) const;
    virtual void    ReadBasket(TBuffer &b);
    virtual void    ReadBasketExport(TBuffer &b, TClonesArray *list, Int_t n);
-   virtual void    ReadValue(istream & s);
+   virtual void    ReadValue(istream& s, Char_t delim = ' ');
    virtual void    SetAddress(void *add=0);
    virtual void    SetMaximum(Long64_t max) {fMaximum = max;}
    virtual void    SetMinimum(Long64_t min) {fMinimum = min;}

commit 0ca1daff346f72ae0802373116c3db7aec881005

Fix errors introduced by r43597: * skip empty lines * for backward compatibility, skip leading whitespace for each column * handle leaves with length > 1 (e.g. /F[2] but *not* C which also sets its fLen!). They need multiple tokens, and they expect them to be separated by ' '. * document the various local variables Fixes roottest.
git-svn-id: https://root.cern.ch/svn/root/trunk@43599 27541ba8-7e3a-0410-8455-c3a389f83636

tree/tree/src/TTree.cxx
@@ -6281,7 +6281,8 @@ Long64_t TTree::ReadFile(const char* filename, const char* branchDescriptor, cha
    //    If the filename ends with extensions .csv or .CSV and a delimiter is
    //    not specified (besides ' '), the delimiter is automatically set to ','.
    //
-   // Lines in the input file starting with "#" are ignored.
+   // Lines in the input file starting with "#" are ignored. Leading whitespace
+   //   for each column data is skipped. Empty lines are skipped.
    //
    // A TBranch object is created for each variable in the expression.
    // The total number of rows read from the file is returned.
@@ -6436,31 +6437,69 @@ Long64_t TTree::ReadStream(istream& inputStream, const char *branchDescriptor, c
       std::getline(in, line, newline);
       ++nlines;
 
-      if (line[0] == '#') {
+      TString sLine(line);
+      sLine = sLine.Strip(TString::kLeading); // skip leading whitespace
+      if (sLine.IsNull()) {
          if (gDebug > 2) {
-            Info("ReadStream", "Skipping comment line '%s'", line.c_str());
+            Info("ReadStream", "Skipping empty line number %lld", nlines);
+         }
+         continue; // silently skip empty lines
+      }
+      if (sLine[0] == '#') {
+         if (gDebug > 2) {
+            Info("ReadStream", "Skipping comment line number %lld: '%s'",
+                 nlines, line.c_str());
          }
          continue;
       }
       if (gDebug > 2) {
-         Info("ReadStream", "Parsing line '%s'", line.c_str());
+         Info("ReadStream", "Parsing line number %lld: '%s'",
+              nlines, line.c_str());
       }
 
       // Loop on branches and read the branch values into their buffer
-      std::stringstream sToken;
-      TString sLine(line);
-      TString tok;
+      TString tok; // one column's data
+      TString leafData; // leaf data, possibly multiple tokens for e.g. /I[2]
+      std::stringstream sToken; // string stream feeding leafData into leaves
       Ssiz_t pos = 0;
       Int_t iBranch = 0;
-      Bool_t goodLine = kTRUE;
+      Bool_t goodLine = kTRUE; // whether the row can be filled into the tree
+      Int_t remainingLeafLen = 0; // remaining columns for the current leaf
       while (goodLine && iBranch < nbranches
              && sLine.Tokenize(tok, pos, sDelim)) {
-         branch = (TBranch*)fBranches.At(iBranch++);
+         tok = tok.Strip(TString::kLeading); // skip leading whitespace
+
+         if (!remainingLeafLen) {
+            // next branch!
+            branch = (TBranch*)fBranches.At(iBranch);
+         }
          TLeaf *leaf = (TLeaf*)branch->GetListOfLeaves()->At(0);
+         if (!remainingLeafLen) {
+            remainingLeafLen = leaf->GetLen();
+            if (leaf->GetMaximum() > 0) {
+               // This is a dynamic leaf length, i.e. most likely a TLeafC's
+               // string size. This still translates into one token:
+               remainingLeafLen = 1;
+            }
+
+            leafData = tok;
+         } else {
+            // append token to laf data:
+            leafData += " ";
+            leafData += tok;
+         }
+         --remainingLeafLen;
+         if (remainingLeafLen) {
+            // need more columns for this branch:
+            continue;
+         }
+         ++iBranch;
+
+         // initialize stringstream with token
          sToken.clear();
          sToken.seekp(0, std::ios_base::beg);
+         sToken.str(leafData.Data());
          sToken.seekg(0, std::ios_base::beg);
-         sToken.str(tok.Data());
          leaf->ReadValue(sToken, 0 /* 0 = "all" */);
          if (gDebug > 3) {
             Info("ReadStream", "%5lld:%3d:%d%d%d%d:%d%d%d%d:%s",