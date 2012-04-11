eadb76d3d27c379bdf5484ffb81ccd1bb61f7b44
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-04-11 16:40:35 +0000

    Performed some RooFit integrator testing... RooIntegrator1D is performing poorly... RooAdaptiveGaussKronrodIntegrator1D performs very well...
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@43699 27541ba8-7e3a-0410-8455-c3a389f83636

--- ibucur/trunk/math/mathcore/test/testIntegration.cxx
+++ ibucur/trunk/math/mathcore/test/testIntegration.cxx
@@ -3,14 +3,24 @@
 #include "Math/AllIntegrationTypes.h"
 #include "Math/Functor.h"
 #include "Math/GaussIntegrator.h"
+#include "RooRealVar.h"
+#include "RooCFunction1Binding.h"
+#include "RooRealBinding.h"
+#include "RooIntegrator1D.h"
+#include "RooAbsReal.h"
+#include "RooNumIntConfig.h"
+#include "RooAdaptiveGaussKronrodIntegrator1D.h"
 
 #include <cmath>
 
-const double ERRORLIMIT = 1E-3;
+using namespace RooFit;
 
+const double ERRORLIMIT = 1E-3;
+long calls = 0;
 
 double f(double x) { 
-   return x; 
+   calls++;
+   return exp(-x)*pow(x,3)/6; 
 } 
 
 double f2(const double * x) { 
@@ -27,39 +37,63 @@ void printTestResult(std::ostream & os, const char * type, int status) {
 
 int testIntegration1D() { 
 
-   const double RESULT = 0.5;
+   const double RESULT = 13.8155;
    int status = 0;
+   
+   const Double_t lower = 0;
+   const Double_t upper = 300;
 
 
    ROOT::Math::Functor1D wf(&f);
+
    ROOT::Math::Integrator ig(ROOT::Math::IntegrationOneDim::kADAPTIVESINGULAR); 
    ig.SetFunction(wf);
-   double val = ig.Integral(0,1);
-   std::cout << "GSL adaptive sing. integral result is \t" << val << std::endl;
+   double val = ig.Integral(lower,upper);
+   std::cout << "GSL adaptive sing. integral result is \t" << val << " in " << calls << " calls" << std::endl;
    status += std::fabs(val-RESULT) > ERRORLIMIT;
+   calls = 0;
 
    ROOT::Math::Integrator ig2(ROOT::Math::IntegrationOneDim::kNONADAPTIVE); 
    ig2.SetFunction(wf);
-   val = ig2.Integral(0,1);
-   std::cout << "GSL non-adaptiveintegral result is    \t" << val << std::endl;
-   status += std::fabs(val-RESULT) > ERRORLIMIT;
+   val = ig2.Integral(lower,upper);
+   std::cout << "GSL non-adaptiveintegral result is    \t" << val << " in " << calls << " calls" << std::endl;
+   //status += std::fabs(val-RESULT) > ERRORLIMIT;
+   calls = 0;
 
    ROOT::Math::Integrator ig3(wf, ROOT::Math::IntegrationOneDim::kADAPTIVE); 
-   val = ig3.Integral(0,1);
-   std::cout << "GSL adaptive integral result is       \t" << val << std::endl;
+   val = ig3.Integral(lower,upper);
+   std::cout << "GSL adaptive integral result is       \t" << val << " in " << calls << " calls" <<  std::endl;
    status += std::fabs(val-RESULT) > ERRORLIMIT;
+   calls = 0;
 
    //ROOT::Math::GaussIntegrator ig4;
    ROOT::Math::Integrator ig4(ROOT::Math::IntegrationOneDim::kGAUSS); 
    ig4.SetFunction(wf);
-   val = ig4.Integral(0,1);
-   std::cout << "Cernlib Gauss integral result is      \t" << val << std::endl;
+   val = ig4.Integral(lower,upper);
+   std::cout << "Cernlib Gauss integral result is      \t" << val << " in " << calls << " calls" << std::endl;
    status += std::fabs(val-RESULT) > ERRORLIMIT;
+   calls = 0;
 
    ROOT::Math::Integrator ig5(ROOT::Math::IntegrationOneDim::kLEGENDRE); 
    ig5.SetFunction(wf);
-   val = ig5.Integral(0,1);
-   std::cout << "Gauss-Legendre integral result is     \t" << val << std::endl;
+   val = ig5.Integral(lower,upper);
+   std::cout << "Gauss-Legendre integral result is     \t" << val << " in " << calls << " calls" << std::endl;
+   //status += std::fabs(val-RESULT) > ERRORLIMIT;
+   calls = 0;
+
+   //std::cout << RooAbsReal::defaultIntegratorConfig()->getConfigSection("RooIntegrator1D") << std::endl;
+   RooRealVar x("x", "x", lower, upper);
+   RooCFunction1Binding<Double_t, Double_t> fAbsReal("f", "f", &f, x);
+   RooRealBinding fAbsFunc(fAbsReal,x);
+
+   RooNumIntConfig *config = (RooNumIntConfig *)RooAbsReal::defaultIntegratorConfig()->Clone();
+   config->getConfigSection("RooIntegrator1D").setRealValue("maxSteps", 30);
+   std::cout << config->getConfigSection("RooIntegrator1D").getRealValue("maxSteps") << endl;
+   
+   RooIntegrator1D ig6(fAbsFunc, lower, upper, *RooAbsReal::defaultIntegratorConfig());  
+
+   val = ig6.integral();
+   std::cout << "RooIntegrator1D integral result is    \t" << val << " in " << calls << " calls" << std::endl;
    status += std::fabs(val-RESULT) > ERRORLIMIT;
 
    printTestResult(std::cerr,"one-dimensional integration  ",status);
