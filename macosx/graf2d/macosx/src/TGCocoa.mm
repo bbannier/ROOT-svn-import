8b47fddb70f393fa04eb37b655ce20e7f48677c2
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-04-12 15:48:02 +0000

    Implement TGCocoa::GrabKey - set/remove passive key grab on a view.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@43715 27541ba8-7e3a-0410-8455-c3a389f83636

--- macosx/graf2d/macosx/src/TGCocoa.mm
+++ macosx/graf2d/macosx/src/TGCocoa.mm
@@ -2633,8 +2633,7 @@ void TGCocoa::SetKeyAutoRepeat(Bool_t /*on = kTRUE*/)
 }
 
 //______________________________________________________________________________
-void TGCocoa::GrabKey(Window_t /*wid*/, Int_t /*keycode*/, UInt_t /*modifier*/,
-                        Bool_t /*grab = kTRUE*/)
+void TGCocoa::GrabKey(Window_t wid, Int_t keyCode, UInt_t modifiers, Bool_t grab)
 {
    // Establishes a passive grab on the keyboard. In the future, the
    // keyboard is actively grabbed, the last-keyboard-grab time is set
@@ -2658,6 +2657,16 @@ void TGCocoa::GrabKey(Window_t /*wid*/, Int_t /*keycode*/, UInt_t /*modifier*/,
    // grab     - a switch between grab/ungrab key
    //            grab = kTRUE  grab the key and modifier
    //            grab = kFALSE ungrab the key and modifier
+   
+   assert(!fPimpl->IsRootWindow(wid) && "GrabKey, called for 'root' window");
+   
+   NSObject<X11Drawable> *drawable = fPimpl->GetDrawable(wid);
+   QuartzView *view = drawable.fContentView;
+
+   if (grab)
+      [view addPassiveKeyGrab : keyCode modifiers : modifiers];
+   else
+      [view removePassiveKeyGrab : keyCode modifiers : modifiers];
 }
 
 //______________________________________________________________________________
