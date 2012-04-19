a3b14f0da9e7583195cd6b78e553b9aea23715e9
couet <couet@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-04-19 15:09:58 +0000

    - A new branch for the mathtext developments
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@43853 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ mathtext/core/meta/inc/TSchemaHelper.h
@@ -0,0 +1,56 @@
+// @(#)root/core:$Id$
+// author: Lukasz Janyst <ljanyst@cern.ch>
+
+/*************************************************************************
+ * Copyright (C) 1995-2010, Rene Brun, Fons Rademakers and al.           *
+ * All rights reserved.                                                  *
+ *                                                                       *
+ * For the licensing terms see $ROOTSYS/LICENSE.                         *
+ * For the list of contributors see $ROOTSYS/README/CREDITS.             *
+ *************************************************************************/
+
+// This include must be outside of the code guard because
+// Rtypes.h includes TGenericClassInfo.h which includes
+// TSchemaHelper.h (this header file) and really need the
+// definition of ROOT::TSchemaHelper.   So in this case,
+// we need the indirect #include to really do the declaration
+// and the direct #include to be a noop.
+#ifndef ROOT_Rtypes
+#include "Rtypes.h"
+#endif
+
+#ifndef ROOT_TSchemaHelper
+#define ROOT_TSchemaHelper
+
+#include <string>
+
+namespace ROOT
+{
+   struct TSchemaHelper
+   {
+      TSchemaHelper(): fTarget(), fSourceClass(),
+       fSource(), fCode(), fVersion(), fChecksum(),
+       fInclude(), fEmbed(kTRUE), fFunctionPtr( 0 ), 
+       fAttributes() {}
+      std::string fTarget;
+      std::string fSourceClass;
+      std::string fSource;
+      std::string fCode;
+      std::string fVersion;
+      std::string fChecksum;
+      std::string fInclude;
+      Bool_t      fEmbed;
+      void*       fFunctionPtr;
+      std::string fAttributes;
+
+      TSchemaHelper(const TSchemaHelper &tsh) :
+       fTarget(tsh.fTarget), fSourceClass(tsh.fSourceClass),
+       fSource(tsh.fSource), fCode(tsh.fCode), fVersion(tsh.fVersion),fChecksum(tsh.fChecksum),
+       fInclude(tsh.fInclude), fEmbed(tsh.fEmbed), fFunctionPtr(tsh.fFunctionPtr),
+       fAttributes(tsh.fAttributes) {}
+
+      TSchemaHelper& operator=(const TSchemaHelper &) {return *this;} // Not implemented
+   };
+}
+
+#endif // ROOT_TSchemaHelper
---
+++ mathtext/net/net/Module.mk
@@ -0,0 +1,96 @@
+# Module.mk for net module
+# Copyright (c) 2000 Rene Brun and Fons Rademakers
+#
+# Author: Fons Rademakers, 29/2/2000
+
+MODNAME      := net
+MODDIR       := $(ROOT_SRCDIR)/net/$(MODNAME)
+MODDIRS      := $(MODDIR)/src
+MODDIRI      := $(MODDIR)/inc
+
+NETDIR       := $(MODDIR)
+NETDIRS      := $(NETDIR)/src
+NETDIRI      := $(NETDIR)/inc
+
+##### libNet #####
+NETL         := $(MODDIRI)/LinkDef.h
+NETDS        := $(call stripsrc,$(MODDIRS)/G__Net.cxx)
+NETDO        := $(NETDS:.cxx=.o)
+NETDH        := $(NETDS:.cxx=.h)
+
+NETH         := $(filter-out $(MODDIRI)/LinkDef%,$(wildcard $(MODDIRI)/*.h))
+NETS         := $(filter-out $(MODDIRS)/G__%,$(wildcard $(MODDIRS)/*.cxx))
+ifeq ($(CRYPTOLIB),)
+NETNOCRYPTO  := -DR__NO_CRYPTO
+NETH         := $(filter-out $(MODDIRI)/TAS3File.h,$(NETH))
+NETH         := $(filter-out $(MODDIRI)/TGSFile.h,$(NETH))
+NETH         := $(filter-out $(MODDIRI)/THTTPMessage.h,$(NETH))
+NETS         := $(filter-out $(MODDIRS)/TAS3File.cxx,$(NETS))
+NETS         := $(filter-out $(MODDIRS)/TGSFile.cxx,$(NETS))
+NETS         := $(filter-out $(MODDIRS)/THTTPMessage.cxx,$(NETS))
+else
+NETNOCRYPTO  :=
+endif
+
+ifeq ($(SSLLIB),)
+NETSSL       :=
+NETH         := $(filter-out $(MODDIRI)/TSSLSocket.h,$(NETH))
+NETS         := $(filter-out $(MODDIRS)/TSSLSocket.cxx,$(NETS))
+else
+NETSSL       := -DR__SSL
+endif
+
+NETO         := $(call stripsrc,$(NETS:.cxx=.o))
+NETDEP       := $(NETO:.o=.d) $(NETDO:.o=.d)
+
+NETLIB       := $(LPATH)/libNet.$(SOEXT)
+NETMAP       := $(NETLIB:.$(SOEXT)=.rootmap)
+
+EXTRANETFLAGS =
+
+# used in the main Makefile
+ALLHDRS      += $(patsubst $(MODDIRI)/%.h,include/%.h,$(NETH))
+ALLLIBS      += $(NETLIB)
+ALLMAPS      += $(NETMAP)
+
+# include all dependency files
+INCLUDEFILES += $(NETDEP)
+
+##### local rules #####
+.PHONY:         all-$(MODNAME) clean-$(MODNAME) distclean-$(MODNAME)
+
+include/%.h:    $(NETDIRI)/%.h
+		cp $< $@
+
+$(NETLIB):      $(NETO) $(NETDO) $(ORDER_) $(MAINLIBS) $(NETLIBDEP)
+		@$(MAKELIB) $(PLATFORM) $(LD) "$(LDFLAGS)" \
+		   "$(SOFLAGS)" libNet.$(SOEXT) $@ "$(NETO) $(NETDO)" \
+		   "$(NETLIBEXTRA) $(CRYPTOLIBDIR) $(CRYPTOLIB) $(SSLLIB)"
+
+$(NETDS):       $(NETH) $(NETL) $(ROOTCINTTMPDEP)
+		$(MAKEDIR)
+		@echo "Generating dictionary $@..."
+		$(ROOTCINTTMP) -f $@ -c $(NETNOCRYPTO) $(NETSSL) $(NETH) $(NETL)
+
+$(NETMAP):      $(RLIBMAP) $(MAKEFILEDEP) $(NETL)
+		$(RLIBMAP) -o $@ -l $(NETLIB) -d $(NETLIBDEPM) -c $(NETL)
+
+all-$(MODNAME): $(NETLIB) $(NETMAP)
+
+clean-$(MODNAME):
+		@rm -f $(NETO) $(NETDO)
+
+clean::         clean-$(MODNAME)
+
+distclean-$(MODNAME): clean-$(MODNAME)
+		@rm -f $(NETDEP) $(NETDS) $(NETDH) $(NETLIB) $(NETMAP)
+
+distclean::     distclean-$(MODNAME)
+
+##### extra rules ######
+ifeq ($(MACOSX_SSL_DEPRECATED),yes)
+$(call stripsrc,$(NETDIRS)/TSSLSocket.o): CXXFLAGS += -Wno-deprecated-declarations
+endif
+$(call stripsrc,$(NETDIRS)/TSSLSocket.o): CXXFLAGS += $(SSLINCDIR:%=-I%)
+$(call stripsrc,$(NETDIRS)/THTTPMessage.o): CXXFLAGS += $(SSLINCDIR:%=-I%)
+$(call stripsrc,$(NETDIRS)/TWebFile.o): CXXFLAGS += $(NETSSL)
