7f3f1e0c00b158ef2e16b702b5816d2c6d927959
ghl <ghl@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-04-25 20:45:18 +0000

    Added function in MakeModelAndMeasurementsFast to only fit the workspace
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/histfactory@43950 27541ba8-7e3a-0410-8455-c3a389f83636

--- roofit/histfactory/src/Sample.cxx
+++ roofit/histfactory/src/Sample.cxx
@@ -40,7 +40,7 @@ void RooStats::HistFactory::Sample::writeToFile( std::string OutputFileName, std
 }
 
 
-void RooStats::HistFactory::Sample::SetValue( double val ) {
+void RooStats::HistFactory::Sample::SetValue( Double_t val ) {
 
   // For use in a number counting measurement
   // Create a 1-bin histogram, 
@@ -215,7 +215,7 @@ void RooStats::HistFactory::Sample::ActivateStatError( std::string StatHistoName
 }
 
 
-void RooStats::HistFactory::Sample::AddOverallSys( std::string SysName, double SysLow, double SysHigh ) {
+void RooStats::HistFactory::Sample::AddOverallSys( std::string SysName, Double_t SysLow, Double_t SysHigh ) {
 
   RooStats::HistFactory::OverallSys sys;
   sys.SetName( SysName );
@@ -226,7 +226,7 @@ void RooStats::HistFactory::Sample::AddOverallSys( std::string SysName, double S
 
 }
 
-void RooStats::HistFactory::Sample::AddNormFactor( std::string SysName, double SysVal, double SysLow, double SysHigh, bool SysConst ) {
+void RooStats::HistFactory::Sample::AddNormFactor( std::string SysName, Double_t SysVal, Double_t SysLow, Double_t SysHigh, bool SysConst ) {
 
   RooStats::HistFactory::NormFactor norm;
 
