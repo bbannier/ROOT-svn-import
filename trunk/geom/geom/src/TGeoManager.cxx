5fb29dc3474b36965e548a6dd9e0eaf5a40a4541
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-06-06 09:13:05 +0000

    merged with trunk - revision 44579
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/ibucur@44580 27541ba8-7e3a-0410-8455-c3a389f83636

--- trunk/geom/geom/src/TGeoManager.cxx
+++ trunk/geom/geom/src/TGeoManager.cxx
@@ -272,6 +272,8 @@
 */
 //End_Html
 
+#include <stdlib.h>
+
 #include "Riostream.h"
 
 #include "TROOT.h"
@@ -1744,7 +1746,7 @@ void TGeoManager::DrawTracks(Option_t *option)
    SetAnimateTracks();
    for (Int_t i=0; i<fNtracks; i++) {
       track = GetTrack(i);
-      track->Draw(option);
+      if (track) track->Draw(option);
    }
    SetAnimateTracks(kFALSE);
    ModifiedPad();
@@ -3639,6 +3641,7 @@ void TGeoManager::UpdateElements()
          }
       } else {
          elem = mat->GetElement();
+         if (!elem) continue;
          elem_table = fElementTable->GetElement(elem->Z());
          if (elem != elem_table) {
             elem_table->SetDefined(elem->IsDefined());
