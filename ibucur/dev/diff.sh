e4f0a22d064ef08dd42bd8212bf38f5838f553b0
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-06-14 15:22:16 +0000

    - built a script that automatically writes DIFF files with respect to the trunk - added / modified some personal development tests in dev folder - updated problems folder
    - IMPORTANT: in last revision (44690), which also included a merge, I improved the RooDataSet constructor for combined data sets
      The extension now allows better handling of combining weighted / unweighted data sets when there is no explicit weight variable
    passed. In the case of weighted imported data sets, the weight values are imported from their own weight variables. The weight
    variables in the imported data sets must share the same name, otherwise the weights are not copied properly. As such, the new code
    tests explicity for this condition to hold. In the case of imported unweighted data sets, the weights will be set to the value 1.0 .
    This allows for weighted and unweighted data sets to be combined.
    
    
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@44691 27541ba8-7e3a-0410-8455-c3a389f83636

--- ibucur/dev/diff.sh
+++ ibucur/dev/diff.sh
@@ -1,10 +1,20 @@
-cp $ROOTEST/stressRooStats* ~/root/test
+target_root=/home/ibucur/root
+source_root=/home/ibucur/rootdev/trunk
 
-cd ~/root
+cd $target_root
 svn update
+cd $OLDPWD
 
-svn diff . > patch.diff
-
-svn revert .
+for source in $@
+do
+	source=$(readlink -f $source)
+	target=${source#$source_root}
+	target=$target_root$target
+	cp $source $(dirname $target)
+	cd $target_root
+	svn diff ${target#$target_root/} > "$ROOTDEV/$(basename $target).diff"
+	cd $OLDPWD
+	svn revert $target
+done
 
 
