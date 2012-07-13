4f7043d9169cb005a6c1d0f489b164d8e8350e78
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-13 12:05:36 +0000

    -- added changes to RooStatsUtils (new RemoveNuisancePdf functions) and modified MakeNuisancePdf to return a simple pdf where a ProdPdf is not needed (only one constraint term, not a product of constraint terms) -- the new functions will be used in SimpleLikelihoodRatioTestStat to allow to simplify the computations (constraint terms are removed since they cancel out in the Likelihood Ratio anyway) -> this will lead to improved performance and possibly to more accurate results (it also fixes the problem of different results across platforms)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/ibucur@45047 27541ba8-7e3a-0410-8455-c3a389f83636

--- trunk/roofit/roostats/inc/RooStats/RooStatsUtils.h
+++ trunk/roofit/roostats/inc/RooStats/RooStatsUtils.h
@@ -103,9 +103,12 @@ namespace RooStats {
 
    void FactorizePdf(RooStats::ModelConfig &model, RooAbsPdf &pdf, RooArgList &obsTerms, RooArgList &constraints);
 
+   // extract constraint terms from pdf
    RooAbsPdf * MakeNuisancePdf(RooAbsPdf &pdf, const RooArgSet &observables, const char *name);
-
    RooAbsPdf * MakeNuisancePdf(const RooStats::ModelConfig &model, const char *name);
+   // remove constraint terms from pdf
+   RooAbsPdf * RemoveNuisancePdf(RooAbsPdf &pdf, const RooArgSet &observables, const char *name);
+   RooAbsPdf * RemoveNuisancePdf(const RooStats::ModelConfig &model, const char *name);
    
    // Create a TTree with the given name and description. All RooRealVars in the RooDataSet are represented as branches that contain values of type Double_t.
    TTree* GetAsTTree(TString name, TString desc, const RooDataSet& data);
