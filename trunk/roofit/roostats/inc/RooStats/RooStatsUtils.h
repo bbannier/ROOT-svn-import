63fa3392b061cf5806427c38dbf1d947594c8492
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-18 09:08:01 +0000

    -- minor code improvement
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/ibucur@45124 27541ba8-7e3a-0410-8455-c3a389f83636

--- trunk/roofit/roostats/inc/RooStats/RooStatsUtils.h
+++ trunk/roofit/roostats/inc/RooStats/RooStatsUtils.h
@@ -107,8 +107,8 @@ namespace RooStats {
    RooAbsPdf * MakeNuisancePdf(RooAbsPdf &pdf, const RooArgSet &observables, const char *name);
    RooAbsPdf * MakeNuisancePdf(const RooStats::ModelConfig &model, const char *name);
    // remove constraints from pdf and return the unconstrained pdf
-   RooAbsPdf * MakeUnconstrainedPdf(RooAbsPdf &pdf, const RooArgSet &observables, const char *name);
-   RooAbsPdf * MakeUnconstrainedPdf(const RooStats::ModelConfig &model, const char *name);
+   RooAbsPdf * MakeUnconstrainedPdf(RooAbsPdf &pdf, const RooArgSet &observables, const char *name = NULL);
+   RooAbsPdf * MakeUnconstrainedPdf(const RooStats::ModelConfig &model, const char *name = NULL);
    
    // Create a TTree with the given name and description. All RooRealVars in the RooDataSet are represented as branches that contain values of type Double_t.
    TTree* GetAsTTree(TString name, TString desc, const RooDataSet& data);
