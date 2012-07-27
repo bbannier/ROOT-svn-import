91beb3c0773f587aba786221f8d9a912c010f411
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-27 12:57:06 +0000

    Add crossing event tests.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cocoa_exp_ml@45258 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ graf2d/cocoa/CocoaTest/CrossingEvents/2/main.cxx
@@ -0,0 +1,31 @@
+#include <fstream>
+
+#include "TApplication.h"
+#include "TestFrame.h"
+
+#include "guibuilder.h"
+
+int main(int argc, char ** argv)
+{
+   using namespace ROOT::CocoaTest;
+
+   TApplication app("test_app", &argc, argv);
+   
+   GuiBuilder bld;
+   std::ifstream inputFile("test1.gui");
+//   std::ifstream inputFile("test2.gui");
+//   std::ifstream inputFile("test3.gui");
+//   std::ifstream inputFile("test4.gui");
+
+
+   try 
+   {
+      bld.BuildGUI(inputFile);
+   } 
+   catch (const std::exception &)
+   {
+      return 0;
+   }
+
+   app.Run();
+}
---
+++ graf2d/cocoa/CocoaTest/CrossingEvents/2/testframe.h
@@ -0,0 +1,38 @@
+#ifndef TESTFRAME_INCLUDED
+#define TESTFRAME_INCLUDED
+
+#include "GuiTypes.h"
+#include "TGFrame.h"
+
+namespace ROOT {
+namespace CocoaTest {
+
+class TestFrame : public TGFrame {
+public:
+   TestFrame(TestFrame *parent, UInt_t width, UInt_t height,
+             UInt_t options, Pixel_t background);
+   ~TestFrame();
+
+   void DoRedraw();
+
+   //Event handlers:   
+   Bool_t HandleCrossing(Event_t *crossingEvent);   
+
+private:
+   void PrintFrameInfo()const;
+   void PrintEventCoordinates(const Event_t *event)const;
+
+   TestFrame(const TestFrame &rhs) = delete;
+   TestFrame(TestFrame &&rhs) = delete;
+   TestFrame &operator = (const TestFrame &rhs) = delete;
+   TestFrame &operator = (TestFrame &&rhs) = delete;
+   
+   //To be used by all test frames.
+   static FontStruct_t font_;
+   static GContext_t textContext_;
+};
+
+}
+}
+
+#endif
