ae8ae4e3b8debaf08392b8db703d831871aff36c
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-31 12:17:32 +0000

    Add test for a passive pointer grab.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cocoa_exp_ml@45308 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ graf2d/cocoa/CocoaTest/PointerGrab/passive/main.cxx
@@ -0,0 +1,25 @@
+#include <fstream>
+
+#include "TApplication.h"
+#include "TVirtualX.h"
+#include "testframe.h"
+
+int main(int argc, char ** argv)
+{
+   using namespace ROOT::CocoaTest;
+
+   TApplication app("test_app", &argc, argv);
+   
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   TestFrame *childFrame = new TestFrame(mainFrame, 300, 300, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame->GetId(), 100, 100);
+   TestFrame *childChildFrame = new TestFrame(childFrame, 100, 100, kChildFrame, 0xff9900);
+   gVirtualX->MoveWindow(childChildFrame->GetId(), 100, 100);
+   gVirtualX->MapSubwindows(childFrame->GetId());
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
+
+   gVirtualX->GrabButton(childFrame->GetId(), kButton1, kKeyShiftMask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);
+
+   app.Run();
+}
