435f17aab0c788863d630ce5f48603d75d9d6add
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-31 12:29:32 +0000

    Add button release mask to the main frame.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45309 27541ba8-7e3a-0410-8455-c3a389f83636

--- cocoa_exp_ml/graf2d/cocoa/CocoaTest/PointerGrab/passive/main.cxx
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/PointerGrab/passive/main.cxx
@@ -19,6 +19,8 @@ int main(int argc, char ** argv)
    gVirtualX->MapSubwindows(mainFrame->GetId());
    gVirtualX->MapRaised(mainFrame->GetId());
 
+   mainFrame->AddInput(kButtonReleaseMask);
+
    gVirtualX->GrabButton(childFrame->GetId(), kButton1, kKeyShiftMask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);
 
    app.Run();
