20424229ae6d11356746b6d0fd7f7b0c5e43c2bc
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-07-31 13:45:59 +0000

    Add test case for event dispatching during activated grab.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45315 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/PointerGrab/grabbed_events/main.cxx
@@ -0,0 +1,32 @@
+#include <fstream>
+
+#include "TApplication.h"
+#include "TVirtualX.h"
+#include "testframe.h"
+
+int main(int argc, char ** argv)
+{
+   using namespace ROOT::CocoaTest;
+
+   TApplication app("test_app", &argc, argv);
+   
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   TestFrame *childFrame = new TestFrame(mainFrame, 300, 300, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame->GetId(), 100, 100);
+   TestFrame *childChildFrame = new TestFrame(childFrame, 100, 100, kChildFrame, 0xff9900);
+   gVirtualX->MoveWindow(childChildFrame->GetId(), 100, 100);
+   gVirtualX->MapSubwindows(childFrame->GetId());
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
+
+   mainFrame->AddInput(kButtonReleaseMask);
+
+//   gVirtualX->GrabButton(childFrame->GetId(), kButton1, kKeyShiftMask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);//1
+//   childFrame->AddInput(kButtonPressMask | kButtonReleaseMask);//2
+//   childFrame->AddInput(kButtonPressMask);//3
+//   gVirtualX->GrabButton(childFrame->GetId(), kButton3, kKeyMod1Mask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);//4
+   gVirtualX->GrabButton(childFrame->GetId(), kButton1, kKeyMod2Mask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);//5
+//   gVirtualX->GrabButton(childFrame->GetId(), kButton1, kKeyLockMask, kButtonPressMask | kButtonReleaseMask, kNone, kNone, kTRUE);//6
+
+   app.Run();
+}
