fa4212736be26fd46606ce8cf55621f78cf408fe
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-08-06 16:02:22 +0000

    -- add CombinedPdf class, designed to be a simpler, more efficient version of RooSimultaneous
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45421 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ ibucur/trunk/roofit/roostats/inc/RooStats/CombinedPdf.h
@@ -0,0 +1,40 @@
+// @(#)root/roostats:$Id$
+// Author: Ioan Gabriel Bucur   30/07/2012
+
+
+#ifndef ROOSTATS_CombinedPdf
+#define ROOSTATS_CombinedPdf
+
+#include "TObject.h"
+#include "RooAbsPdf.h"
+#include "RooArgList.h"
+#include "RooDataSet.h"
+#include "RooSimultaneous.h"
+#include <map>
+#include <string>
+
+namespace RooStats {
+
+   class CombinedPdf : public RooAbsPdf {
+   public:
+      // Constructors, destructors, assignment
+      CombinedPdf(const char *name, const char *title, RooArgList &pdfList);
+      CombinedPdf(const char *name, const char *title, const std::map<std::string, RooAbsPdf*> &pdfMap);
+      CombinedPdf(const CombinedPdf& other, const char *name = 0);
+      virtual TObject *clone(const char *newname) const { return new CombinedPdf(*this, newname); }
+      virtual ~CombinedPdf();
+
+      RooAbsPdf* GetPdf(const char *catName) const; // considering deleting it
+      RooDataSet *GenerateGlobalObs(const RooArgSet& vars, Int_t nEvents);
+      virtual Double_t evaluate() const;
+
+   private:
+      std::map<std::string, RooAbsPdf *> fChannels;
+      RooAbsPdf *fCurrentPdf;
+   protected:
+      ClassDef(CombinedPdf,0)
+   };
+}
+
+#endif
+
