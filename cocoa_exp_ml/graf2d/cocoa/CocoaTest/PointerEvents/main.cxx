ad821e4c2f36fe9b186633e83682863fc32b9b96
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-08-07 08:36:21 +0000

    Test case
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45437 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/PointerEvents/main.cxx
@@ -0,0 +1,33 @@
+#include <fstream>
+
+#include "TApplication.h"
+#include "TVirtualX.h"
+#include "testframe.h"
+
+#define TEST1 1
+
+int main(int argc, char ** argv)
+{
+   using namespace ROOT::CocoaTest;
+
+   TApplication app("test_app", &argc, argv);
+   
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   TestFrame *childFrame = new TestFrame(mainFrame, 300, 300, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame->GetId(), 100, 100);
+   TestFrame *childChildFrame = new TestFrame(childFrame, 200, 100, kChildFrame, 0xff9900);
+   gVirtualX->MoveWindow(childChildFrame->GetId(), 100, 100);
+   gVirtualX->MapSubwindows(childFrame->GetId());
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
+
+   //Test case 1: "middle" window has a grab, and
+#ifdef TEST1
+   mainFrame->AddInput(kEnterWindowMask | kLeaveWindowMask);
+   childFrame->AddInput(kEnterWindowMask | kLeaveWindowMask);
+   childChildFrame->AddInput(kEnterWindowMask | kLeaveWindowMask);
+#elif defined(TEST2)
+#endif
+
+   app.Run();
+}
---
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/PointerEvents/testframe.h
@@ -0,0 +1,42 @@
+#ifndef TESTFRAME_INCLUDED
+#define TESTFRAME_INCLUDED
+
+#include "GuiTypes.h"
+#include "TGFrame.h"
+
+namespace ROOT {
+namespace CocoaTest {
+
+class TestFrame : public TGFrame {
+public:
+   TestFrame(TestFrame *parent, UInt_t width, UInt_t height,
+             UInt_t options, Pixel_t background);
+   ~TestFrame();
+
+   void DoRedraw();
+
+   //Event handlers:   
+   Bool_t HandleCrossing(Event_t *crossingEvent);
+   Bool_t HandleButton(Event_t *buttonEvent);
+   Bool_t HandleMotion(Event_t *motionEvent);
+
+private:
+
+   void PrintFrameInfo()const;
+   void PrintEventCoordinates(const Event_t *event)const;
+
+   TestFrame(const TestFrame &rhs);
+   TestFrame &operator = (const TestFrame &rhs);
+   
+   //To be used by all test frames.
+   Window_t windowID_;
+
+   static Window_t currentWindowID_;
+   static FontStruct_t font_;
+   static GContext_t textContext_;
+};
+
+}
+}
+
+#endif
