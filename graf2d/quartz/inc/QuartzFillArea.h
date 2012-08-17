4004ae63f2372dba1df6ba83f7f530b5f78a7659
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-08-17 10:51:53 +0000

    TGQuartz and quartz module refactored.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/cocoa_exp_ml@45649 27541ba8-7e3a-0410-8455-c3a389f83636

--- graf2d/quartz/inc/QuartzFillArea.h
+++ graf2d/quartz/inc/QuartzFillArea.h
@@ -41,16 +41,12 @@ namespace ROOT {
 namespace Quartz {
 
 Bool_t SetFillColor(CGContextRef ctx, Color_t colorIndex);
-void DrawBox(CGContextRef ctx, Int_t x1, Int_t y1, Int_t x2, Int_t y2, Int_t mode);
+Bool_t SetFillAreaParameters(CGContextRef ctx, unsigned *patternIndex);
+void DrawBox(CGContextRef ctx, Int_t x1, Int_t y1, Int_t x2, Int_t y2, bool hollow);
 void DrawBoxGradient(CGContextRef ctx, Int_t x1, Int_t y1, Int_t x2, Int_t y2, const TColorGradient *extendedColor, Bool_t drawShadow);
 
 void DrawFillArea(CGContextRef ctx, Int_t n, TPoint * xy, Bool_t drawShadow);
 void DrawFillAreaGradient(CGContextRef ctx, Int_t nPoints, const TPoint *xy, const TColorGradient *extendedColor, Bool_t drawShadow);
-
-void SetFillStyle(CGContextRef ctx, Int_t style, 
-                  Float_t r, Float_t g, Float_t b, Float_t a);
-void SetStencilPattern(CGContextRef ctx, 
-                       Float_t r, Float_t g, Float_t b, Float_t a);
 
 }
 }
