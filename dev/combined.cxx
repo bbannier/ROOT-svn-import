a7552166887e078da594c137391674f7fd397386
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-09-03 18:25:25 +0000

    - added new development source files
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/ibucur@45803 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ dev/combined.cxx
@@ -0,0 +1,15 @@
+#include "RooSimultaneous.h"
+#include "RooStats/CombinedPdf.h"
+#include "RooRealVar.h"
+#include "RooGaussian.h"
+#include "RooWorkspace.h"
+
+int main()
+{
+
+   RooWorkspace w("w", kTRUE);
+
+
+   return 0;
+}
+
