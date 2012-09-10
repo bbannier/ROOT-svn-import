6f2d0ecc48924dfa7ff01d5f662f93e3555e27a4
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-09-10 08:30:05 +0000

    Add tests for a clipping algorithm.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45910 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/ClipTest/testframe.h
@@ -0,0 +1,37 @@
+#ifndef TESTFRAME_INCLUDED
+#define TESTFRAME_INCLUDED
+
+#include "GuiTypes.h"
+#include "TGFrame.h"
+
+namespace ROOT {
+namespace CocoaTest {
+
+class TestFrame : public TGFrame {
+public:
+   TestFrame(TestFrame *parent, UInt_t width, UInt_t height,
+             UInt_t options, Pixel_t background);
+   ~TestFrame();
+
+   void DoRedraw();
+
+   //Event handlers:   
+   Bool_t HandleButton(Event_t *buttonEvent);
+
+private:
+
+   TestFrame(const TestFrame &rhs);
+   TestFrame &operator = (const TestFrame &rhs);
+   
+   //To be used by all test frames.
+   Window_t windowID_;
+
+   static Window_t currentWindowID_;
+   static FontStruct_t font_;
+   static GContext_t textContext_;
+};
+
+}
+}
+
+#endif
