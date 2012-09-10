81d7af9e05ec2171f06d7f621619dd059068a55d
tpochep <tpochep@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-09-10 12:09:17 +0000

    Add a more complex test case with overlapping siblings and siblings of ancestors.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@45916 27541ba8-7e3a-0410-8455-c3a389f83636

--- cocoa_exp_ml/graf2d/cocoa/CocoaTest/ClipTest/main.cxx
+++ cocoa_exp_ml/graf2d/cocoa/CocoaTest/ClipTest/main.cxx
@@ -3,7 +3,7 @@
 
 #include "testframe.h"
 
-#define TEST6
+#define TEST9
 
 int main(int argc, char ** argv)
 {
@@ -93,8 +93,68 @@ int main(int argc, char ** argv)
 
    gVirtualX->MapSubwindows(mainFrame->GetId());
    gVirtualX->MapRaised(mainFrame->GetId());
+#elif defined(TEST7)
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   mainFrame->AddInput(kButtonPressMask);
+   
+   TestFrame *childFrame1 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame1->GetId(), 0, 0);
+
+   TestFrame *childFrame2 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame2->GetId(), 50, 50);
+
+   TestFrame *childFrame3 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xff6600);
+   gVirtualX->MoveWindow(childFrame3->GetId(), 100, 100);
 
 
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
+#elif defined(TEST8)
+
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   mainFrame->AddInput(kButtonPressMask);
+   
+   TestFrame *childFrame1 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xffffff);
+   gVirtualX->MoveWindow(childFrame1->GetId(), 200, 100);
+
+   TestFrame *childFrame2 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xffffff);
+   gVirtualX->MoveWindow(childFrame2->GetId(), 100, 200);
+
+   TestFrame *childFrame3 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xffffff);
+   gVirtualX->MoveWindow(childFrame3->GetId(), 200, 200);
+
+   TestFrame *childFrame4 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xffffff);
+   gVirtualX->MoveWindow(childFrame4->GetId(), 300, 200);
+
+   TestFrame *childFrame5 = new TestFrame(mainFrame, 100, 100, kChildFrame, 0xffffff);
+   gVirtualX->MoveWindow(childFrame5->GetId(), 200, 300);
+
+
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
+#elif defined(TEST9)
+   TestFrame *mainFrame = new TestFrame(0, 500, 500, kMainFrame, 0xff0000);
+   
+   TestFrame *child1 = new TestFrame(mainFrame, 200, 200, kChildFrame, 0xffff00);
+   child1->AddInput(kButtonPressMask);
+   gVirtualX->MoveWindow(child1->GetId(), 100, 100);
+
+   TestFrame *child1ofChild1 = new TestFrame(child1, 200, 100, kChildFrame, 0xff00ff);
+   gVirtualX->MoveWindow(child1ofChild1->GetId(), 100, 50);
+   child1ofChild1->AddInput(kButtonPressMask);
+   
+   gVirtualX->MapSubwindows(child1->GetId());
+   
+   TestFrame *child1ofChild1ofChild1 = new TestFrame(child1ofChild1, 50, 50, kChildFrame, 0xffaaff);
+   gVirtualX->MoveWindow(child1ofChild1ofChild1->GetId(), 20, 35);
+   
+   gVirtualX->MapSubwindows(child1ofChild1->GetId());
+   
+   TestFrame *child2 = new TestFrame(mainFrame, 200, 200, kChildFrame, 0xadadad);
+   gVirtualX->MoveWindow(child2->GetId(), 250, 200);
+
+   gVirtualX->MapSubwindows(mainFrame->GetId());
+   gVirtualX->MapRaised(mainFrame->GetId());
 #endif
 
    app.Run();
