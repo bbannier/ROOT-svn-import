a1dacccf2e8914e702f60cf3328121927b6a7cb3
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-10-03 16:30:00 +0000

    - delete 2nd development trunk (making fresh copy)
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@46299 27541ba8-7e3a-0410-8455-c3a389f83636

--- ibucur/trunk2/interpreter/cling/test/ErrorRecovery/Overloads.h
+++
@@ -1,7 +0,0 @@
-#include <fstream>
-#include <string>
-
-void Overloads () {
-  std::string s;
-  std::ofstream("file.txt") << s << std::endl;
-}
--- ibucur/trunk2/interpreter/cling/test/Prompt/faildecls.C
+++
@@ -1,5 +0,0 @@
-// RUN: cat %s | %cling -Xclang -verify
-
-struct {int j;}; // expected-error {{anonymous structs and classes must be class members}}
-
-.q
