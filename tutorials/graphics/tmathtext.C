00e175e2ff00df585b0816b761e2d85aae82bac4
couet <couet@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-10-17 07:35:45 +0000

    first version of the doc.
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/mathtext_trunk@46588 27541ba8-7e3a-0410-8455-c3a389f83636

--- tutorials/graphics/tmathtext.C
+++ tutorials/graphics/tmathtext.C
@@ -1,7 +1,10 @@
-void tmathtexttest(void)
+// This macro various formula in a canvas and prints the canvas
+// as a Postscript file using TMathText.
+//Author: Yue Shi Lai
+
 {
 	TCanvas *c1 = new TCanvas("c1");
-#if 1
+
 	TMathText l;
 	l.SetTextAlign(23);
 	l.SetTextSize(0.06);
@@ -12,19 +15,12 @@ void tmathtexttest(void)
 	l.DrawMathText(0.50, 0.330, "p(n) = {1\\over\\pi\\sqrt{2}} \\sum_{k = 1}^\\infty \\sqrt{k} A_k(n) {d\\over dn} {\\sinh \\left\\{ {\\pi\\over k} \\sqrt{2\\over 3} \\sqrt{n - {1\\over 24}} \\right\\} \\over \\sqrt{n - {1\\over 24}}}");
 	l.DrawMathText(0.15, 0.130, "{(\\ell+1)C_{\\ell}^{TE} \\over 2\\pi}");
 	l.DrawMathText(0.60, 0.100, "\\hbox{RHIC スピン物理 Нью-Йорк}");
-#else
-	TLatex l;
-	l.SetTextAlign(23);
-	l.SetTextSize(0.06);
-	l.DrawLatex(0.5, 0.950, "#prod_{j#geq0} #left(#sum_{k#geq0} a_{jk}z^{k}#right) = #sum_{n#geq0} z^{n} #left(#sum_{k_{0},k_{1},#3dots#geq0} a_{0k_{0}}a_{1k_{1}} #3dots #right)");
-	l.DrawLatex(0.5, 0.800, "W_{#delta_{1}#rho_{1}#sigma_{2}}^{3#beta} = U_{#delta_{1}#rho_{1}#sigma_{2}}^{3#beta} + #frac{1}{8#pi^{2}} #int_{#alpha_{1}}^{#alpha_{2}} d#alpha'_{2} #left[ #frac{U_{#delta_{1}#rho_{1}}^{2#beta} - #alpha'_{2} U_{#rho_{1}#sigma_{2}}^{1#beta}}{U_{#rho_{1}#sigma_{2}}^{0#beta}} #right]");
-	l.DrawLatex(0.5, 0.600, "d#Gamma = #frac{1}{2m_{A}} #left( #prod_{f} #frac{d^{3}p_{f}}{(2#pi)^{3}} #frac{1}{2E_{f}} #right) #left| M #left(m_{A} - #left{p_{f}#right} #right) #right|^{2} (2#pi)^{4} #delta^{(4)} #left(p_{A} - #sum p_{f} #right)");
-	l.DrawLatex(0.5, 0.425, "4 Re #left{ #frac{2}{1 - #Delta#alpha} #chi(s) #[]{#hat{g}_{#nu}^{e}#hat{g}_{#nu}^{f} (1 + cos^{2}#theta) + 2 #hat{g}_{a}^{e}#hat{g}_{a}^{f} cos#theta) } #right}");
-	l.DrawLatex(0.5, 0.300, "p(n) = #frac{1}{#pi#sqrt{2}} #sum_{k = 1}^{#infty} #sqrt{k} A_{k}(n) #frac{d}{dn} #frac{sinh #left{ #frac{#pi}{k} #sqrt{#frac{2}{3}} #sqrt{n - #frac{1}{24}} #right}}{#sqrt{n - #frac{1}{24}}}");
-#endif
+
 	c1->Print("c1.gif");
 	c1->Print("c1.jpg");
 	c1->Print("c1.png");
 	c1->Print("c1.ps");
 	c1->Print("c1.eps");
+   
+   return c1;
 }
