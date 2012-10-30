bf3a210f2c0549bc1a9517a2a899d661a1e7571a
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-10-30 17:48:05 +0000

    - fix merge with trunk
    git-svn-id: https://root.cern.ch/svn/root/branches/dev@46915 27541ba8-7e3a-0410-8455-c3a389f83636

---
+++ ibucur/trunk/graf2d/mathtext/src/mathrenderstyle.d
@@ -0,0 +1,1 @@
+  graf2d/mathtext/src/mathrenderstyle.o graf2d/mathtext/src/mathrenderstyle.d: $(wildcard include/mathrender.h include/mathtext.h /home/ibucur/roottrunk/graf2d/mathtext/src/table/mathfontparam.h)
---
+++ ibucur/trunk/graf2d/mathtext/src/mathrendertoken.d
@@ -0,0 +1,1 @@
+  graf2d/mathtext/src/mathrendertoken.o graf2d/mathtext/src/mathrendertoken.d: $(wildcard include/mathrender.h include/mathtext.h)
