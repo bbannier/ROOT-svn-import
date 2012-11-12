af4236f6650836cda810eda1bc591a442705285c
ibucur <ibucur@27541ba8-7e3a-0410-8455-c3a389f83636>
2012-11-12 15:43:46 +0000

    - print cleanup in progress
    git-svn-id: https://root.cern.ch/svn/root/branches/dev/ibucur@47194 27541ba8-7e3a-0410-8455-c3a389f83636

--- dev/tfn.cxx
+++ dev/tfn.cxx
@@ -49,28 +49,41 @@ int cint_func() {
 
 int main(int argc, char* argv[]) {
 
-   Double_t min[DIM] = { 0.0, 1.0, -2.0, 2.0, 0.0 };
-   Double_t max[DIM] = { 1.0, 4.0, 2.0, 3.0, 1.0 };
+   // TODO: solve overload issue
    Double_t min0 = 0.0;
    Double_t max0 = 1.0;
    Double_t min2[2] = {0.0, -2.0};
    Double_t max2[2] = {3.0, 2.0};
 
-//   TF1 f1("OneDimFn", "x + 2", 0.0, 1.0);
-   // TODO: solve overload problem
+
+   // FIRST SET OF TESTS
+   Double_t min[DIM] = { 0.0, 1.0, -2.0, 2.0, 0.0 };
+   Double_t max[DIM] = { 1.0, 4.0, 2.0, 3.0, 1.0 };
    TFn fsum("FiveDimTFn", DIM, &sum, min, max);
    TFn fsum_copy(fsum);
    TFn fsum_copy_assign = fsum_copy; 
-   std::cout << "FIRST TEST "; fsum_copy_assign.Print(); std::cout << std::endl;
-      std::cout << "The final object used for this test is actually derived from an original object, by first applying the copy constructor, and then the assignment operator. This way, we are testing both the basic specification of the TFn class, as well as the essential C++ copy functionality." << std::endl;
+   std::cout << "FIRST SET OF TESTS "; fsum_copy_assign.Print();
    std::cout << "The underlying mathematical function that describes this object is 'exp(x[0]) + log(x[1]) - x[2] + x[3] * x[3] - x[4]'" << std::endl;
+   std::cout << "The final object used for this test is actually derived from an original object, by first applying the copy constructor, and then the assignment operator. This way, we are testing both the basic specification of the TFn class, as well as the essential C++ copy functionality." << std::endl << std::endl;
 
-   std::cout << "Normalisation constant: " << fsum_copy_assign.Integral() << std::endl;
-   std::cout << "GetMinimum  " << fsum_copy_assign.GetMinimum(NULL, NULL, 1e-6, 1000)  << std::endl;
-   std::cout << "GetMaximum  " << fsum_copy_assign.GetMaximum(NULL, NULL, 1e-6, 1000)  << std::endl;
-   std::cout << "GetMinimum fx " << fsum_copy_assign.GetMinimum(NULL, NULL, 1e-6, 1000)  << std::endl;
-   std::cout << "GetMaximum fx " << fsum_copy_assign.GetMaximum(NULL, NULL, 1e-6, 1000)  << std::endl;
+   std::cout << "Normalisation constant (::Integral over full domain): " << fsum_copy_assign.Integral() << std::endl;
+   std::cout << "Function global minimum (::GetMinimum): " << fsum_copy_assign.GetMinimum(NULL, NULL, 1e-6, 1000)  << std::endl;
+   std::cout << "Function global maximum (::GetMaximum): " << fsum_copy_assign.GetMaximum(NULL, NULL, 1e-6, 1000)  << std::endl;
 
+   Double_t *minX = fsum_copy_assign.GetMinimumX(NULL, NULL, 1e-6, 1000);
+   Double_t *maxX = fsum_copy_assign.GetMaximumX(NULL, NULL, 1e-6, 1000);
+   std::cout << "Coordinates of global minimum (::GetMinimumX): "; for(UInt_t i = 0; i < DIM; ++i) std::cout << minX[i] << " "; std::cout << std::endl;
+   std::cout << "Coordinates of global maximum (::GetMaximumX): "; for(UInt_t i = 0; i < DIM; ++i) std::cout << maxX[i] << " "; std::cout << std::endl;
+   std::cout << "Partial derivatives at range lower bound: "; for(UInt_t i = 0; i < DIM; ++i) std::cout << fsum_copy_assign.Derivative(min, i) << " "; 
+   std::cout << std::endl;
+   std::cout << "Partial derivatives at range upper bound: "; for(UInt_t i = 0; i < DIM; ++i) std::cout << fsum_copy_assign.Derivative(max, i) << " "; 
+   std::cout << std::endl;
+
+   TF1* fsum_proj1d = fsum_copy_assign.Projection1D(0);
+   std::cout << "1D Projection: "; fsum_proj1d->Print();
+   Double_t x = 0; std::cout << "TF1 proj1d: proj1d(0.0) = " << (*fsum_proj1d)(&x);
+   x = 0.5; std::cout << "; proj1d(0.5) = " << (*fsum_proj1d)(&x);
+   x = 1.0; std::cout << "; proj1d(1.0) = " << (*fsum_proj1d)(&x) << std::endl;
 
    TF1 f1("OneDimTFn", &sum1, 2.0, 3.0);
    TFn fn("FiveDim", "x[0] + x[1] + x[2] + x[3] + x[4]", min, max);
@@ -78,27 +91,6 @@ int main(int argc, char* argv[]) {
    TFn f2("TwoDim", "x[0]^2 + x[1]", min2, max2);
    TFn_Distribution tnd(f2);
 
-   Double_t *minX = fsum_copy_assign.GetMinimumX(NULL, NULL, 1e-6, 1000);
-   Double_t *maxX = fsum_copy_assign.GetMaximumX(NULL, NULL, 1e-6, 1000);
-   
-   std::cout << "GetMinimumX ";
-   for(int i = 0; i < DIM; ++i) std::cout << minX[i] << " ";
-   std::cout << std::endl;
-
-   std::cout << "GetMaximumX ";
-   for(int i = 0; i < DIM; ++i) std::cout << maxX[i] << " ";
-   std::cout << std::endl;
-
-   for(Int_t i = 0; i < DIM; ++i) 
-      std::cout << std::setprecision(6) << "P.deriv.coord" << i <<": " << fsum_copy_assign.Derivative(min, i) << " " << fsum_copy_assign.Derivative(max, i) << std::endl;
-
-   TF1* f1_c0 = fsum_copy_assign.Projection1D(0);
-
-   Double_t x = 0;
-
-   std::cout << "TF1 coord 0: (0) = " << (*f1_c0)(&x);
-   x = 0.5; std::cout << "; (1/2) = " << (*f1_c0)(&x);
-   x = 1.0; std::cout << "; (1.0) = " << (*f1_c0)(&x) << std::endl;
 
 //   cint_func();
 
